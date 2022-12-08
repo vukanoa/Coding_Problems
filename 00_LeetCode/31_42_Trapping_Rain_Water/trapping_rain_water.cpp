5a9eded94e068cc27fb351af0cd14067d929e87c
Noa Vukasovic <noavukasovic@gmail.com>
2022-12-08 12:35:08 +0100

    Add LeetCode: 31_42_Trapping_Rain_Water(Using Stack)

--- 00_LeetCode/31_42_Trapping_Rain_Water/trapping_rain_water.cpp
+++ 00_LeetCode/31_42_Trapping_Rain_Water/trapping_rain_water.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <climits>
 
 
@@ -87,6 +88,26 @@
 	And then at the very end iterate through an entire array and:
 		total += std::min(dp_left_max[i], dp_right_max[i] - height[i]);
 
+
+
+	============
+	=== O(n) ===
+	============
+	(Using Stacks)
+
+	Instead of storing the largest bar up to an index as in Dynamic Progrmaming
+	approach, we can use stack to keep track of the bars that are bounded by
+	longer bars and hence - may store water.
+
+	We keep a stack and iterate over the array.
+	We add the index of the bar to the stack if bar is smaller than or equal to
+	the bar at the top of the stack, which means that the current bar is
+	bounded by the previous bar in the stack.
+	If we found a bar longer than that at the top, we are sure that the bar at
+	the top of the stack is bounded by the current bar and a previous bar in
+	the stack, hence, we can pop and add resulting trapped water to "total".
+
+
 */
 
 
@@ -164,11 +185,55 @@ public:
 	}
 };
 
+
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_stack{
+public:
+	int trap(std::vector<int>& height)
+	{
+		// Since the very left "margin" (coordinate system) cannot hold water
+		if (height.size() < 3)
+			return 0;
+
+		int n = height.size();
+		int curr_index = 0;
+		int total = 0;
+
+		std::stack<int> stack;
+
+		while (curr_index < n)
+		{
+			while (!stack.empty() && height[stack.top()] < height[curr_index])
+			{
+				int prev_top = stack.top();
+				stack.pop();
+
+				if (stack.empty())
+					break;
+
+				int distance = curr_index - stack.top() - 1;
+				int bounded = std::min(height[curr_index], height[stack.top()]) - height[prev_top];
+
+				total += distance * bounded;
+			}
+
+			stack.push(curr_index++);
+		}
+
+		return total;
+	}
+};
+
+
+
 int
 main()
 {
 	// Solution_brute sol_brute;
-	Solution_dp sol_dp;
+	// Solution_dp sol_dp;
+	Solution_stack sol_stack;
 
 	/* Example 1 */
 	std::vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
@@ -193,7 +258,8 @@ main()
 	std::cout << "]\n";
 
 	// int total = sol_brute.trap(height);
-	int total = sol_dp.trap(height);
+	// int total = sol_dp.trap(height);
+	int total = sol_stack.trap(height);
 
 	std::cout << "\n\tTotal: " << total << "\n\n";
 
