2a87587317569946d8ae6e8b107db2fbe18255de
Noa Vukasovic <noavukasovic@gmail.com>
2022-12-31 04:14:23 +0100

    Add Explanation(IDEA) for LeetCode Problem: 07_08_String_To_Integer_atoi

--- 00_LeetCode/07_08_String_To_Integer_atoi/string_to_integer_atoi.cpp
+++ 00_LeetCode/07_08_String_To_Integer_atoi/string_to_integer_atoi.cpp
@@ -9,84 +9,154 @@ static const int ASCII_NINE = 57;
 	==============
 	=== MEDIUM ===
 	==============
-	
+
 	===========================
 	8) String to Integer (atoi)
 	===========================
-	
+
 	============
 	Description:
 	============
-	
+
 	Implement the myAtoi(string s) function, which converts a string to a
 	32-bit signed integer(similar to C/C++'s atoi function).
-	
+
 	The algorithm for myAtoi(string s) is as follows:
-	
+
 	Read in and ignore any leading whitespace.
-	
+
 	Check if the next character(if not already at the end of the string) is
 	'-' or '+'. Read this character in if it is either. This determines if the
-	final resust is negative or positive respectively. Assume the resust is
+	final result is negative or positive respectively. Assume the result is
 	positive if neither is present.
-	
+
 	Read in next the characters until the next non-digit character or the end
 	of the input is reached. The rest of the string is ignored.
-	
+
 	Convert these digits into an integer(i.r. "123" -> 123, "0032" -> 32). If
 	no digits were read, then the integer is 0. Change the sign as necessary
 	(from step 2).
-	
-	If integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1], 
+
+	If integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1],
 	then clamp the integer so that it remains in the range. Specifically,
 	integer less than -2^31 should be clamped to -2^31, and integers greater
 	than 2^31 - 1 should be clamped to 2^31 - 1.
-	
+
 	Return the integer as the final result.
-	
+
 	=====
 	Node:
 	=====
-	
+
 	- Only the space character ' ' is considered a whitespace character.
 	- Do NOT ignore any characters other than the leading whitespace or the
 	  rest of the string after the digits.
-	
+
 	===============================
 	FUNCTION: int myAtoi(string s);
 	===============================
-	
+
 	==========================================================================
 	================================ EXAMPLES ================================
 	==========================================================================
-	
+
 	--- Example 1 ---
 	Input:  s = "42"
 	Output: 42
-	
+
 	--- Example 2 ---
 	Input:  s = "    -42"
 	Output: -42
-	
+
 	--- Example 3 ---
 	Input:  s = "4193 with words"
 	Output: 4193
-	
+
 	*** Constraints ***
 	0 <= s.length <= 200
 	s consists of English Letters(lower-case and upper-case), digits(0-9), ' ',
 	'+', '-', and '.'.
 */
 
-using namespace std;
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	ASCII values
+
+	48 -> 0
+	49 -> 1
+	50 -> 2
+	51 -> 3
+	52 -> 4
+	53 -> 5
+	54 -> 6
+	55 -> 7
+	56 -> 8
+	57 -> 9
+
+	If the String begins with leading whitespaces - iterate to first
+	not-whitespace or to the very last element.
+
+	check if first non-whitespace character is:
+		1. Either '-'
+		2. Or '+'
+		4. Or digit
+
+	If it's a '-' set flag "negative" to true and move to next character.
+
+	While we're on a digit in the String s, fill the vector "number" with them.
+	Only the first consecutive digits after:
+		1. Optional leading zeroes(whitespaces)
+		2. Optional arithmetic sign
+		3. Consecutive digits(until we reach end or whitespace)
+
+	Since s[i] is a character we subtract '0'(ASCII 48) from it just so we
+	can push that number in std::vector<int>.
+
+	Now we iterate until we have poccessed all the elements in number's vector.
+
+	The part in if's is just lke the previous problem 07_Reverse_integer
+	if "integer" is greater than:
+		INT_MAX / 10
+	then multiplying by 10 and adding any integer causes Overflow.
+
+	On the other hand, if it is exactly:
+		INT_MAX / 10
+	then Overflow is caused only if number[i] > 7
+
+	That means that if "integer" is greater than:
+		INT_MAX / 10
+	return INT_MAX
+
+	or if "integer" is equal to exactly:
+		INT_MAX / 10
+	and number[i] >=7 // if it's exactly 7 then that's INT_MAX already
+	returning INT_MAX either way.
+
+	However if "integer" is less than INT_MAX / 10 then check if:
+		integer >= ((INT_MAX / 10) + number[i])
+	if it is then return INT_MAX.
+
+	And at the end of our "for loop" we ask if our original number is
+	negative or not and "construct the 'integer' based on previous value.
+	(always tens digit)
+
+	And at the very end just return that constructed "integer".
+
+*/
+
+
 
 class Solution{
 public:
-	int myAtoi(const string& s)
+	int myAtoi(const std::string& s)
 	{
 		std::vector<int> number;
 
-		int integer = 0;
+		int  integer  = 0;
 		bool negative = false;
 
 		int i = 0;
@@ -107,9 +177,9 @@ public:
 
 		for (int i = 0; i < number.size(); i++)
 		{
-			if ((integer >= INT_MAX / 10 && number[i] >= 7) || (integer >= ((INT_MAX / 10) + number[i])))
+			if ((integer > INT_MAX / 10) || (integer == INT_MAX / 10 && number[i] >= 7) || (integer >= ((INT_MAX / 10) + number[i])))
 				return INT_MAX;
-			else if ((integer <= INT_MIN / 10 && number[i] >= 8) || (integer <= ((INT_MIN / 10) - number[i])))
+			else if ((integer < INT_MIN / 10) || (integer == INT_MIN / 10 && number[i] >= 8) || (integer <= ((INT_MIN / 10) - number[i])))
 				return INT_MIN;
 
 			if (negative)
@@ -130,23 +200,18 @@ main()
 
 	/* Example 1 */
 	// string s = "42";
-	// int num = sol.myAtoi(s);
 
 	/* Example 2 */
 	// string s = "   42";
-	// int num = sol.myAtoi(s);
 
 	/* Example 3 */
 	// string s = "   -42";
-	// int num = sol.myAtoi(s);
 
 	/* Example 4 */
 	// string s = "   -42with words";
-	// int num = sol.myAtoi(s);
 
 	/* Example 5 */
 	// string s = "4193 with words";
-	// int num = sol.myAtoi(s);
 
 	/* Example 6 */
 	// string s = "0032";
@@ -154,18 +219,23 @@ main()
 
 	/* Example 7 */
 	// string s = "2147483647";
-	// int num = sol.myAtoi(s);
 
 	/* Example 8 */
 	// string s = "-2147483648";
-	// int num = sol.myAtoi(s);
 
 	/* Example 9 */
-	string s = "2147483646";
+	std::string s = "2147483646";
+
 	int num = sol.myAtoi(s);
+	std::cout << "\n\t================================";
+	std::cout << "\n\t=== STRING TO INTEGER (ATOI) ===";
+	std::cout << "\n\t=================================\n";
+
+	/* Solution */
+	num = sol.myAtoi(s);
 
 	std::cout << "\n\tString \"" << s << "\" converted is " << num << "\n\n";
-	
+
 	return 0;
 }
 
