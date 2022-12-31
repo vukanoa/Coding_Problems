3c6a50f6728e9e8f9e3434898bdf10756542d946
Noa Vukasovic <noavukasovic@gmail.com>
2022-12-31 04:40:25 +0100

    Add Explanation(IDEA) for LeetCode Problem: 08_09_Palindrome_Number

--- 00_LeetCode/08_09_Palindrome_Number/palindrome_number.cpp
+++ 00_LeetCode/08_09_Palindrome_Number/palindrome_number.cpp
@@ -6,38 +6,38 @@
 	============
 	=== EASY ===
 	============
-	
+
 	====================
 	9) Palindrome Number
 	====================
-	
+
 	============
 	Description:
 	============
 
 	Given an integer x, return "true" if x is a palindrome, and "false"
 	otherwise.
-	
+
 	===================================
 	FUNCTION: bool isPalindrome(int x);
 	===================================
-	
+
 	==========================================================================
 	================================ EXAMPLES ================================
 	==========================================================================
-	
+
 	--- Example 1 ---
 	Input: x = 121
 	Output: true
-	
+
 	--- Example 2 ---
 	Input: x = -121
 	Output: false
-	
+
 	--- Example 3 ---
 	Input: x = 10
 	Output: false
-	
+
 	*** Constraints ***
 	-2^(31) <= x <= 2^(31) - 1
 
@@ -45,6 +45,38 @@
 	FOLLOW UP: Could you solve it without converting the integer to a string?
 	=========================================================================
 */
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Negative numbers are never palindromes because of a sign '-'.
+	Also if a number is not 0, meaning it's greater than 0(since we know, at
+	this point that our number x isn't negative or else we would've been out
+	of this functions already).
+
+	If it's greater than 0, then check if the last digit of that number is 0.
+	If it is 0 then that means that there is no corresponding Palindrome since
+	we are sure our initial value "x" does NOT contain leading zeroes.
+
+	If initial number x is == 0, then that is already a Palindrome so just
+	return true.
+
+	In while loop we do the same thing as in Problem: 06_07_Reverse_Integer
+	if x_rev > INT_MAX / 10 then whichever number we are adding will always
+	Overflow, therefore:
+		return false.
+
+	If we successfully finish the while loop then just check if now constructed
+	value x_rev is equal to initial value.
+
+	If it is INDEED then that's a Palidrome, return true;
+	If it is NOT    then return false;
+
+*/
+
 
 class Solution {
 public:
@@ -84,7 +116,7 @@ main()
 	// int x = 123455221;
 	// int x = INT_MAX;
 	// int x = INT_MIN;
-	// int x = 1234567899; 
+	// int x = 1234567899;
 
 	std::cout << "\n\t=========================";
 	std::cout << "\n\t=== PALINDROME NUMBER ===";
@@ -95,7 +127,7 @@ main()
 		std::cout << "\n\tNumber: " << x << " is INDEED a Palindrome!\n\n";
 	else
 		std::cout << "\n\tNumber: " << x << " is NOT a Palindrome!\n\n";
-	
+
 
 	return 0;
 }
