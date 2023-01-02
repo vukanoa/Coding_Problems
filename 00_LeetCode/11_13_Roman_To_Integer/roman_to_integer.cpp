4da2310c98bfa79cee78c48f62b24b342d59f724
Noa Vukasovic <noavukasovic@gmail.com>
2023-01-02 01:02:53 +0100

    Add Explanation(IDEA) for LeetCode Problem: 11_13_Roman_To_Integer

--- 00_LeetCode/11_13_Roman_To_Integer/roman_to_integer.cpp
+++ 00_LeetCode/11_13_Roman_To_Integer/roman_to_integer.cpp
@@ -5,18 +5,18 @@
 	============
 	=== EASY ===
 	============
-	
+
 	===========================
 	13) Roman to Integer
 	===========================
-	
+
 	============
 	Description:
 	============
 
 	Roman numerals are represented by seven different symbols:
 	I, V, X, L, C, D and M.
-	
+
 	Symbol    Value
 	I         1
 	V         5
@@ -42,25 +42,25 @@
 		1. 'I' can be placed before 'V' (5) and 'X'(10) to make 4 and 9.
 		2. 'X' can be placed before 'L' (50) and 'C'(100) to make 40 and 90.
 		3. 'C' can be placed before 'D' (500) and 'M'(1000) ot make 400 and 900.
-	
+
 	Given a roman numeral, convert it to an integer.
-	
+
 	==========================================================================
 	================================ EXAMPLES ================================
 	==========================================================================
-	
+
 	--- Example 1 ---
 	Input:  s = "III"
 	Output: 3
-	
+
 	--- Example 2 ---
 	Input:  s = "LVIII"
 	Output 58
-	
+
 	--- Example 3 ---
 	Input:  s = "MCMXCIV"
 	Output: 1994
-	
+
 	*** Constraints ***
 	1 <= s.length <= 15
 	s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
@@ -68,15 +68,46 @@
 
 */
 
-using namespace std;
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	We have an unordered Hash Map [char, int] corresponding to Roman characters
+	and its values.
+
+	We iterate through a given Roman number from left to right.
+	Consider Example:
+		MCIV
+		^
+
+	If we find that the current one is greater than its right Roman character
+	then we just add its value to our variable "number" which we return as a
+	final result.
+
+	However, if the current one is less than its right Roman character(less in
+	values) then we subtract current Roman character's value from our variable
+	"number".
+
+	Why?
+	If we are at position 2(at character 'I' for the above example) then
+	if its right Roman character is greater in value than the current one('I')
+	then that means that this character represent "decreasing part" of the very
+	next Roman character. Thus we subtract this one and add the next one.
+
+*/
+
+
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(n) */
 class Solution{
 public:
-	int romanToInt(string s)
+	int romanToInt(std::string s)
 	{
-		unordered_map<char,int> umap;
+		std::unordered_map<char,int> umap;
 		int number = 0;
 
 		umap.insert({'I', 1});
@@ -106,19 +137,19 @@ main()
 	Solution sol;
 
 	/* Example 1 */
-	// string s = "III";
+	// std::string s = "III";
 
 	/* Example 2 */
-	// string s = "LVIII";
+	// std::string s = "LVIII";
 
 	/* Example 3 */
-	// string s = "MCMXCIV";
+	// std::string s = "MCMXCIV";
 
 	/* Example 4 */
-	string s = "MDLXXIX";
+	std::string s = "MDLXXIX";
 
 	/* Example 5 */
-	// string s = "XIV";
+	// std::string s = "XIV";
 
 	std::cout << "\n\t========================";
 	std::cout << "\n\t=== ROMAN TO INTEGER ===";
