e6552487aa281e4ef3acae62dce6ebc3f2859163
Noa Vukasovic <noavukasovic@gmail.com>
2023-01-03 14:56:08 +0100

    Add Explanation(IDEA) for LeetCode Problem: 16_18_Four_Sum

--- 00_LeetCode/16_18_Four_Sum/four_sum.cpp
+++ 00_LeetCode/16_18_Four_Sum/four_sum.cpp
@@ -7,11 +7,11 @@
 	==============
 	=== MEDIUM ===
 	==============
-	
+
 	===========================
 	18) 4Sum
 	===========================
-	
+
 	============
 	Description:
 	============
@@ -25,23 +25,23 @@
 	=====
 	Node: You may return the answer in any order.
 	=====
-	
+
 	=====================================================================
 	FUNCTION: vector<vector<int>> fourSum(vector<int>& nums, int target);
 	=====================================================================
-	
+
 	==========================================================================
 	================================ EXAMPLES ================================
 	==========================================================================
-	
+
 	--- Example 1 ---
 	Input:  nums = [1, 0, -1, 0, -2, 2], target = 0
 	Output: [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
-	
+
 	--- Example 2 ---
 	Input:  nums = [2, 2, 2, 2, 2], target = 8
 	Output: [[2, 2, 2, 2]]
-	
+
 	*** Constraints ***
 	1 <= nums.length <= 200
 	-10^9 <= nums[i] <= 10^9
@@ -50,26 +50,129 @@
 */
 
 
-using namespace std;
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	This problem is a follow-up of 3Sum, so take a look at that problem first
+	if you haven't already.
+
+	4Sum and 3Sum are very similar; the difference is that we are looking for
+	unique quadruplets instead of triplets.
+
+	As you see, 3Sum just wraps Two Sum in an outer loop. As it iterates
+	through each value 'v', it finds all pairs whose sum is equal to
+	"target - v" using one of these approaches:
+
+		1. "Two Sum" uses a hash set to check for a matching value.
+		2. "Two Sum II" uses the two pointers technique in a sorted array.
+
+	Following a similar logic, we can implement 4Sum by wrapping 3 Sum in
+	another loop.
+	But wait - there is a catch. If an interviewer asks you to solve 4Sum, they
+	can follow-up with 5Sum, 6Sum, and so on. What they are really expecting at
+	this point is a kSum solution. Therefore, we will focus on a generalized
+	implementation here.
+
+	Approach 1: Two Pointers Technique
+
+	The two pointers technique requires the array to be sorted, so we do that
+	first. Also, it's easier to deal with duplicates if the array is sorted:
+	repeated values are next to each otehr and easy to skip.
+
+	For 3Sum, we enumarate each value in a single loop, and use the two
+	pointers technique for the rest of the array. For kSum, we will have k - 2
+	nested loops to enumerate all combinations of k - 2 values.
+
+	rear  -----------------------------------
+	                                        |
+	front -----------------                 |
+	                      |                 |
+	                      |                 |
+	                      v     Two Sum     v
+	                      ________^___________
+	1    2    ...    5    10    15    20    25
+	k   k-1          ___________ _____________
+	                            v
+	                           3Sum
+
+	We can implement k - 2 loops using a recursion. We will pass the starting
+	point and 'k' as the parameters. When k == 2, we will call twoSum,
+	terminating the recursion.
+
+	1. For the main function:
+		- Sort the input array "nums"
+		- Calls kSum with start = 0, k = 4, and "target", and return the result
+
+	2. For kSum function:
+		- At the start of the kSum function, we will check three conditions:
+			a) Have we run out of numbers to choose from?
+			b) Is the smallest number remaining greater than target / k?
+				If so, then any 'k' numbers we choose will be too large.
+			c) Is the largest number remaining smaller than target / k?
+				If so, then any k numbers we choose will be too small.
+				If any of these conditions is true, there is no need to
+				continue as no combination of the remaining elements can sum to
+				"target.
+
+		- If 'k' equals 2, call twoSum and return the result.
+
+		- Iterate 'i' through the array from "start" :
+			- If the current value is the same as the one before, skip it.
+			- Recursively call kSum with:
+				start = i + 1
+				k = k - 1
+				target - nums[i]
+			- For each returned "subset" of values:
+				- Include the current value nums[i] into "subset".
+				- Add "subset" to the result "vec_results".
+
+			- Return the result "vec_results";
+
+	3. For twoSum function:
+		- Set the low pointer "front" to "start", and "rear" pointer to the
+		  last index.
+
+		- While "front" pointer is smaller than "rear":
+			- If the sum of "nums[front]" and "nums[rear]" is less than
+			  "target", increment "front".
+				- Also increment "front" if the value is the same as front - 1
+			- If the sum is greater than "target", decrement "rear"
+				- Also decrement "rear" if the value is the same as rear + 1
+			- Otherwise, we found a pair:
+				- Add it to the result "vec_results"
+				- Decrement "rear" and increment "front".
+
+		- Return the result "vec_results"
+
+*/
 
 
-/* Time  Complexity: O(n^(k - 1)), or O(n^3) for 4Sum. We have k - 2 loops, and
-two_sum is O(n)*/
-/* Space Complexity: O(n). We need O(k) space for the recursion. k can be the
-same as n in the worst case for the generalized algorithm.*/
+
+/*
+	Time  Complexity:
+	O(n^(k - 1)), or O(n^3) for 4Sum.
+	We have k - 2 loops, and two_sum is O(n)
+*/
+/*
+	Space Complexity:
+	O(n). We need O(k) space for the recursion.
+	k can be the same as n in the worst case for the generalized algorithm.
+*/
 class Solution{
 public:
-	vector<vector<int>>
-	fourSum(vector<int>& nums, int target)
+	std::vector<std::vector<int>>
+	fourSum(std::vector<int>& nums, int target)
 	{
 		sort(nums.begin(), nums.end());
 		return k_sum(nums, target, 0, 4);
 	}
 
-	vector<vector<int>>
-	k_sum(vector<int>& nums, long long target, int start, int k)
+	std::vector<std::vector<int>>
+	k_sum(std::vector<int>& nums, long long target, int start, int k)
 	{
-		vector<vector<int>> vec_results;
+		std::vector<std::vector<int>> vec_results;
 
 		if (start == nums.size())
 			return vec_results;
@@ -85,12 +188,12 @@ public:
 
 		if (k == 2)
 			return two_sum(nums, target, start);
-		
+
 		for (int i = start; i < nums.size(); i++)
 		{
 			if (i == start || nums[i - 1] != nums[i])
 			{
-				for (vector<int>& subset : k_sum(nums, static_cast<long long>(target) - nums[i], i + 1, k - 1))
+				for (std::vector<int>& subset : k_sum(nums, static_cast<long long>(target) - nums[i], i + 1, k - 1))
 				{
 					vec_results.push_back({nums[i]});
 					vec_results.back().insert(vec_results.back().end(), subset.begin(), subset.end());
@@ -101,9 +204,9 @@ public:
 		return vec_results;
 	}
 
-	vector<vector<int>> two_sum(vector<int>& nums, long long target, int start)
+	std::vector<std::vector<int>> two_sum(std::vector<int>& nums, long long target, int start)
 	{
-		vector<vector<int>> vec_results;
+		std::vector<std::vector<int>> vec_results;
 		int front = start;
 		int rear  = nums.size() - 1;
 
@@ -130,34 +233,34 @@ main()
 	Solution sol;
 
 	/* Example 1 */
-	vector<int> nums = {1, 0, -1, 0, -2, 2};
+	std::vector<int> nums = {1, 0, -1, 0, -2, 2};
 	int target = 0;
 
 	/* Example 2 */
-	// vector<int> nums = {2, 2, 2, 2, 2};
+	// std::vector<int> nums = {2, 2, 2, 2, 2};
 	// int target = 8;
 
 	std::cout << "\n\t================";
 	std::cout << "\n\t=== Four Sum ===";
 	std::cout << "\n\t================\n";
 
-	vector<vector<int>> vec_results = sol.fourSum(nums, target);
+	std::vector<std::vector<int>> vec_results = sol.fourSum(nums, target);
 
-	cout << "\n\tOriginal Vector: ";
+	std::cout << "\n\tOriginal Vector: ";
 	for (const auto& x : nums)
-		cout << x << " ";
-	cout << "\n\tTarget: " << target << "\n";
+		std::cout << x << " ";
+	std::cout << "\n\tTarget: " << target << "\n";
 
-	cout << "\n\tResults:";
-	vector<vector<int>> ::iterator it;
+	std::cout << "\n\tResults:";
+	std::vector<std::vector<int>> ::iterator it;
 	it = vec_results.begin();
 	for (it; it != vec_results.end(); it++)
 	{
-		cout << "\n\t" << it - vec_results.begin() + 1 << ") ";
-		for(vector<int>::iterator it2 = it->begin(); it2 != it->end(); ++it2)
-			cout << *it2 << " ";
+		std::cout << "\n\t" << it - vec_results.begin() + 1 << ") ";
+		for(std::vector<int>::iterator it2 = it->begin(); it2 != it->end(); ++it2)
+			std::cout << *it2 << " ";
 	}
-	cout << "\n\n";
+	std::cout << "\n\n";
 
 	return 0;
 }
