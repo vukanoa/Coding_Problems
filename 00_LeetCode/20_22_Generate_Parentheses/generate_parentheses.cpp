f2a12639ce0e41d2b18996f9524a249c6e869137
Noa Vukasovic <noavukasovic@gmail.com>
2023-01-04 18:56:00 +0100

    Add Explanation(IDEA) for LeetCode Problem: 20_22_Generate_Parenteses

--- 00_LeetCode/20_22_Generate_Parentheses/generate_parentheses.cpp
+++ 00_LeetCode/20_22_Generate_Parentheses/generate_parentheses.cpp
@@ -6,48 +6,94 @@
 	==============
 	=== MEDIUM ===
 	==============
-	
+
 	===========================
 	22) Generate Parenthesis
 	===========================
-	
+
 	============
 	Description:
 	============
 
-	Given "n" pairs ofparentheses, write a function to generate all
+	Given "n" pairs of parentheses, write a function to generate all
 	combinations of well-formed parenthesis.
-	
+
 	==============================================================
 	FUNCTION: std::vector<std::string> generateParenthesis(int n);
 	==============================================================
-	
+
 	==========================================================================
 	================================ EXAMPLES ================================
 	==========================================================================
-	
+
 	--- Example 1 ---
 	Input:  n = 3
 	Output: ["((()))", "(()())", "(())()", "()(())", "()()()"]
-	
+
 	--- Example 2 ---
 	Input: n = 1
 	Output: ["()"]
-	
+
 	*** Constraints ***
 	1 <= n <= 8
 
 */
 
-/*
 
-	((()))
-	(()())
-	(())()
-	()(())
-	()()()
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Vector of strings "valid_combinations" is going to store every valid
+	combination we bump upon.
+
+	curr_str is a string until this moment down this backtrack path.
+
+	Since every valid combination will have exactly 2 * n characters, we check
+	if our variable "curr_str" at this moment is of length 2 * n. If it is then
+	we push "curr_str" into our vector of strings "valid_combinations" and
+	start returning from the back.
+
+	However if we still haven't formed a valid combination, meaning our
+	variable "curr_str" doesn't hold up a valid combination of length 2 * n
+	then first check if all the opened parentheses are used, that is:
+
+	if "open" is less than "n", since valid combination is going to be
+	2 * n, that means that we will have exactly "n" opened and "n" closed
+	parentheses.
+
+	So if not all opened parentheses were used, append our current string
+	"curr_str" and backtrack with a newly formed string "curr_str" and
+	with a +1 value in variable "open".
+
+	If all the open parentheses were used, then check if a variable "open" is
+	less than "close" to indicate that 'there is indeed something to close', if
+	that is the case then append a "curr_str" with a closing parenthesis and
+	call this function with newly formed string "curr_str" and with a +1 in
+	variable "close".
+
+	The best advice is to try and go through forming of first 3-4
+	valid_combinations to get a "feel" of how this works.
+
 */
 
+
+/*
+	N = 3, all 5/5 combinations in order of forming
+
+	1. ((()))
+	2. (()())
+	3. (())()
+	4. ()(())
+	5. ()()()
+*/
+
+
+//                               (2n)
+/* n-th Catalan number 1/(n + 1) ( n)*/
+/* Time  Complexity: O(4^n/sqrt(n)) */
+/* Space Complexity: O(4^n/sqrt(n)) */
 class Solution{
 public:
 	std::vector<std::string> generateParenthesis(int n)
@@ -106,7 +152,7 @@ main()
 	int i = 1;
 	for(const auto& x : valid_combinations)
 		std::cout << "\n\t" << i++ << ". \"" << x << "\"\n";
-	
+
 	std::cout << "\n";
 
 	return 0;
