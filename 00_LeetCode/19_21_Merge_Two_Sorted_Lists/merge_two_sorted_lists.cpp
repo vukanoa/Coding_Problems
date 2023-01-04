dab4e4d3e798806290a1737351a62aa6242e8344
Noa Vukasovic <noavukasovic@gmail.com>
2023-01-04 17:53:24 +0100

    Add Explanation(IDEA) for LeetCode Problem: 19_21_Merge_Two_Sorted_Lists

--- 00_LeetCode/19_21_Merge_Two_Sorted_Lists/merge_two_sorted_lists.cpp
+++ 00_LeetCode/19_21_Merge_Two_Sorted_Lists/merge_two_sorted_lists.cpp
@@ -4,11 +4,11 @@
 	============
 	=== EASY ===
 	============
-	
+
 	===========================
 	21) Merge Two Sorted Lists
 	===========================
-	
+
 	============
 	Description:
 	============
@@ -19,27 +19,27 @@
 	splicing together the nodes of the first two lists.
 
 	Return the head o fhte merged linked list.
-	
+
 	====================================================================
 	FUNCTION: ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
 	====================================================================
-	
+
 	==========================================================================
 	================================ EXAMPLES ================================
 	==========================================================================
-	
+
 	--- Example 1 ---
 	Input:  list1 = [1, 2, 4], list2 = [1, 3, 4]
 	Output: [1, 1, 2, 3, 4, 4]
-	
+
 	--- Example 2 ---
 	Input:  list1 = [], list2 = []
 	Output: []
-	
+
 	--- Example 3 ---
 	Input:  list1 = [], list2 = [0]
 	Output: [0]
-	
+
 	*** Constraints ***
 	The number of nodes in both lists is in the range [0, 50]
 	-100 <= Node.val <= 100
@@ -47,6 +47,59 @@
 
 */
 
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Check if either of lists(list1 or list2) is nullptr, if it is, then return
+	the other. Even if the other is a nullputr as well, we would essentially
+	just return a nullptr which is a corect answer in a case where both lists
+	are indeed nullptr's.
+
+	Since it is noted that the lists are SORTED, our finally merged list have
+	to begin with a smaller head of list1 & list2.
+
+	So if list1->val is smaller than list2->val then our global head of the new
+	merged list which we will return as a result, named "head", have to point
+	to this node.
+
+	And make list1 point to its next node in the list.
+
+	If it was the opposite, then make "head" point to list2 and make list2
+	point to its next node in the list.
+
+	Let tail point to "head" before we begin "weaving" these two lists.
+
+	Now enter the while loop which iterates while BOTH list1 and list2 point to
+	atleast one node. Meaning neither is nullptr.
+
+	If either or both are nullptr, break the while loop.
+
+	Inside of the while loop, always check which one points to a lower value.
+	If list1->val is smaller than list2->val then let tail's next point to this
+	(smaller node of the list1 or list2, list1 in this case) node and update
+	tail to now point to that node.
+
+	If list2->val is smaller or equal to list1 then do the same as above, just
+	but with list2.
+
+	After breaking the while loop check if list1 is empty, meaning that list1
+	is pointing to nullptr, if that is indeed the case, then make sure that
+	tail->next points to the other, potentially non-empty, list, list2 in this
+	case.
+
+	If it was the opposite, then do the same as above, but swap the place of
+	words "list1" and "list2".
+
+	However, if both were pointing to nullptr then we would've entered the
+	first if statement and that would just make tail's next point to a nullptr
+	which basically does nothing.
+
+	And at the very end, just return the "head".
+
+*/
 
 // Definition for singly-linked list.
 typedef struct ListNode {
@@ -57,6 +110,13 @@ typedef struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 }ListNode;
 
+
+
+/*	Time  Complexity: O(m + n)
+	Where m is the length of list1
+	and n is the length of list2
+*/
+/*	Space Complexity: O(1) */
 class Solution{
 public:
 	ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
