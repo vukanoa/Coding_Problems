1d5fabce05badd5082119bc65b4f4422d13a5b7e
Noa Vukasovic <noavukasovic@gmail.com>
2023-01-28 00:27:34 +0100

    Add LeetCode: 76_160_Majority_Element(Using: Boyer-Moore Voting Algorithm)

--- 00_LeetCode/76_160_Majority_Element/majority_element.cpp
+++ 00_LeetCode/76_160_Majority_Element/majority_element.cpp
@@ -82,7 +82,7 @@
 	enumerate all logC bits for each number in "nums".
 */
 /* Space Complexity: O(1) */
-class Solution{
+class Solution_Bit_Manipulation{
 public:
 	int majorityElement(std::vector<int>& nums)
 	{
@@ -112,13 +112,89 @@ public:
 };
 
 
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	************************************
+	*** Boyer-Moore Voting Algorithm ***
+	************************************
+
+	Intuition
+	If we had some way of counting instances of the majority element as +1 and
+	instances of any other as -1, summing them would make it obvious that the
+	majority element is indeed the majority lement.
+
+	Algorithm
+	Essentially, what Boyer-Moore does is look for a suffix "suf" of "nums"
+	where suf[0] is the majority element in that suffix. To do this, we
+	maintain a count, which is incremented whenever we see an instance of our
+	current candidate for majority element and decremented whenever we see
+	anything else. Whenever "count" equals 0, we effectively forget about
+	everything in "nums" up to the current index and consider the current
+	number as the candidate for majority element. It is not immediately
+	obvious why we can get away with forgetting prefixes of "nums" - consider
+	the following examples (pipes are inserted to separate runs of nonzero
+	"count").
+
+	[7, 7, 5, 7, 5, 1 | 5, 7, | 5, 5, 7, 7 | 7, 7, 7, 7]
+
+	Here, the 7 at index 0 is selected to be the first candidate for majority
+	element. "count" will eventually reach 0 after index 5 is processed, so the
+	5 at index 6 will be the next candidate. In this case, 7 is the true
+	majority element, so by disregarding this prefix, we are ignoring an equal
+	number of majority and minority elements - therefore, 7 will still be the
+	majority element in the suffix formed by throwing away the first prefix.
+
+	[7, 7, 5, 7, 5, 1 | 5, 7 | 5, 5, 7, 7 | 5, 5, 5, 5]
+
+	Now, the majority element is 5 (we changed the last run of the array from
+	7s to 5s), but our first candidate is still 7. In this case, our candidate
+	is not the true majority element, but we still cannot discard more majority
+	elements than minority elements (this would imply that "count" could reach
+	-1 before we reassign "candidate", which is obivously false).
+
+	Therefore, given that it is impossible (in both cases) to discard more
+	majority elements than minority elements, we are safe in discarding the
+	prefix and attempting to recursively solve the majority element problem for
+	the suffix. Eventually, a suffix will be found for which "count" does not
+	hit 0, and the majority element of that suffix will necessarily be the same
+	as the majority element of the overall array.
+
+*/
+
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution{
+public:
+	int majorityElement(std::vector<int>& nums)
+	{
+		int count = 0;
+		int candidate = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (count == 0)
+				candidate = nums[i];
+
+			count += (nums[i] == candidate) ? 1 : -1;
+		}
+
+		return candidate;
+	}
+};
+
+
 int
 main()
 {
+	Solution_Bit_Manipulation sol_bin_manipulation;
 	Solution sol;
 
 	/* Example 1 */
-	std::vector<int> nums = {3, 2, 3};
+	// std::vector<int> nums = {3, 2, 3};
 
 	/* Example 2 */
 	// std::vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
@@ -126,11 +202,15 @@ main()
 	/* Example 3 */
 	// std::vector<int> nums = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 7, 7, 7, 7};
 
+	/* Example 4 */
+	std::vector<int> nums = {7, 7, 5, 7, 5, 1, 5, 7, 5, 5, 7, 7, 5, 5, 5, 5};
+
 	std::cout << "\n\t========================";
 	std::cout << "\n\t=== MAJORITY ELEMENT ===";
 	std::cout << "\n\t========================\n";
 
 
+	/* Write Input */
 	bool first = true;
 	std::cout << "\n\tArray: [";
 	for (auto x: nums)
@@ -145,8 +225,10 @@ main()
 
 
 	/* Solution */
+	// int majority_element = sol_bit_manipulation.majorityElement(nums);
 	int majority_element = sol.majorityElement(nums);
 
+	/* Write Output */
 	std::cout << "\n\tMajority Element: " << majority_element << "\n\n";
 
 	return 0;
