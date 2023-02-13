31c3ba0b81cabb4f86180f62d4bb605ce856df4c
Noa Vukasovic <noavukasovic@gmail.com>
2023-02-13 22:06:55 +0100

    Add LeetCode: 92_283_Move_Zeroes(lastNonZeroFoundAt)

--- 00_LeetCode/92_283_Move_Zeroes/move_zeroes.cpp
+++ 00_LeetCode/92_283_Move_Zeroes/move_zeroes.cpp
@@ -158,6 +158,55 @@ public:
 };
 
 
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	This approach works the same way as above, i.e., first fulfills one
+	requirement and then another. The catch? It does it in a clever way. The
+	above problem can also be stated in alternate way - "Bring all the non 0
+	elements to the front of array keeping their relative order same".
+
+	This is a 2 pointer approach. The fast pointer which is denoted by variable
+	"cur" does the job of processing new elements. If the newly found element
+	is not a 0, we record it just after the last found non-0 element. The
+	position of last found non-0 element is denoted by the slow pointer
+	"lastNonZeroFoundAt" variable. As we keep finding new non-0 elements, we
+	just overwrite them at the "lastNonZeroFoundAt + 1" 'th index. This
+	overwrite will not result in any loss of data because we already processed
+	what was there(if it were non-0, it already is now written at it's
+	corresponding index, or if it were 0 it will be handled later in time).
+
+	After the "curr" index reaches the end of array, we now know that all the
+	non-0 elements have been moved to beginning of array in their original
+	order. Now comes the time to fulfill other requirement. "Move all 0's to
+	the end". We now simply need to fill all the indexes after the
+	"lastNonZeroFoundAt" index with 0.
+*/
+
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution_Leet_2{
+public:
+	void moveZeroes(std::vector<int>& nums)
+	{
+		int lastNonZeroFoundAt = 0;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (nums[i] != 0)
+				nums[lastNonZeroFoundAt++] = nums[i];
+		}
+
+		for (int i = lastNonZeroFoundAt; i < nums.size(); i++)
+			nums[i] = 0;
+	}
+};
+
+
 /*
 	------------
 	--- IDEA ---
@@ -248,6 +297,7 @@ main()
 	Solution_inefficient sol_ineff;
 	Solution sol;
 	Solution_Space_Inefficient sol_space;
+	Solution_Leet_2 sol_leet_2;
 
 	/* Example 1 */
 	// std::vector<int> nums = {0, 1, 0, 3, 12};
@@ -283,7 +333,8 @@ main()
 	/* Solution */
 	// sol_ineff.moveZeroes(nums);
 	// sol.moveZeroes(nums);
-	sol_space.moveZeroes(nums);
+	// sol_space.moveZeroes(nums);
+	sol_leet_2.moveZeroes(nums);
 
 	std::cout << "\t\t*** MOVE ZEROES ***\n";
 
