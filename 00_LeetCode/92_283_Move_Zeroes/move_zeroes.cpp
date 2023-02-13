df92b2d5f0bd90bcaf44ab4c4dbdca7537f13a21
Noa Vukasovic <noavukasovic@gmail.com>
2023-02-13 22:20:48 +0100

    Add LeetCode: 92_283_Move_Zeroes(Optimal)

--- 00_LeetCode/92_283_Move_Zeroes/move_zeroes.cpp
+++ 00_LeetCode/92_283_Move_Zeroes/move_zeroes.cpp
@@ -47,6 +47,8 @@
 
 
 
+
+
 /*
 	------------
 	--- IDEA ---
@@ -99,6 +101,9 @@ public:
         }
     }
 };
+
+
+
 
 
 /*
@@ -159,6 +164,9 @@ public:
 
 
 
+
+
+
 /*
 	------------
 	--- IDEA ---
@@ -205,6 +213,73 @@ public:
 			nums[i] = 0;
 	}
 };
+
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Approach #3 (Optimal)
+	The total number of opeartions of the previous approach is sub-optimal. For
+	example, the array which has all (except last) leading zeroes:
+		[0, 0, 0, 0, ..., 1]
+	How many write operations to the array? For the previous approach, it's
+	0's n - 1 times, which is not necessary. We could have instead written just
+	once. How? ... By only fixing the non-0 element, i.e., 1.
+
+	The optimal approach is again a subtle extension of above solution. A
+	simple realization is if the current element is non-0, its correct position
+	can at best be its current position or a position earlier. If it's the
+	latter, the current position will be eventually occupied by a non-0, or a
+	0, which lies at a index greater than "cur" index. We fill the current
+	position by 0 right away, so tha tunlike the previous solution, we don't
+	need to come back here in next iteration.
+
+	In other words, the code will maintain the following invariant:
+		1. All elements before the slow pointer (lastNonZeroFoundAt) are
+		   non-zeroes.
+
+		2. All elements between the current and slow pointer are zeroes.
+
+	Therefore, when we encounter a non-zero element, we need to swap elements
+	pointed by current and slow pointer, then advance both pointers. If it's
+	zero element, we justadvance current pointer.
+
+	With this invariant in-place, it's easy to see that the algorithm will work
+
+*/
+
+
+/*
+	Time  Complexity: O(n)
+	However, the total number of operations are optimal. The total operations
+	(array writes) that code does is Number of non-0 elements. This gives us a
+	much better best-case (when most of the elements are 0) complexity than
+	last solution.
+	However, the worst-case (when all elements are non-0) complexity for both
+	the algorithms is same.
+*/
+/*
+	Space Complexity: O(1)
+*/
+class Solution_Leet_3{
+public:
+	void moveZeroes(std::vector<int>& nums)
+	{
+		for (int lastNonZeroFoundAt = 0, curr = 0; curr < nums.size(); curr++)
+		{
+			if (nums[curr] != 0)
+				std::swap(nums[lastNonZeroFoundAt++], nums[curr]);
+		}
+	}
+};
+
+
+
 
 
 /*
@@ -298,6 +373,7 @@ main()
 	Solution sol;
 	Solution_Space_Inefficient sol_space;
 	Solution_Leet_2 sol_leet_2;
+	Solution_Leet_3 sol_leet_3;
 
 	/* Example 1 */
 	// std::vector<int> nums = {0, 1, 0, 3, 12};
@@ -334,7 +410,8 @@ main()
 	// sol_ineff.moveZeroes(nums);
 	// sol.moveZeroes(nums);
 	// sol_space.moveZeroes(nums);
-	sol_leet_2.moveZeroes(nums);
+	// sol_leet_2.moveZeroes(nums);
+	sol_leet_3.moveZeroes(nums);
 
 	std::cout << "\t\t*** MOVE ZEROES ***\n";
 
