9be328ff6da39da10aedffbe7381d700950e1247
Noa Vukasovic <noavukasovic@gmail.com>
2023-02-17 07:42:59 +0100

    Add LeetCode: 95_300_Longest_Increasing_Subsequence(Using: Greedy with Binary Search)

--- 00_LeetCode/95_300_Longest_Increasing_Subsequence/longest_increasing_subsequence.cpp
+++ 00_LeetCode/95_300_Longest_Increasing_Subsequence/longest_increasing_subsequence.cpp
@@ -66,7 +66,7 @@
 
 /* Time  Complexity: O(n^2) */
 /* Space Complexity: O(n) */
-class Solution{
+class Solution_DP{
 public:
 	int lengthOfLIS(std::vector<int>& nums)
 	{
@@ -87,10 +87,107 @@ public:
 };
 
 
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Greedy with Binary Search
+
+	Let's construct the idea from following example.
+	Consider the example nums = [2, 6, 8, 3, 4, 5, 1], let's try to build the
+	increasing subsequences starting with an ampty one sub1 = [].
+
+	1. Let pick the first element, sub1 = [2]
+
+	2. 6 is greater than the previous number, sub1 = [2, 6]
+
+	3. 8 is greater than previous number, sub1 = [2, 6, 8]
+
+	4. 3 is less than previous number, we can't extend the subsequence sub1,
+	   but we must keep 3 because in the future there may have the longest
+	   subsequence start with [2, 3], sub1 = [2, 6, 8], sub2 = [2, 3].
+
+	5. With 4, we can't extend sub1, but we can extend sub2, so
+	   sub1 = [2, 6, 8], sub2 = [2, 3, 4]
+
+	6. With 5, we can't extend sub1, but we can extend sub2, so
+	   sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5]
+
+	7. With 1, we can't extend neighter sub1 nor sub2, but we need to keep 1,
+	   so sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5], sub3 = [1]
+
+	8. Finally, length of longest increase subsequence = len(sub2) = 4
+
+
+	In the above steps, we need to keep different "sub" arrays
+	(sub1, sub2, sub3, ..., subk)
+	which causes poor performance. But we notice that we can just keep one
+	"sub" array, when new number 'x' is not greater than the last element of
+	the subsequence "sub" we d binary search to find the smallest
+	element >= x in "sub", and replace with number x.
+
+
+	Let's run that example: nums = [2, 6, 8, 3, 4, 5, 1] again:
+	1. Let's pick the first element, sub = [2]
+
+	2. 6 is greater than previous number, sub = [2, 6]
+
+	3. 8 is greater than previous number, sub = [2, 6, 8]
+
+	4. 3 is less than previous number, so we can't extend the subsequence sub.
+	   We need to find the smallest number >= 3 in "sub", it's 6. Then we
+	   overwrite it, now sub = [2, 3, 8].
+
+	5. 4 is less than previous number so we can't extend the subsequence sub.
+	   We overwrite 8 by 4, so sub = [2, 3, 4]
+
+	6. 5 is greater than previous number, sub = [2, 3, 4, 5]
+
+	7, 1 is less than previous number, so we can't extend the subsequence sub,
+	   We overwrite 2 by 1, so sub = [1, 3, 4, 5].
+
+	8. Finally, length of longest increase subsequence = len(sub) = 4
+
+*/
+
+
+
+/*	Time  Complexity: O(n * logn) */
+/*
+	Space Complexity: O(n)
+	We can achieve O(1) in space by overwriting values of "sub" into original
+	"nums" array.
+*/
+class Solution_Greedy_Binary{
+public:
+	int lengthOfLIS(std::vector<int> &nums)
+	{
+		std::vector<int> sub;
+
+		for (int x : nums)
+		{
+			if (sub.empty() || sub[sub.size() - 1] < x)
+				sub.push_back(x);
+			else
+			{
+				auto it = std::lower_bound(sub.begin(), sub.end(), x);
+				*it = x;
+			}
+		}
+
+		return sub.size();
+	}
+};
+
+
+
 int
 main()
 {
-	Solution sol;
+	Solution_DP sol_dp;
+	Solution_Greedy_Binary sol_grd_bin;
 
 	/* Example 1 */
 	std::vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
@@ -121,7 +218,9 @@ main()
 
 
 	/* Solution */
-	int longest = sol.lengthOfLIS(nums);
+	// int longest = sol_dp.lengthOfLIS(nums);
+	int longest = sol_grd_bin.lengthOfLIS(nums);
+
 
 	std::cout << "\n\tLongest Increasing Subsequence: " << longest << "\n\n";
 
