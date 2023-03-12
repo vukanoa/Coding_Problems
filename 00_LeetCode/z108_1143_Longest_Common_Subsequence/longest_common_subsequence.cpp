eb569df459f7795a4e365b1e233361be9d41f0a6
Noa Vukasovic <noavukasovic@gmail.com>
2023-03-12 23:34:17 +0100

    Add Explanation(IDEA) for LeetCode Problem: 108_1143_Longest_Common_Subsequence

--- 00_LeetCode/z108_1143_Longest_Common_Subsequence/longest_common_subsequence.cpp
+++ 00_LeetCode/z108_1143_Longest_Common_Subsequence/longest_common_subsequence.cpp
@@ -71,8 +71,302 @@ ace
 	--- IDEA ---
 	------------
 
+	Consider this example:
+
+	1. "abcde"
+	    #
+
+	2. "ace"
+	    #
 
 
+	Let's look at the first character in the string. In this case: They match.
+	So does it, atleast make sense to you that if both of the characters match
+	each other then we can break it into a subproblem.
+
+	Now the new problem we're looking at is: Find the longest common
+	subsequence between the remainder of both strings, with these:
+
+	1. "bcde"
+	2. "ce"
+
+	Plus 1 because the first two characters matched. ('a' and 'a')
+	We know that the longest common subsequence in this subproblem is going
+	to be at least 1, because of the previous two a's.
+
+	And that is what the Dynamic Programming is all about - Finding the
+	subproblem.
+
+	Now what if the initial example looked like this:
+	(We replaced 'a', in the first string, with 'b')
+
+	1. "bbcde"
+	    #
+
+	2. "ace"
+	    #
+
+	Now these two characters don't match, so what do we do then?
+	We can't do what we just did. We can't add 1 and find the longest common
+	subsequence in the subproblem:
+
+	1. "bcde"
+	2. "ce"
+
+	We can't do that, but we can break it into the different subproblem.
+	We can say that it's possible that the longest common subsequence could be
+	between these two:
+
+	1. "bbcde"
+	2. "ce"
+
+		Or it could be between:
+
+	1. "bcde"
+	2. "ace"
+
+
+	Let's draw all of this, using the 1st Example:
+
+	1. "abcde"
+	2. "ace"
+
+                   j
+
+           a     c     e
+        +-----+-----+-----+=====+
+     a  |     |     |     |     =
+        +-----+-----+-----+=====+
+     b  |     |     |     |     =
+ i      +-----+-----+-----+=====+
+     c  |     |     |     |     =
+        +-----+-----+-----+=====+
+     d  |     |     |     |     =
+        +-----+-----+-----+=====+
+     e  |     |     |     |     =
+        +-----+-----+-----+=====+
+        =     =     =     =     =
+        +=====+=====+=====+=====+
+
+
+	So the entire longest commong subsequence value is going to be at:
+		dp['a']['a'] <==> dp[0][0]
+
+	We want to compute that value. However, you'll find out that to compute
+	that you want to solve
+
+		dp[0][1] // RIGHT
+		dp[1][0] // LEFT
+		dp[1][1] // DIAGONAL
+
+
+	So, in this example:
+
+                   j
+           #
+           a     c     e
+        +-----+-----+-----+=====+
+   # a  |     |     |     |     =
+        +-----+-----+-----+=====+
+     b  |     |     |     |     =
+ i      +-----+-----+-----+=====+
+     c  |     |     |     |     =
+        +-----+-----+-----+=====+
+     d  |     |     |     |     =
+        +-----+-----+-----+=====+
+     e  |     |     |     |     =
+        +-----+-----+-----+=====+
+        =     =     =     =     =
+        +=====+=====+=====+=====+
+
+	'a' in the first string and 'a' in the second string match, thus we have
+	to continue computing - Diagonally.
+	That will represent doing a subproblem:
+
+	1. "bcde"
+	2. "ce"
+
+	Now:
+
+                   j
+                 #
+           a     c     e
+        +-----+-----+-----+=====+
+     a  |     |     |     |     =
+        +-----+-----+-----+=====+
+   # b  |     |     |     |     =
+ i      +-----+-----+-----+=====+
+     c  |     |     |     |     =
+        +-----+-----+-----+=====+
+     d  |     |     |     |     =
+        +-----+-----+-----+=====+
+     e  |     |     |     |     =
+        +-----+-----+-----+=====+
+        =     =     =     =     =
+        +=====+=====+=====+=====+
+
+	Since 'b' in the first stirng and 'c' in the second do not match, now we
+	won't go diagonally. We're going to look at a different subproblem.
+	Since these two characters do not match, we have to check two different
+	subproblems.
+
+	Either in this one:
+
+	// RIGHT
+	1. "bcde"
+	2. "e"
+
+		Or in this one:
+
+	// DOWN
+	1. "cde"
+	2. "ce"
+
+
+	Now just by looking at this first:
+	// RIGHT
+	1. "bcde"
+	2. "e"
+
+	You can see that the longest common subsequence is 1.
+
+	But when you look at the other:
+	// DOWN
+	1. "cde"
+	2. "ce"
+
+	This is going to end up being 2.
+
+	Now if we follow this rule, at one point we will end up in this situation:
+
+
+                   j
+
+          'a'   'c'   'e'   ''
+        +-----+-----+-----+=====+
+    'a' |     |     |     |     =
+        +-----+-----+-----+=====+
+    'b' |     |     |     |     =
+ i      +-----+-----+-----+=====+
+    'c' |     |     |     |     =
+        +-----+-----+-----+=====+
+    'd' |     |     |     |     =
+        +-----+-----+-----+=====+
+    'e' |     |     |     |     =
+        +-----+-----+-----+=====+
+     '' =     =     =     =     =
+        +=====+=====+=====+=====+
+
+	First of all, what are these bold parts of the grid?
+	There are empty string. For example:
+
+	1. "cde"
+	2. ''
+
+	The longest commont subsequence between these is: 0
+
+	So these cells will all have 0 and since we're doing a Bottom-Up approach
+	, going from the most righ the most down upwards, that will the
+	"initial" value.
+
+
+                   j
+
+          'a'   'c'   'e'   ''
+        +-----+-----+-----+=====+
+    'a' |     |     |     |  0  =
+        +-----+-----+-----+=====+
+    'b' |     |     |     |  0  =
+ i      +-----+-----+-----+=====+
+    'c' |     |     |     |  0  =
+        +-----+-----+-----+=====+
+    'd' |     |     |     |  0  =
+        +-----+-----+-----+=====+
+    'e' |     |     |     |  0  =
+        +-----+-----+-----+=====+
+     '' =  0  =  0  =  0  =  0  =
+        +=====+=====+=====+=====+
+
+
+
+
+
+	So our path would look like:
+
+
+                   j
+
+          'a'   'c'   'e'   ''
+        +-----+-----+-----+=====+
+    'a' |  3  |     |     |  0  =
+        +-----+-----+-----+=====+
+    'b' |     |  2  |     |  0  =
+ i      +-----+-----+-----+=====+
+    'c' |     |  2  |     |  0  =
+        +-----+-----+-----+=====+
+    'd' |     |     |  1  |  0  =
+        +-----+-----+-----+=====+
+    'e' |     |     |  1  |  0  =
+        +-----+-----+-----+=====+
+     '' =  0  =  0  =  0  =  0  =
+        +=====+=====+=====+=====+
+
+	This is how we got the result.
+
+	However the way we're going to solve this code is with "Brute Force-ish".
+	It's still Dynammic Programming but what we have to do is for every value
+	in this cell(except for the 0-cells).
+	Since we're going to do a Bottom up we will have to fill the values like
+	this: (I will number from 1 onwards)
+
+	THIS ARE NOT VALUES. THIS IS JUST THE ORDER IN WHICH WE COMPUTE THE VALUES
+	IN CELLS.
+
+
+                   j
+
+          'a'   'c'   'e'   ''
+        +-----+-----+-----+=====+
+    'a' |  15 |  14 | 13  |  0  =
+        +-----+-----+-----+=====+
+    'b' |  12 |  11 | 10  |  0  =
+ i      +-----+-----+-----+=====+
+    'c' |  9  |  8  |  7  |  0  =
+        +-----+-----+-----+=====+
+    'd' |  6  |  5  |  4  |  0  =
+        +-----+-----+-----+=====+
+    'e' |  3  |  2  |  1  |  0  =
+        +-----+-----+-----+=====+
+     '' =  0  =  0  =  0  =  0  =
+        +=====+=====+=====+=====+
+
+
+
+
+
+	However, these below, are indeed all the values:
+
+
+                   j
+
+          'a'   'c'   'e'   ''
+        +-----+-----+-----+=====+
+    'a' |  3  |  2  |  1  |  0  =
+        +-----+-----+-----+=====+
+    'b' |  2  |  2  |  1  |  0  =
+ i      +-----+-----+-----+=====+
+    'c' |  2  |  2  |  1  |  0  =
+        +-----+-----+-----+=====+
+    'd' |  1  |  1  |  1  |  0  =
+        +-----+-----+-----+=====+
+    'e' |  1  |  1  |  1  |  0  =
+        +-----+-----+-----+=====+
+     '' =  0  =  0  =  0  =  0  =
+        +=====+=====+=====+=====+
+
+	So this is a Bottom Up Dynammic Programming Solution.
+
+	The code is, now, really simple. It's self-explanatory of the IDEA above.
 */
 
 
