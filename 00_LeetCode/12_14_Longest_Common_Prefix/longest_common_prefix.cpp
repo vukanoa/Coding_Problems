557e8104f19cfff67edd21881795bb97bc57b778
Noa Vukasovic <noavukasovic@gmail.com>
2023-03-20 15:31:02 +0100

    Add two more Solutions to LeetCode Problem: 12_14_Longest_Common_Prefix & Add Time and Space Beats(All 3 Beat 100% Time)

--- 00_LeetCode/12_14_Longest_Common_Prefix/longest_common_prefix.cpp
+++ 00_LeetCode/12_14_Longest_Common_Prefix/longest_common_prefix.cpp
@@ -34,7 +34,7 @@
 	--- Example 2 ---
 	Input:  strs = ["dog", "racecar", "car"]
 	Output: ""
-	Explanation: THere is no common prefix among the input stringsn.
+	Explanation: There is no common prefix among the input stringsn.
 
 	*** Constraints ***
 	1 <= strs.length() <= 200
@@ -56,6 +56,8 @@
 */
 
 
+/* Time  Beats: 100%   */
+/* Space Beats: 83.19% */
 /* Time  Complexity: O(S), where S is the sum of all characters of all strings in the vector */
 /* Space Complexity: O(1) */
 class Solution{
@@ -82,10 +84,123 @@ public:
 };
 
 
+
+/* Time  Beats: 100%    */
+/* Space Beats: 27.88%% */
+/* Time  Complexity: O(S) */
+/* Space Complexity: O(1) */
+class Solution_2{
+public:
+	std::string longestCommonPrefix(std::vector<std::string>& strs)
+	{
+		if (strs.empty())
+			return "";
+		else if (strs.size() == 1)
+			return strs[0];
+		else if (strs[0].empty() || strs[1].empty())
+			return "";
+		else if (strs[0][0] != strs[1][0])
+			return "";
+
+		std::string prefix = strs[0];
+		for (int i = 1; i < strs.size(); i++)
+		{
+			if (strs[i] == "" || prefix == "")
+				return "";
+
+			while (prefix != strs[i].substr(0, prefix.size()))
+				prefix.pop_back();
+		}
+
+		return prefix;
+	}
+};
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	We iterate through all the words for letter at index 0, then
+	thorugh all the words for letter at index 1, etc.
+
+	As soon as we find en empty character, meaning all the letters in that word
+	were alerady checked, that means that is the longest commong prefix and
+	we just return substring of any word from (0, prefix.size);
+
+	If at any point in iteration through the words we find out that two
+	adjacent letters are not the same, we immediately return substring of any
+	word from (0, prefix.size)
+
+	It's important to note that we're iterating thorugh to word from:
+		0 to n - 2
+	and NOT
+		0 to n - 1
+
+	That is because we always check current letter x with it's right neighbor.
+	Last character doesn't have a neighbor.
+
+*/
+
+
+/* Time  Beats: 100%   */
+/* Space Beats: 83.19% */
+/*
+	Time  Complexity: O(p)
+	Where p is the longestCommonPrefixSize * strs.size()
+	Meaning:
+	all the letters that are a part of longest common prefix * numbe of words
+
+	In theory it's faster than O(S) solution since we don't have to iterate
+	through every letter of every word, but they are both beating 100%, so
+	I guess it's not that important.
+
+*/
+/* Space Complexity: O(1) */
+class Solution_3{
+public:
+	std::string longestCommonPrefix(std::vector<std::string>& strs)
+	{
+		if (strs.empty())
+			return "";
+		else if (strs[0].empty())
+			return "";
+		else if (strs.size() == 1)
+			return strs[0];
+
+		int prefix_size = 0;
+		int n = strs.size();
+
+		int x = 0;
+		for (;;) // For every prefix letter
+		{
+			// i < n MINUS 1, not n
+			for (int i = 0; i < n-1; i++) // In every word
+			{
+				if (x >= strs[i].length())
+					return strs[0].substr(0, prefix_size);
+
+				if (strs[i][x] != strs[i + 1][x])
+					return strs[0].substr(0, prefix_size);
+			}
+
+			prefix_size++; // Could use just 'x', but it's more verbose
+			x++;
+		}
+
+		return strs[0].substr(0, prefix_size);
+	}
+};
+
+
 int
 main()
 {
 	Solution sol;
+	Solution_2 sol_2;
+	Solution_3 sol_3;
 
 	/* Example 1 */
 	std::vector<std::string> strs = {"flower", "flow", "flight"};
@@ -106,6 +221,7 @@ main()
 	std::cout << "\n\t=== LONGEST COMMON PREFIX ===";
 	std::cout << "\n\t=============================\n";
 
+	/* Write Input */
 	bool first = true;
 	std::cout << "\n\tStrings: [";
 	for (auto s: strs)
@@ -118,7 +234,12 @@ main()
 	}
 	std::cout << "]\n";
 
+	/* Solution */
 	std::string prefix = sol.longestCommonPrefix(strs);
+	// std::string prefix = sol_2.longestCommonPrefix(strs);
+	// std::string prefix = sol_3.longestCommonPrefix(strs);
+
+	/* Write Output */
 	std::cout << "\n\tLongest Common Prefix is: \"" << prefix << "\"\n\n";
 
 	return 0;
