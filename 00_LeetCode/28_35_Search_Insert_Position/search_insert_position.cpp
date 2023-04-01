3323fceed17c577baf27e7bc679c17e9594bf8e6
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-01 21:38:10 +0200

    Replace Solution for LeetCode: 28_35_Search_Insert_Position & Add better Explanation(IDEA)

--- 00_LeetCode/28_35_Search_Insert_Position/search_insert_position.cpp
+++ 00_LeetCode/28_35_Search_Insert_Position/search_insert_position.cpp
@@ -52,6 +52,8 @@
 */
 
 
+
+
 /*
 	------------
 	--- IDEA ---
@@ -60,93 +62,129 @@
 	Since we're told we must have a runtime complexity of O(log n), that means
 	we have to use a Binary Search.
 
-	Let's cover the base cases first.
-		1. Target value is less than or equal to the very first element in the
-		array. That means we should return 0, since if we were to insert it
-		in this sorted array, it would've been inserted index 0.
+	It's a basic Binary Search. There's only one thing that is important to be
+	explained here.
 
-		2. Target value is greater than the very last element in the arrat.
-		That means we should return n. If we were to insert and it is
-		greater than the last element, then we would've insert it at index n.
+	Why are we returning the "left" pointer after the while loop?
+
+~~~~~~~~~~~~~~
+~~ Smaller ~~~ (target smaller than the last we're checking in the while loop)
+~~~~~~~~~~~~~~
+
+	Consider this example:
+	    nums = [2]
+	indexes:    0
+
+	target = 1;
+
+	At the beginning of the while loop, both "left" and "right" are at index 0.
+	"mid" is also going to be 0.
+
+	           mid
+	            R
+	            L
+	    nums = [2]
+	indexes:    0
+
+
+	Now since nums[mid] != target, we have to update either our "left" or our
+	"right" pointer.
+
+	target < nums[mid]
+	  1   <   2
 	
-	We do a simple Binary search and in each iteration we're only checking
-	the position of nums[mid] relative to our target value.
+	Therefore we have to update our "right" pointer to mid - 1.
 
-	If nums[mid] == target, that's it. We've found it. Return mid.
-	If target < nums[mid], just update high to point to mit and repeat.
-	If nums[mid] < target, update low to point to mid if it wasn't mid, and if
-	low and mid are the same, then make low = mid + 1.
+	Once we do that, our "right" becomes less than "left" which breaks our
+	while loop.
 
-	Why do we do this?
-	Let's say that we've always done low = mid + 1;
+	Now we're in this position:
 
-	std::vector<int> nums = {1, 3, 5, 6};
-	int target = 4;
+                    R  L
+            nums =    [2]
+        indexes:   -1  0
+                    ^
+out of bounds ______|
 
-	1st iteration:
-	low  = 0 [1]
-	mid  = 1 [3]
-	high = 3 [6]
+	Look at the above "picture". Let's ask a question - Where should we place
+	our target(1)?
+	Well, at position 0, where our "left" pointer points.
 
-	2nd iteration
-	low  = 2 [5]
-	mid  = 2 [5]
-	high = 3 [6]
 
-	and we would return "prev_high" which after the 2nd iteration would be:
-		prev_high = 3
+
+	Now let's consider a target which is greater than the last item we're
+	checking.
+
+
+~~~~~~~~~~~~~~
+~~ Greater ~~~ (target greater than the last we're checking in the while loop)
+~~~~~~~~~~~~~~
+
+	Consider this example: (Same example, different target)
+	    nums = [2]
+	indexes:    0
+
+	target = 3;
+
+	At the beginning both "left and "right" are at index 0, and "mid" as well.
+
+	           mid
+	            R
+	            L
+	    nums = [2]
+	indexes:    0
+
+	Now since:
+		target > nums[mid]
+			L = mid + 1;
 	
-	which isn't right.
-	We have gone over the right bound, i.e. over the very next element of our
-	target value.
-
-	So if our low != mid, always make low = mid, ONLY if low == mid
-	make low = mid + 1 so our iteration doens't get stuck.
+	We update our "left" pointer and we're in this situation:
 	
-	Also - What is this "prev_high" value? Why do we use that?
-	Since we've jumped over Base Cases, meaning our target value isn't less or
-	equal to out first element nor it is greater than our greater or last
-	element in the array, then that certainly means that whether or not our
-	target value is present in our array, the index is > 0 && < n.
+                    R  L
+            nums = [2]
+        indexes:    0  1
+                       ^
+out of bounds _________|
 
-	That's why we memorize "prev_high" which is previous value of high, since
-	when our while loop statement gets to be false, that is - low >= high
-	we're sure we should only return the index of prev_high.
-	
+	Again, let's ask ourselves the same question - At which index should our
+	target(3) be inserted?
+	Well, at index 1. Which pointer points to index 1?
+	The "left" one again!
+~~~~~~~
+
+	That's the whole "trick" of this problem.
+
 */
 
 
+/* Time  Beats: 100% */
+/* Space Beats: 35.98% */
 
+/* Time  Complexity: O(logn) */
+/* Space Complexity: O(1) */
 class Solution{
 public:
 	int searchInsert(std::vector<int>& nums, int target)
 	{
 		int n = nums.size();
-		int low  = 0;
-		int high = n - 1;
 
-		if (target <= nums[low])
-			return 0;
-		else if (nums[high] < target)
-			return n;
-		
-		int prev_high;
-		
-		while (low < high)
+		int left  = 0;
+		int right = n - 1;
+
+		while (left <= right)
 		{
-			int mid = (low + high) / 2;
+			int mid = (left + right) / 2;
 
-			prev_high = high;
-
-			if (nums[mid] == target)
+			if (target == nums[mid])
 				return mid;
-			else if (nums[mid] < target)
-				low == mid ? low = mid + 1 : low = mid;
-			else if (target < nums[mid])
-				high = mid;
+
+			if (target < nums[mid])
+				right = mid - 1;
+			else
+				left = mid + 1;
 		}
 
-		return prev_high;
+		return left;
 	}
 };
 
@@ -188,6 +226,13 @@ main()
 	// std::vector<int> nums = {1};
 	// int target = -7;
 
+
+	std::cout << "\n\t==============================";
+	std::cout << "\n\t=== SEARCH INSERT POSITION ===";
+	std::cout << "\n\t==============================\n";
+
+
+	/* Write Input */
 	bool first = true;
 	std::cout << "\n\tInput: [";
 	for (auto x: nums)
@@ -201,10 +246,13 @@ main()
 	std::cout << "]";
 	std::cout << "\n\tTarget: " << target << "\n";
 
-	// Solution
+
+	/* Solution */
 	int output = sol.searchInsert(nums, target);
 
-	std::cout << "\n\tOutput: " << output << "\n\n";
+
+	/* Write Output */
+	std::cout << "\n\tInsert at index: " << output << "\n\n";
 
 	return 0;
 }
