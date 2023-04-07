50ee9099b2cc9efd5f195b4df6b6d46f73781d81
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-08 01:19:21 +0200

    Improve Explanation(IDEA) for LeetCode: 32_45_Jump_Game_II

--- 00_LeetCode/32_45_Jump_Game_II/jump_game_ii.cpp
+++ 00_LeetCode/32_45_Jump_Game_II/jump_game_ii.cpp
@@ -80,9 +80,32 @@
 	This is BFS-structure problem.
 	Consider the very first Example:
 		nums = [2, 3, 1, 1, 4]
+	indexes:    0  1  2  3  4
+
+	Graph with values:
+
+            1 - 1
+           /
+          3 - 1 - 4 <-target
+         / \
+start ->2   4 <- target
+         \
+          1 - 1 - 4 <- target
+
+
+
+	Graph with indexes:
+
+            2 - 3 - 4 <- target
+           /
+          1 - 3 - 4 <-target
+         / \
+start ->0   4 <- target
+         \
+          2 - 3 - 4 <- target
 
 	We are initially at position 0. Then we can move at most nums[0] steps
-	from it. So, after one move, we may reach nums[1] == 3 or nums[2] == 1.
+	from it. So, after one move, we may jump to nums[1] == 3 or nums[2] == 1.
 	So these node are reachable in 1 jump.
 
 	From these nodes, we can further move to nums[3] = 1 and nums[4] = 4. Now
@@ -99,26 +122,77 @@
 	It isn't true that the farthest jump is always the most preferred.
 
 	nums = [2, 4, 2, 1, 4, 1, 2, 1, 1]
+	Graphs with values:
 
-	In this case the shortest path is: 0, 2, 4 (indexes) = 3 jumps
+               1..
+              /
+             2 - 4..  1 - 4..   1 - 1 <--
+            /        /         /        |
+           4 -------<   1..   2 - 1     |
+          / \        \ /     /    ^     |
+start -> 2   1..      4 ----<     |     |
+          \            \     \    |     |
+           2 - 1..      1     1   |     |
+            \   1..     ^     |   |     |
+             \ /  2..   |     1   |     |
+              4 -<      |     ^   |     |
+               \  1..   |     |   |     |
+                \       |     |   |     |
+                 1      |     |   |     |                        
+                 ^      |     |   |     |                     
+target __________|______|_____|___|_____|
+
+
+
+
+
+	nums = [2, 4, 2, 1, 4, 1, 2, 1, 1]
+	indexes 0  1  2  3  4  5  6  7  8
+	Graphs with indexes:
+
+               3..
+              /
+             2 - 4..  3 - 4..   7 - 8 <--
+            /        /         /        |
+           1 -------<   5..   6 - 8     |
+          / \        \ /     /    ^     |
+start -> 0   5..      4 ----<     |     |
+          \            \     \    |     |
+           2 - 3..      8     7   |     |
+            \   5..     ^     |   |     |
+             \ /  6..   |     8   |     |
+              4 -<      |     ^   |     |
+               \  7..   |     |   |     |
+                \       |     |   |     |
+                 8      |     |   |     |                        
+                 ^      |     |   |     |                     
+target __________|______|_____|___|_____|
+
+
+
+	nums = [2, 4, 2, 1, 4, 1, 2, 1, 1]
+	indexes 0  1  2  3  4  5  6  7  8
+	In this case the shortest path is: 0, 2, 4 (indexes) = 3 jumps. Or 0, 1, 4.
 	And one would assume that we should've gone for 0, 1, 5, 6 = 4 jumps
 
 	So, how do we get around this?
-	When we are at nums[0] = 2, how to know if we should jump 4 or 2?
-	Well, we should exceed our max_end that is the longest reach which can
-	either be > end + 1 or end+1 itself.
+	When we are at nums[0] = 2, how to know if we should jump to 4 or 2?
 
-	In either case, we move "start" to 'next level'(think of it as BFS).
-	next_level would be "end + 1". Now since our max_end(longest reach) is
-	either the same as the index we're currently on, or longer, now we will
-	check if inside THAT range we can find a jump that gets us to us desired
-	place.
+	At each "level" we calculate what is the maximum reach from the current
+	position. The "level" is defined as "current index + nums[current index]".
+	So at index 'i' there's nums[i] = j.
+
+	"Level" are all the elements from 'i' to "i + j", inclusive.
+
+	Then, from within that "level", we're calculating what is the maximum reach
+	so far, from each position within that "level". At each calculation we're
+	checking if we've hit the target. If yes, just return the number of jumps.
 
 	That's how, even if "taking 4" as an option isn't going to make the
 	shortest path, this approach will. We aren't necessarily taking each jump,
 	we are just "stretching" our max_end(longest reach) and checking if from
-	within that range we can actually get to our desired goal which is the
-	very last element.
+	within that range("level") we can actually get to our desired goal which
+	is the very last element.
 
 */
 
