9f41482b6c9966f48f4ad9fb8e6bc249ba945ff9
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-14 14:29:34 +0200

    Improve format structure to Explanation(IDEA) & Fix one Solution to LeetCode: 41_62_Unique_Paths

--- 00_LeetCode/41_62_Unique_Paths/unique_paths.cpp
+++ 00_LeetCode/41_62_Unique_Paths/unique_paths.cpp
@@ -48,7 +48,6 @@
 */
 
 
-
 /*
 	------------
 	--- IDEA ---
@@ -62,86 +61,15 @@
 	Moreover, we have the base cases dp[0][j] = dp[i][0] = 1 for all valid
 	i and j.
 
-	------------------
-	--- Solution 1 ---
-	------------------
-
-	int uniquePaths(int m, int n)
-	{
-		std::vector<std::vector<int>> dp(m, std::vector<int>(n, 1));
-
-		for (int i = 1; i < m; i++)
-		{
-			for (int j = 1; j < n; j++)
-				dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-		}
-
-		return dp[m - 1][n - 1];
-	}
-
-
-
-
-	The above solutions runs in O(m * n) time and costs O(m * n) space.
-	However, you may have noticed that each time when we update dp[i][j], we
-	only need:
-		dp[i - 1][j](at the previous row) and
-		dp[i][j - 1](at the current  row)
-
-	So we can reduce memory usage to jus two rows => O(n) space.
-
-	------------------
-	--- Solution 2 ---
-	------------------
-
-	int uniquePaths(int m, int n)
-	{
-		std::vector<int> previous_row(n, 1);
-		std::vector<int> current_row (n, 1);
-
-		for (int i = 1; i < m; i++)
-		{
-			for (int j = 1; j < n; j++)
-				current_row[j] = previous_row[j] + current_row[j - 1];
-		}
-
-		std::swap(previous_row, current_row);
-
-		return previous_row[n - 1];
-	}
-
-
-
-
-	Further inspecting the above code:
-		previous_row[j] is just the current_row[j] before the update.
-
-	So we can further reduce the memory usage to one row.
-
-	------------------
-	--- Solution 3 ---
-	------------------
-
-	int uniquePaths(int m, int n)
-	{
-		std::vector<int> current_row (n, 1);
-
-		for (int i = 1; i < m; i++)
-		{
-			for (int j = 1; j < n; j++)
-				current_row[j] += current_row[j - 1];
-		}
-
-		return previous_row[n - 1];
-	}
-
 */
 
 
+/* Time  Beats: 23.53% */
+/* Space Beats: 56.13% */
 
 /* Time  Complexity: O(m * n) */
 /* Space Complexity: O(m * n) */
-class Solution{
+class Solution_1{
 public:
 	int uniquePaths(int m, int n)
 	{
@@ -158,6 +86,95 @@ public:
 };
 
 
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	The above solutions runs in O(m * n) time and costs O(m * n) space.
+	However, you may have noticed that each time when we update dp[i][j], we
+	only need:
+		dp[i - 1][j](previous row)    and
+		dp[i][j - 1](previous column)
+
+	So we can reduce memory usage to just two rows => O(n) space.
+	
+*/
+
+
+/* Time  Beats: 100% */
+/* Space Beats: 89.82% */
+
+/* Time  Complexity: O(m * n) */
+/* Space Complexity: O(n) */
+class Solution_2{
+public:
+	int uniquePaths(int m, int n)
+	{
+		std::vector<int> previous_row(n, 1);
+		std::vector<int> current_row (n, 1);
+
+		for (int i = 1; i < m; i++)
+		{
+			for (int j = 1; j < n; j++)
+			{
+				if (i % 2 == 0)
+					previous_row[j] = current_row[j] + previous_row[j - 1];
+				else
+					current_row[j]  = previous_row[j] + current_row[j - 1];
+			}
+		}
+
+		if (m % 2 == 0)
+			return current_row[n - 1];
+
+		return previous_row[n - 1];
+	}
+};
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Further inspecting the above code:
+		previous_row[j] is just the current_row[j] before the update.
+
+	So we can further reduce the memory usage to one row.
+	
+	Note:
+		Space Beats on LeetCode shows worse percentage than Solution 2, but
+		that depends on LeetCode. Generally, Solution 3 is always preffered.
+*/
+
+
+/* Time  Beats: 100% */
+/* Space Beats: 75.81% */
+
+/* Time  Complexity: O(m * n) */
+/* Space Complexity: O(n) */
+class Solution_3{
+public:
+	int uniquePaths(int m, int n)
+	{
+		std::vector<int> current_row (n, 1);
+
+		for (int i = 1; i < m; i++)
+		{
+			for (int j = 1; j < n; j++)
+				current_row[j] += current_row[j - 1];
+		}
+
+		return current_row[n - 1];
+	}
+};
+
+
 void
 print_grid(int m, int n)
 {
@@ -170,7 +187,7 @@ print_grid(int m, int n)
 			if (i == 0 && j == 0)
 				std::cout << "R ";
 			else if (i == m - 1 && j == n - 1)
-				std::cout << "S ";
+				std::cout << "E ";
 			else
 				std::cout << "_ ";
 		}
@@ -184,7 +201,9 @@ print_grid(int m, int n)
 int
 main()
 {
-	Solution sol;
+	Solution_1 sol_1;
+	Solution_2 sol_2;
+	Solution_3 sol_3;
 
 	/* Example 1 */
 	int m = 3;
@@ -210,15 +229,21 @@ main()
 	std::cout << "\n\t=== UNIQUE PATHS ===";
 	std::cout << "\n\t====================\n\n";
 	std::cout << "\n\t\t\tLegend:";
-	std::cout << "\n\t\t\t\tR - Robot(Start)\n\t\t\t\tS - Star (Finish)";
+	std::cout << "\n\t\t\t\tR - Robot(Start)\n\t\t\t\tS - End  (Finish)";
 
 
+	/* Write Input */
 	std::cout << "\n\tGrid:\n";
 	print_grid(m, n);
 
-	/* Solution */
-	int result = sol.uniquePaths(m, n);
 
+	/* Solution */
+	// int result = sol_1.uniquePaths(m, n);
+	// int result = sol_2.uniquePaths(m, n);
+	int result = sol_3.uniquePaths(m, n);
+
+
+	/* Write Output */
 	std::cout << "\n\tNumber of unique paths: " << result << "\n\n";
 
 	return 0;
