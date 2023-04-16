36693d9be6586c2c37adcf200fd6456cca7bb05c
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-16 19:04:07 +0200

    Add Binary Search Solution to LeetCode: 46_74_Search_A_2D_Matrix & Add Time and Space Beats

--- 00_LeetCode/46_74_Search_A_2D_Matrix/search_a_2d_matrix.cpp
+++ 00_LeetCode/46_74_Search_A_2D_Matrix/search_a_2d_matrix.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 
-
 /*
 	==============
 	=== MEDIUM ===
@@ -53,6 +52,122 @@
 	--- IDEA ---
 	------------
 
+	First check if target is within the last row since we're doing a Vertical
+	Binary Search on 0-th column, thus elements of the last row aren't checked.
+
+	If it is within the last row:
+		Binary Search Horizontally.
+		
+	Otherwise:
+		Binary Search Vertically.
+		Binary Search Horizontally.
+	
+	Theoricatically this Solution should be more efficient in Time than the one
+	below but, somehow, LeetCode doesn't account that. They are both beating
+	100% of accepted Solutions.
+	
+*/
+
+
+/* Time  Beats: 100% */
+/* Space Beats: 80.78% */
+
+/* Time  Complexity: O(log^2(n)) */
+/* Space Complexity: O(1) */
+class Solution_BS {
+public:
+	bool searchMatrix(std::vector<std::vector<int>>& matrix, int target)
+	{
+		int m = matrix.size();
+		int n = matrix[0].size();
+
+		if (target < matrix[0][0] || target > matrix[m - 1][n - 1])
+			return false;
+
+		int up   = 0;
+		int down = m - 1;
+		int prev_mid = -1; // This is if Binary Search "Gets stuck"
+
+		/* Last Row */
+		if (matrix[down][0] < target)
+		{
+			/* Binary Search */
+			int left  = 0;
+			int right = n - 1;
+			while (left <= right)
+			{
+				int mid = (left + right) / 2;
+
+				if (matrix[down][mid] == target)
+					return true;
+				else if (matrix[down][left]  == target)
+					return true;
+				else if (matrix[down][right] == target)
+					return true;
+				else if (target < matrix[down][mid])
+					right = mid - 1;
+				else if (matrix[down][mid] < target)
+					left = mid + 1;
+			}
+
+			return false;
+		}
+
+		/* Binary Search inside Binary Search */
+		while(up <= down)
+		{
+			int mid = (up + down) / 2;
+
+			if (mid == prev_mid)
+			{
+				int left  = 0;
+				int right = n - 1;
+
+				while (left <= right)
+				{
+					int mid = (left + right) / 2;
+
+					if (matrix[prev_mid][mid] == target)
+						return true;
+					else if (matrix[prev_mid][left]  == target)
+						return true;
+					else if (matrix[prev_mid][right] == target)
+						return true;
+					else if (target < matrix[prev_mid][mid])
+						right = mid - 1;
+					else if (matrix[prev_mid][mid] < target)
+						left  = mid + 1;
+				}
+
+				return false;
+			}
+			else
+				prev_mid = mid;
+
+			if (target == matrix[mid][0])
+				return true;
+			else if (matrix[up][0]   == target)
+				return true;
+			else if (matrix[down][0] == target)
+				return true;
+			else if (target < matrix[mid][0])
+				down = mid;
+			else if (matrix[mid][0] < target)
+				up = mid;
+		}
+
+		return false;
+	}
+};
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
 	Since integers are sorted from left to right and since the first integer of
 	each row is greater than the last integer of the previous row, we don't
 	have to search through an entire matrix.
@@ -76,10 +191,10 @@
 	In either case, return false since the target is certainly not inside of
 	matrix.
 
-	I
 */
 
-
+/* Time  Beats: 100% */
+/* Space Beats: 96.94% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
@@ -129,11 +244,8 @@ print_matrix(std::vector<std::vector<int>>& matrix)
 int
 main()
 {
+	Solution_BS sol_bs;
 	Solution sol;
-
-	std::cout << "\n\t==========================";
-	std::cout << "\n\t=== SEARCH A 2D MATRIX ===";
-	std::cout << "\n\t==========================\n\n";
 
 	/* Example 1 */
 	std::vector<std::vector<int>> matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
@@ -155,12 +267,24 @@ main()
 	// std::vector<std::vector<int>> matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
 	// int target = 16;
 
+	std::cout << "\n\t==========================";
+	std::cout << "\n\t=== SEARCH A 2D MATRIX ===";
+	std::cout << "\n\t==========================\n\n";
+
+
+	/* Write Input */
 	std::cout << "\n\tMatrix:";
 	print_matrix(matrix);
 	std::cout << "\n\tTarget: " << target << "\n";
 
 
-	if (sol.searchMatrix(matrix, target))
+	/* Solution */
+	bool found = sol_bs.searchMatrix(matrix, target);
+	// bool found = sol.searchMatrix(matrix, target);
+
+
+	/* Write Output */
+	if (found)
 		std::cout << "\n\n\tOutput: True\n\n";
 	else
 		std::cout << "\n\n\tOutput: False\n\n";
