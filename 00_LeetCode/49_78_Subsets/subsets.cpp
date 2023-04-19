1f9088cc23712b0a4b7cc151d6e0338e3c0ff989
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-19 21:55:58 +0200

    Add Backtracking Solution to LeetCode: 49_78_Subsets & Add Time and Space Complexity Explanations & Add Time and Space Beats

--- 00_LeetCode/49_78_Subsets/subsets.cpp
+++ 00_LeetCode/49_78_Subsets/subsets.cpp
@@ -31,7 +31,7 @@
 
 	--- Example 1 ---
 	Input:  nums = [1, 2, 3]
-	Output: [[], [1], [2], [3], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+	Output: [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
 
 	--- Example 2 ---
 	Input:  nums = [0]
@@ -52,7 +52,7 @@
 
 	(Beats 100%)
 
-	Let's start from empty subest in output list. At each step one takes new
+	Let's start from empty subset in output list. At each step one takes new
 	integer into consideration and generates new subsets from the existing
 	ones.
 
@@ -74,6 +74,34 @@
 */
 
 
+
+
+/* Time  Beats: 100% */
+/* Space Beats: 73.12% */
+
+/*
+	Time  Complexity: O(2^n * n)
+
+	Where n is the length of the input vector nums. The outer loop runs n
+	times, iterating over each element of nums. The inner loop runs O(2^k)
+	times, where k is the size of the subsets generated up to that point. Since
+	each subset can have up to n elements, the total number of subsets
+	generated is 2^0 + 2^1 + ... + 2^n-1, which equals 2^n. Therefore, the
+	total time complexity is O(n * 2^n * n), which simplifies to O(2^n * n).
+
+*/
+/*
+	Space Complexity: O(2^n * n)
+
+	Where n is the length of the input vector nums. The algorithm generates all
+	possible subsets, which in the worst case can be 2^n subsets. Each subset
+	has an average length of n/2, so the total space required to store all
+	subsets is O(2^n * n/2), which simplifies to O(2^n * n). Additionally, the
+	algorithm uses a temporary vector to store each subset, which can have a
+	length of up to n. THerefore, the space complexity is O(2^n * n) due to the
+	subsets generated and O(n) due to the temporary vector used in the inner
+	loop, for a total of O(2^n * n + n).
+*/
 class Solution{
 public:
 	std::vector<std::vector<int>> subsets(std::vector<int>& nums)
@@ -109,10 +137,87 @@ public:
 };
 
 
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Basic Backtracking.
+
+	This Solution will produce output in a different order than in the previous
+	example.
+
+	It's possible that they were looking for the above Solution since that
+	matches the output order, however in the Description of this Problem it is
+	noted that we can return solution in any order.
+
+	So, there's an additional approach.
+	
+*/
+
+
+/* Time  Beats: 100% */
+/* Space Beats: 58.45% */
+
+/*
+	Time  Complexity: O(2^n * n)
+
+	Where n is the length of the input vector nums. This is because for each
+	element in nums, the algorihtm makes a choice of either including or
+	excluding that element, resulting in a total of 2&n possible subsets. For
+	each subset, the algorithm copies the current subset(which takes O(n) time)
+	and appends or removes an element from it(which takes O(1) time).
+	Therefore, the overall Time Complexity is O(2^n * n).
+*/
+/*
+	Space Complexity: O(2^n * n)
+
+	The algorithm generates all possible subsets, which in the worst case can
+	be 2^n subsets. Each subset has an average length of n/2, so the total
+	space required to store all subsets is O(2^n * n/2), which simplifies to
+	O(2^n * n). Additionally, the algorithm uses recursion to generate the
+	subsets, which can result in a maximum depth of n in the call stack.
+	Therefore, the space complexity is O(2^n * n) due to the subsets generated
+	and O(n) due to the recursion call stack, for a total of O(2^n * n + n).
+*/
+class Solution_Backtracking {
+private:
+	void backtracking(
+	                  std::vector<int>& nums,
+	                  int start_from,
+	                  std::vector<int> current,
+	                  std::vector<std::vector<int>>& results
+	                 )
+	{
+		results.push_back(current);
+
+		for (int i = start_from; i < nums.size(); i++)
+		{
+			current.push_back(nums[i]);
+			backtracking(nums, i + 1, current, results);
+			current.pop_back();
+		}
+	}
+
+public:
+	std::vector<std::vector<int>> subsets(std::vector<int>& nums)
+	{
+		std::vector<std::vector<int>> results;
+
+		backtracking(nums, 0, {}, results);
+
+		return results;
+	}
+};
+
+
 int
 main()
 {
 	Solution sol;
+	Solution_Backtracking sol_back;
 
 	/* Example 1 */
 	std::vector<int> nums = {1, 2, 3};
@@ -126,10 +231,10 @@ main()
 	/* Example 4 */
 	// std::vector<int> nums = {1, 2, 3, 4};
 
-
 	std::cout << "\n\t===============";
 	std::cout << "\n\t=== SUBSETS ===";
 	std::cout << "\n\t===============\n";
+
 
 	/* Write Input */
 	bool first = true;
@@ -144,8 +249,11 @@ main()
 	}
 	std::cout << "]\n";
 
+
 	/* Solution */
 	std::vector<std::vector<int>> results = sol.subsets(nums);
+	// std::vector<std::vector<int>> results = sol_back.subsets(nums);
+
 
 	/* Write Output */
 	first = true;
