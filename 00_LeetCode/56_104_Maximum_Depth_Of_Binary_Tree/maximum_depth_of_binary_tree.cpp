1648d65ce512b5b036b5cc838a8b1f1277231405
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-24 22:11:54 +0200

    Add another Solution to LeetCode: 56_104_Maximum_Depth_Of_Binary_Tree & Add Time and Space Beats

--- 00_LeetCode/56_104_Maximum_Depth_Of_Binary_Tree/maximum_depth_of_binary_tree.cpp
+++ 00_LeetCode/56_104_Maximum_Depth_Of_Binary_Tree/maximum_depth_of_binary_tree.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <queue>
 
 /*
 	============
@@ -57,14 +58,15 @@ struct TreeNode {
 	--- IDEA ---
 	------------
 
-	Let's assume that we're already given the maximum depth of root's let and
+	Let's assume that we're already given the maximum depth of root's left and
 	right subtrees by recursion.
+
 	So to find the maximum depth of this binary tree, we will have to take out
-	the maximum of hte 2 depths given to us by recursion and add 1 to that to
+	the maximum of the 2 depths given to us by recursion and add 1 to that to
 	consider the current level i.e. root's level into our depth.
 
 	Base Case:
-		We can easily analyse tha if we are at a leaf node as root, then its
+		We can easily analyze that if we are at a leaf node as root, then its
 		left and right subtrees will have 0 depth, and consecutively, this leaf
 		node will have max depth of 1.
 
@@ -72,7 +74,8 @@ struct TreeNode {
 
 */
 
-
+/* Time  Beats: 73.50% */
+/* Space Beats: 63.71% */
 
 /*	Time  Complexity: O(n) */
 /*
@@ -93,10 +96,134 @@ public:
 	}
 };
 
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Almost equivalent idea, but coded a bit differently.
+	
+*/
+
+/* Time  Beats: 73.50% */
+/* Space Beats: 19.35% */
+
+/*	Time  Complexity: O(n) */
+/*
+	Space Complexity: O(h)
+	Where 'h' is the height of the tree, for the recursive stack.
+*/
+class Solution_2 {
+private:
+	void dfs(TreeNode* root, int current, int& max)
+	{
+		if (root == nullptr)
+			return;
+
+		// If current is not nullptr, add it to the current path and check max
+		max = std::max(max, current + 1);
+
+		dfs(root->left , current + 1, max);
+		dfs(root->right, current + 1, max);
+	}
+
+public:
+	int maxDepth(TreeNode* root)
+	{
+		int max = 0;
+		dfs(root, 0, max);
+
+		return max;
+	}
+};
+
+
+
+
+/*
+	=============================
+	=== This is just printing ===
+	=============================
+*/
+
+void
+print_array(std::vector<std::string>& nums)
+{
+	bool first = true;
+	std::cout << "\n\t*** Level Order ***";
+	std::cout << "\n\tTree: [";
+	for (auto x: nums)
+	{
+		if (!first)
+			std::cout << ", ";
+
+		std::cout << x;
+		first = false;
+	}
+	std::cout << "]\n";
+}
+
+
+void
+print_levelorder(TreeNode* root)
+{
+	if (root == nullptr)
+		return;
+	
+	std::queue<TreeNode*> queue;
+	queue.push(root);
+
+	std::vector<std::string> vector_print;
+
+	while (!queue.empty())
+	{
+		int size = queue.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			TreeNode* node = queue.front();
+			queue.pop();
+
+			if (node == nullptr)
+			{
+				vector_print.push_back("null");
+				continue;
+			}
+			else
+				vector_print.push_back(std::to_string(node->val));
+
+			if (node->left != nullptr)
+				queue.push(node->left);
+			else
+				queue.push(nullptr);
+
+			if (node->right != nullptr)
+				queue.push(node->right);
+			else
+				queue.push(nullptr);
+		}
+	}
+
+	int x = vector_print.size() - 1;
+	while (vector_print[x] == "null")
+	{
+		vector_print.pop_back();
+		x--;
+	}
+
+	print_array(vector_print);
+}
+
+
 int
 main()
 {
-	Solution sol;
+	Solution   sol;
+	Solution_2 sol_2;
+
 
 	/* Example 1 */
 	// TreeNode fifteen(15);
@@ -149,12 +276,20 @@ main()
 
 	std::cout << "\n\t======================================";
 	std::cout << "\n\t=== MAXIMUM DEPTH OF A BINARY TREE ===";
-	std::cout << "\n\t======================================\n\n";
+	std::cout << "\n\t======================================\n";
+
+	
+	/* Write Input */
+	print_levelorder(root);
+
 
 	/* Solution */
 	int depth = sol.maxDepth(root);
 
+
+	/* Write Output */
 	std::cout << "\n\tMaximum Depth is: " << depth << "\n\n";
+
 
 	return 0;
 }
