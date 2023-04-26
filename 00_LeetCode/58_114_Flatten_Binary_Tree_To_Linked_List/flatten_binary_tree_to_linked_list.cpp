142294eb6ed52fca88261581778107438faf74c7
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-26 16:52:48 +0200

    Exchange Solution for a better one for LeetCode: 58_114_Flatten_Binary_Tree_To_Linked_List & Add Time and Space Beats

--- 00_LeetCode/58_114_Flatten_Binary_Tree_To_Linked_List/flatten_binary_tree_to_linked_list.cpp
+++ 00_LeetCode/58_114_Flatten_Binary_Tree_To_Linked_List/flatten_binary_tree_to_linked_list.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stack>
-
+#include <queue>
 
 /*
 	==============
@@ -65,66 +65,73 @@ struct TreeNode {
 	------------
 	--- IDEA ---
 	------------
+	
+	It's very intuitive.
+	Since we're required to make a Linked List in preorder, we'll do a DFS on
+	the BST and while iterating through nodes, we'll push right subtrees on the
+	stack.
 
-	Base case:
-	If root is nullptr just return immediately.
+	Since it is required to form a Linked list using only "right" pointers and
+	since it is required to make "left" pointers point to nullptr at the end,
+	we have to do a few things.
+	
+	Since we're doing a DFS, we'll always go to the left. However, since we
+	also have to make every "left" pointer point to nullptr, we have to save
+	left subtree in a temporary pointer called "left_subtree" which we will
+	pass as the next node to process and assign it to root->right.
+	We'll also say: root->left = nullptr.
 
-	Until you find a node with both "left" and "right" pointers pointing to
-	nullptr and stack is Empty do:
+	Though, we only have to save left subtree if it is not nullptr.
 
-	If root->right does exist, push it onto the stack.
-
-	If root->left does exist, make root's right pointer point to this node and
-	make root->left point to nullptr since that is a requirement from the
-	Description problem.
-
-	If root->left does NOT exist then make root's right point to the node that
-	is on top of the stack and pop the stack.
-
-	However if neither root->left exists nor does stack contain an element
-	make root->right = nullptr, just in case, and break the while loop.
-
-	At the end of the while loop always move root to root->right(next element).
-
+	Since we're doing DFS recursively, every time we enter the function, check
+		if the root argument is nullptr then:
+			pop from the stack(right subtree) and continue from there.
+	
+	There's only one edge case. If the root argument is nullptr and the stack
+	is empty(That happens only once we try to process the left subtree of the
+	very last node in the tree, which is the deepest, right most node).
+	In that case immediately return nullptr.
+	
 */
 
+/* Time  Beats: 92.94% */
+/* Space Beats: 20.96% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(n) */
-class Solution {
+class Solution{
+private:
+	std::stack<TreeNode*> stack;
+
+	TreeNode* dfs(TreeNode* root)
+	{
+		if (root == nullptr)
+		{
+			if (stack.empty())
+				return nullptr;
+
+			root = stack.top();
+			stack.pop();
+		}
+
+		if (root->right != nullptr)
+			stack.push(root->right);
+
+		TreeNode* left_subtree = nullptr;
+		if (root->left != nullptr)
+			left_subtree = root->left;
+
+		root->left = nullptr;
+		root->right = dfs(left_subtree);
+
+		return root;
+	}
+
 public:
-    void flatten(TreeNode* root)
-    {
-        if (root == nullptr)
-            return;
-
-        std::stack<TreeNode *> stack;
-
-        while (1)
-        {
-            if (root->right != nullptr)
-                stack.push(root->right);
-
-            if (root->left != nullptr)
-            {
-                root->right = root->left;
-                root->left = nullptr;
-            }
-            else if (!stack.empty())
-            {
-                TreeNode* node = stack.top();
-                stack.pop();
-                root->right = node;
-            }
-            else
-            {
-                root->right = nullptr;
-                break;
-            }
-
-            root = root->right;
-        }
-    }
+	void flatten(TreeNode* root)
+	{
+		root = dfs(root);
+	}
 };
 
 
@@ -132,7 +139,7 @@ void
 print_list(TreeNode *head)
 {
     TreeNode* cur = head;
-	printf("\n\t");
+	printf("\t\tList: ");
 
 	if (head == nullptr)
 		printf("Empty");
@@ -151,58 +158,139 @@ print_list(TreeNode *head)
 }
 
 
+
+
+/*
+	=============================
+	=== This is just printing ===
+	=============================
+*/
+
+void
+print_array(std::vector<std::string>& nums)
+{
+	bool first = true;
+	std::cout << "\n\t\t\t(TODO: Implement a Visual representation of a Binary Tree)\n\n";
+	std::cout << "\n\t*** Level Order ***";
+	std::cout << "\n\t\tTree: [";
+	for (auto x: nums)
+	{
+		if (!first)
+			std::cout << ", ";
+
+		std::cout << x;
+		first = false;
+	}
+	std::cout << "]\n\n";
+}
+
+
+void
+print_levelorder(TreeNode* root)
+{
+	if (root == nullptr)
+		return;
+	
+	std::queue<TreeNode*> queue;
+	queue.push(root);
+
+	std::vector<std::string> vector_print;
+
+	while (!queue.empty())
+	{
+		int size = queue.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			TreeNode* node = queue.front();
+			queue.pop();
+
+			if (node == nullptr)
+			{
+				vector_print.push_back("null");
+				continue;
+			}
+			else
+				vector_print.push_back(std::to_string(node->val));
+
+			if (node->left != nullptr)
+				queue.push(node->left);
+			else
+				queue.push(nullptr);
+
+			if (node->right != nullptr)
+				queue.push(node->right);
+			else
+				queue.push(nullptr);
+		}
+	}
+
+	int x = vector_print.size() - 1;
+	while (vector_print[x] == "null")
+	{
+		vector_print.pop_back();
+		x--;
+	}
+
+	print_array(vector_print);
+}
+
+
 int
 main()
 {
 	Solution sol;
 
 	/* Example 1 */
-	// TreeNode fifteen(15);
-	// TreeNode seven(7);
-	// TreeNode twenty(20, &fifteen, &seven);
-	// TreeNode nine(9);
-	// TreeNode three(3, &nine, &twenty);
-	// TreeNode* root = &three;
 	/*
 		___________
 		_____3_____
 		__9_____20_
 		______15__7
 	*/
+	// TreeNode fifteen(15);
+	// TreeNode seven(7);
+	// TreeNode twenty(20, &fifteen, &seven);
+	// TreeNode nine(9);
+	// TreeNode three(3, &nine, &twenty);
+	// TreeNode* root = &three;
 
 
 	/* Example 2 */
+	/*
+		___-1___
+	*/
 	// TreeNode one(-1);
 	// TreeNode* root = &one;
 
 
 	/* Example 3 */
+	/*
+		___1___
+	*/
 	// TreeNode one(1);
 	// TreeNode* root = &one;
 
 
 	/* Example 4 */
+	/*
+		Empty
+	*/
 	// TreeNode* root = nullptr;
 
 
 	/* Example 5 */
-	// TreeNode two(2);
-	// TreeNode one(1, &two, nullptr);
-	// TreeNode* root = &one;
 	/*
 		_____
 		__1__
 		2____
 	*/
+	// TreeNode two(2);
+	// TreeNode one(1, &two, nullptr);
+	// TreeNode* root = &one;
 
 
 	/* Example 6 */
-	// TreeNode five(5);
-	// TreeNode four(4, &five, nullptr);
-	// TreeNode three(3, &four, nullptr);
-	// TreeNode two(2, &three, nullptr);
-	// TreeNode one(1, &two, nullptr);
-	// TreeNode* root = &one;
 	/*
 		_______________________1_______________________
 		___________2___________________________________
@@ -210,9 +298,22 @@ main()
 		__4____________________________________________
 		5______________________________________________
 	*/
+	// TreeNode five(5);
+	// TreeNode four(4, &five, nullptr);
+	// TreeNode three(3, &four, nullptr);
+	// TreeNode two(2, &three, nullptr);
+	// TreeNode one(1, &two, nullptr);
+	// TreeNode* root = &one;
 
 
 	/* Example 7 */
+	/*
+		_______________________
+		___________1___________
+		_____2___________3_____
+		__4_____5___________6__
+		______7________________
+	*/
 	TreeNode seven(7);
 	TreeNode five(5, &seven, nullptr);
 	TreeNode four(4);
@@ -221,24 +322,25 @@ main()
 	TreeNode three(3, nullptr, &six);
 	TreeNode one(1, &two, &three);
 	TreeNode* root = &one;
-	/*
-		_______________________
-		___________1___________
-		_____2___________3_____
-		__4_____5___________6__
-		______7________________
-	*/
 
 
 	std::cout << "\n\t==========================================";
 	std::cout << "\n\t=== FLATTEN BINARY TREE TO LINKED LIST ===";
-	std::cout << "\n\t==========================================\n\n";
+	std::cout << "\n\t==========================================\n";
 
-	std::cout << "\n\tFlattened Binary Tree to Linked List:\n";
+
+	/* Write Input */
+	print_levelorder(root);
+
 
 	/* Solution */
 	sol.flatten(root);
+
+
+	/* Write Output */
+	std::cout << "\n\tFlattened Binary Tree to Linked List:\n";
 	print_list(root);
+
 
 	return 0;
 }
