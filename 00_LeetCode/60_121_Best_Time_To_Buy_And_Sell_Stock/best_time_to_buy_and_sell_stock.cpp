9b87f549e7dac40106737e55d4a69828cf663227
Noa Vukasovic <noavukasovic@gmail.com>
2023-04-28 15:56:38 +0200

    Add Solution_DP & Improve Solution_Sliding_Window for LeetCode: 60_121_Best_Time_To_Buy_And_Sell_Stock & Add Time and Space Beats

--- 00_LeetCode/60_121_Best_Time_To_Buy_And_Sell_Stock/best_time_to_buy_and_sell_stock.cpp
+++ 00_LeetCode/60_121_Best_Time_To_Buy_And_Sell_Stock/best_time_to_buy_and_sell_stock.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 
-
 /*
 	============
 	=== EASY ===
@@ -21,11 +20,11 @@
 	You want to maximize your profit by choosing a single day to buy one stock
 	and choosing a different day in the future to sell that stock.
 
-	Return the maximum profit you can achieve from this transactin. If you
+	Return the maximum profit you can achieve from this transaction. If you
 	cannot achieve any profit, return 0.
 
 	============================================
-	FUNCTION: int maxProfit(vecto<int>& prices);
+	FUNCTION: int maxProfit(vector<int>& prices);
 	============================================
 
 	==========================================================================
@@ -52,7 +51,90 @@
 	--- IDEA ---
 	------------
 
-	Two pointers.
+	Dynamic Programming.
+
+	NOTE: This Solution is not optimal due to the use of additional vector.
+	It also iterates through the vector 2 times instead of 1, however both of
+	those are constants so the Time Complexity is O(n) in both Solutions.
+
+	Since we have the insight in the stock market in the next n given days, we
+	can calculate the maximum price of the stock from that day until the end of
+	the period we're given.
+
+	So we go from the back.
+	How does that help us though?
+
+	Consider this First Example:
+		[7, 1, 5, 3, 6, 4]
+	
+	Our dp would look like:
+		[7, 6, 6, 6, 6, 4]
+	
+	So intuitively you'd want to say:
+	"Find minimum stock price in vector 'prices' and then subtract that from
+	the corresponding higher stock price from that day onwards, which resides in
+	vector 'dp'"
+
+	However that would be incorrect. Consider this example:
+		[ 7, 19, 1, 5, 3, 6, 4]
+	
+	Our dp would look like this:
+		[19, 19, 6, 6, 6, 6, 4]
+	
+	If we were to take the minimum stock price in this given period, we would
+	subtract 1 from its corresponding highest value from that day onwards, which
+	in this case is 6, so the max would look like it's 5, which is wrong.
+
+	We can clearly notice that the maximum is gained if we buy on day 0 and
+	sell on the very next day, day 1.
+
+	If we do that, then it's: 19 - 7 = 12 which is correct.
+
+	So how do we get around this?
+	Simply iterate through both of the vector simultaneously and subtract:
+		dp[i] - prices[i]
+	
+	and if that is greater than the max profit until this point, then update
+	variable max.
+
+	Return max.
+
+*/
+
+/* Time  Beats:  5.30% */
+/* Space Beats:  5.38% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_DP{
+public:
+	int maxProfit(std::vector<int>& prices)
+	{
+		int n = prices.size();
+		std::vector<int> dp(n, 0);
+		dp[n-1] = prices[n-1];
+
+		// i + 1 is "previous", since we are iterating backwards
+		for (int i = n-2; i >= 0; i--)
+			dp[i] = std::max(dp[i + 1], prices[i]);
+
+		int max = 0;
+		for (int i = 0; i < n; i++)
+			max = std::max(max, dp[i] - prices[i]);
+
+		return max;
+	}
+};
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Sliding Window technique.
 
 	Imagine a coordinate system.
 	Values on the Y axis are the prices.
@@ -64,7 +146,7 @@
 	If we find that prices[right] is lower than prices[left](which is the
 	current day) then we make "left" point to "right" and we increment "right"
 	by one.
-	We don't want to buy it today it tomorrow's price is going to be lower.
+	We don't want to buy it today if tomorrow's price is going to be lower.
 
 	However, if prices[right] is greater than prices[left], meaning we would
 	make some profit, then calculate if that profit is the maximum and also
@@ -73,18 +155,19 @@
 	Once we get "right" pointer to the end of the array "prices" we are done.
 	Just return "max".
 
-	We are sure that is the maximum since if we were to find out that some
+	We are sure that this is the maximum since if we were to find out that some
 	other value was lower than our fixed "left" pointer, we would've moved it
 	and calculate profit from that point.
 
 	We pass through the array only once, thus the Complexity is O(n).
 */
 
-
+/* Time  Beats: 82.77% */
+/* Space Beats: 57.32% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
-class Solution{
+class Solution_Sliding_Window{
 public:
 	int maxProfit(std::vector<int>& prices)
 	{
@@ -95,13 +178,10 @@ public:
 
 		while (right < prices.size())
 		{
-			if (prices[left] > prices[right])
-				left = right;
+			if (prices[left] < prices[right])
+				max = std::max(max, prices[right] - prices[left]);
 			else
-			{
-				if (prices[right] - prices[left] > max)
-					max = prices[right] - prices[left];
-			}
+				left = right;
 
 			right++;
 		}
@@ -114,7 +194,8 @@ public:
 int
 main()
 {
-	Solution sol;
+	Solution_DP             sol_dp;
+	Solution_Sliding_Window sol_sliding;
 
 	/* Example 1 */
 	std::vector<int> prices = {7, 1, 5, 3, 6, 4};
@@ -124,12 +205,10 @@ main()
 
 	std::cout << "\n\t=======================================";
 	std::cout << "\n\t=== BEST TIME TO BUY AND SELL STOCK ===";
-	std::cout << "\n\t=======================================\n\n";
-
-	/* Solution */
-	int max = sol.maxProfit(prices);
+	std::cout << "\n\t=======================================\n";
 
 
+	/* Write Input */
 	bool first = true;
 	std::cout << "\n\tPrices: [";
 	for (auto x: prices)
@@ -143,7 +222,14 @@ main()
 	std::cout << "]\n";
 
 
+	/* Solution */
+	// int max = sol_dp.maxProfit(prices);
+	int max = sol_sliding.maxProfit(prices);
+
+
+	/* Write Output */
 	std::cout << "\n\tMax profit is: " << max << "\n\n";
+
 
 	return 0;
 }
