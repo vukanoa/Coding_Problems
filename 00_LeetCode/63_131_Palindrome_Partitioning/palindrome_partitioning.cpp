cc41fb1621b3205ec14ee5c02ac265be0dc93f6a
Noa Vukasovic <noavukasovic@gmail.com>
2023-05-01 14:18:17 +0200

    Change Explanation(IDEA) for LeetCode: 63_131_Palindrome_Partitioning & Add Time and Space Beats

--- 00_LeetCode/63_131_Palindrome_Partitioning/palindrome_partitioning.cpp
+++ 00_LeetCode/63_131_Palindrome_Partitioning/palindrome_partitioning.cpp
@@ -46,73 +46,100 @@
 	--- IDEA ---
 	------------
 
-	Intuition:
-	- The structure of the question is quite clear that it promotes the use of
-	  recursion. The recursion approach of this question can be like: we start
-	  iteration from beginning and step by step we increase the size of substr
-	  that we are checking for palindromes. Once the palindrome substring is
-	  found, the ubstr needs to go to a vector storing those palindromes
-	  substrings. This is how the brute force recursion will work.
+	For the first partition we have 3 choices:
+		1) "a" by itself if we took just the first character of the string.
+		2) "aa", if we took first two characters.
+		3) "aab", if we took the entire string as our first partition.
 
-	- The optimization of the above recursion is backtracking.
-	  A backtracking algorithm uses the depth-first search method. When it
-	  starts exploring the solutions, a bounding or a helper function is
-	  applied so that the algorithm can check if the so-far built solution
-	  satisfies the constraints. If it does, it continues searching. If it
-	  doesn't, the branch would be eliminated, and the algorithm goes back to
-	  the level above. In simple words - Backtraking starts from some
-	  intermediate position where there is still a hope to get a new
-	  palindrome.
+	                    *** Backtracking ***
 
-	- In backtracking algorithm there is always a helper method or a safe
-	  checker method which checks whether the built solution satisfies the
-	  conditions or not. This one thing is guaranteed.
+	                      _____"aab"_____
+	                     /       |       \
+	                   "a"      "aa"      "aab"
+
+		1) "a" is indeed a palindrome, so we continue down that path with
+		   remaining characters, which are "ab" in this case.
+
+	                    *** Backtracking ***
+
+	                      _____"aab"_____
+	                     /       |       \
+	                   "a"      "aa"      "aab"
+	                   / \
+	                 "a" "ab"
+
+	Now we have two options:
+		1) "a" by itself, if we took just the first character of the
+		   remaining string "ab".
+
+		2) "ab", if we took the entire remaining string.
+
+		
+		Since 1) "a", is indeed a palindrome, we coninue down that path.
+
+	                    *** Backtracking ***
+
+	                      _____"aab"_____
+	                     /   
+	                   "a"   
+	                   / \
+	                 "a" "ab"
+	                 /   
+	               "b"
+	              (end)
+
+		"b" was the only remaining character and it is a palindrome by itself
+		so we push it in current_vec.
+
+		Now we go dfs(i + 1) but that is out of bounds, so we push the entire
+		current_vec, which at this point is ["a", "a", "b"] to vector "results"
+		and we Backtrack.
+
+		We pop "b" and Backtrack again since that is the last character.
+		We pop "a" and go dfs(i + 1) and now we're checking the "ab"
 
 
-	Algorithm:
-	1. As we've alread said - Backtracking always needs a helper method. In our
-	   problem we will need a bool helper method which will check if a given
-	   substring is indeed a palindrome. Let's name it: bool palindrome()
+	                    *** Backtracking ***
 
-	2. Variables:
-		- "results" is the vector for storing the vectors of palindromic
-		  substrings.
+	                      _____"aab"_____
+	                     /   
+	                   "a"   
+	                   / \
+	                 "a" "ab"
+	                 /    ^^
+	               "b"
+	              (end)
 
-		- "current_vec" is the vector for storing the ongoing substring which
-		  has the potential of satisfying the conditions of palindrome.
+		We can clearly see that it's not a palindrome and thus Backtrack.
 
-		- "start" stores the starting position of the current substring
+		At the very end the Backtracking Tree look like this:
 
-		- Index 'i' for traversing inside the substring.
 
-	3. We search from "start" till the end of the string. Once we reach a
-	   position 'i' such that the substring from "start" to 'i', meaning
-	   s.substr(start, i - start + 1) is a palindrome, we add it to our
-	   "current_vec" vector. Then we recursively call the same method to
-	   execute the remaining substring. Once we reach the end of the string,
-	   we add palindromes "current_vec" into the "results" of all the possible
-	   partitioning.
+	                    *** Backtracking ***
 
-	4. Remember that at position 'i' we find s.substr(start, i - start + 1) to
-	   be a palindrome and we immediately add it to "current_vec". Now imagine
-	   that there may be a position j, such that j > i, and
-	   s.substr(start, j - start + 1) is also a palindrome. That means we need
-	   to go back to our "start" before adding s.substr(start, i - start + 1)
-	   to "current_vec" and continue to find the next palindrome position after
-	   'i'. And after this evertime we simply need to remove or pop
-	   s.substr(start, i - start + 1) out of "current_vec" to execute the
-	   backtracking algorithm.
+	                      _____"aab"_____
+	                     /       |       \
+	                   "a"      "aa"      "aab"
+	                   / \       |         ^^^
+	                 "a" "ab"   "b"
+	                 /    ^^   (end)
+	               "b"
+	              (end)
+	
+	Legend:
+		  ^^^ => Backtrack since it is NOT a Palindrome. Do not store it.
 
-	5. The steps we executed in the above step is none other than the DFS.
-
+		(end) => No more characters to go "down", push the current_vec.
 */
 
-
+/* Time  Beats: 82.73% */
+/* Space Beats: 96.39% */
 
 /* Time  Complexity: O(n * 2^n) */
-/* Space Complexity: O(n) */
+/* Space Complexity: O(n^2) */
 class Solution {
 private:
+	// O(2^n)
 	void backtracking_dfs(std::string& s, int start, std::vector<std::string>& current_vec, std::vector<std::vector<std::string>>& results)
 	{
 		if (start == s.length())
@@ -131,6 +158,7 @@ private:
 		}
 	}
 
+	// O(n)
 	bool palindrome(std::string& str, int left, int right)
 	{
 		while (left < right)
@@ -161,7 +189,7 @@ main()
 	Solution sol;
 
 	/* Example 1 */
-	std::string s = "aab";
+	// std::string s = "aab";
 
 	/* Example 2 */
 	// std::string s = "a";
@@ -178,10 +206,17 @@ main()
 	/* Example 6 */
 	// std::string s = "bab";
 
+	/* Example 7 */
+	std::string s = "abcdcba";
+
+	/* Example 8 */
+	// std::string s = "aaa";
+
 
 	std::cout << "\n\t===============================";
 	std::cout << "\n\t=== PALINDROME PARTITIONING ===";
 	std::cout << "\n\t===============================\n\n";
+
 
 	/* Write Input */
 	std::cout << "\tString: \"";
@@ -189,8 +224,10 @@ main()
 		std::cout << x;
 	std::cout << "\"\n";
 
+
 	/* Solution */
 	std::vector<std::vector<std::string>> results = sol.partition(s);
+
 
 	/* Write Output */
 	bool first = true;
