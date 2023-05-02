d0f866c79527e2df49b03e85c44949fbe02b2373
Noa Vukasovic <noavukasovic@gmail.com>
2023-05-02 16:52:27 +0200

    Add Solution_Neat to LeetCode: 64_134_Gas_Station & Add Time and Space Beats & Add Note

--- 00_LeetCode/64_134_Gas_Station/gas_station.cpp
+++ 00_LeetCode/64_134_Gas_Station/gas_station.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <numeric> // for accumulate
 
 /*
 	==============
@@ -70,10 +71,30 @@
 */
 
 
+
+/*
+	============
+	=== NOTE ===
+	============
+
+	This Problem was taken down from Top 100 Liked Questions on LeetCode.
+
+	This Problem is not intuitive to me at all. It's a very weird form of
+	Greedy Algorithm.
+	
+*/
+
+
+
 /*
 	------------
 	--- IDEA ---
 	------------
+
+	They say that this Problem is similar to 53_Maximum_Subarray because it
+	also uses Kadane's Algorithm.
+
+
 
 	It is said that if there is a solution it is guaranteed to be unique.
 	So either there is a solution and it's the only one, or there isn't.
@@ -113,10 +134,12 @@
 
 */
 
+/* Time  Beats: 54.90% */
+/* Space Beats: 88.30% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
-class Solution{
+class Solution_Kadane {
 public:
 	int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost)
 	{
@@ -140,10 +163,182 @@ public:
 	}
 };
 
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	The "std::accumulate" function takes three arguments:
+		1. Starting point of the vector
+		2. Ending point of the vector
+		3. Initial value
+	
+	It's equivalent to Python function: sum(array)
+
+	This Problem is not intuitive to me at all. It's a very weird form of
+	Greedy Algorithm.
+
+	Important thing to note is that if the total sum of gas isn't greater or
+	equal to total sum of cost, then there is NO Solution. In that case just
+	return -1.
+
+	We're doing that with std::accumulate.
+
+	However, afterwards, this is absolutely not intuitive.
+
+	In Example 1:
+		gas  = [ 1,  2,  3,  4,  5]
+		cost = [ 3,  4,  5,  1,  2]
+	
+		diff = [-2, -2, -2,  3,  3]
+	
+	Once we start from the index 3, where the value is positive, and we
+	continue and not "dip" under the 0 until the end, then THAT index is the
+	Solution.
+	We DON'T go around trying to subtract the remaining ones. This is why it's
+	absolutely not intuitive.
+
+
+	There are 3 comments that I've found that are explaining this a bit better.
+
+	I)
+		Actually, the reason why it works is simple, and it happens because of
+		two factors.
+			1) If you moved to some value, and your total sum is greater than
+			   zero, then it means, that previous values did bring some value
+			   to the outcome.
+
+			   For example, we have gas = [2,3,0] and cost = [0,0,5].
+
+			   If we take just solely value 3 without 2, it wouldn't be enough
+			   to pass the last station, but previous values definitely bring
+			   some value to the outcome.
+
+			2) If we know, that there's definitely has to be a solution.
+			   Then, we may assume, that it has to be the smallest possible
+			   value, as I said before it may bring the most value to the
+			   result.
+
+
+	
+	II) 
+		The solution would click if you try to apply the pattern as in:
+			Kadane's Algorithm.
+		Try to build a solution from the starting index, once you are certain
+		that it can no longer be an answer, forget everything and consider the
+		next index.
+		
+
+
+	III)
+		I will try to explain it in my own way-- please be patient and read it,
+		hope you will get the intuition behind the algorithm.
+
+		There are 4 parts to it.
+
+		Part 1) 
+			Sum of gas array>=sum of cost array, Therefore: very intuitive, we
+			should always have enough gas.
+
+
+		Part 2)
+			We are keeping a total+=gas[i]-cost[i] for each i, and whenever it
+			is < 0 we are skipping that point and moving forward,
+			making total 0, Therefore, it means we ran out of gas if we started
+			at some point which was <= current pos of i, so now we have to find
+			a new starting position, which wall be > curr pos of i.
+
+			Now think, why will this new start lie ahead of curr pos i, not
+			anywhere before it,  you could think, we started from point
+				A------>B(total till +ve)------->C(total<0),
+			as per this algo we try to find start ahead of C, what if we
+			started from B and skipped A instead, well that won't work.
+			You moved from A--------> B with some positive value(or 0), or else
+			you would have stopped right at B and made total to 0. So add A
+			improved our chances of having a positive total, so there is no
+			point in looking for the new position start anywhere behind point
+			C.
+
+
+		Part 3)
+			When the total stays +ve, we dn't do anything to the start point,
+			our start pointer points to the first index when our total became
+			positive.
+
+			Again this is similar to the above explanation.
+			Lets suppose we start from
+				X(-ve)--->Y(-ve)--->A(+ve)---->B(+ve)---->C(+ve),
+			where C is the end of the array, our start(which is also the ans)
+			would be A.
+
+			Why not B? why not C?
+
+			It is because we moved from A to B with some +ve value or atleast
+			0, whereas if we started from B we would have had only the value of
+			B so earlier point added some value to our total, so its more
+			favorable to help us reach the ans, hence earliest point is always
+			better.
+
+
+		Part 4)
+			Why we just stop at point C and don t complete the cycle and check.
+
+			It is because from Part 1 we would have already identified that if
+			the given set of inputs will have an ans, so if we have reached to
+			Part 3 it means we surely have an ans, and it is mentioned in the
+			question that there is only one valid ans, so we will always choose
+			the most favorable ans-- which is also the fundamental idea of
+			Greedy Algorithims. There is also a mathematical proof for this,
+			that if we got a start point given our total gas >=total cost, we
+			will be able to reach back to that point with just enough gas. 
+
+*/
+
+/* Time  Beats: 83.49% */
+/* Space Beats: 88.30% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution_Neat {
+public:
+	int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost)
+	{
+		int gas_sum  = std::accumulate(gas.begin(),  gas.end(), 0);
+		int cost_sum = std::accumulate(cost.begin(), cost.end(), 0);
+
+		/* There is NO Solution */
+		if (gas_sum < cost_sum)
+			return -1;
+
+		/* There is certainly a Solution */
+		int n = gas.size();
+
+		int total = 0;
+		int start = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			total += gas[i] - cost[i];
+
+			if (total < 0)
+			{
+				total = 0;
+				start = i + 1;
+			}
+		}
+
+		return start;
+	}
+};
+
+
 int
 main()
 {
-	Solution sol;
+	Solution_Kadane sol_kadane;
+	Solution_Neat sol_neat;
 
 	/* Example 1 */
 	std::vector<int> gas  = {1, 2, 3, 4, 5};
@@ -203,11 +398,15 @@ main()
 	}
 	std::cout << "]\n";
 
+
 	/* Solution */
-	int solution = sol.canCompleteCircuit(gas, cost);
+	int solution = sol_kadane.canCompleteCircuit(gas, cost);
+	// int solution = sol_neat.canCompleteCircuit(gas, cost);
+
 
 	/* Write output */
 	std::cout << "\n\tStarting Gas Station is: " << solution << "\n\n";
+
 
 	return 0;
 }
