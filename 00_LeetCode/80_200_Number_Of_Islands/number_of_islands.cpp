989e7b15963d64052620f741543a0fa0acbcc74d
Noa Vukasovic <noavukasovic@gmail.com>
2023-05-14 20:45:44 +0200

    Add BFS Solution to LeetCode: 80_200_Number_Of_Islands

--- 00_LeetCode/80_200_Number_Of_Islands/number_of_islands.cpp
+++ 00_LeetCode/80_200_Number_Of_Islands/number_of_islands.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<queue>
 
 /*
 	==============
@@ -69,6 +70,8 @@
 	If that wasn't the case then we would've had to use an unordered_set and/or
 	we could implement a BFS Solution instead of DFS.
 
+	There is also an elegant Find&Union Solution, but I won't implement it yet.
+
 */
 
 /* Time  Beats: 98.84% */
@@ -80,7 +83,7 @@
 /* Space Complexity: O(n)
 	Recursion can go up to N levels deep since we can have all 1's in "grid".
 */
-class Solution{
+class Solution_DFS {
 public:
 	int numIslands(std::vector<std::vector<char>>& grid)
 	{
@@ -123,10 +126,94 @@ private:
 };
 
 
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	It's a similar IDEA, just implemented using BFS.
+
+	I wanted to implement it in as different way as possible, that's why I've
+	used: lambda function, directions, pairs, etc.
+
+	BFS does NOT use recursion. BFS uses a Queue, instead.
+
+*/
+
+/* Time  Beats: 91.73% */
+/* Space Beats: 26.98% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_BFS{
+public:
+	int numIslands(std::vector<std::vector<char>>& grid)
+	{
+		if (grid.size() == 0 || grid[0].size() == 0)
+			return 0;
+
+		int m = grid.size();
+		int n = grid[0].size();
+
+		std::vector<std::pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+		int islands = 0;
+
+		/* Lambda */
+		auto inside = [&](int i, int j)
+		{
+			return i >= 0 && j >= 0 && i < m && j < n;
+		};
+
+		for (int i = 0; i < m; i++)
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (inside(i, j) && grid[i][j] == '1')
+				{
+					islands++;
+					/* If we are not allowed to modify original given "grid",
+					   then use vector<vector<bool>> visisted.
+					*/
+					grid[i][j] = 'x'; // Mark as processed
+
+					/* BFS */
+					std::queue<std::pair<int, int>> queue;
+					queue.push({i, j});
+
+					while (!queue.empty())
+					{
+						std::pair<int, int> curr = queue.front();
+						queue.pop();
+
+						for (const std::pair<int, int>& dir : directions)
+						{
+							int row = curr.first  + dir.first;
+							int col = curr.second + dir.second; 
+
+							if (inside(row, col) && grid[row][col] == '1')
+							{
+								grid[row][col] = 'x';
+								queue.push({row, col}); // Mark as processed
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return islands;
+	}
+};
+
+
 int
 main()
 {
-	Solution sol;
+	Solution_DFS sol_dfs;
+	Solution_BFS sol_bfs;
+
 
 	/* Example 1 */
 	// std::vector<std::vector<char>> grid {
@@ -176,7 +263,8 @@ main()
 
 
 	/* Solution */
-	int islands = sol.numIslands(grid);
+	int islands = sol_dfs.numIslands(grid);
+	// int islands = sol_bfs.numIslands(grid);
 
 
 	/* Write Output */
