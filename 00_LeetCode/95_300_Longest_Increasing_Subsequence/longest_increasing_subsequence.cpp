8c5c5f18485f2583b2cd47cfa86b255da1aa454c
Noa Vukasovic <noavukasovic@gmail.com>
2023-05-28 16:47:44 +0200

    Add TLE Solution & Add DFS_With_Cache_DP Solution to LeetCode: 95_300_Longest_Increasing_Subsequence

--- 00_LeetCode/95_300_Longest_Increasing_Subsequence/longest_increasing_subsequence.cpp
+++ 00_LeetCode/95_300_Longest_Increasing_Subsequence/longest_increasing_subsequence.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map> // TLE Solution
 
 /*
 	==============
@@ -48,6 +49,84 @@
 	-10^4 <= nums[i] <= 10^4
 
 */
+
+
+class Solution_TLE {
+public:
+	int lengthOfLIS(std::vector<int>& nums)
+	{
+		int n = nums.size();
+		std::unordered_map<int, std::vector<int>> umap;
+		int max = 1;
+
+		for (int i = 0; i <= n; i++)
+			umap[i] = {};
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			umap[1].push_back(nums[i]);
+
+			for (int j = i; j > 0; j--)
+			{
+				if (umap[j].empty())
+					continue;
+
+				bool exist = false;
+				for (int k = 0; k < umap[j].size(); k++)
+				{
+					if (umap[j][k] < nums[i])
+					{
+						umap[j + 1].push_back(nums[i]);
+						
+						if (j + 1 > max)
+							exist = true;
+					}
+				}
+
+				if (exist)
+					max++;
+			}
+		}
+
+		return max;
+	}
+};
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	DFS with Cache.
+	TODO
+	
+*/
+
+
+/* Time  Beats: 35.56% */
+/* Space Beats: 50.84% */
+class Solution_DFS_with_Cache_DP {
+public:
+	int lengthOfLIS(std::vector<int>& nums)
+	{
+		int n = nums.size();
+		std::vector<int> LIS (n, 1);
+
+		for (int i = n - 1; i >= 0; i--)
+		{
+			for (int j = i + 1; j < n; j++)
+			{
+				if (nums[i] < nums[j])
+					LIS[i] = std::max(LIS[i], 1 + LIS[j]);
+			}
+		}
+
+		return *std::max_element(LIS.begin(), LIS.end());
+	}
+};
 
 
 
@@ -103,59 +182,115 @@ public:
 	Greedy with Binary Search
 
 	Let's construct the idea from following example.
-	Consider the example nums = [2, 6, 8, 3, 4, 5, 1], let's try to build the
-	increasing subsequences starting with an ampty one sub1 = [].
+
+		nums = [2, 6, 8, 3, 4, 5, 1],
+
+	let's try to build the increasing subsequences starting with an empty one
+
+		sub1 = [].
 
 	1. Let pick the first element, sub1 = [2]
 
-	2. 6 is greater than the previous number, sub1 = [2, 6]
 
-	3. 8 is greater than previous number, sub1 = [2, 6, 8]
+	2. 6 is greater than the previous number.
 
-	4. 3 is less than previous number, we can't extend the subsequence sub1,
-	   but we must keep 3 because in the future there may have the longest
-	   subsequence start with [2, 3], sub1 = [2, 6, 8], sub2 = [2, 3].
+		   sub1 = [2, 6]
 
-	5. With 4, we can't extend sub1, but we can extend sub2, so
-	   sub1 = [2, 6, 8], sub2 = [2, 3, 4]
 
-	6. With 5, we can't extend sub1, but we can extend sub2, so
-	   sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5]
+	3. 8 is greater than previous number.
 
-	7. With 1, we can't extend neighter sub1 nor sub2, but we need to keep 1,
-	   so sub1 = [2, 6, 8], sub2 = [2, 3, 4, 5], sub3 = [1]
+		   sub1 = [2, 6, 8]
 
-	8. Finally, length of longest increase subsequence = len(sub2) = 4
 
+	4. 3 is less than previous number, therefore we can't extend the
+	   subsequence sub1, but we must keep 3 because in the future the longest
+	   subsequence may start with [2, 3]. Now we have:
+
+		sub1 = [2, 6, 8],
+		sub2 = [2, 3]
+
+
+	5. With 4, we can't extend sub1, but we can extend sub2, so:
+
+		   sub1 = [2, 6, 8],
+		   sub2 = [2, 3, 4]
+
+	6. With 5, we can't extend sub1, but we can extend sub2, so:
+
+		   sub1 = [2, 6, 8],
+		   sub2 = [2, 3, 4, 5]
+
+
+	7. With 1, we can't extend neither sub1 nor sub2, but we need to keep 1,
+	   so:
+		   sub1 = [2, 6, 8],
+		   sub2 = [2, 3, 4, 5],
+		   sub3 = [1]
+
+
+	8. Finally, length of the longest increase subsequence = len(sub2) = 4
+
+
+	~~~~~~~~~~~~
+	~~~ Note ~~~
+	~~~~~~~~~~~~
+	Now this optimization is not intuitive to me AT ALL. If you find it hard
+	to grasp, it's okay.
 
 	In the above steps, we need to keep different "sub" arrays
-	(sub1, sub2, sub3, ..., subk)
-	which causes poor performance. But we notice that we can just keep one
-	"sub" array, when new number 'x' is not greater than the last element of
-	the subsequence "sub" we d binary search to find the smallest
+
+		(sub1, sub2, sub3, ..., subk)
+
+	which causes poor performance. But we are noticing that we can just keep
+	one "sub" array, when new number 'x' is not greater than the last element
+	of the subsequence "sub" we do a binary search to find the smallest
 	element >= x in "sub", and replace with number x.
 
 
-	Let's run that example: nums = [2, 6, 8, 3, 4, 5, 1] again:
-	1. Let's pick the first element, sub = [2]
+	Let's run that example again:
+		
+		nums = [2, 6, 8, 3, 4, 5, 1] again:
 
-	2. 6 is greater than previous number, sub = [2, 6]
+	1. Let's pick the first element,
 
-	3. 8 is greater than previous number, sub = [2, 6, 8]
+		   sub = [2]
 
-	4. 3 is less than previous number, so we can't extend the subsequence sub.
-	   We need to find the smallest number >= 3 in "sub", it's 6. Then we
-	   overwrite it, now sub = [2, 3, 8].
+
+	2. 6 is greater than the previous number,
+
+		   sub = [2, 6]
+
+
+	3. 8 is greater than the previous number,
+
+		   sub = [2, 6, 8]
+
+
+	4. 3 is less than the previous number, so we can't extend the subsequence
+	   sub. We need to find the smallest number that is >= 3 in "sub", it's 6.
+	   Then we overwrite it, now:
+
+		   sub = [2, 3, 8].
+
 
 	5. 4 is less than previous number so we can't extend the subsequence sub.
-	   We overwrite 8 by 4, so sub = [2, 3, 4]
+	   We overwrite 8 by 4, so:
 
-	6. 5 is greater than previous number, sub = [2, 3, 4, 5]
+		   sub = [2, 3, 4]
 
-	7, 1 is less than previous number, so we can't extend the subsequence sub,
-	   We overwrite 2 by 1, so sub = [1, 3, 4, 5].
 
-	8. Finally, length of longest increase subsequence = len(sub) = 4
+	6. 5 is greater than the previous number:
+
+		   sub = [2, 3, 4, 5]
+
+
+	7. 1 is less than previous number, so we can't extend the subsequence sub,
+	   We overwrite 2 by 1, so:
+
+		   sub = [1, 3, 4, 5].
+
+
+	8. Finally, length of the longest increase subsequence = len(sub) = 4
 
 */
 
@@ -166,7 +301,8 @@ public:
 /*
 	Space Complexity: O(n)
 	We can achieve O(1) in space by overwriting values of "sub" into original
-	"nums" array.
+	"nums" array. But in order to do that we'd have to be sure that is
+	permitted.
 */
 class Solution_Greedy_Binary{
 public:
@@ -180,6 +316,7 @@ public:
 				sub.push_back(x);
 			else
 			{
+				// Search for first element e such that x <= e
 				auto it = std::lower_bound(sub.begin(), sub.end(), x);
 				*it = x;
 			}
@@ -199,9 +336,9 @@ public:
 
 	Binary Indexed Tree(Increase base of "nums" into one-basing indexing)
 
-	- Let f[x] is the length of longest increase subsequence, where all number
-	in the subsequence <= x. This is the max element in indices [1..x] if we
-	build the Binary Indexed Tree(BIT)
+	- Let f[x] be the length of longest increase subsequence, where all number
+	in the subsequence are <= x. This is the max element in indices [1..x] if
+	we build the Binary Indexed Tree(BIT)
 
 	- Since -10^4 <= nums[i] <= 10^4, we can convert nums into:
 	      1 <= nums[i] <= 2 * 10^4 + 1 by plus base=10001 to store into the BIT
@@ -314,7 +451,7 @@ public:
 
 /* Time  Complexity: O(n * logn) */
 /* Space Complexity: O(n) */
-class Solution_Compress {
+class Solution_BIT_Compress {
 public:
 	int lengthOfLIS(std::vector<int>& nums)
 	{
@@ -346,10 +483,12 @@ public:
 int
 main()
 {
-	Solution_DP sol_dp;
-	Solution_Greedy_Binary sol_grd_bin;
-	Solution_BIT sol_bit;
-	Solution_Compress sol_com;
+	Solution_DFS_with_Cache_DP sol_dfs_cache_dp;
+	Solution_DP                sol_dp;
+	Solution_Greedy_Binary     sol_grd_bin; // Efficient!
+	Solution_BIT               sol_bit;
+	Solution_BIT_Compress      sol_bit_com;
+
 
 	/* Example 1 */
 	std::vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
@@ -380,10 +519,11 @@ main()
 
 
 	/* Solution */
+	// int longest = sol_dfs_cache_dp.lengthOfLIS(nums);
 	// int longest = sol_dp.lengthOfLIS(nums);
 	int longest = sol_grd_bin.lengthOfLIS(nums);
 	// int longest = sol_bit.lengthOfLIS(nums);
-	// int longest = sol_com.lengthOfLIS(nums);
+	// int longest = sol_bit_com.lengthOfLIS(nums);
 
 
 	std::cout << "\n\tLongest Increasing Subsequence: " << longest << "\n\n";
