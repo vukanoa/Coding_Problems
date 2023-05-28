ed09c1adf5f48e7044149cbf1184b80bc97edf89
Noa Vukasovic <noavukasovic@gmail.com>
2023-05-28 18:22:34 +0200

    Add Time and Space Beats to LeetCode: 96_322_Coin_Change

--- 00_LeetCode/96_322_Coin_Change/coin_change.cpp
+++ 00_LeetCode/96_322_Coin_Change/coin_change.cpp
@@ -20,7 +20,7 @@
 	denominations and an integer "amount" representing a total amount of money.
 
 	Return the fewest number of coins that you need to make up that amount. If
-	that amount of money cannot be made up by any comination of the coins,
+	that amount of money cannot be made up by any combination of the coins,
 	return -1.
 
 	You may assume that you have an infinite number of each kind of coin.
@@ -196,33 +196,47 @@
 
 */
 
+/* Time  Beats: 97.90% */
+/* Space Beats:   100% */
 
+/*
+	Time  Complexity: O(amount * coins.size())
 
-/* Time  Complexity: O(amount * coins.size()) */
+	Sorting is: O(coins.size() * log(coins.size()))
+	but:
+		O(coins.size() * log(coins.size())) < O(amount * coins.size())
+	
+	That's why O(amount * coins.size()) is the overall Complexity.
+
+*/
 /* Space Complexity: O(amount) */
 class Solution {
 public:
-    int coinChange(std::vector<int>& coins, int amount)
+	int coinChange(std::vector<int>& coins, int amount)
 	{
-        int dp[amount + 1];
-        dp[0] = 0;
+		int dp[amount + 1];
+		dp[0] = 0;
 
-        std::sort(begin(coins), end(coins));
+		// O(coins.size() * log(coins.size()))
+		std::sort(begin(coins), end(coins));
 
-        for (int a = 1; a <= amount; a++)
-        {
-            dp[a] = INT_MAX;
-            for (int c: coins)
-            {
-                if (a - c < 0)
-                    break;
-                else if (dp[a - c] != INT_MAX)
-                    dp[a] = std::min(dp[a], 1 + dp[a - c]);
-            }
-        }
-        return dp[amount] == INT_MAX ? -1 : dp[amount];
-    }
+		// O(amount * coins.size())
+		for (int a = 1; a <= amount; a++)
+		{
+			dp[a] = INT_MAX;
+			for (const int& c : coins)
+			{
+				if (a - c < 0)
+					break;
+				else if (dp[a - c] != INT_MAX)
+					dp[a] = std::min(dp[a], 1 + dp[a - c]);
+			}
+		}
+
+		return dp[amount] == INT_MAX ? -1 : dp[amount];
+	}
 };
+
 
 int
 main()
@@ -242,12 +256,20 @@ main()
 	// int amount = 0;
 
 	/* Example 4 */
-	std::vector<int> nums = {1, 3, 4};
-	int amount = 6;
+	// std::vector<int> nums = {1, 3, 4};
+	// int amount = 6;
+
+	/* Example 5 */
+	// std::vector<int> nums = {1, 3, 5};
+	// int amount = 7;
+
+	/* Example 6 */
+	std::vector<int> nums = {1, 2, 5, 10};
+	int amount = 18;
 
 	std::cout << "\n\t===================";
 	std::cout << "\n\t=== COIN CHANGE ===";
-	std::cout << "\n\t===================\n\n";
+	std::cout << "\n\t===================\n";
 
 
 	/* Write Input */
@@ -265,10 +287,14 @@ main()
 	}
 	std::cout << "]\n";
 
+
 	/* Solution */
 	int number = sol.coinChange(nums, amount);
 
+	
+	/* Write Output */
 	std::cout << "\n\n\tMin coins used: " << number << "\n\n";
+
 
 	return 0;
 }
