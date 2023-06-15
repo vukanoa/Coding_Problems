2ccfc147d8f0fbad2838e37fe091ceeb011227e9
Noa Vukasovic <noavukasovic@gmail.com>
2023-06-15 23:53:19 +0200

    Add LeetCode: 13_202_Happy_Number to Top_150

---
+++ 1_LeetCode_Top_150_Interview_Questions/13_202_Happy_Number/happy_number.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <cmath>
+#include <unordered_set>
+
+/*
+	============
+	=== EASY ===
+	============
+
+	===========================
+	202) Happy Number
+	===========================
+
+	============
+	Description:
+	============
+
+	Write an algorithm to determine if a number n is happy.
+
+	A happy number is a number defined by the following process:
+
+	- Starting with any positive integer, replace the number by the sum of the
+	  squares of its digits.
+
+	- Repeat the process until the number equals 1 (where it will stay), or it
+	  loops endlessly in a cycle which does not include 1.
+
+	- Those numbers for which this process ends in 1 are happy.
+
+	Return true if n is a happy number, and false if not.
+
+	==============================
+	FUNCTION: bool isHappy(int n);
+	==============================
+
+	==========================================================================
+	================================ EXAMPLES ================================
+	==========================================================================
+
+	--- Example 1 ---
+	Input: n = 19
+	Output: true
+	Explanation:
+	1^2 + 9^2 = 82
+	8^2 + 2^2 = 68
+	6^2 + 8^2 = 100
+
+	1^2 + 0^2 + 0^2 = 1
+
+
+	--- Example 2 ---
+	Input: n = 2
+	Output: false
+
+
+	*** Constraints ***
+	1 <= n <= 2^31 - 1
+
+*/
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Intuitive. Self-Explanatory.
+	
+*/
+
+/* Time  Complexity: O(logn) */
+/* Space Complexity: O(logn) */
+class Solution{
+public:
+	bool isHappy(int n)
+	{
+		int sum = n;
+		std::unordered_set<int> uset;
+
+		while (sum >= 0)
+		{
+			int curr = sum;
+			sum = 0;
+
+			while (curr > 0)
+			{
+				sum += std::pow(curr % 10, 2);
+				curr /= 10;
+			}
+
+			if (sum == 1)
+				return true;
+
+			if (uset.find(sum) != uset.end())
+				return false;
+
+			uset.insert(sum);
+		}
+
+		return false;
+	}
+};
