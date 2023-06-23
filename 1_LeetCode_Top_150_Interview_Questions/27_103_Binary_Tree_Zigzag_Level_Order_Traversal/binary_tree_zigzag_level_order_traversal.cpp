122ae1ee2e9672a13febb2f02aaee35288d3bfb5
Noa Vukasovic <noavukasovic@gmail.com>
2023-06-24 00:15:28 +0200

    Add LeetCode: 27_103_Binary_Tree_ZigZag_Level_Order_Traversal to Top_150

---
+++ 1_LeetCode_Top_150_Interview_Questions/27_103_Binary_Tree_Zigzag_Level_Order_Traversal/binary_tree_zigzag_level_order_traversal.cpp
@@ -0,0 +1,374 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+/*
+	==============
+	=== MEDIUM ===
+	==============
+
+	=============================================
+	103) Binary Tree Zigzag Level Order Traversal
+	=============================================
+
+	============
+	Description:
+	============
+
+	Given the root of a binary tree, return the zigzag level order traversal of
+	its nodes' values. (i.e., from left to right, then right to left for the
+	next level and alternate between).
+
+	===============================================================
+	FUNCTION: vector<vector<int>> zigzagLevelOrder(TreeNode* root);
+	===============================================================
+
+	==========================================================================
+	================================ EXAMPLES ================================
+	==========================================================================
+
+	--- Example 1 ---
+		___________
+		_____3_____
+		__9____20__
+		_____15___7
+
+	Input: root = [3,9,20,null,null,15,7]
+	Output: [[3],[20,9],[15,7]]
+
+
+
+
+	--- Example 2 ---
+
+		-----
+		__1__
+
+	Input: root = [1]
+	Output: [[1]]
+
+
+
+
+	--- Example 3 ---
+			
+
+	Input: root = []
+	Output: []
+
+
+
+
+	*** Constraints ***
+	The number of nodes in the tree is in the range [0, 2000].
+	-100 <= Node.val <= 100
+
+*/
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	_______________________
+	___________1___________
+	_____2___________3_____
+	__4_____5_____6_____7__
+	8___9_10_11_12_13_14_15
+
+	
+*/
+
+/* Time  Beats:  100% */
+/* Space Beats: 27.23% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(max nodes at a single level) */
+class Solution{
+public:
+	std::vector<std::vector<int>> zigzagLevelOrder(TreeNode* root)
+	{
+		if (root == nullptr)
+			return {};
+
+		std::vector<std::vector<int>> results;
+		results.push_back({root->val});
+
+		std::vector<TreeNode*> left_to_right;
+		std::vector<TreeNode*> right_to_left;
+
+		bool zag = true;
+
+		// Left
+		if (root->left != nullptr)
+			right_to_left.push_back(root->left);
+
+		// Right
+		if (root->right != nullptr)
+			right_to_left.push_back(root->right);
+
+		while (!left_to_right.empty() || !right_to_left.empty())
+		{
+			if (zag)
+			{
+				std::vector<int> level;
+
+				while (!right_to_left.empty())
+				{
+					TreeNode* tmp = right_to_left.back();
+					right_to_left.pop_back();
+
+					level.push_back(tmp->val);
+
+					// Right
+					if (tmp->right != nullptr)
+						left_to_right.push_back(tmp->right);
+
+					// Left
+					if (tmp->left != nullptr)
+						left_to_right.push_back(tmp->left);
+				}
+
+				results.push_back(level);
+				zag = false;
+			}
+			else // left_to_right
+			{
+				std::vector<int> level;
+
+				while (!left_to_right.empty())
+				{
+					TreeNode* tmp = left_to_right.back();
+					left_to_right.pop_back();
+
+					level.push_back(tmp->val);
+
+					// Left
+					if (tmp->left != nullptr)
+						right_to_left.push_back(tmp->left);
+
+					// Right
+					if (tmp->right != nullptr)
+						right_to_left.push_back(tmp->right);
+				}
+
+				results.push_back(level);
+				zag = true;
+			}
+		}
+
+		return results;
+	}
+};
+
+
+/*
+	=============================
+	=== This is just printing ===
+	=============================
+*/
+
+void
+print_array(std::vector<std::string>& nums)
+{
+	bool first = true;
+	std::cout << "\n\t*** Level Order ***";
+	std::cout << "\n\tTree: [";
+	for (auto x: nums)
+	{
+		if (!first)
+			std::cout << ", ";
+
+		std::cout << x;
+		first = false;
+	}
+	std::cout << "]\n";
+}
+
+
+void
+print_levelorder(TreeNode* root)
+{
+	if (root == nullptr)
+		return;
+	
+	std::queue<TreeNode*> queue;
+	queue.push(root);
+
+	std::vector<std::string> vector_print;
+
+	while (!queue.empty())
+	{
+		int size = queue.size();
+
+		for (int i = 0; i < size; i++)
+		{
+			TreeNode* node = queue.front();
+			queue.pop();
+
+			if (node == nullptr)
+			{
+				vector_print.push_back("null");
+				continue;
+			}
+			else
+				vector_print.push_back(std::to_string(node->val));
+
+			if (node->left != nullptr)
+				queue.push(node->left);
+			else
+				queue.push(nullptr);
+
+			if (node->right != nullptr)
+				queue.push(node->right);
+			else
+				queue.push(nullptr);
+		}
+	}
+
+	int x = vector_print.size() - 1;
+	while (vector_print[x] == "null")
+	{
+		vector_print.pop_back();
+		x--;
+	}
+
+	print_array(vector_print);
+}
+
+
+int
+main()
+{
+	Solution sol;
+
+
+	/* Example 1 */
+	/*
+		___________
+		_____3_____
+		__9____20__
+		_____15___7
+	*/
+	// TreeNode fifteen(15);
+	// TreeNode seven(7);
+	// TreeNode twenty(20, &fifteen, &seven);
+	// TreeNode nine(9);
+	// TreeNode three(3, &nine, &twenty);
+
+	// TreeNode* root = &three;
+
+
+
+	/* Example 2 */
+	// TreeNode one(1);
+
+	// TreeNode* root = &one;
+
+
+
+	/* Example 3 */
+	// TreeNode* root = nullptr;
+
+
+
+	/* Example 4 */
+	// TreeNode two(2);
+	// TreeNode one(1, &two, nullptr);
+
+	// TreeNode* root = &one;
+
+
+	/* Example 5 */
+	// TreeNode five(5);
+	// TreeNode four(4, &five, nullptr);
+	// TreeNode three(3, &four, nullptr);
+	// TreeNode two(2, &three, nullptr);
+	// TreeNode one(1, &two, nullptr);
+
+	// TreeNode* root = &one;
+
+
+
+
+	/* Example 6 */
+	/*
+		_______________________
+		___________1___________
+		_____2___________3_____
+		__4_____5_____6_____7__
+		8___9_10_11_12_13_14_15
+	*/
+	TreeNode fifteen (15);
+	TreeNode fourteen(14);
+	TreeNode thirteen(13);
+	TreeNode twelve  (12);
+	TreeNode eleven  (11);
+	TreeNode ten     (10);
+	TreeNode nine    (9);
+	TreeNode eight   (8);
+
+	TreeNode seven(7, &fourteen, &fifteen);
+	TreeNode six  (6, &twelve,   &thirteen);
+	TreeNode five (5, &ten,      &eleven);
+	TreeNode four (4, &eight,    &nine);
+
+	TreeNode three(3, &six,  &seven);
+	TreeNode two  (2, &four, &five);
+	TreeNode one  (1, &two,  &three);
+
+	TreeNode* root = &one;
+
+	
+
+	std::cout << "\n\t================================================";
+	std::cout << "\n\t=== BINARY TREE ZIGZAG LEVEL ORDER TRAVERSAL ===";
+	std::cout << "\n\t================================================\n";
+
+
+	/* Write Input */
+	print_levelorder(root);
+
+
+	/* Solution */
+	std::vector<std::vector<int>> results = sol.zigzagLevelOrder(root);
+
+
+	/* Write Output */
+	bool first = true;
+	std::cout << "\n\tResults: [";
+	for (auto x: results)
+	{
+		if (!first)
+			std::cout << ", ";
+
+		bool first_first = true;
+		std::cout << "[";
+		for (const auto& xx : x)
+		{
+			if (!first_first)
+				std::cout << ", ";
+
+			std::cout << xx;
+			first_first = false;
+		}
+		std::cout << "]";
+
+		first = false;
+	}
+	std::cout << "]\n\n";
+	
+
+	return 0;
+}
