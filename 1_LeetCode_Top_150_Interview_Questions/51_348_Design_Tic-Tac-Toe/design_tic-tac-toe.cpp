f60346cd22b8abc91ed812af40cf056b243e7e15
Noa Vukasovic <noavukasovic@gmail.com>
2023-07-12 05:17:51 +0200

    Add LeetCode: 51_348_Design_Tic-Tac-Toe(Efficient Solution) to Top_150

--- 1_LeetCode_Top_150_Interview_Questions/51_348_Design_Tic-Tac-Toe/design_tic-tac-toe.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/51_348_Design_Tic-Tac-Toe/design_tic-tac-toe.cpp
@@ -98,6 +98,16 @@
 
 */
 
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	TODO
+	
+*/
+
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(n^2) */
 class TicTacToe {
@@ -187,6 +197,70 @@ public:
 private:
     int n;
     std::vector<std::vector<int>> board;
+};
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	TODO
+	
+*/
+
+// ==================
+// === SOLUTION 2 ===
+// ==================
+
+/* Time  Complexity: O(1) */
+/* Space Complexity: O(n) */
+class TicTacToe {
+public:
+	/** Initialize your data structure here. */
+	TicTacToe(int n)
+		: n(n), diagonal(0), anti_diagonal(0), rows(n,0), cols(n,0)
+	{}
+
+	/*
+		Player {player} makes a move at ({row}, {col}).
+		@param row The row of the board.
+		@param col The column of the board.
+		@param player The player, can be either 1 or 2.
+			@return The current winning condition, can be either:
+				0: No one wins.
+				1: Player 1 wins.
+				2: Player 2 wins.
+		*/
+
+	int move(int row, int col, int player)
+	{
+		int add = (player == 1) ? 1 : -1;
+
+		rows[row] += add;
+		cols[col] += add;
+
+		if (row == col)
+			diagonal += add;
+
+		if (row == n - col - 1)
+			anti_diagonal += add;
+
+		return (abs(rows[row]) == n || abs(cols[col]) == n || abs(diagonal) == n || abs(anti_diagonal) == n) ? player : 0;
+
+	}
+
+private:
+	int n, diagonal, anti_diagonal;
+	vector<int> rows, cols;
 };
 
 /**
