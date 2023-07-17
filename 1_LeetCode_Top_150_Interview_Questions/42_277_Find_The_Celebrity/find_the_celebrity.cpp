5e9db84ea8235791ef77a72436722ed7dd20f4a3
Noa Vukasovic <noavukasovic@gmail.com>
2023-07-17 22:44:39 +0200

    Add Explanation(IDEA) for LeetCode: 42_277_Find_The_Celebrity to Top_150

--- 1_LeetCode_Top_150_Interview_Questions/42_277_Find_The_Celebrity/find_the_celebrity.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/42_277_Find_The_Celebrity/find_the_celebrity.cpp
@@ -42,18 +42,63 @@
 	==========================================================================
 
 	--- Example 1 ---
-	
+	Input: n = 2; // next n * (n - 1) lines 
+		In the back this is the situation:
+			0 knows 1
+			1 does not know 0
+
+	Output: 1
+
+	Explanation: Everyone knows 1, and 1 knows no one.
+
+
 
 	--- Example 2 ---
-	<++>
+	Input: n = 3; // next n * (n - 1) lines 
+		In the back this is the situation:
+		0 does not know 1
+		0 does not know 2
+		1 knows 0
+		1 does not know 2
+		2 knows 0
+		2 knows 1
+
+	Output: 0
+
+	Explanation: Everyone knows 0,and 0 knows no one.
+	             0 does not know 1,and 1 knows 0.
+	             2 knows everyone,but 1 does not know 2.
+
 
 	--- Example 3 ---
-	<++>
+	<Unknown>
 
 	*** Constraints ***
-	<++>
+	<Unknown>
 
 */
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Since we're told that there will be **exactly** one celebrity if he/she is
+	in the party, we should go through all the n people and ask them:"Hey, do
+	you know the next guy?", if they do know them, then the candidate is that
+	next guy.
+
+	So to be a candidate, it must not know other people that came after him.
+
+	Now that we have a possible celebrity, we have to go through all the people
+	and see if each and every one of them(except himself personally) know the
+	candidate and if the candidate doesn't know any one of them back.
+
+	That's the whole idea.
+
+*/
+
+bool knows(int a, int b);
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
