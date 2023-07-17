374b98f97a2250c295d063f1bfea86895174f93a
Noa Vukasovic <noavukasovic@gmail.com>
2023-07-17 23:43:48 +0200

    Add Explanation(IDEA) for LeetCode: 44_285_Successor_In_BST to Top_150

--- 1_LeetCode_Top_150_Interview_Questions/44_285_Successor_In_BST/successor_in_bst.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/44_285_Successor_In_BST/successor_in_bst.cpp
@@ -48,6 +48,127 @@ struct TreeNode {
 };
 
 
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Since we're given a BST, it means that the next node of given 'p' is always
+	the leftmost node in the right subtree.
+	
+	The basecase is that the root node is the 'p' node, in that case if the
+	right subtree exists, return the leftmost node of the right subtree.
+	(that is what "successor" function does)
+
+	So let's consider one Example:
+
+	_______________________8_______________________
+	___________4______________________15___________
+	_____2___________7___________12________________
+	________3_____6___________11____14_____________
+	________________________10____13_______________
+
+	There are basic Examples and Tricky Examples.
+
+	Basic Example: p = 2
+		In this case, we just have to find the 2 in a BST and then if the right
+		subtree isn't null, return the leftmost node in the right subtree.
+
+		Pretty much the same as in the base case.
+	
+	Tricky Example: p = 7
+		If you tried the same approach on p = 7, you would returned nullptr,
+		however that would not the true.
+
+		7 doesn't have the right subtree, but it doesn't mean that it does not
+		have a successor node.
+
+		In this case, the successor is the root node.
+
+		So how can we know that the 7 has a successor node if he doesn't have
+		a right subtree?
+
+		We will keep track of the path we've gone to in finding a node, 7 in
+		this case. However, we have to notice that the Tricky Example always
+		happens once we go to the right at last after which we're on the 'p'
+		node.
+
+		So in keeping the track of the path we've gone to find a 'p' node, we
+		are going to push a pair of int:(right | left).
+
+		"left" will be designated with a false boolean value, whereas "right"
+		will be the opposite - true.
+
+		Again, let's draw the same example for the ease of reading:
+
+		_______________________8_______________________
+		___________4______________________15___________
+		_____2___________7___________12________________
+		________3_____6___________11____14_____________
+		________________________10____13_______________
+
+		If 'p' is 7, then the Stack will look like this:
+
+		First we're at the root 8, since that is not equal to 'p', we push the
+		value of that node, but we have to see if we're going to push it with
+		a false("left") or true("right") value.
+
+		How are we going to determine that?
+		Well, it's simply a traversing of a BST. Is 7 less than current node,
+		if it is, then go left, but before going left, push the pair on the
+		stack.
+
+			The pair is: {15, false};
+
+		So our Stack looks like this:
+
+		|  8: false |
+		+-----------+
+		|   Stack   |
+		+-----------+
+
+		Now we're on the node 4. Is 4 a 'p' node(7)? Not it's not. Is 4 less
+		than 7? No, then go right, but before going right push the pair on the
+		stack:
+			The pair is: {4, true}
+
+		So our Stack looks like this:
+
+		|  4: true  |
+		+-----------+
+		|  8: false |
+		+-----------+
+		|   Stack   |
+		+-----------+
+
+		Now we're on the node 7, which is the 'p' node. What do we do know?
+
+		Well, first we have to see if the 'p' node has the right subtree. Since
+		it does not, then we have to take the use of the Stack.
+
+		So what are we going to do?
+		We have to pop elements off of the Stack as long as the top element is
+		not with the second value of "false", meaning that we turned left.
+
+		Essentially we're trying to find the last occassion at which we have
+		gone to the left. The node which from we've gone to the left is our
+		solution, i.e. the successor of the 'p' node.
+
+		After the popping, the Stack looks like this:
+
+		|  8: false |
+		+-----------+
+		|   Stack   |
+		+-----------+
+
+		And we just return the top's element first value. If the Stack is empty
+		return null, meaning there is no successor in the tree. (15 is an
+		example of that).
+
+*/
+
+/* Time  Complexity: O(log n) */
+/* Space Complexity: O(log n) */
 class Solution{
 public:
 	TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p)
@@ -68,6 +189,7 @@ public:
 
 		while (root != nullptr)
 		{
+			// Basic Example
 			if (root->left == p)
 			{
 				if (root->left->right != nullptr)
@@ -75,6 +197,7 @@ public:
 
 				return root;
 			}
+			// Tricky Example
 			else if (root->right == p)
 			{
 				if (root->right->right != nullptr)
@@ -90,6 +213,7 @@ public:
 						return pair.first;
 				}
 			}
+			// Go either left or right:
 			else
 			{
 				if (p->val < root->val)
