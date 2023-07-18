21815f9459caf7ad5f673268127dc149fa0d7e88
Noa Vukasovic <noavukasovic@gmail.com>
2023-07-18 21:03:58 +0200

    Add in IDEA for LeetCode: 51_348_Design_Tic-Tac-Toe to Top_150

--- 1_LeetCode_Top_150_Interview_Questions/51_348_Design_Tic-Tac-Toe/design_tic-tac-toe.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/51_348_Design_Tic-Tac-Toe/design_tic-tac-toe.cpp
@@ -53,59 +53,74 @@
 	TicTacToe toe = new TicTacToe(3);
 
 	toe.move(0, 0, 1); - > Returns 0 (no one wins)
-	|X| | |
-	| | | |    // Player 1 makes a move at (0, 0).
-	| | | |
+	| X |   |   |
+	+---+---+---+
+	|   |   |   |    // Player 1 makes a move at (0, 0).
+	+---+---+---+
+	|   |   |   |
+
 
 	toe.move(0, 2, 2); -> Returns 0 (no one wins)
-	|X| |O|
-	| | | |    // Player 2 makes a move at (0, 2).
-	| | | |
+	| X |   | O |
+	+---+---+---+
+	|   |   |   |    // Player 2 makes a move at (0, 2).
+	+---+---+---+
+	|   |   |   |
+
 
 	toe.move(2, 2, 1); -> Returns 0 (no one wins)
-	|X| |O|
-	| | | |    // Player 1 makes a move at (2, 2).
-	| | |X|
+	| X |   | O |
+	+---+---+---+
+	|   |   |   |    // Player 1 makes a move at (2, 2)
+	+---+---+---+
+	|   |   | X |
+
 
 	toe.move(1, 1, 2); -> Returns 0 (no one wins)
-	|X| |O|
-	| |O| |    // Player 2 makes a move at (1, 1).
-	| | |X|
+	| X |   | O |
+	+---+---+---+
+	|   | O |   |    // Player 2 makes a move at (1, 1)
+	+---+---+---+
+	|   |   | X |
+
 
 	toe.move(2, 0, 1); -> Returns 0 (no one wins)
-	|X| |O|
-	| |O| |    // Player 1 makes a move at (2, 0).
-	|X| |X|
+	| X |   | O |
+	+---+---+---+
+	|   | O |   |    // Player 1 makes a move at (2, 0)
+	+---+---+---+
+	| X |   | X |
+
 
 	toe.move(1, 0, 2); -> Returns 0 (no one wins)
-	|X| |O|
-	|O|O| |    // Player 2 makes a move at (1, 0).
-	|X| |X|
+	| X |   | O |
+	+---+---+---+
+	| O | O |   |    // Player 2 makes a move at (1, 0)
+	+---+---+---+
+	| X |   | X |
+
 
 	toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
-	|X| |O|
-	|O|O| |    // Player 1 makes a move at (2, 1).
-	|X|X|X|
-
-
-
-
-
+	| X |   | O |
+	+---+---+---+
+	| O | O |   |    // Player 1 makes a move at (2, 1)
+	+---+---+---+
+	| X | X | X |
 
 
 	*** Constraints ***
-	Unknown
+	<Unknown>
 
 */
-
 
 /*
 	------------
 	--- IDEA ---
 	------------
 
-	TODO
-	
+	This is just an implementation of the description of the game. Not much
+	to explain here.
+
 */
 
 /* Time  Complexity: O(n) */
@@ -213,8 +228,9 @@ private:
 	--- IDEA ---
 	------------
 
-	TODO
-	
+	This is also just an implementation of the description of the game. Not
+	much to explain here.
+
 */
 
 // ==================
