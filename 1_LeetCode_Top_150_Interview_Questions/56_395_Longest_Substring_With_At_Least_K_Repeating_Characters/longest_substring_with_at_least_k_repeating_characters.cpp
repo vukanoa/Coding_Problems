d310811167bf028d2eef63904327b75b96bcc7f5
Noa Vukasovic <noavukasovic@gmail.com>
2023-07-19 22:29:18 +0200

    Add Explanation(IDEA) for LeetCode: 56_395_Longest_Substring_With_At_Least_K_Repeating_Characters to Top_150

--- 1_LeetCode_Top_150_Interview_Questions/56_395_Longest_Substring_With_At_Least_K_Repeating_Characters/longest_substring_with_at_least_k_repeating_characters.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/56_395_Longest_Substring_With_At_Least_K_Repeating_Characters/longest_substring_with_at_least_k_repeating_characters.cpp
@@ -49,8 +49,49 @@
 	--- IDEA ---
 	------------
 
-	TODO
+	Let's look at an example:
 	
+	    k = 2
+	    s = "ababbcaabc"
+	
+	We will first count the occurrence of each character between the whole
+	string.
+
+	Occurrences:
+
+	| a | b | c |
+	+---+---+---+
+	| 4 | 5 | 1 |
+
+	Once we have a count with us, we'll start from the 0th character and we
+	check:
+		
+		if occurrence of ith(0 in this case) is greater than K(2 in our case):
+			i++;
+
+		We will be repeating this until we find a character which occur less
+		than K times or if we reach the end of the string.
+
+	In our Example, we'd stop at:
+	    k = 2
+	    s = " a b a b b c a a b c"
+	          0 1 2 3 4 5 6 7 8 9
+	                    i
+
+	At this particular index, we break the string into two:
+	                    | 
+	    s = " a b a b b | c a a b c"
+	          0 1 2 3 4 | 5 6 7 8 9
+	                    | i
+	                    |
+
+	The answer lies in one of the substrings. It's either left or right.
+	This is now done recursively.
+
+	We'll apply the same logic again with updated "start" and "end" value.
+
+	Now just look at the code and it'll be become obvious.
+
 */
 
 /* Time  Beats:   100% */
@@ -71,7 +112,7 @@ private:
 		if (end - start < k)
 			return 0;
 
-		std::vector<int> count(26, 0);
+		std::vector<int> count(26, 0); // Hash Map
 		for (int i = start; i < end; i++)
 			count[s[i] - 'a']++;
 
