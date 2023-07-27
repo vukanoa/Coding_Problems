e0c07afdabb6859de5f36a2534eefab16a834e3c
Noa Vukasovic <noavukasovic@gmail.com>
2023-07-27 22:47:12 +0200

    Add LeetCode: 63_212_Word_Search_II to Top_150

--- 1_LeetCode_Top_150_Interview_Questions/63_212_Word_Search_II/word_search_ii.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/63_212_Word_Search_II/word_search_ii.cpp
@@ -175,3 +175,101 @@ private:
 		return false;
 	}
 };
+
+
+
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	TODO
+	
+*/
+
+/* Time  Beats: 95.83% */
+/* Space Beats: 69.66% */
+
+class Solution {
+	struct TrieNode {
+	TrieNode *children[26];
+	string word;
+
+	TrieNode() : word("")
+	{
+		for (int i = 0; i < 26; i++)
+			children[i] = nullptr;
+	}
+};
+
+public:
+	std::vector<std::string> findWords(std::vector<std::vector<char>>& board, std::vector<std::string>& words)
+	{
+		TrieNode *root = buildTrie(words);
+		std::vector<std::string> result;
+
+		for (int i = 0; i < board.size(); i++)
+		{
+			for (int j = 0; j < board[0].size(); j++)
+			{
+				dfs(board, i, j, root, result);
+			}
+		}
+
+			return result;
+	}
+
+	/* Inserts a word into the Trie */
+	TrieNode *buildTrie(std::vector<std::string>& words)
+	{
+		TrieNode *root = new TrieNode();
+
+		for (int j = 0; j < words.size(); j++)
+		{
+			std::string word = words[j];
+			TrieNode *curr = root;
+
+			for (int i = 0; i < word.length(); i++) 
+			{
+				char c = word[i] - 'a';
+
+				if (curr->children[c] == nullptr)
+					curr->children[c] = new TrieNode();
+
+				curr = curr->children[c];
+			}
+
+			curr->word = word;
+		}
+
+		return root;
+	}
+
+	void dfs(std::vector<std::vector<char>>& board, int i, int j, TrieNode *p, std::vector<std::string>& result)
+	{
+		char c = board[i][j];
+
+		if (c == '#' || !p->children[c - 'a'])
+			return;
+
+		p = p->children[c - 'a'];
+
+		if (p->word.size() > 0)
+		{
+			result.push_back(p->word);
+			p->word = "";
+		}
+
+		int m = board.size();
+		int n = board[0].size();
+
+		board[i][j] = '#';
+		if (i > 0)     dfs(board, i - 1, j    , p, result);
+		if (j > 0)     dfs(board, i    , j - 1, p, result);
+		if (i < m - 1) dfs(board, i + 1, j    , p, result);
+		if (j < n - 1) dfs(board, i    , j + 1, p, result);
+
+		board[i][j] = c;
+	}
+};
