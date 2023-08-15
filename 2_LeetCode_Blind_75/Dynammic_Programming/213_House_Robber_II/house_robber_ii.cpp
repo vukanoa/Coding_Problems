371a524fc78be8256db41a1e457e4a0da86cb405
Noa Vukasovic <noavukasovic@gmail.com>
2023-08-15 11:00:28 +0200

    Add Explanation(IDEA) to LeetCode: 213_House_Robber_II to Blind_75

--- 2_LeetCode_Blind_75/Dynammic_Programming/213_House_Robber_II/house_robber_ii.cpp
+++ 2_LeetCode_Blind_75/Dynammic_Programming/213_House_Robber_II/house_robber_ii.cpp
@@ -63,41 +63,58 @@
 	--- IDEA ---
 	------------
 
-	TODO
+	Since the very last house is the neighbor of the 0th house, we can't just
+	do the same as in the first version of this problem.
+
+	But it's not much different either.
+
+	We have to do two "house robber"(first version) problems:
+	        1. From 0 to n-2
+			2. From 1 to n-1
 	
+	And then just return max out of those two.
+
+	We also didn't use a vector dp, but only two variables, to make this more
+	efficient.
+	So, since we haven't used it, it's maybe unclear.
+	
+	Ultimately we return max(dp[n - 2], dp[n - 1])
+
+	That's it.
+
 */
 
 /* Time  Beats: 100% */
-/* Space Beats: 57.02% */
+/* Space Beats: 77.95% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
 class Solution {
 public:
-	int rob(vector<int>& nums)
+	int rob(std::vector<int>& nums)
 	{
 		int n = nums.size(); 
 
 		if (n < 2)
 			return n ? nums[0] : 0;
 
-		return max(robber(nums, 0, n - 2), robber(nums, 1, n - 1));
+		return std::max(house_robber(nums, 0, n - 2), house_robber(nums, 1, n - 1));
 	}
 
 private:
-	int robber(vector<int>& nums, int start, int end)
+	int house_robber(std::vector<int>& nums, int start, int end)
 	{
-	    int pre  = 0;
+		int prev = 0;
 		int curr = 0;
 
-	    for (int i = start; i <= end; i++)
+		for (int i = start; i <= end; i++)
 		{
-	        int tmp = max(pre + nums[i], curr);
+			int tmp = std::max(prev + nums[i], curr);
 
-	        pre  = curr;
-	        curr = tmp;
-	    }
+			prev = curr;
+			curr = tmp;
+		}
 
-	    return curr;
+		return curr;
 	}
 };
