7a674a832c12053cd807823cd9e252bc2172448f
Noa Vukasovic <noavukasovic@gmail.com>
2023-08-19 10:34:22 +0200

    Add Improved Solutions to LeetCode: 1249_Minimum_Remove_To_Make_Valid_Parentheses to Orderless_Problems

--- Orderless_Problems/1249_Minimum_Remove_To_Make_Valid_Parentheses/minimum_remove_to_make_valid_parentheses.cpp
+++ Orderless_Problems/1249_Minimum_Remove_To_Make_Valid_Parentheses/minimum_remove_to_make_valid_parentheses.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 /*
 	==============
@@ -55,6 +57,13 @@
 	s[i] is either'(' , ')', or lowercase English letter.
 
 */
+
+/*
+	There are 3 Solutions in here. The 3rd one(down most one) is the best.
+*/
+
+
+
 
 /* Time  Beats: 35.03% */
 /* Space Beats: 23.66% */
@@ -120,4 +129,94 @@ public:
 
 		return final_str;
 	}
+};
+
+
+
+
+/* Time  Beats: 92.39% */
+/* Space Beats: 75.75% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution {
+public:
+    std::string minRemoveToMakeValid(std::string s)
+	{
+        int open = 0;
+		int position = 0;
+
+		std::string str(s.size(), '#');
+
+        for (char character : s)
+		{
+			if (character == '(')
+				open++;
+			else if (character == ')')
+			{
+				if (open > 0)
+					open--;
+				else
+					continue;
+			}
+
+			str[position++] = character;
+        }
+
+        // Properly sizing res
+        std::string final_str(position - open, '*');
+
+        for (int i = position - 1, j = position - open - 1; i >= 0; i--)
+		{
+			if (str[i] != '(' || open-- <= 0)
+				final_str[j--] = str[i];
+        }
+
+        return final_str;
+    }
+};
+
+
+
+
+/* Time  Beats: 97.20% */
+/* Space Beats: 95.32% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution {
+public:
+    std::string minRemoveToMakeValid(std::string s)
+	{
+        int open = 0;
+		int position = 0;
+
+		std::string str(s.size(), '#');
+
+        for (char character : s)
+		{
+			if (character == '(')
+				open++;
+			else if (character == ')')
+			{
+				if (open > 0)
+					open--;
+				else
+					continue;
+			}
+
+			str[position++] = character;
+        }
+
+        // Properly sizing res
+		s.resize(position - open);
+
+        for (int i = position - 1, j = position - open - 1; i >= 0; i--)
+		{
+			if (str[i] != '(' || open-- <= 0)
+				s[j--] = str[i];
+        }
+
+        return s;
+    }
 };
