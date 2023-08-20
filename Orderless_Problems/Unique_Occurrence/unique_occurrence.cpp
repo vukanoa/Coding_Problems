0069edafc82c885d50b34450b3a1d2685583945d
Noa Vukasovic <noavukasovic@gmail.com>
2023-08-20 16:53:15 +0200

    Add Unique_Occurrence to Orderless_Problems

--- Orderless_Problems/Unique_Occurrence/unique_occurrence.cpp
+++ Orderless_Problems/Unique_Occurrence/unique_occurrence.cpp
@@ -3,82 +3,57 @@
 #include <deque>
 #include <unordered_map>
 
-int solution(std::vector<int>& A)
-{
-	int n = A.size();
-	std::unordered_map<int, int> umap;
-
-	// O(n)
-	// Count the occurrences of each number
-	for (int i = 0; i < n; i++)
-		umap[A[i]]++;
-
-	std::vector<int> occur(n + 1, 0);
-
-	// O(n)
-	// Vector of occurrences
-	for (const auto& entry : umap)
-		occur[entry.second]++;
-
-
-	std::deque<int> deque;
-	bool flag = false;
-	// O(n)
-	// Push all occurences that don't happen in this array. But push only those
-	// that are less than the largest occurrence in the array since we are
-	// allower to delete only
-	for (int i = n; i >= 0; i--)
+class Solution{
+public:
+	int solution(std::vector<int>& A)
 	{
-		if (flag == false && occur[i] != 0)
-			flag = true;
+		std::unordered_map<int, int> umap_num;
+		std::unordered_map<int, int> umap_freq;
 
-		if (flag == true && occur[i] == 0)
-			deque.push_back(i);
-	}
+		for(const int& num : A)
+			umap_num[num]++;
 
-	std::unordered_map<int, int> map_occur;
+		for (const auto& entry : umap_num)
+			umap_freq[entry.second]++;
 
-	// O(n)
-	// Make a Hash Map of occurrences as a key
-	for (const auto& entry : umap)
-	{
-		if (map_occur.find(entry.second) == map_occur.end())
-			map_occur.insert({entry.second, 1});
-		else
-			map_occur[entry.second]++;
-	}
+		umap_freq[0] = 0;
+		int deletions = 0;
 
-	int deletions = 0;
-	// O(n)
-	for (auto& entry : map_occur)
-	{
-		while (entry.second > 1)
+		for (const auto& entry : umap_num)
 		{
-			if (deque.empty())
-				deletions += entry.first;
-			else
-			{
-				deletions += entry.first - deque.front();
-				deque.pop_front();
-			}
+			int freq = entry.second;
 
-			entry.second--;
-		}
+			if(umap_freq[freq] > 1)
+			{                        
+				for(int j = freq-1; j >= 0; j--)
+				{
+					if(j == 0)
+						deletions += freq;
+					else if(umap_freq.count(j) == 0)
+					{
+						umap_freq[j] = 1;
+						deletions += freq - j;
+
+						break;
+					} 
+				}
+
+				umap_freq[freq]--;
+			}   
+		} 
+
+		return deletions;
 	}
-
-	return deletions;
-}
+};
 
 
 int
 main()
 {
-	std::cout << "\n\t=========================";
-	std::cout << "\n\t=== UNIQUE OCCURRENCE ===";
-	std::cout << "\n\t=========================\n";
+	Solution sol;
 
 	/* Example 1 */
-	// std::vector<int> A = {1, 1, 1, 2, 2, 2};
+	std::vector<int> A = {1, 1, 1, 2, 2, 2};
 
 	/* Example 2 */
 	// std::vector<int> A = {5, 3, 3, 2, 5, 2, 3, 2};
@@ -87,7 +62,12 @@ main()
 	// std::vector<int> A = {127, 15, 3, 8, 10};
 
 	/* Example 4 */
-	std::vector<int> A = {1000000, 1000000, 5, 5, 5, 2, 2, 2, 0, 0};
+	// std::vector<int> A = {1000000, 1000000, 5, 5, 5, 2, 2, 2, 0, 0};
+
+	std::cout << "\n\t=========================";
+	std::cout << "\n\t=== UNIQUE OCCURRENCE ===";
+	std::cout << "\n\t=========================\n";
+
 
 	/* Write Input */
 	std::cout << "\n\tInput:";
@@ -105,13 +85,12 @@ main()
 
 
 	/* Solution */
-	int sol = solution(A);
+	int deletions = sol.solution(A);
 
 
 	/* Write Output */
 	std::cout << "\n\tSolution:";
-	std::cout << "\n\t\tResult: " << solution(A);
-	std::cout << "\n\n";
+	std::cout << "\n\t\tResult: " << deletions << "\n\n";
 
 	return 0;
 }
