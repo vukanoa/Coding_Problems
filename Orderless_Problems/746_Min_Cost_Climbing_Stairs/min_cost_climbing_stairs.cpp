093346a4ab12879f438d9822eb27743007165ec3
Noa Vukasovic <noavukasovic@gmail.com>
2023-09-20 16:49:38 +0200

    Add LeetCode: 746_Min_Cost_Climbing_Stairs to Orderless_Problems

---
+++ Orderless_Problems/746_Min_Cost_Climbing_Stairs/min_cost_climbing_stairs.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+
+/*
+	============
+	=== EASY ===
+	============
+
+	=============================
+	746) Min Cost Climbing Stairs
+	=============================
+
+	============
+	Description:
+	============
+
+	You are given an integer array cost where cost[i] is the cost of ith step
+	on a staircase. Once you pay the cost, you can either climb one or two
+	steps.
+
+	You can either start from the step with index 0, or the step with index 1.
+
+	Return the minimum cost to reach the top of the floor.
+
+	=======================================================
+	FUNCTION: int minCostClimbingStairs(vector<int>& cost);
+	=======================================================
+
+	==========================================================================
+	================================ EXAMPLES ================================
+	==========================================================================
+
+	--- Example 1 ---
+	Input: cost = [10,15,20]
+	Output: 15
+	Explanation: You will start at index 1.
+	- Pay 15 and climb two steps to reach the top.
+	The total cost is 15.
+
+	--- Example 2 ---
+	Input: cost = [1,100,1,1,1,100,1,1,100,1]
+	Output: 6
+	Explanation: You will start at index 0.
+	- Pay 1 and climb two steps to reach index 2.
+	- Pay 1 and climb two steps to reach index 4.
+	- Pay 1 and climb two steps to reach index 6.
+	- Pay 1 and climb one step to reach index 7.
+	- Pay 1 and climb two steps to reach index 9.
+	- Pay 1 and climb one step to reach the top.
+	The total cost is 6.
+
+
+	*** Constraints ***
+	2 <= cost.length <= 1000
+	0 <= cost[i] <= 999
+
+*/
+
+/*
+	------------
+	--- IDEA ---
+	------------
+
+	Self-explanatory.
+
+*/
+
+/* Time  Beats: 100% */
+/* Space Beats: 42.98% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+	int minCostClimbingStairs(vector<int>& cost)
+	{
+		if (cost.size() == 2)
+			return std::min(cost[0], cost[1]);
+
+		int dp_2 = cost[0];
+		int dp_1 = cost[1];
+
+		for (int i = 2; i < cost.size(); i++)
+		{
+			int curr = cost[i] + std::min(dp_2, dp_1);
+			
+			dp_2 = dp_1;
+			dp_1 = curr;
+		}
+
+		return std::min(dp_2, dp_1);
+	}
+};
