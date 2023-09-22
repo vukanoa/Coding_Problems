b5126c63b1c974f85a17fda1b1e9081139cb78b2
Noa Vukasovic <noavukasovic@gmail.com>
2023-09-22 13:00:07 +0200

    Add Explanation(IDEA) to LeetCode: 875_Koko_Eating_Bananas in N_150

--- 3_N_150/Binary_Search/875_Koko_Eating_Bananas/koko_eating_bananas.cpp
+++ 3_N_150/Binary_Search/875_Koko_Eating_Bananas/koko_eating_bananas.cpp
@@ -61,15 +61,118 @@
 	--- IDEA ---
 	------------
 
-	TODO
+	Think about what would be a Brute Force for this Problem and then try to
+	optimize it.
 
-	Hint: Think of a Brute Force Solution and then use Binary Search to
-	optimize the "outer step".
+	It would be to start from k=1 and try to eat all the bananas in given 'h'.
+	The first 'k' that successfully does this is the Solution.
 
+	Consider the very first example:
+	Input:  piles = [3, 6, 7, 11], h = 8
+	Output: 4
+
+	We can start from k=1. How many bananas can we eat in h=8?
+	Before:       piles = [3, 6, 7, 11]
+
+	Hour 1: 1     piles = [2, 6, 7, 11]
+	Hour 2: 1     piles = [1, 6, 7, 11]
+	Hour 3: 1     piles = [0, 6, 7, 11]
+	Hour 4: 1     piles = [0, 5, 7, 11]
+	Hour 5: 1     piles = [0, 4, 7, 11]
+	Hour 6: 1     piles = [0, 3, 7, 11]
+	Hour 7: 1     piles = [0, 2, 7, 11]
+	Hour 8: 1     piles = [0, 1, 7, 11]
+
+	We can see that we didn't eat all of them, so we have to increase our k and
+	repeat this until we find our 'k' that would result in all eaten bananas.
+
+	However, there is a simpler way to check if we can eat all the bananas in
+	a given 'h'.
+	We can try to divide each pile with current 'k' and round up.
+	Add all those rounded up values and you'll get the amount of hours you need
+	for that k value, to eat all the bananas.
+
+	Let's see it in action:
+
+	### OLD WAY ###
+	Start from k=2. How many bananas can we eat in h=8?
+	Before:       piles = [3, 6, 7, 11]
+
+	Hour 1: 2     piles = [1, 6, 7, 11]
+	Hour 2: 2     piles = [0, 6, 7, 11]
+	Hour 3: 2     piles = [0, 4, 7, 11]
+	Hour 4: 2     piles = [0, 2, 7, 11]
+	Hour 5: 2     piles = [0, 0, 5, 11]
+	Hour 6: 2     piles = [0, 0, 3, 11]
+	Hour 7: 2     piles = [0, 0, 1, 11]
+	Hour 8: 2     piles = [0, 0, 0, 11]
+	
+
+	### NEW WAY ###
+	Start from k=2. How many bananas can we eat in h=8?
+	piles = [3,6,7,11]
+
+	In how many hours can we eat all the bananas if k=2?
+	piles = [3, 6, 7, 11]
+	         |  |  |  |
+	----------  |  |  ---------
+	|       ____|  |____      |
+	|       |          |      v
+	|       |          |    ceil(11/2) = 6 hours to eat the fourth(3rd) pile.
+	|       |          v
+	|       |      ceil(7/2) = 4 hours to eat the third(2nd) pile.
+	|       v
+	|    ceil(6/2) = 3 hours to eat the second(1st) pile.
+	V
+	ceil(3/2) = 2 hours to eat the first(0-th) pile.
+
+	So: 2 + 3 + 4 + 6 = 15 hours
+
+	Is 15 <= h=8?
+	No.
+
+	They told us an important thing in the Description and that is:
+	piles.length() <= h
+
+	Which means that it is guaranteed that piles.length() is smaller than or
+	equal than the 'h'.
+
+	Why is that important?
+
+	It's important because that helps us know what is the upper bound up to
+	which our 'k' can go to.
+
+	Obviously, we can start k from 1, but what is the upper bound?
+	The upper bound of value of k must be such that we can eat all the bananas
+	in exactly piles.length() steps.
+
+	Now ask yourself - What is that k value?
+	It's the maximum value in the vector "piles".
+
+	Okay, so now we know both the lower and the upper bound. Can we use that to
+	our advantage somehow?
+
+	Do we really need to go from 1 to max_element(piles) and try each k until
+	we find which k results in all bananas being eaten?
+
+	Obviously not.
+	Do you see it now?
+
+	Since we have a range [1, 11](in this case) and we know that the result of
+	k is in that inclusive range. How can we optimize this Solution?
+
+	Since we've agreed that we don't need to try every possible k, starting
+	from 1, we can try using a Binary Search.
+
+	Each time we fail to eat all the bananas, or each time the number of needed
+	hours to eat all the bananas is greater than 'h', we're going to cut all
+	the options of 'k' that are less than or equal to the current 'k' value.
+
+	'k' value is "mid" in a Binary Search.
 */
 
-/* Time  Beats: 57.03% */
-/* Space Beats: 16.71% */
+/* Time  Beats: 76.01% */
+/* Space Beats: 14.52% */
 
 /* Time  Complexity: O(log( max_element(piles) ) * N) */
 /* Space Complexity: O(1) */
@@ -84,19 +187,19 @@ public:
 
 		while (left <= right)
 		{
-			int mid = (left + right) / 2;
+			int k = (left + right) / 2;
 
 			unsigned hours = 0; // It cannot fit into "int"
 			for (int& pile : piles)
-				hours += std::ceil(1.0 * pile / mid);
+				hours += std::ceil(1.0 * pile / k);
 
 			if (hours <= h)
 			{
-				result = std::min(result, mid);
-				right = mid - 1;
+				result = std::min(result, k);
+				right = k - 1;
 			}
 			else
-				left = mid + 1;
+				left = k + 1;
 		}
 
 		return result;
