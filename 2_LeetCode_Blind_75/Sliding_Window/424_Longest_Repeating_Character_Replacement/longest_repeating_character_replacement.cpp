3fb3119319fb46f2f75519bf7a7a1e256968b61c
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-01 21:52:17 +0200

    Remove redundant Solution & Rename variables & Reformat LeetCode: 424_Longest_Repeating_Character_Replacement in Blind_75

--- 2_LeetCode_Blind_75/Sliding_Window/424_Longest_Repeating_Character_Replacement/longest_repeating_character_replacement.cpp
+++ 2_LeetCode_Blind_75/Sliding_Window/424_Longest_Repeating_Character_Replacement/longest_repeating_character_replacement.cpp
@@ -1,61 +1,61 @@
 #include <iostream>
 
 /*
-	============
-	=== HARD ===
-	============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	============================================
-	424) Longest Repeating Character Replacement
-	============================================
+    ============================================
+    424) Longest Repeating Character Replacement
+    ============================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	You are given a string s and an integer k. You can choose any character of
-	the string and change it to any other uppercase English character. You can
-	perform this operation at most k times.
+    You are given a string s and an integer k. You can choose any character of
+    the string and change it to any other uppercase English character. You can
+    perform this operation at most k times.
 
-	Return the length of the longest substring containing the same letter you
-	can get after performing the above operations.
+    Return the length of the longest substring containing the same letter you
+    can get after performing the above operations.
 
-	====================================================
-	FUNCTION: int characterReplacement(string s, int k); 
-	====================================================
+    ====================================================
+    FUNCTION: int characterReplacement(string s, int k);
+    ====================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: s = "ABAB", k = 2
-	Output: 4
-	Explanation: Replace the two 'A's with two 'B's or vice versa.
-
-
-	--- Example 2 ---
-	Input: s = "AABABBA", k = 1
-	Output: 4
-	Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
-	The substring "BBBB" has the longest repeating letters, which is 4.
-	There may exists other ways to achive this answer too.
+    --- Example 1 ---
+    Input: s = "ABAB", k = 2
+    Output: 4
+    Explanation: Replace the two 'A's with two 'B's or vice versa.
 
 
-	*** Constraints ***
-	1 <= s.length <= 10^5
-	s consists of only uppercase English letters.
-	0 <= k <= s.length
+    --- Example 2 ---
+    Input: s = "AABABBA", k = 1
+    Output: 4
+    Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+    The substring "BBBB" has the longest repeating letters, which is 4.
+    There may exists other ways to achive this answer too.
+
+
+    *** Constraints ***
+    1 <= s.length <= 10^5
+    s consists of only uppercase English letters.
+    0 <= k <= s.length
 
 */
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	TODO
-	
+    TODO
+
 */
 
 /* Time  Beats: 83.15% */
@@ -70,106 +70,55 @@
 */
 class Solution {
 public:
-	int characterReplacement(std::string s, int k)
-	{
-		int longest = 0;
-		std::vector<int> map_count(26, 0);
+    int characterReplacement(std::string s, int k)
+    {
+        std::vector<int> letters(26, 0);
 
-		int left = 0;
-		int right = 0;
+        int left = 0;
+        int right = 0;
 
-		while (right < s.length())
-		{
-			map_count[s[right] - 'A']++;
-			int max_f = 0;
+        int longest = 0;
+        while (right < s.length())
+        {
+            letters[s[right] - 'A']++;
 
-			// O(26) -> constant -> O(1)
-			for (int i = 0; i < 26; i++)
-				max_f = std::max(max_f, map_count[i]);
+            // O(26) -> constant -> O(1)
+            int max_freq = *std::max_element(letters.begin(), letters.end());
 
-			// current_window_length = right - left + 1
-			if ((right - left + 1) - max_f > k)
-			{
-				map_count[s[left] - 'A']--;
-				left++;
-			}
+            // current_window_length = right - left + 1
+            if ((right - left + 1) - max_freq > k)
+            {
+                letters[s[left] - 'A']--;
+                left++;
+            }
 
-			// current_window_length = right - left + 1
-			longest = std::max(longest, right - left + 1);
+            // current_window_length = right - left + 1
+            longest = std::max(longest, right - left + 1);
 
-			right++;
-		}
+            right++;
+        }
 
-		return longest;
-	}
+        return longest;
+    }
 };
 
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Completely equivallent Solution, just written more in a spirit of C++.
+    We can notice that if we don't decrease max_freq, once its reached some
+    value, the Solution will still work since we're Overestimating.
 
-*/
+    If and only if we find a max_freq that is greater than the previous value,
+    only then can we increase the size of the longest substring, thus we don't
+    have to keep finding the max_freq for each current window.
 
-/* Time  Beats: 83.15% */
-/* Space Beats: 70.87% */
+    // TODO: Explain this better
 
-/*
-    Time  Complexity: O(n)
-    It's O(26 * n) --> O(1 * n) --> O(n)
-*/
-/*
-    Space Complexity: O(1)
-*/
-class Solution_CPP {
-public:
-	int characterReplacement(std::string s, int k)
-	{
-		int longest = 0;
-		std::vector<int> map_count(26, 0);
-
-		int left = 0;
-		int right = 0;
-
-		while (right < s.length())
-		{
-			map_count[s[right] - 'A']++;
-
-			// O(26) --> constant --> O(1)
-			int max_f = *std::max_element(std::begin(map_count), std::end(map_count)); 
-
-			// current_window_length = right - left + 1
-			if ((right - left + 1) - max_f > k)
-			{
-				map_count[s[left] - 'A']--;
-				left++;
-			}
-
-			// current_window_length = right - left + 1
-			longest = std::max(longest, right - left + 1);
-
-			right++;
-		}
-
-		return longest;
-	}
-};
-
-
-
-
-/*
-	------------
-	--- IDEA ---
-	------------
-
-	TODO
-	
 */
 
 /* Time  Beats: 98.55% */
@@ -179,33 +128,35 @@ public:
 /* Space Complexity: O(1) */
 class Solution_Flat_O_of_n {
 public:
-	int characterReplacement(std::string s, int k)
-	{
-		int longest = 0;
-		std::vector<int> map_count(26, 0);
+    int characterReplacement(std::string s, int k)
+    {
+        std::vector<int> letters(26, 0);
 
-		int left  = 0;
-		int right = 0;
-		int max_f = 0;
+        int left  = 0;
+        int right = 0;
+        int max_freq = 0;
 
-		while (right < s.length())
-		{
-			map_count[s[right] - 'A']++;
-			max_f = std::max(max_f, map_count[s[right] - 'A']);
+        int longest = 0;
+        while (right < s.length())
+        {
+            letters[s[right] - 'A']++;
 
-			// current_window_length = right - left + 1
-			if ((right - left + 1) - max_f > k)
-			{
-				map_count[s[left] - 'A']--;
-				left++;
-			}
+            // This is the optimization.
+            max_freq = std::max(max_freq, letters[s[right] - 'A']);
 
-			// current_window_length = right - left + 1
-			longest = std::max(longest, right - left + 1);
+            // current_window_length = right - left + 1
+            if ((right - left + 1) - max_freq > k)
+            {
+                letters[s[left] - 'A']--;
+                left++;
+            }
 
-			right++;
-		}
+            // current_window_length = right - left + 1
+            longest = std::max(longest, right - left + 1);
 
-		return longest;
-	}
+            right++;
+        }
+
+        return longest;
+    }
 };
