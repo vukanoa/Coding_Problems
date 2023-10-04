e78f9420d6cf7a5fe500e42397447397ea94a689
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-04 08:50:43 +0200

    Reformat in LeetCode: 013_15___Three_Sum in Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/013_15___Three_Sum/three_sum.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/013_15___Three_Sum/three_sum.cpp
@@ -3,125 +3,125 @@
 #include <algorithm>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	===========================
-	15) 3Sum
-	===========================
+    ===========================
+    15) 3Sum
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given an integer array nums, return all the triplets:
-	[nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and
-	num[i] + nums[j] + nums[k] == 0.
+    Given an integer array nums, return all the triplets:
+    [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and
+    num[i] + nums[j] + nums[k] == 0.
 
-	=====
-	Node: Notice that the solution set must not contain duplicate triplets.
-	=====
+    =====
+    Node: Notice that the solution set must not contain duplicate triplets.
+    =====
 
-	==========================================================
-	FUNCTION: vector<vector<int>> threeSum(vector<int>& nums);
-	==========================================================
+    ==========================================================
+    FUNCTION: vector<vector<int>> threeSum(vector<int>& nums);
+    ==========================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  nums = [-1, 0, 1, 2, -1, -4]
-	Output: [[-1, -1, 2], [-1, 0, 1]]
-	Explanation:
-	nums[0] + nums[1] + nums[2] = (-1) + 0 +   1  = 0
-	nums[0] + nums[2] + nums[4] =   0  + 1 + (-1) = 0
-	nums[i] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0
+    --- Example 1 ---
+    Input:  nums = [-1, 0, 1, 2, -1, -4]
+    Output: [[-1, -1, 2], [-1, 0, 1]]
+    Explanation:
+    nums[0] + nums[1] + nums[2] = (-1) + 0 +   1  = 0
+    nums[0] + nums[2] + nums[4] =   0  + 1 + (-1) = 0
+    nums[i] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0
 
-	The distinct triples are [-1, 0, 1] and [-1, -1, 2].
-	Notice that the order of the output and the order of the triplets
-	does not matter.
+    The distinct triples are [-1, 0, 1] and [-1, -1, 2].
+    Notice that the order of the output and the order of the triplets
+    does not matter.
 
-	--- Example 2 ---
-	Input:  nums = [0, 1, 1]
-	Output: []
-	Explanation: The only possible triplet does not sum up to 0.
+    --- Example 2 ---
+    Input:  nums = [0, 1, 1]
+    Output: []
+    Explanation: The only possible triplet does not sum up to 0.
 
-	--- Example 3 ---
-	Input: nums = [0, 0, 0]
-	Output [[0, 0, 0]]
-	Explanation: The only possible triplet sums up to 0
+    --- Example 3 ---
+    Input: nums = [0, 0, 0]
+    Output [[0, 0, 0]]
+    Explanation: The only possible triplet sums up to 0
 
-	*** Constraints ***
-	3 <= nums.length <= 3000
-	-10^5 <= nums[i] <= 10^5
+    *** Constraints ***
+    3 <= nums.length <= 3000
+    -10^5 <= nums[i] <= 10^5
 */
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Two Sum II should be done prior to this Problem. After it, the solution
-	is straightforward.
+    Two Sum II should be done prior to this Problem. After it, the solution
+    is straightforward.
 
-	Note: "Two Sum II" is NOT the same as "Two sum".
+    Note: "Two Sum II" is NOT the same as "Two sum".
 
-	Make sure you do:
-		Two Sum II
+    Make sure you do:
+        Two Sum II
 
-	First we should sort the vector "nums". Why? So that we don't have
-	duplicate triplets.
+    First we should sort the vector "nums". Why? So that we don't have
+    duplicate triplets.
 
-	Consider this example:
-		-3 3 4 -3 1 2
+    Consider this example:
+        -3 3 4 -3 1 2
 
-	First Triplet: -3, 1, 2
-	That's when the first element starts with -3 at position 0.
+    First Triplet: -3, 1, 2
+    That's when the first element starts with -3 at position 0.
 
-	But a bit later, our first element is again going to start at value -3, but
-	this time at position 3 and since the other two elements that sum up to 0
-	are AFTER either of our two -3's, then that means we will have a duplicate
-	triplet which is something we are warned about. It's something that we must
-	avoid.
+    But a bit later, our first element is again going to start at value -3, but
+    this time at position 3 and since the other two elements that sum up to 0
+    are AFTER either of our two -3's, then that means we will have a duplicate
+    triplet which is something we are warned about. It's something that we must
+    avoid.
 
-	So how do we fulfill this requirement?
-	We sort the array and in each outer loop we only ask if 'i' is greater than
-	0 and if 'i' is equal to 'i - 1'.
+    So how do we fulfill this requirement?
+    We sort the array and in each outer loop we only ask if 'i' is greater than
+    0 and if 'i' is equal to 'i - 1'.
 
-	We ask if 'i' is greater than 0 because if we weren't then for 0th element
-	we would've asked if 0th element is equal to an element before the array
-	which is some random value(out of bounds). So to avoid this we check
-	if 'i' is greater than 0.
+    We ask if 'i' is greater than 0 because if we weren't then for 0th element
+    we would've asked if 0th element is equal to an element before the array
+    which is some random value(out of bounds). So to avoid this we check
+    if 'i' is greater than 0.
 
-	And we ask if nums[i] == nums[i - 1] because only the very first element
-	in a "chain of duplicates" in our, now sorted, array should be considered.
+    And we ask if nums[i] == nums[i - 1] because only the very first element
+    in a "chain of duplicates" in our, now sorted, array should be considered.
 
-	So the previous example:
-		-3 3 4 -3 1 2
-	Now becomes
-		-3 -3 1 2 3 4
+    So the previous example:
+        -3 3 4 -3 1 2
+    Now becomes
+        -3 -3 1 2 3 4
 
-	Thus the 2nd -3 won't even be considered. We are just going to skip it.
+    Thus the 2nd -3 won't even be considered. We are just going to skip it.
 
-	And now this problem is divided to two:
-		1. This outer for loop
-		2. Two Sum II
+    And now this problem is divided to two:
+        1. This outer for loop
+        2. Two Sum II
 
-	Two Sum II is solved using Two Pointer Technique. In the above (sorted)
-	example we pick -3 for our first value, and we put two pointers to the end
-	of the array and to the first non-duplicate element to the right.
+    Two Sum II is solved using Two Pointer Technique. In the above (sorted)
+    example we pick -3 for our first value, and we put two pointers to the end
+    of the array and to the first non-duplicate element to the right.
 
-	Now sum all three elements and see if it is greater than 0, lower than 0 or
-	equal to 0.
+    Now sum all three elements and see if it is greater than 0, lower than 0 or
+    equal to 0.
 
-	If it is greater than 0, then decrement the "right" pointer by one.
-	If it is lower than 0, then increment the "left" pointer by one.
-	If it is equal to 0, put all three elements in vector and push that vector
-	to vector of vectors "triplets"(which we are returning).
+    If it is greater than 0, then decrement the "right" pointer by one.
+    If it is lower than 0, then increment the "left" pointer by one.
+    If it is equal to 0, put all three elements in vector and push that vector
+    to vector of vectors "triplets"(which we are returning).
 
 */
 
@@ -136,45 +136,45 @@
 /* Space Complexity: O(1) or O(n), depends on the sort. Heapsort uses O(1) */
 class Solution{
 public:
-	std::vector<std::vector<int>> threeSum(std::vector<int>& nums)
-	{
-		int n = nums.size();
-		std::vector<std::vector<int>> triplets;
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums)
+    {
+        int n = nums.size();
+        std::vector<std::vector<int>> triplets;
 
-		// O(n * logn)
-		std::sort(nums.begin(), nums.end());
+        // O(n * logn)
+        std::sort(nums.begin(), nums.end());
 
-		// O(n^2)
-		for (int i = 0; i < n - 2; i++)
-		{
-			if (i > 0 && nums[i] == nums[i - 1])
-				continue;
+        // O(n^2)
+        for (int i = 0; i < n - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-			// Two Sum II
-			int left  = i + 1;
-			int right = n - 1;
-			while (left < right)
-			{
-				int sum = nums[i] + nums[left] + nums[right];
+            // Two Sum II
+            int left  = i + 1;
+            int right = n - 1;
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
 
-				if (sum > 0)
-					right--;
-				else if (sum < 0)
-					left++;
-				else
-				{
-					std::vector<int> vec = {nums[i], nums[left], nums[right]};
-					triplets.push_back(vec);
+                if (sum > 0)
+                    right--;
+                else if (sum < 0)
+                    left++;
+                else
+                {
+                    std::vector<int> vec = {nums[i], nums[left], nums[right]};
+                    triplets.push_back(vec);
 
-					left++;
-					while (nums[left] == nums[left - 1] && left < right)
-						left++;
-				}
-			}
-		}
+                    left++;
+                    while (nums[left] == nums[left - 1] && left < right)
+                        left++;
+                }
+            }
+        }
 
-		return triplets;
-	}
+        return triplets;
+    }
 
 };
 
@@ -182,59 +182,59 @@ public:
 int
 main()
 {
-	Solution sol;
+    Solution sol;
 
-	/* Example 1 */
-	// std::vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    /* Example 1 */
+    // std::vector<int> nums = {-1, 0, 1, 2, -1, -4};
 
-	/* Example 2 */
-	// std::vector<int> nums = {0, 1, 1};
+    /* Example 2 */
+    // std::vector<int> nums = {0, 1, 1};
 
-	/* Example 3 */
-	// std::vector<int> nums = {0, 0, 0};
+    /* Example 3 */
+    // std::vector<int> nums = {0, 0, 0};
 
-	/* Example 4 */
-	// std::vector<int> nums = {};
+    /* Example 4 */
+    // std::vector<int> nums = {};
 
-	/* Example 5 */
-	std::vector<int> nums = {5, 2, 1, 0, -7, -3, 7, 8};
+    /* Example 5 */
+    std::vector<int> nums = {5, 2, 1, 0, -7, -3, 7, 8};
 
-	std::cout << "\n\t=================";
-	std::cout << "\n\t=== THREE SUM ===";
-	std::cout << "\n\t=================\n";
+    std::cout << "\n\t=================";
+    std::cout << "\n\t=== THREE SUM ===";
+    std::cout << "\n\t=================\n";
 
 
-	/* Write Input */
-	bool first = true;
-	std::cout << "\n\t=== Array ===\n\t[";
-	for (auto x: nums)
-	{
-		if (!first)
-			std::cout << ", ";
+    /* Write Input */
+    bool first = true;
+    std::cout << "\n\t=== Array ===\n\t[";
+    for (auto x: nums)
+    {
+        if (!first)
+            std::cout << ", ";
 
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n\n";
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n\n";
 
-	/* Solution */
-	std::vector<std::vector<int>> triplets = sol.threeSum(nums);
+    /* Solution */
+    std::vector<std::vector<int>> triplets = sol.threeSum(nums);
 
-	/* Write Output */
-	std::cout << "\n\t=== Triplets ===";
-	if (triplets.empty())
-		std::cout << "\n\t[]\n";
+    /* Write Output */
+    std::cout << "\n\t=== Triplets ===";
+    if (triplets.empty())
+        std::cout << "\n\t[]\n";
 
-	int cnt = 1;
-	for(auto triplet : triplets)
-	{
-		std::cout << "\n\t" << cnt++ << ") ";
+    int cnt = 1;
+    for(auto triplet : triplets)
+    {
+        std::cout << "\n\t" << cnt++ << ") ";
 
-		for (auto x : triplet)
-			std::cout << x << " ";
-		std::cout << "\n";
-	}
-	std::cout << "\n";
+        for (auto x : triplet)
+            std::cout << x << " ";
+        std::cout << "\n";
+    }
+    std::cout << "\n";
 
-	return 0;
+    return 0;
 }
