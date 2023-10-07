583b79c99c6ccaf1e2689e66b6c13dfa78dadece
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-07 11:19:49 +0200

    Rename & Restructure & Reformat LeetCode: 684_Redundant_Connection in Orderless_Problems

--- Orderless_Problems/684_Redundant_Connection/redundant_connection.cpp
+++ Orderless_Problems/684_Redundant_Connection/redundant_connection.cpp
@@ -2,82 +2,82 @@
 #include <vector>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	===========================
-	684) Redundant Connection
-	===========================
+    ===========================
+    684) Redundant Connection
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	In this problem, a tree is an undirected graph that is connected and has no
-	cycles.
+    In this problem, a tree is an undirected graph that is connected and has no
+    cycles.
 
-	You are given a graph that started as a tree with n nodes labeled from 1 to
-	n, with one additional edge added. The added edge has two different
-	vertices chosen from 1 to n, and was not an edge that already existed. The
-	graph is represented as an array edges of length n where edges[i] = [ai,
-	bi] indicates that there is an edge between nodes ai and bi in the graph.
+    You are given a graph that started as a tree with n nodes labeled from 1 to
+    n, with one additional edge added. The added edge has two different
+    vertices chosen from 1 to n, and was not an edge that already existed. The
+    graph is represented as an array edges of length n where edges[i] = [ai,
+    bi] indicates that there is an edge between nodes ai and bi in the graph.
 
-	Return an edge that can be removed so that the resulting graph is a tree of
-	n nodes. If there are multiple answers, return the answer that occurs last
-	in the input.
+    Return an edge that can be removed so that the resulting graph is a tree of
+    n nodes. If there are multiple answers, return the answer that occurs last
+    in the input.
 
-	==========================================================================
-	FUNCTION: vector<int> findRedundantConnection(vector<vector<int>>& edges);
-	==========================================================================
+    ==========================================================================
+    FUNCTION: vector<int> findRedundantConnection(vector<vector<int>>& edges);
+    ==========================================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
+    --- Example 1 ---
 
-	        1 --- 2
-	        |   /
-	        |  /
-	        | /
-	        |/
-	        3
+            1 --- 2
+            |   /
+            |  /
+            | /
+            |/
+            3
 
-	Input: edges = [[1,2],[1,3],[2,3]]
-	Output: [2,3]
+    Input: edges = [[1,2],[1,3],[2,3]]
+    Output: [2,3]
 
-	--- Example 2 ---
+    --- Example 2 ---
 
-	        2 --- 1 --- 5
-	        |     |
-	        |     |
-	        |     |
-	        3 --- 4
+            2 --- 1 --- 5
+            |     |
+            |     |
+            |     |
+            3 --- 4
 
-	Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
-	Output: [1,4]
+    Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+    Output: [1,4]
 
 
-	*** Constraints ***
-	n == edges.length
-	3 <= n <= 1000
-	edges[i].length == 2
-	1 <= ai < bi <= edges.length
-	ai != bi
-	There are no repeated edges.
-	The given graph is connected.
+    *** Constraints ***
+    n == edges.length
+    3 <= n <= 1000
+    edges[i].length == 2
+    1 <= ai < bi <= edges.length
+    ai != bi
+    There are no repeated edges.
+    The given graph is connected.
 
 */
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	TODO
+    TODO
 
-	Union & Find
+    Union & Find
 
 */
 
@@ -88,59 +88,58 @@
 /* Space Complexity: O(n) */
 class Solution{
 public:
-	std::vector<int> findRedundantConnection(std::vector<std::vector<int>>& edges)
-	{
-		int n = edges.size();
+    std::vector<int> findRedundantConnection(std::vector<std::vector<int>>& edges)
+    {
+        int n = edges.size();
 
-		std::vector<int> parents(n + 1); // We don't need 0
-		std::iota(parents.begin(), parents.end(), 0);
+        std::vector<int> parents(n + 1); // We don't need 0
+        std::iota(parents.begin(), parents.end(), 0);
 
-		std::vector<int> ranks(n + 1, 1);
-		std::vector<int> result;
-		for (auto& edge : edges)
-		{
-			result = Union(edge[0], edge[1], parents, ranks);
+        std::vector<int> ranks(n + 1, 1);
+        std::vector<int> result;
 
-			if (!result.empty())
-				return result;
-		}
+        for (auto& edge : edges)
+        {
+            result = Union(edge, parents, ranks);
 
-		return result;
-	}
+            if (!result.empty())
+                return result;
+        }
+
+        return result;
+    }
 
 private:
-	int Find(int node, std::vector<int>& parents)
-	{
-		int tmp = node;
+    int Find(int node, std::vector<int>& parents)
+    {
+        while (node != parents[node])
+        {
+            parents[node] = parents[parents[node]];
+            node = parents[node];
+        }
 
-		while (tmp != parents[tmp])
-		{
-			parents[tmp] = parents[parents[tmp]];
-			tmp = parents[tmp];
-		}
+        return node;
+    }
 
-		return tmp;
-	}
+    std::vector<int> Union(std::vector<int>& edge, std::vector<int>& parents, std::vector<int>& ranks)
+    {
+        int p1 = Find(edge[0], parents);
+        int p2 = Find(edge[1], parents);
 
-	std::vector<int> Union(int node_1, int node_2, std::vector<int>& parents, std::vector<int>& ranks)
-	{
-		int parent_1 = Find(node_1, parents);
-		int parent_2 = Find(node_2, parents);
+        if (p1 == p2)
+            return {edge[0], edge[1]};
 
-		if (parent_1 == parent_2)
-			return {node_1, node_2};
+        if (ranks[p1] >= ranks[p2])
+        {
+            parents[p2] = p1;
+            ranks[p1]++;
+        }
+        else
+        {
+            parents[p1] = p2;
+            ranks[p2]++;
+        }
 
-		if (ranks[parent_1] >= ranks[parent_2])
-		{
-			parents[parent_2] = parent_1;
-			ranks[parent_1]++;
-		}
-		else
-		{
-			parents[parent_1] = parent_2;
-			ranks[parent_2]++;
-		}
-
-		return {};
-	}
+        return {};
+    }
 };
