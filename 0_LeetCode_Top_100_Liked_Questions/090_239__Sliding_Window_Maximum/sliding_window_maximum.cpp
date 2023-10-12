622443ad4d7688840dec7918293ac999ebf19889
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-12 10:25:31 +0200

    Exchange Solution & Reformat LeetCode: 090_239__Sliding_Window_Maximum to Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/090_239__Sliding_Window_Maximum/sliding_window_maximum.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/090_239__Sliding_Window_Maximum/sliding_window_maximum.cpp
@@ -3,157 +3,143 @@
 #include <deque>
 
 /*
-	============
-	=== HARD ===
-	============
+    ============
+    === HARD ===
+    ============
 
-	===========================
-	239) Sliding Window Maximum
-	===========================
+    ===========================
+    239) Sliding Window Maximum
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	You are given an array of integers "nums", there is a sliding window of
-	size 'k' which is moving from the very left of the array to the very right.
-	You can only see the 'k' numbers in the window. Each time the sliding
-	window moves right by one position.
+    You are given an array of integers "nums", there is a sliding window of
+    size 'k' which is moving from the very left of the array to the very right.
+    You can only see the 'k' numbers in the window. Each time the sliding
+    window moves right by one position.
 
-	Return the max sliding window.
+    Return the max sliding window.
 
-	=================================================================
-	FUNCTION: vector<int> maxSlidingWindow(vector<int>& nums, int k);
-	=================================================================
+    =================================================================
+    FUNCTION: vector<int> maxSlidingWindow(vector<int>& nums, int k);
+    =================================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
-	Output: [3, 3, 5, 5, 6, 7]
-	Window position                     Max
-	---------------                    -----
-	[1   3  -1] -3   5   3   6   7        3
-	 1  [3  -1  -3]  5   3   6   7        3
-	 1   3 [-1  -3   5]  4   6   7        5
-	 1   3  -1 [-3   5   4]  6   7        5
-	 1   3  -1  -3  [5   4   6]  7        6
-	 1   3  -1  -3   5  [4   6   7]       7
+    --- Example 1 ---
+    Input:  nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+    Output: [3, 3, 5, 5, 6, 7]
+    Window position                     Max
+    ---------------                    -----
+    [1   3  -1] -3   5   3   6   7        3
+     1  [3  -1  -3]  5   3   6   7        3
+     1   3 [-1  -3   5]  4   6   7        5
+     1   3  -1 [-3   5   4]  6   7        5
+     1   3  -1  -3  [5   4   6]  7        6
+     1   3  -1  -3   5  [4   6   7]       7
 
-	--- Example 2 ---
-	Input:  nums = [1], k = 1
-	Output: [1]
+    --- Example 2 ---
+    Input:  nums = [1], k = 1
+    Output: [1]
 
-	*** Constraints ***
-	1 <= nums.length <= 10^5
-	-10^4 <= nums[i] <= 10^4
-	1 <= k <= nums.length
+    *** Constraints ***
+    1 <= nums.length <= 10^5
+    -10^4 <= nums[i] <= 10^4
+    1 <= k <= nums.length
 
 */
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Let's start with:
-
-
-	============================
-	=== Brute Force Solution ===
-	============================
-
-	We can write a nested loop, the outer loop goes through each window and
-	the inner loop finds the max within the window. This is O(N^2) time
-	complexity. Since at worst k == n. If k was always smaller than n, it would
-	be O(n * k).
-
-	To optimize on brute force, we can either reduce outer or inner loop
-	complexity. Since we have toe examine each element at least once(there's no
-	way to find the maximum if we don't know what the values are), there is not
-	much we can do for outer loop. So we have to work on the inner loop.
-
-	*** Preserving inner elements maximum ***
-	Currently, to get the max of the sliding window, we look at each element in
-	the window and compare them. Analogous to sliding window sum problem (given
-	an array and a window size, return the sum of each window), when a window
-	slides, only two elements change - the leftmost gets removed and a new
-	element gets added to the right.
-
-	Everything in the middle(let's call them "inner elements") is unchanged,
-	and the maximum element among these inner elements are unchanged as well.
-	They key to reducing inner loop complexity is to persist the maximum of
-	the inner elements as we slide the window. Ideally, we want to be able to
-	access the maximum element in less than O(n) time and updating it in less
-	than O(n) time.
+    Let's start with:
 
 
-	===================================
-	=== Balanced Binary Search Tree ===
-	===================================
+    ============================
+    === Brute Force Solution ===
+    ============================
 
-	One way to achieve this goal is to save the window elements in a
-	self-balancing binary tree. Because it's self-balancing, the depth of the
-	tree is guaranteed to be O(logN), so find max, insert and delete are all:
-		O(logN) operations.
+    We can write a nested loop, the outer loop goes through each window and
+    the inner loop finds the max within the window. This is O(N^2) time
+    complexity. Since at worst k == n. If k was always smaller than n, it would
+    be O(n * k).
 
-	Every time we slide the window, we remove the node that's out of the window
-	and add the one that comes into the window to the tree. Overall, this gives
-	us O(N * logK) since the number of tree nodes is K and we slide max N
-	times. (actually we slide (n - k) times, but at worst we slide N).
+    To optimize on brute force, we can either reduce outer or inner loop
+    complexity. Since we have toe examine each element at least once(there's no
+    way to find the maximum if we don't know what the values are), there is not
+    much we can do for outer loop. So we have to work on the inner loop.
 
-	This is pretty good already, but can we do better?
+    *** Preserving inner elements maximum ***
+    Currently, to get the max of the sliding window, we look at each element in
+    the window and compare them. Analogous to sliding window sum problem (given
+    an array and a window size, return the sum of each window), when a window
+    slides, only two elements change - the leftmost gets removed and a new
+    element gets added to the right.
 
-	*** Larger elements entering window invalidates smaller elements ***
-	A question we can ask ourselves is "do we need to keep all the window
-	elements in our state?".
-	An important observation is for two elements:
-		arr[left] and arr[right]
-	where:
-		left < right
-
-	arr[left] leaves the window earlier as we slide. If arr[right] is larger
-	than arr[left], then there is no point keeping arr[left] in our state
-	since arr[right] is always gonna be larger during the time arr[left] is in
-	the window. Therefore, arr[left] can never be the maximum.
+    Everything in the middle(let's call them "inner elements") is unchanged,
+    and the maximum element among these inner elements are unchanged as well.
+    They key to reducing inner loop complexity is to persist the maximum of
+    the inner elements as we slide the window. Ideally, we want to be able to
+    access the maximum element in less than O(n) time and updating it in less
+    than O(n) time.
 
 
-	=======================
-	=== Monotonic Deque ===
-	=======================
+    =======================
+    === Monotonic Deque ===
+    =======================
 
-	Here we introduce an interesting data structure. It's a deque with an
-	interesting property - the elements in the deque from head to tail are in
-	decreasing order (hence the name monotonic)
+    Here we introduce an interesting data structure. It's a deque with an
+    interesting property - the elements in the deque from head to tail are in
+    decreasing order (hence the name monotonic)
 
-	To achieve this property, we modify the push operation so that:
-		Before we push an element into the deque, we first pop everything
-		smaller than it out of the deque.
+    To achieve this property, we modify the push operation so that:
+        Before we push an element into the deque, we first pop everything
+        smaller than it out of the deque.
 
-	This enfroces the decreasing order.
+    This enfroces the decreasing order.
 
-	The time complexity is O(N). This is because each element in the original
-	array can be pushed into and popped out of the deque only once.
+    The overall time complexity is O(N). This is because each element in the
+    original array can be pushed into and popped out of the deque only once.
 
-	The key why monotonic deque works is it stores both magnitude and position
-	information. From head to tail, the elements get smaller and further to the
-	right of the array.
+    Which indicates that both "push" and "pop" operations are performed in O(1)
 
-	Consider this Example:
-		[1, 3, -1, -3, 5, 3, 6, 7], k = 4;
-	
+    The key why monotonic deque works is it stores both magnitude and position
+    information. From head to tail, the elements get smaller and further to the
+    right of the array.
 
-	0)
-		        i
-		nums = [1] 3  -1  -3  5  3  6  7
-		        0  1   2   3  4  5  6  7
+    Consider this Example:
+        [1, 3, -1, -3, 5, 3, 6, 7], k = 4;
 
-		Deque: (It stores INDICES, not values)
-              
+
+    We start pushing nums[deque.front()] into "results" once (r + 1 becomes  k)
+    i.e.:
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
+
+        right++; However "right" pointer is always incrementing.
+
+
+
+    Also that's when we start incrementing our left pointer.
+
+
+
+    0)          l
+                r
+        nums = [1] 3  -1  -3  5  3  6  7
+                0  1   2   3  4  5  6  7
+
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -164,23 +150,22 @@
              |
              ------ back
 
-		results = Empty
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if (i >= k-1) // if (0 >= 4-1) 
-			false
-
-
+        right++; We always increment "right" pointer.
 
 
 
-	1)
-		           i
-		nums = [1  3] -1  -3  5  3  6  7
-		        0  1   2   3  4  5  6  7
 
-		Deque: (It stores INDICES, not values)
-              
+    1)
+                l  r
+        nums = [1  3] -1  -3  5  3  6  7
+                0  1   2   3  4  5  6  7
+
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -191,23 +176,22 @@
              |
              ------ back
 
-		results = Empty
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if 2i >= k-1) // if (1 >= 4-1) 
-			false
-
-
+        right++; We always increment "right" pointer.
 
 
 
-	2)
-		               i
-		nums = [1  3  -1] -3  5  3  6  7
-		        0  1   2   3  4  5  6  7
 
-		Deque: (It stores INDICES, not values)
-              
+    2)
+                l      r
+        nums = [1  3  -1] -3  5  3  6  7
+                0  1   2   3  4  5  6  7
+
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -218,23 +202,22 @@
                  |
                  --- back
 
-		results = Empty
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if (i >= k-1) // if (2 >= 4-1) 
-			false
-
-
+        right++; We always increment "right" pointer.
 
 
 
-	3)
-		                   i
-		nums = [1  3  -1  -3] 5  3  6  7
-		        0  1   2   3  4  5  6  7
 
-		Deque: (It stores INDICES, not values)
-              
+    3)
+                l          r
+        nums = [1  3  -1  -3] 5  3  6  7
+                0  1   2   3  4  5  6  7
+
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -245,24 +228,22 @@
                      |
                      --- back
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if (i >= k-1) // if (3 >= 4-1) 
-			true
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-			results.add(nums[front]) => Resuls = {3};
-
-
+        right++; We always increment "right" pointer.
 
 
 
 
-	4)
-		                      i
-		nums =  1 [3  -1  -3  5] 3  6  7
-		        0  1   2   3  4  5  6  7
+    4)
+                   l          r
+        nums =  1 [3  -1  -3  5] 3  6  7
+                0  1   2   3  4  5  6  7
 
-		Deque: (It stores INDICES, not values)
-              
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -273,26 +254,22 @@
              |
              --- back
 
-		// Once we expand the window to its size k, start pushing onwards
-		if (i >= k-1) // if (4 >= 4-1) 
-			true
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-			results.add(nums[front]) => Resuls = {3, 5};
-
-
+        right++; We always increment "right" pointer.
 
 
 
 
+    5)
+                       l         r
+        nums =  1  3 [-1  -3  5  3] 6  7
+                0  1   2   3  4  5  6  7
 
+        Deque: (It stores INDICES, not values)
 
-	5)
-		                         i
-		nums =  1  3 [-1  -3  5  3] 6  7
-		        0  1   2   3  4  5  6  7
-
-		Deque: (It stores INDICES, not values)
-              
     front ----
              |
              v
@@ -303,23 +280,22 @@
                  |
                  --- back
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if (i >= k-1) // if (5 >= 4-1) 
-			true
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-			results.add(nums[front]) => Resuls = {3, 5, 5};
-
-
+        right++; We always increment "right" pointer.
 
 
 
-	6)
-		                            i
-		nums =  1  3  -1 [-3  5  3  6] 7
-		        0  1   2   3  4  5  6  7
 
-		Deque: (It stores INDICES, not values)
-              
+    6)
+                           l        r
+        nums =  1  3  -1 [-3  5  3  6] 7
+                0  1   2   3  4  5  6  7
+
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -330,23 +306,22 @@
              |
              --- back
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if (i >= k-1) // if (6 >= 4-1) 
-			true
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-			results.add(nums[front]) => Resuls = {3, 5, 5, 6};
-
-
+        right++; We always increment "right" pointer.
 
 
 
-	7)
-		                               i
-		nums =  1  3  -1  -3 [5  3  6  7]
-		        0  1   2   3  4  5  6  7
 
-		Deque: (It stores INDICES, not values)
-              
+    7)
+                              l        r
+        nums =  1  3  -1  -3 [5  3  6  7]
+                0  1   2   3  4  5  6  7
+
+        Deque: (It stores INDICES, not values)
+
     front ----
              |
              v
@@ -357,109 +332,118 @@
              |
              --- back
 
-		// Once we expand the window to its size k, start pushing front onwards
-		if (i >= k-1) // if (7 >= 4-1) 
-			true
+        if (r + 1 >= k)
+            results.push_back(nums[deque.back()]);
+            left++;
 
-			results.add(nums[front]) => Resuls = {3, 5, 5, 6, 7};
+        right++; We always increment "right" pointer.
 
 
 
-	==================
-	=== End Result ===
-	==================
 
-		Resuls = {3, 5, 5, 6, 7};
+    ==================
+    === End Result ===
+    ==================
+
+        Resuls = {3, 5, 5, 6, 7};
 
 */
 
-/* Time  Beats: 39.67% */
-/* Space Beats: 97.79% */
+/* Time  Beats: 97.53% */
+/* Space Beats: 46.64% */
 
 /* Time  Complexity: O(n) */
-/* Space Complexity: O(k) */
-class Solution{
+/* Space Complexity: O(k) */  // It is said that k can be equal to n, so O(n)
+class Solution {
 public:
-	std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k)
-	{
-		std::deque<int>  deque;   // Stores indices
-		std::vector<int> results;
+    std::vector<int> maxSlidingWindow(std::vector<int>& nums, int k)
+    {
+        std::vector<int> results;
+        std::deque<int>  deque;   // Stores indices
 
-		for (int i = 0; i < nums.size(); i++)
-		{
-			while (!deque.empty() && nums[deque.back()] <= nums[i])
-				deque.pop_back();
+        int left  = 0;
+        int right = 0;
+        while (right < nums.size())
+        {
+            while (!deque.empty() && nums[deque.back()] < nums[right])
+                deque.pop_back();
 
-			deque.push_back(i);
+            deque.push_back(right);
 
-			// Remove first element if it's outside the window
-			if (deque.front() == i - k)
-				deque.pop_front();
+            /* Remove leftmost element if it's outside the window */
+            if (left > deque.front()) // deque.front() is an INDEX!
+                deque.pop_front();
 
-			/* If window has 'k' elements, add front of the deque to results */
-			if (i >= k - 1)
-				results.push_back(nums[deque.front()]);
-		}
+            /* If window has 'k' elements, add front of the deque to results */
+            if (right + 1 >= k)
+            {
+                results.push_back(nums[deque.front()]);
+                left++;
+            }
 
-		return results;
-	}
+            right++;
+        }
+
+        return results;
+    }
 };
 
 
 int
 main()
 {
-	Solution sol;
+    Solution      sol;
 
-	/* Example 1 */
-	// std::vector<int> nums {1, 3, -1, -3, 5, 3, 6, 7};
-	// int k = 3;
+    /* Example 1 */
+    // std::vector<int> nums {1, 3, -1, -3, 5, 3, 6, 7};
+    // int k = 3;
 
-	/* Example 2 */
-	// std::vector<int> nums {1};
-	// int k = 1;
+    /* Example 2 */
+    // std::vector<int> nums {1};
+    // int k = 1;
 
-	/* Example 3 */
-	std::vector<int> nums {1, 3, -1, -3, 5, 3, 6, 7};
-	int k = 4;
-
-
-	std::cout << "\n\t==============================";
-	std::cout << "\n\t=== SLIDING WINDOW MAXIMUM ===";
-	std::cout << "\n\t==============================\n";
+    /* Example 3 */
+    std::vector<int> nums {1, 3, -1, -3, 5, 3, 6, 7};
+    int k = 4;
 
 
-	/* Write Input */
-	bool first = true;
-	std::cout << "\n\tArray:  [";
-	for (auto x: nums)
-	{
-		if (!first)
-			std::cout << ", ";
-
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n";
+    std::cout << "\n\t==============================";
+    std::cout << "\n\t=== SLIDING WINDOW MAXIMUM ===";
+    std::cout << "\n\t==============================\n";
 
 
-	/* Solution */
-	std::vector<int> output = sol.maxSlidingWindow(nums, k);
+    /* Write Input */
+    std::cout << "\n\tK: " << k;
+    bool first = true;
+    std::cout << "\n\tArray:  [";
+    for (auto x: nums)
+    {
+        if (!first)
+            std::cout << ", ";
+
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n";
 
 
-	/* Write Output */
-	first = true;
-	std::cout << "\n\tOutput: [";
-	for (auto x: output)
-	{
-		if (!first)
-			std::cout << ", ";
-
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n\n";
+    /* Solution */
+    std::vector<int> output = sol.maxSlidingWindow(nums, k);
 
 
-	return 0;
+    /* Write Output */
+    first = true;
+    std::cout << "\n\tOutput: [";
+    for (auto x: output)
+    {
+        if (!first)
+            std::cout << ", ";
+
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n\n";
+
+
+    return 0;
 }
