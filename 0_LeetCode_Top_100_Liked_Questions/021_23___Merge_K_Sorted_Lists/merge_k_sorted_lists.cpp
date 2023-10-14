66e7f110278d7138bc9c4f18887b44ed54956857
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-14 14:57:30 +0200

    Add Efficient Solution in LeetCode: 021_23___Merge_K_Sorted_Lists in Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/021_23___Merge_K_Sorted_Lists/merge_k_sorted_lists.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/021_23___Merge_K_Sorted_Lists/merge_k_sorted_lists.cpp
@@ -4,94 +4,94 @@
 
 
 /*
-	============
-	=== HARD ===
-	============
+    ============
+    === HARD ===
+    ============
 
-	===========================
-	23) Merge k Sorted Lists
-	===========================
+    ===========================
+    23) Merge k Sorted Lists
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	You are given an array of k linked-lists "lists", each linked-list is
-	sorted in ascending order.
+    You are given an array of k linked-lists "lists", each linked-list is
+    sorted in ascending order.
 
-	Merge all the linked-lists into one sorted linked-list and return it.
+    Merge all the linked-lists into one sorted linked-list and return it.
 
-	=============================================================================
-	FUNCTION: struct ListNode* mergeKLists(std::vector<struct ListNode*>& lists);
-	=============================================================================
+    =============================================================================
+    FUNCTION: struct ListNode* mergeKLists(std::vector<struct ListNode*>& lists);
+    =============================================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
-	Output: [1, 1, 2, 3, 4, 4, 5, 6]
-	Explanation: The linked-lists are:
-	[
-		1 -> 4 -> 5,
-		1 -> 3 -> 4,
-		2 -> 6
-	]
-	merging them into one sorted list:
-	1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6
+    --- Example 1 ---
+    Input:  lists = [[1, 4, 5], [1, 3, 4], [2, 6]]
+    Output: [1, 1, 2, 3, 4, 4, 5, 6]
+    Explanation: The linked-lists are:
+    [
+        1 -> 4 -> 5,
+        1 -> 3 -> 4,
+        2 -> 6
+    ]
+    merging them into one sorted list:
+    1 -> 1 -> 2 -> 3 -> 4 -> 4 -> 5 -> 6
 
-	--- Example 2 ---
-	Input:  lists = []
-	Output: []
+    --- Example 2 ---
+    Input:  lists = []
+    Output: []
 
 
-	--- Example 3 ---
-	Input:  lists = [[]]
-	Output: []
+    --- Example 3 ---
+    Input:  lists = [[]]
+    Output: []
 
-	*** Constraints ***
-	k == lists.length
-	0 <= k <= lists[i].length <= 500
-	- 10^4 <= lists[i][j] <= 10^4
-	lists[i] is sorted in ascending order
-	The sum of lists[i].length will not exceed 10^4
+    *** Constraints ***
+    k == lists.length
+    0 <= k <= lists[i].length <= 500
+    - 10^4 <= lists[i][j] <= 10^4
+    lists[i] is sorted in ascending order
+    The sum of lists[i].length will not exceed 10^4
 
 */
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Make a dummy node and set both head and tail to point to it.
-	Set "null_cnt" to 0. "null_cnt" will count how many of the lists are now
-	pointing to nullptr. We have to use each and every one of nodes in each and
-	every one of lists, so until that happens(until null_cnt is less than n),
-	do the while loop.
+    Make a dummy node and set both head and tail to point to it.
+    Set "null_cnt" to 0. "null_cnt" will count how many of the lists are now
+    pointing to nullptr. We have to use each and every one of nodes in each and
+    every one of lists, so until that happens(until null_cnt is less than n),
+    do the while loop.
 
-	In the while loop immediately set local variable "min" to INT_MAX and index
-	to -1.
+    In the while loop immediately set local variable "min" to INT_MAX and index
+    to -1.
 
-	Now iterate through ALL the first nodes in each list and save the lowest
-	value in variable "min" and save the index od the list with a lowest
-	first node in a variable "index".
+    Now iterate through ALL the first nodes in each list and save the lowest
+    value in variable "min" and save the index od the list with a lowest
+    first node in a variable "index".
 
-	Make tail's next point to that node with the current lowest of all the
-	first nodes in lists and make tail point now point to that node.
+    Make tail's next point to that node with the current lowest of all the
+    first nodes in lists and make tail point now point to that node.
 
-	And make sure a list that contained that lowest first value now points to
-	its next node:
-			lists[index] = lists[index]->next;
+    And make sure a list that contained that lowest first value now points to
+    its next node:
+            lists[index] = lists[index]->next;
 
-	If that "next node" is nullptr then null_cnt++.
+    If that "next node" is nullptr then null_cnt++.
 
-	Repeat this until every node in every list is processed, that is every
-	list points to nullptr.
+    Repeat this until every node in every list is processed, that is every
+    list points to nullptr.
 
-	Since our head points to dummy node, return head->next. We don't care about
-	our dummy node.
+    Since our head points to dummy node, return head->next. We don't care about
+    our dummy node.
 */
 
 
@@ -114,52 +114,52 @@ struct ListNode {
 /* Space Complexity: O(1) */
 class Solution{
 public:
-	ListNode* mergeKLists(std::vector<ListNode*>& lists)
-	{
-		int n = lists.size();
+    ListNode* mergeKLists(std::vector<ListNode*>& lists)
+    {
+        int n = lists.size();
 
-		ListNode* dummy_node = new ListNode();
-		ListNode* tail = dummy_node;
+        ListNode* dummy_node = new ListNode();
+        ListNode* tail = dummy_node;
 
-		std::vector<ListNode*> null_vec(n, nullptr);
-		int idx = -1;
+        std::vector<ListNode*> null_vec(n, nullptr);
+        int idx = -1;
 
-		while (lists != null_vec)
-		{
-			int min = INT_MAX;
+        while (lists != null_vec)
+        {
+            int min = INT_MAX;
 
-			for (int i = 0; i < n; i++)
-			{
-				if (lists[i] != nullptr && lists[i]->val < min)
-				{
-					idx = i;
-					min = lists[i]->val;
+            for (int i = 0; i < n; i++)
+            {
+                if (lists[i] != nullptr && lists[i]->val < min)
+                {
+                    idx = i;
+                    min = lists[i]->val;
 
-					std::cout << "\n\tOvde sam: " << lists[i]->val << "\n";
-				}
-			}
+                    std::cout << "\n\tOvde sam: " << lists[i]->val << "\n";
+                }
+            }
 
-			tail->next = lists[idx];
-			lists[idx] = lists[idx]->next;
+            tail->next = lists[idx];
+            lists[idx] = lists[idx]->next;
 
-			tail = tail->next;
-			tail->next = nullptr;
-		}
+            tail = tail->next;
+            tail->next = nullptr;
+        }
 
-		return dummy_node->next;
-	}
+        return dummy_node->next;
+    }
 };
 
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	It's a kind of Merge Sort Solution.
-	
+    It's a kind of Merge Sort Solution.
+
 */
 
 /* Time  Beats: 31.16% */
@@ -169,15 +169,15 @@ public:
     Time  Complexity: O(n * logk)
 
     Divide and Conquer is used to iterate on the O(n * k) approach of merging
-	one by one. For each step, 'n' nodes(all of them) are compared during the
-	marge process - O(n).
+    one by one. For each step, 'n' nodes(all of them) are compared during the
+    marge process - O(n).
 
-	At the end of each step, the number of lists that need to be merged has
-	been halfed, therefore the number of iterations needed is the number of
-	times you can half 'k' until you get to 1, that is "logk"(base 2), so each
-	step will be done "logk" times - O(logk)
+    At the end of each step, the number of lists that need to be merged has
+    been halfed, therefore the number of iterations needed is the number of
+    times you can half 'k' until you get to 1, that is "logk"(base 2), so each
+    step will be done "logk" times - O(logk)
 
-	Thus the time Complexity is O(n * logk)
+    Thus the time Complexity is O(n * logk)
 */
 /* Space Complexity: O(n) */
 class Solution_Divide_And_Conquer{
@@ -234,72 +234,152 @@ private:
 };
 
 
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This is a much easier to grasp "Divide & Conquer" Implementation.
+
+*/
+
+/* Time  Beats: 91.60% */
+/* Space Beats: 38.05% */
+
+/* Time  Complexity: O(n * logk) */
+/* Space Complexity: O(n) */
+class Solution_Divide_And_Conquer_Neat {
+public:
+    ListNode* mergeKLists(std::vector<ListNode*>& lists)
+    {
+        if (lists.empty())
+            return nullptr;
+
+        while (lists.size() > 1)
+        {
+            std::vector<ListNode*> tmp_merged_lists;
+
+            for (int i = 0; i < lists.size(); i+=2)
+            {
+                ListNode* list_1 = lists[i];
+                ListNode* list_2 = i+1 < lists.size() ? lists[i+1] : nullptr;
+
+                tmp_merged_lists.push_back(mergeTwoSortedLists(list_1, list_2));
+            }
+
+            lists.clear();
+            lists = tmp_merged_lists;
+        }
+
+        return lists[0];
+    }
+
+private:
+    ListNode* mergeTwoSortedLists(ListNode* list_1, ListNode* list_2)
+    {
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail  = dummy;
+
+        while (list_1 && list_2)
+        {
+            if (list_1->val <= list_2->val)
+            {
+                tail->next = list_1;
+                list_1 = list_1->next;
+            }
+            else
+            {
+                tail->next = list_2;
+                list_2 = list_2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        if (!list_1)
+            tail->next = list_2;
+        else
+            tail->next = list_1;
+
+        ListNode* ret = dummy->next;
+        delete dummy; // Prevent Memory Leak
+
+        return ret;
+    }
+};
+
+
+
+
 void
 print_list(struct ListNode* head)
 {
-	std::cout << "\n\t[";
+    std::cout << "\n\t[";
 
-	bool first = true;
-	while (head)
-	{
-		if (!first)
-			std::cout << " -> ";
-		else
-			first = false;
+    bool first = true;
+    while (head)
+    {
+        if (!first)
+            std::cout << " -> ";
+        else
+            first = false;
 
-		std::cout << head->val;
-		head = head->next;
-	}
+        std::cout << head->val;
+        head = head->next;
+    }
 
-	std::cout << "]";
+    std::cout << "]";
 }
-
 
 
 int
 main()
 {
-	Solution sol;
-	Solution_Divide_And_Conquer sol_divide_and_conquer;
+    Solution                          sol;
+    Solution_Divide_And_Conquer       sol_divide_and_conquer;
+    Solution_Divide_And_Conquer_Neat  sol_divide_and_conquer_neat;
 
-	/* Example 1 */
-	struct ListNode five1(5);
-	struct ListNode four1(4, &five1);
-	struct ListNode one1 (1, &four1);
+    /* Example 1 */
+    struct ListNode five1(5);
+    struct ListNode four1(4, &five1);
+    struct ListNode one1 (1, &four1);
 
-	struct ListNode four2 (4);
-	struct ListNode three2(3, &four2);
-	struct ListNode one2  (1, &three2);
+    struct ListNode four2 (4);
+    struct ListNode three2(3, &four2);
+    struct ListNode one2  (1, &three2);
 
-	struct ListNode two3(2);
-	struct ListNode six3(1, &two3);
+    struct ListNode two3(2);
+    struct ListNode six3(1, &two3);
 
-	std::vector<struct ListNode*> lists;
-	lists.push_back(&one1);
-	lists.push_back(&one2);
-	lists.push_back(&six3);
+    std::vector<struct ListNode*> lists;
+    lists.push_back(&one1);
+    lists.push_back(&one2);
+    lists.push_back(&six3);
 
 
-	/* Example 2 */
-	// std::vector<struct ListNode*> lists;
+    /* Example 2 */
+    // std::vector<struct ListNode*> lists;
 
-	std::cout << "\n\t============================";
-	std::cout << "\n\t=== MERGE K SORTED LISTS ===";
-	std::cout << "\n\t============================\n\n";
+    std::cout << "\n\t============================";
+    std::cout << "\n\t=== MERGE K SORTED LISTS ===";
+    std::cout << "\n\t============================\n\n";
 
-	/* Write Input */
-	std::cout << "\n\tOriginal Lists:";
-	for (const auto& x : lists)
-		print_list(x);
+    /* Write Input */
+    std::cout << "\n\tOriginal Lists:";
+    for (const auto& x : lists)
+        print_list(x);
 
-	/* Solution */
-	// struct ListNode* head = sol.mergeKLists(lists);
-	struct ListNode* head = sol_divide_and_conquer.mergeKLists(lists);
+    /* Solution */
+    // struct ListNode* head = sol.mergeKLists(lists);
+    // struct ListNode* head = sol_divide_and_conquer.mergeKLists(lists);
+    struct ListNode* head = sol_divide_and_conquer_neat.mergeKLists(lists);
 
-	/* Write Output */
-	std::cout << "\n\n\n\tMerged k Sorted Lists:";
-	print_list(head);
-	std::cout << "\n\n";
+    /* Write Output */
+    std::cout << "\n\n\n\tMerged k Sorted Lists:";
+    print_list(head);
+    std::cout << "\n\n";
 
-	return 0;
+    return 0;
 }
