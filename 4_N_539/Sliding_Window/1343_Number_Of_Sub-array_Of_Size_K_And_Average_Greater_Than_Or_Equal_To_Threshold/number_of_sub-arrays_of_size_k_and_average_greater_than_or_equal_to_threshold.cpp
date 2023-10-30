2078ae0289abb583bdfb39aaa1c32600503f34f0
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-30 10:55:15 +0100

    Add LeetCode: 1343_Number_Of_Sub-arrays_Of_Size_K_And_Average_Greater_Than_Or_Equal_To_Threshold to N_539

---
+++ 4_N_539/Sliding_Window/1343_Number_Of_Sub-array_Of_Size_K_And_Average_Greater_Than_Or_Equal_To_Threshold/number_of_sub-arrays_of_size_k_and_average_greater_than_or_equal_to_threshold.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===================================================================================
+    1343) Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold
+    ===================================================================================
+
+    ============
+    Description:
+    ============
+
+    Given an array of integers arr and two integers k and threshold, return the
+    number of sub-arrays of size k and average greater than or equal to
+    threshold.
+
+    =====================================================================
+    FUNCTION: int numOfSubarrays(vector<int>& arr, int k, int threshold);
+    =====================================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: arr = [2,2,2,2,5,5,5,8], k = 3, threshold = 4
+    Output: 3
+    Explanation: Sub-arrays [2,5,5],[5,5,5] and [5,5,8] have averages 4, 5 and
+                 6 respectively. All other sub-arrays of size 3 have averages
+                 less than 4 (the threshold).
+
+
+    --- Example 2 ---
+    Input: arr = [11,13,17,23,29,31,7,5,2,3], k = 3, threshold = 5
+    Output: 6
+    Explanation: The first 6 sub-arrays of size 3 have averages greater than 5.
+                 Note that averages are not integers.
+
+
+    *** Constraints ***
+    1 <= arr.length <= 10^5
+    1 <= arr[i] <= 10^4
+    1 <= k <= arr.length
+    0 <= threshold <= 10^4
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Basic Sliding Window technique.
+
+*/
+
+/* Time  Beats: 82.02% */
+/* Space Beats: 77.30% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int numOfSubarrays(std::vector<int>& arr, int k, int threshold)
+    {
+        int count = 0;
+        int total = 0;
+
+        for (int i = 0; i < arr.size(); i++) // Sliding Window
+        {
+            total += arr[i];
+            int average = total / k;
+
+            if (i-k+1 >= 0)
+            {
+                if (average >= threshold)
+                    count++;
+
+                total -= arr[i-k+1];
+            }
+        }
+
+        return count;
+    }
+};
