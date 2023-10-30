88620a574faee5373fec510b7c69b0bfa66e788d
Noa Vukasovic <noavukasovic@gmail.com>
2023-10-30 10:24:57 +0100

    Add LeetCode: 1456_Maximum_Number_Of_Vowels_In_A_Substring_Of_Given_Length to N_539

---
+++ 4_N_539/Sliding_Window/1456_Maximum_Number_Of_Vowels_In_A_Substring_Of_Given_Length/maximum_number_of_vowels_in_a_substring_of_given_length.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <unordered_map>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    =============================================================
+    1456) Maximum Number of Vowels in a Substring of Given Length
+    =============================================================
+
+    ============
+    Description:
+    ============
+
+    Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+    Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+    =========================================
+    FUNCTION: int maxVowels(string s, int k);
+    =========================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: s = "abciiidef", k = 3
+    Output: 3
+    Explanation: The substring "iii" contains 3 vowel letters.
+
+    --- Example 2 ---
+    Input: s = "aeiou", k = 2
+    Output: 2
+    Explanation: Any substring of length 2 contains 2 vowels.
+
+    --- Example 3 ---
+    Input: s = "leetcode", k = 3
+    Output: 2
+    Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+
+    *** Constraints ***
+    1 <= s.length <= 10^5
+    s consists of lowercase English letters.
+    1 <= k <= s.length
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-Explanatory
+
+*/
+
+/* Time  Beats: 30.75% */
+/* Space Beats: 20.93% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int maxVowels(string s, int k)
+    {
+        std::unordered_set<char> uset = {'a', 'e', 'i', 'o', 'u'};
+
+        int max_vowels = 0;
+        int vowels = 0;
+        for (int i = 0; i < k; i++)
+        {
+            if (uset.find(s[i]) != uset.end())
+                vowels++;
+        }
+
+        max_vowels = std::max(max_vowels, vowels);
+
+        int left  = 0;
+        int right = k;
+        while (right < s.length())
+        {
+            if (uset.count(s[right]))
+                vowels++;
+
+            if (uset.count(s[left]))
+                vowels--;
+
+            max_vowels = std::max(max_vowels, vowels);
+            left++;
+            right++;
+        }
+
+        return max_vowels;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-Explanatory
+    
+*/
+
+/* Time  Beats: 27.51% */
+/* Space Beats: 20.93% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int maxVowels(std::string s, int k)
+    {
+        std::unordered_set<char> uset = {'a', 'e', 'i', 'o', 'u'};
+
+        int max_vowels = 0;
+        int vowels = 0;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            vowels += uset.find(s[i]) != uset.end() ? 1 : 0;
+
+            if (i - k >= 0)
+                vowels -= uset.find(s[i - k]) != uset.end() ? 1 : 0;
+
+            max_vowels = max(max_vowels, vowels);
+        }
+
+        return max_vowels;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+    (Same approach, but this uses Bit Mask instead of a Hash Set)
+
+    Computers are much better at performing bitwise operations.
+
+*/
+
+/* Time  Beats: 98.58% */
+/* Space Beats: 72.36% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int maxVowels(std::string s, int k)
+    {
+        auto is_vowel = [](char c)
+        {
+            static const uint32_t b1 = 0x208222;
+            static const uint8_t  b2 = 0x1f;
+            static const uint8_t  b3 = 0x1;
+
+            return (b1 >> (c & b2)) & b3;
+        };
+
+        int max_vowels = 0;
+        int vowels = 0;
+        for(int i = 0, j = i - k; i < s.size(); ++i, ++j)
+        {
+            vowels += is_vowel(s[i]);
+            if (i >= k)
+                vowels -= is_vowel(s[j]);
+
+            max_vowels = max(max_vowels, vowels);
+        }
+
+        return max_vowels;
+    };
+};
