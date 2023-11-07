263a713ffb8438997fcef8847952c7a2704f2239
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-07 12:05:55 +0100

    Add Solution_CPP & Reformat Tabs to Spaces in LeetCode: 027_34___Find_First_And_Last_Position_Of_Elements_In_Sorted_Array to Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/027_34___Find_First_And_Last_Position_Of_Elements_In_Sorted_Array/find_first_and_last_position_of_element_in_sorted_array.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/027_34___Find_First_And_Last_Position_Of_Elements_In_Sorted_Array/find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -1,157 +1,158 @@
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
+#include <iterator>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
-	
-	===========================================================
-	34) Find First and Last Position of Element in Sorted Array
-	===========================================================
-	
-	============
-	Description:
-	============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	Given an array of integers "nums" sorted in non-decreasing order, find the
-	starting and ending position of a given "target" value.
+    ===========================================================
+    34) Find First and Last Position of Element in Sorted Array
+    ===========================================================
 
-	If "target" is not found in the array, return [-1, -1].
+    ============
+    Description:
+    ============
 
-	=====
-	Node: You must write an algorithm with O(log n) runtime complexity.
-	=====
-	
-	=================================================================
-	FUNCTION: vector<int> searchRange(vector<int>& nums, int target);
-	=================================================================
-	
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
-	
-	--- Example 1 ---
-	Input:  nums = [5, 7, 7, 8, 8, 10], target = 8
-	Output: [3, 4]
-	
-	--- Example 2 ---
-	Input:  nums = [5, 7, 7, 8, 8, 10], target = 6
-	Output: [-1, -1]
-	
-	--- Example 3 ---
-	Input:  nums = [], target = 0
-	Output: [-1, -1]
-	
-	*** Constraints ***
-	0 <= nums.length <= 10^5
-	-10^9 <= nums[i] <= 10^9
-	nums in a non-decreasing array.
-	-10^9 <= target <= 10^9
+    Given an array of integers "nums" sorted in non-decreasing order, find the
+    starting and ending position of a given "target" value.
+
+    If "target" is not found in the array, return [-1, -1].
+
+    =====
+    Node: You must write an algorithm with O(log n) runtime complexity.
+    =====
+
+    =================================================================
+    FUNCTION: vector<int> searchRange(vector<int>& nums, int target);
+    =================================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input:  nums = [5, 7, 7, 8, 8, 10], target = 8
+    Output: [3, 4]
+
+    --- Example 2 ---
+    Input:  nums = [5, 7, 7, 8, 8, 10], target = 6
+    Output: [-1, -1]
+
+    --- Example 3 ---
+    Input:  nums = [], target = 0
+    Output: [-1, -1]
+
+    *** Constraints ***
+    0 <= nums.length <= 10^5
+    -10^9 <= nums[i] <= 10^9
+    nums in a non-decreasing array.
+    -10^9 <= target <= 10^9
 
 */
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Since we're told to solve this problem in O(log n) runtime complexity, we
-	have to use Binary search.
+    Since we're told to solve this problem in O(log n) runtime complexity, we
+    have to use Binary search.
 
-	However, we will divide this problem to two searches, since we have to
-	return two indices anyway.
-	First we search for the left index, and then, using some informaion of the
-	previous search, we search for the right index as well.
+    However, we will divide this problem to two searches, since we have to
+    return two indices anyway.
+    First we search for the left index, and then, using some informaion of the
+    previous search, we search for the right index as well.
 
-	We initialize:
-		low  = 0;
-		high = n - 1;
-	
-	In each step we calculate "mid"(mid = (low + high) / 2).
+    We initialize:
+        low  = 0;
+        high = n - 1;
 
-	Left range:
+    In each step we calculate "mid"(mid = (low + high) / 2).
 
-	Now there are three possibilities:
-		1. If nums[mid] < target, then the range must begin on the right of mid
-		That is => low = mid + 1;
+    Left range:
 
-		2. If nums[mid] > target, then the range must begin on the left of mid
-		That is => high = mid - 1;
+    Now there are three possibilities:
+        1. If nums[mid] < target, then the range must begin on the right of mid
+        That is => low = mid + 1;
 
-		3. If nums[mid] == target, then the range must begin on the left or at
-		the mid. That is => high = mid;
+        2. If nums[mid] > target, then the range must begin on the left of mid
+        That is => high = mid - 1;
 
-		We can merge 2nd and 3rd case in:
-			2*. If nums[mid] >= target, then
-				high = mid
+        3. If nums[mid] == target, then the range must begin on the left or at
+        the mid. That is => high = mid;
 
-	The while loop loops until low < high.
+        We can merge 2nd and 3rd case in:
+            2*. If nums[mid] >= target, then
+                high = mid
 
-	After we are finished with searching for the left index, we can simply
-	check if (nums[low] == target) and if that is indeed the case, then that is
-	the left boundary of the range.
+    The while loop loops until low < high.
 
-	If it is not, we are done. There is no number "target"in our array and we
-	can return {-1, -1} immediately.
+    After we are finished with searching for the left index, we can simply
+    check if (nums[low] == target) and if that is indeed the case, then that is
+    the left boundary of the range.
+
+    If it is not, we are done. There is no number "target"in our array and we
+    can return {-1, -1} immediately.
 
 
-	For the right range we can use a similar logic, however there's one caveat.
-	First, again, there are three possibilities:
-		1. If nums[mid] > target, then the range must begin on the left of mid
-		That is => high = mid - 1
+    For the right range we can use a similar logic, however there's one caveat.
+    First, again, there are three possibilities:
+        1. If nums[mid] > target, then the range must begin on the left of mid
+        That is => high = mid - 1
 
-		2. If nums[mid] < target, then the range must beginon the right od mif
-		That is => low = mid + 1
+        2. If nums[mid] < target, then the range must beginon the right od mif
+        That is => low = mid + 1
 
-		3. If nums[mid] == target, then the range must begin on the right of or
-		at mid. That is => low = mid
+        3. If nums[mid] == target, then the range must begin on the right of or
+        at mid. That is => low = mid
 
-	Again, we can merge 2nd and 3rd case into:
-		2*. If nums[mid] <= target, then
-			low = mid
-	
-	However, as I've said, there's on caveat. Consider the following case:
-	nums = [5, 7], target = 5
+    Again, we can merge 2nd and 3rd case into:
+        2*. If nums[mid] <= target, then
+            low = mid
 
-	Now nums[mid] == 5, then according to the rule 2, we set low = mid. This
-	practically does nothing because "low" is already equal to mid. As a
-	result, the search range is NOT MOVED AT ALL!
+    However, as I've said, there's on caveat. Consider the following case:
+    nums = [5, 7], target = 5
 
-	The solution is by using a small trick: instead of calculating "mid" as
-		mid = (low + high) / 2
-	
-	we do:
-		mid = (low + high) / 2 + 1
+    Now nums[mid] == 5, then according to the rule 2, we set low = mid. This
+    practically does nothing because "low" is already equal to mid. As a
+    result, the search range is NOT MOVED AT ALL!
 
-	
-	Why does this work?
+    The solution is by using a small trick: instead of calculating "mid" as
+        mid = (low + high) / 2
 
-	When we use:
-		mid = (low + high) / 2
-	the "mid" is rounded down. In other words, "mid" is always "biased" towards
-	the left. This means we could have:
-		low == mid    when    high - low == mid
+    we do:
+        mid = (low + high) / 2 + 1
 
-	but we NEVER have:
-		high == mid
-	
-	So in order to keep the search range moving, you must make sure the new
-	"low" is set to something different than "mid", otherwise we are at the
-	risk that "low" gets stuck.
-	But for the new "high", it is okay if we set it to "mid", since it was not
-	equal to mid anyways. Our two rules in search of the left boundary happen
-	to satisfy these requirements, so it works perfectly in that situation.
-	Similarly, when we search for the right boundary, we must make sure "low"
-	won't get stuck when we set the new "low" to:
-		low = mid
 
-	The easiest way to achieve this is by making "mid" biased to the right.
-	i.e. mid = (low + high) / 2 + 1
-	
-	
+    Why does this work?
+
+    When we use:
+        mid = (low + high) / 2
+    the "mid" is rounded down. In other words, "mid" is always "biased" towards
+    the left. This means we could have:
+        low == mid    when    high - low == mid
+
+    but we NEVER have:
+        high == mid
+
+    So in order to keep the search range moving, you must make sure the new
+    "low" is set to something different than "mid", otherwise we are at the
+    risk that "low" gets stuck.
+    But for the new "high", it is okay if we set it to "mid", since it was not
+    equal to mid anyways. Our two rules in search of the left boundary happen
+    to satisfy these requirements, so it works perfectly in that situation.
+    Similarly, when we search for the right boundary, we must make sure "low"
+    won't get stuck when we set the new "low" to:
+        low = mid
+
+    The easiest way to achieve this is by making "mid" biased to the right.
+    i.e. mid = (low + high) / 2 + 1
+
+
 */
 
 
@@ -162,141 +163,177 @@
 /* Space Complexity: O(1) */
 class Solution{
 public:
-	std::vector<int>
-	searchRange(std::vector<int>& nums, int target)
-	{
-		std::vector<int> vec_result(2, -1);
+    std::vector<int>
+    searchRange(std::vector<int>& nums, int target)
+    {
+        std::vector<int> vec_result(2, -1);
 
-		// Base case
-		if (nums.empty())
-			return vec_result;
-		
-		int n = nums.size();
+        // Base case
+        if (nums.empty())
+            return vec_result;
 
-		int low  = 0;
-		int high = n - 1;
+        int n = nums.size();
 
-		// Find the left one
-		while (low < high)
-		{
-			int mid = (low + high) / 2;
-			
-			if (nums[mid] < target)
-				low = mid + 1;
-			else
-				high = mid;
-		}
+        int low  = 0;
+        int high = n - 1;
 
-		if (nums[low] != target)
-			return {-1, -1};
-		else
-			vec_result[0] = low;
+        // Find the left one
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        if (nums[low] != target)
+            return {-1, -1};
+        else
+            vec_result[0] = low;
 
 
-		// Find the right one
-		high = n - 1; // 'i' can start where it is
-		
-		while (low < high)
-		{
-			int mid = (low + high) / 2 + 1; // Make mid "biased" to the right
+        // Find the right one
+        high = n - 1; // 'i' can start where it is
 
-			if (nums[mid] > target)
-				high = mid - 1;
-			else
-				low = mid; // So that this won't make the search range stuck
-		}
+        while (low < high)
+        {
+            int mid = (low + high) / 2 + 1; // Make mid "biased" to the right
 
-		vec_result[1] = high;
+            if (nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid; // So that this won't make the search range stuck
+        }
 
-		return vec_result;
-	}
+        vec_result[1] = high;
+
+        return vec_result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same IDEA, written in the spirit of CPP.
+
+*/
+
+/* Time  Beats: 91.99% */
+/* Space Beats: 89.61% */
+
+/* Time  Complexity: O(log n) */
+/* Space Complexity: O(1) */
+class Solution_CPP_Way {
+public:
+    std::vector<int> searchRange(std::vector<int>& nums, int target)
+    {
+        auto lower = std::lower_bound(nums.begin(), nums.end(), target);
+        auto upper = std::upper_bound(nums.begin(), nums.end(), target);
+
+        if (lower == upper)
+            return {-1, -1};
+
+        int left  = std::distance(nums.begin(), lower);
+        int right = std::distance(nums.begin(), upper - 1);
+
+        return {left, right};
+    }
 };
 
 
 int
 main()
 {
-	Solution sol;
+    Solution         sol;
+    Solution_CPP_Way sol_cpp;
 
-	/* Example 1 */
-	std::vector<int> nums = {5, 7, 7, 8, 8, 10};
-	int target = 8;
+    /* Example 1 */
+    std::vector<int> nums = {5, 7, 7, 8, 8, 10};
+    int target = 8;
 
-	/* Example 2 */
-	// std::vector<int> nums = {5, 7, 7, 8, 8, 10};
-	// int target = 6;
+    /* Example 2 */
+    // std::vector<int> nums = {5, 7, 7, 8, 8, 10};
+    // int target = 6;
 
-	/* Example 3 */
-	// std::vector<int> nums = {};
-	// int target = 0;
+    /* Example 3 */
+    // std::vector<int> nums = {};
+    // int target = 0;
 
-	/* Example 4 */
-	// std::vector<int> nums = {1, 2, 2, 4, 5, 7, 8, 8, 8, 9, 11};
-	// int target = 8;
+    /* Example 4 */
+    // std::vector<int> nums = {1, 2, 2, 4, 5, 7, 8, 8, 8, 9, 11};
+    // int target = 8;
 
-	/* Example 5 */
-	// std::vector<int> nums = {1, 2, 3, 4, 8, 8, 8, 9, 9, 9, 10};
-	// int target = 8;
+    /* Example 5 */
+    // std::vector<int> nums = {1, 2, 3, 4, 8, 8, 8, 9, 9, 9, 10};
+    // int target = 8;
 
-	/* Example 6 */
-	// std::vector<int> nums = {1, 8, 8, 8, 9, 9, 10, 10, 11, 12, 13};
-	// int target = 8;
+    /* Example 6 */
+    // std::vector<int> nums = {1, 8, 8, 8, 9, 9, 10, 10, 11, 12, 13};
+    // int target = 8;
 
-	/* Example 7 */
-	// std::vector<int> nums = {1, 8, 8, 8, 9, 9, 10, 10, 11, 12, 13};
-	// int target = 11;
+    /* Example 7 */
+    // std::vector<int> nums = {1, 8, 8, 8, 9, 9, 10, 10, 11, 12, 13};
+    // int target = 11;
 
-	/* Example 8 */
-	// std::vector<int> nums = {2, 2};
-	// int target = 2;
+    /* Example 8 */
+    // std::vector<int> nums = {2, 2};
+    // int target = 2;
 
-	/* Example 9 */
-	// std::vector<int> nums = {1, 4};
-	// int target = 4;
+    /* Example 9 */
+    // std::vector<int> nums = {1, 4};
+    // int target = 4;
 
-	/* Example 10 */
-	// std::vector<int> nums = {1};
-	// int target = 1;
+    /* Example 10 */
+    // std::vector<int> nums = {1};
+    // int target = 1;
 
-	/* Example 11 */
-	// std::vector<int> nums = {2};
-	// int target = 3;
+    /* Example 11 */
+    // std::vector<int> nums = {2};
+    // int target = 3;
 
-	std::cout << "\n\t===============================================================";
-	std::cout << "\n\t=== FIND FIRST AND LAST POSITION OF ELEMENT IN SORTED ARRAY ===";
-	std::cout << "\n\t===============================================================\n";
+    std::cout << "\n\t===============================================================";
+    std::cout << "\n\t=== FIND FIRST AND LAST POSITION OF ELEMENT IN SORTED ARRAY ===";
+    std::cout << "\n\t===============================================================\n";
 
-	/* Write Input */
-	bool first = true;
-	std::cout << "\n\tArray: [";
-	for (auto x: nums)
-	{
-		if (!first)
-			std::cout << ", ";
+    /* Write Input */
+    bool first = true;
+    std::cout << "\n\tArray: [";
+    for (auto x: nums)
+    {
+        if (!first)
+            std::cout << ", ";
 
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]";
-	std::cout << "\n\tTarget: " << target << "\n";
-
-
-	/* Solution */
-	std::vector<int> result = sol.searchRange(nums, target);
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]";
+    std::cout << "\n\tTarget: " << target << "\n";
 
 
-	/* Write Output */
-	first = true;
-	std::cout << "\n\tFIRST & LAST Index of Target:\n\t[";
-	for (auto x: result)
-	{
-		if (!first)
-			std::cout << ", ";
+    /* Solution */
+    // std::vector<int> result = sol.searchRange(nums, target);
+    std::vector<int> result = sol_cpp.searchRange(nums, target);
 
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n\n";
 
-	return 0;
+    /* Write Output */
+    first = true;
+    std::cout << "\n\tFIRST & LAST Index of Target:\n\t[";
+    for (auto x: result)
+    {
+        if (!first)
+            std::cout << ", ";
+
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n\n";
+
+    return 0;
 }
