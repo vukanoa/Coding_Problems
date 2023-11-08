c5576792f80232fcc6dc15f9643dde5f218a39d3
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-08 13:37:07 +0100

    Exchange Recursive Solution & Add Iterative & Reformat LeetCOde: 052_94___Binary_Tree_Inorder_Traversal to Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/052_94___Binary_Tree_Inorder_Traversal/binary_tree_inorder_traversal.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/052_94___Binary_Tree_Inorder_Traversal/binary_tree_inorder_traversal.cpp
@@ -1,45 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	=================================
-	94) Binary Tree Inorder Traversal
-	=================================
+    =================================
+    94) Binary Tree Inorder Traversal
+    =================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given the "root" of a binary tree, return the inorder traversal of its
-	nodes' values.
+    Given the "root" of a binary tree, return the inorder traversal of its
+    nodes' values.
 
-	=======================================================
-	FUNCTION: vector<int> inorderTraversal(TreeNode* root);
-	=======================================================
+    =======================================================
+    FUNCTION: vector<int> inorderTraversal(TreeNode* root);
+    =======================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  root = [1, null, 2, 3]
-	Output: [1, 3, 2]
+    --- Example 1 ---
+    Input:  root = [1, null, 2, 3]
+    Output: [1, 3, 2]
 
-	--- Example 2 ---
-	Input:  root = []
-	Output: []
+    --- Example 2 ---
+    Input:  root = []
+    Output: []
 
-	--- Example 3 ---
-	Input:  root = [1]
-	Output: [1]
+    --- Example 3 ---
+    Input:  root = [1]
+    Output: [1]
 
-	*** Constraints ***
-	The number of nodes in the tree is in the range [0, 100]
-	-100 <= Node.val <= 100
+    *** Constraints ***
+    The number of nodes in the tree is in the range [0, 100]
+    -100 <= Node.val <= 100
 
 */
 
@@ -53,90 +54,134 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-
 /* Time  Beats:   100% */
 /* Space Beats: 98.93% */
 
-
 /*
-	Time  Complexity: O(n)
-	
-	Each node is visited only once.
+    Time  Complexity: O(n)
+
+    Each node is visited only once.
 */
 /*
-	Space Complexity: O(n)
-	
-	Recursive calls of "inorder" function create a call stack which requires
-	O(n) space in the worst case.
+    Space Complexity: O(n)
 
-	Also, using the vector "vec" requires O(n) space.
+    Recursive calls of "inorder" function create a call stack which requires
+    O(n) space in the worst case.
 
-	So O(n) + O(n) => O(n + n) => O(2 * n) => O(n)
+    Also, using the vector "vec" requires O(n) space.
+
+    So O(n) + O(n) => O(n + n) => O(2 * n) => O(n)
 */
-class Solution{
-	void inorder(std::vector<int>& vec, TreeNode* root)
-	{
-		if (root == nullptr)
-			return;
-
-		inorder(vec, root->left);
-		vec.push_back(root->val);
-		inorder(vec, root->right);
-	}
+class Solution_Recursive {
 public:
-	std::vector<int> inorderTraversal(TreeNode* root)
-	{
-		std::vector<int> vec;
-		inorder(vec, root);
+    std::vector<int> inorderTraversal(TreeNode* root)
+    {
+        std::vector<int> result;
+        inorder(root, result);
 
-		return vec;
-	}
+        return result;
+    }
+
+private:
+   void inorder(TreeNode* root, std::vector<int>& result)
+    {
+        if (!root)
+            return;
+
+        inorder(root->left, result);
+        result.push_back(root->val);
+        inorder(root->right, result);
+    }
 };
 
+
+
+
+/* Time  Beats:   100% */
+/* Space Beats: 73.88% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_Iterative {
+public:
+    std::vector<int> inorderTraversal(TreeNode* root)
+    {
+        if (!root)
+            return {};
+
+        std::vector<int> result;
+        std::stack<TreeNode*> stack;
+
+        while (true)
+        {
+            if (root)
+            {
+                stack.push(root);
+                root = root->left;
+            }
+            else
+            {
+                if (stack.empty())
+                    break;
+
+                root = stack.top();
+                stack.pop();
+
+                result.push_back(root->val);
+
+                root = root->right;
+            }
+        }
+
+        return result;
+    }
+};
 
 int
 main()
 {
-	Solution sol;
+    Solution_Recursive sol_rec;
+    Solution_Iterative sol_iter;
 
-	/* Example 1 */
-	TreeNode three(3);
-	TreeNode two(2, &three, nullptr);
-	TreeNode* one = new TreeNode(1, nullptr, &two);
+    /* Example 1 */
+    TreeNode three(3);
+    TreeNode two(2, &three, nullptr);
+    TreeNode* one = new TreeNode(1, nullptr, &two);
 
-	/* Example 2 */
-	// Root is nullptr
-	// TreeNode* one = nullptr;
+    /* Example 2 */
+    // Root is nullptr
+    // TreeNode* one = nullptr;
 
-	/* Example 3 */
-	// TreeNode* one = new TreeNode(1);
+    /* Example 3 */
+    // TreeNode* one = new TreeNode(1);
 
-	std::cout << "\n\t=====================================";
-	std::cout << "\n\t=== BINARY TREE INORDER TRAVERSAL ===";
-	std::cout << "\n\t=====================================\n";
-
-
-	/* Write Input */
-	std::cout << "\n\tTODO: Visual print of a binary Tree\n";
+    std::cout << "\n\t=====================================";
+    std::cout << "\n\t=== BINARY TREE INORDER TRAVERSAL ===";
+    std::cout << "\n\t=====================================\n";
 
 
-	/* Solution */
-	std::vector<int> result = sol.inorderTraversal(one);
+    /* Write Input */
+    std::cout << "\n\tTODO: Visual print of a binary Tree\n";
 
 
-	/* Write Output */
-	bool first = true;
-	std::cout << "\n\tInorder: [";
-	for (auto x: result)
-	{
-		if (!first)
-			std::cout << ", ";
-
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n\n";
+    /* Solution */
+    // std::vector<int> result = sol_rec.inorderTraversal(one);
+    std::vector<int> result = sol_iter.inorderTraversal(one);
 
 
-	return 0;
+    /* Write Output */
+    bool first = true;
+    std::cout << "\n\tInorder: [";
+    for (auto x: result)
+    {
+        if (!first)
+            std::cout << ", ";
+
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n\n";
+
+
+    return 0;
 }
