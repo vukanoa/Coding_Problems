9552a6a908cff820b697a0253cb7adebc0986b34
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-14 11:31:20 +0100

    Reformat Tabs to Spaces LeetCode: 1137_Tribonacci_Number in Orderless_Problems

--- Orderless_Problems/1137_Nth_Tribonacci_Number/nth_tribonacci_number.cpp
+++ Orderless_Problems/1137_Nth_Tribonacci_Number/nth_tribonacci_number.cpp
@@ -1,88 +1,88 @@
 #include <iostream>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	============================
-	1137) N-th Tribonacci Number
-	============================
+    ============================
+    1137) N-th Tribonacci Number
+    ============================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	The Tribonacci sequence Tn is defined as follows: 
+    The Tribonacci sequence Tn is defined as follows:
 
-	T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
 
-	Given n, return the value of Tn.
+    Given n, return the value of Tn.
 
-	================================
-	FUNCTION: int tribonacci(int n);
-	================================
+    ================================
+    FUNCTION: int tribonacci(int n);
+    ================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: n = 4
-	Output: 4
-	Explanation:
-	T_3 = 0 + 1 + 1 = 2
-	T_4 = 1 + 1 + 2 = 4
-
-
-	--- Example 2 ---
-	Input: n = 25
-	Output: 1389537
+    --- Example 1 ---
+    Input: n = 4
+    Output: 4
+    Explanation:
+    T_3 = 0 + 1 + 1 = 2
+    T_4 = 1 + 1 + 2 = 4
 
 
-	*** Constraints ***
+    --- Example 2 ---
+    Input: n = 25
+    Output: 1389537
+
+
+    *** Constraints ***
     0 <= n <= 37
-	The answer is guaranteed to fit within a 32-bit integer, i.e. 
-	answer <= 2^31 - 1.
+    The answer is guaranteed to fit within a 32-bit integer, i.e.
+    answer <= 2^31 - 1.
 
 */
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Basic Fibonacci Bottom-Up Dynamic Problem Solution.
+    Basic Fibonacci Bottom-Up Dynamic Problem Solution.
 
 */
 
-/* Time  Beats:  100% */
-/* Space Beats: 5.75% */
+/* Time  Beats:   100% */
+/* Space Beats: 85.68% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
 class Solution{
 public:
-	int tribonacci(int n)
-	{
-		if (n == 0)
-			return 0;
-		else if (n == 1 || n == 2)
-			return 1;
+    int tribonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1 || n == 2)
+            return 1;
 
-		int dp3 = 0;
-		int dp2 = 1;
-		int dp1 = 1;
+        int dp3 = 0;
+        int dp2 = 1;
+        int dp1 = 1;
 
-		for (int i = 4; i <= n; i++)
-		{
-			int dp = dp3 + dp2 + dp1;
+        for (int i = 4; i <= n; i++)
+        {
+            int dp = dp3 + dp2 + dp1;
 
-			dp3 = dp2;
-			dp2 = dp1;
-			dp1 = dp;
-		}
+            dp3 = dp2;
+            dp2 = dp1;
+            dp1 = dp;
+        }
 
-		return dp3 + dp2 + dp1;
-	}
+        return dp3 + dp2 + dp1;
+    }
 };
