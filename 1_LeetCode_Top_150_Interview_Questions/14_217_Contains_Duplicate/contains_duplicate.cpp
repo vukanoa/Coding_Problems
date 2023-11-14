db0b308b707dee6e3a86d5645050aa35ff072984
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-14 11:33:24 +0100

    Add another Implementation in LeetCode: 14_217_Contains_Duplicate to Top_150_Interview

--- 1_LeetCode_Top_150_Interview_Questions/14_217_Contains_Duplicate/contains_duplicate.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/14_217_Contains_Duplicate/contains_duplicate.cpp
@@ -2,60 +2,60 @@
 #include <unordered_set>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	===========================
-	217) Contains Duplicate
-	===========================
+    ===========================
+    217) Contains Duplicate
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given an integer array nums, return true if any value appears at least
-	twice in the array, and return false if every element is distinct.
+    Given an integer array nums, return true if any value appears at least
+    twice in the array, and return false if every element is distinct.
 
-	====================================================
-	FUNCTION: bool containsDuplicate(vector<int>& nums); 
-	====================================================
+    ====================================================
+    FUNCTION: bool containsDuplicate(vector<int>& nums);
+    ====================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: nums = [1, 2, 3, 1]
-	Output: true
+    --- Example 1 ---
+    Input: nums = [1, 2, 3, 1]
+    Output: true
 
-	--- Example 2 ---
-	Input: nums = [1, 2, 3, 4]
-	Output: false
+    --- Example 2 ---
+    Input: nums = [1, 2, 3, 4]
+    Output: false
 
-	--- Example 3 ---
-	Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
-	Output: true
+    --- Example 3 ---
+    Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+    Output: true
 
-	*** Constraints ***
-	1 <= nums.length <= 105
-	-109 <= nums[i] <= 109
+    *** Constraints ***
+    1 <= nums.length <= 105
+    -109 <= nums[i] <= 109
 
 */
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Iterate through the array and check if that number was already visited.
-	How are we going to do that?
+    Iterate through the array and check if that number was already visited.
+    How are we going to do that?
 
-	By putting the number in a Hash Set if it doesn't exist already.
+    By putting the number in a Hash Set if it doesn't exist already.
 
-	If it does exist, then we're done - return true.
-	
+    If it does exist, then we're done - return true.
+
 */
 
 /* Time  Beats: 79.53% */
@@ -66,17 +66,43 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums)
-	{
-		std::unordered_set<int> uset;
+    {
+        std::unordered_set<int> uset;
 
-		for (int& num : nums)
-		{
-			if (uset.find(num) != uset.end())
-				return true;
+        for (int& num : nums)
+        {
+            if (uset.find(num) != uset.end())
+                return true;
 
-			uset.insert(num);
-		}
+            uset.insert(num);
+        }
 
-		return false;
+        return false;
+    }
+};
+
+
+
+
+/* Time  Beats: 99.28% */
+/* Space Beats: 71.44% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_Faster {
+public:
+    bool containsDuplicate(vector<int>& nums)
+    {
+        std::unordered_set<int> uset;
+
+        for (int& num : nums)
+        {
+            if (uset.count(num) == 0)
+                uset.insert(num);
+            else
+                return true;
+        }
+
+        return false;
     }
 };
