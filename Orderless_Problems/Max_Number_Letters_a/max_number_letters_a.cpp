db7c0c3075049b0c8aecaa8fae2466a0e5f1d20e
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-19 14:43:28 +0100

    Exchange Solution for an Improved one in: Max_Number_Letters_a in Orderless_Problems

--- Orderless_Problems/Max_Number_Letters_a/max_number_letters_a.cpp
+++ Orderless_Problems/Max_Number_Letters_a/max_number_letters_a.cpp
@@ -1,62 +1,35 @@
 #include <iostream>
 
-class Solution{
+/* Time  Complexity: O(1 * n) --> O(n) */ // Single pass
+/* Space Complexity: O(1)              */
+class Solution {
 public:
-	int solution(std::string& s)
-	{
-		int left = 0;
+    int solution(std::string& S)
+    {
+        int N = S.length();
+        if (N == 1) return S[0] == 'a' ? 1 : 4;
 
-		// Find leftmost non-a letter
-		while (left < s.length())
-		{
-			if (left == 3)
-				return -1;
+        int can_be_inserted = 0;
+        for (int i = 0; i < N; i++)
+        {
+            int a = 0;
+            while (i < S.length() && S[i] == 'a' && a < 3){
+                a++;
+                i++;
+            }
 
-			if (s[left] != 'a')
-				break;
+            if      (a == 3) return -1;
+            else if (i == N) break;
 
-			left++;
-		}
+            if      (i-1 < 0 || S[i-1] != 'a') can_be_inserted += 2;
+            else if (i-2 < 0 || S[i-2] != 'a') can_be_inserted += 1;
+        }
 
-		int count = 0;
+        if      (S[N-1] != 'a') can_be_inserted += 2;
+        else if (S[N-2] != 'a') can_be_inserted += 1;
 
-		if (left == s.length()) // We haven't found non-a letter
-		{
-			if (left == 3)
-				return -1;
-			else if (left == 2)
-				return 0;
-			else
-				return 1;
-		}
-		else if (left < 2) // left: 1 => "ab.." -> add 1   |  left: 0 => "b.." -> add 2
-			count += 2 - left; // Takes care of the front
-
-		int right = left + 1;
-
-		// Sliding Window
-		while (right < s.length())
-		{
-			if (right - left > 3)
-				return -1; // substring "aaa"
-
-			if (s[right] == 'a')
-				right++;
-			else
-			{
-				// 2 -> max permissible nubmer of consecuive a's
-				count += 2 - (right - left - 1);
-
-				left  = right;
-				right = right + 1;
-			}
-		}
-
-		// Takes care of the ending
-		count += 2 - (right - left - 1);
-
-		return count;
-	}
+        return can_be_inserted;
+    }
 };
 
 
@@ -66,7 +39,7 @@ main()
 	Solution sol;
 
 	/* Example 1 */
-	std::string s = "aabab";
+	// std::string s = "aabab";
 
 	/* Example 2 */
 	// std::string s = "dog";
@@ -95,20 +68,38 @@ main()
 	/* Example 10 */
 	// std::string s = "aaa";
 
+	/* Example 11 */
+    // std::string s = "cac";
+
+	/* Example 12 */
+    std::string s = "acca";
+
+	/* Example 13 */
+    // std::string s = "aacca";
+
+	/* Example 14 */
+    // std::string s = "accaa";
+
+	/* Example 15 */
+    // std::string s = "aaccaa";
+
+	/* Example 16 */
+    // std::string s = "aacacaa";
+
+
 	std::cout << "\n\t=============================";
 	std::cout << "\n\t=== MAX NUMBERS LETTERS A ===";
 	std::cout << "\n\t=============================\n\n";
 
 
 	/* Write Input */
-	std::cout << "\n\tInput: " << s << "\n";
+	std::cout << "\n\tInput: \"" << s << "\"\n";
 
 	/* Solution */
-	int count = sol.solution(s);
+	int count   = sol.solution(s);
 
 	/* Write Output */
 	std::cout << "\n\tOut: " << count << "\n\n";
 
 	return 0;
 }
-
