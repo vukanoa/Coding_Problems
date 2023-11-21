18133c15135e2ecdecc4c40d83c1af92930d9b5c
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-21 11:55:30 +0100

    Add LeetCode: 450_Delete_Node_In_A_BST to N_539

---
+++ 4_N_539/Trees/450_Delete_Node_In_A_BST/delete_node_in_a_bst.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===========================
+    450) Delete Node In a BST
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    Given a root node reference of a BST and a key, delete the node with the
+    given key in the BST. Return the root node reference (possibly updated) of
+    the BST.
+
+    Basically, the deletion can be divided into two stages:
+
+        1. Search for a node to remove.
+        2. If the node is found, delete the node.
+
+
+    Follow up: Could you solve it with time complexity O(height of tree)?
+
+    ========================================================
+    FUNCTION: TreeNode* deleteNode(TreeNode* root, int key);
+    ========================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: root = [5,3,6,2,4,null,7], key = 3
+    Output: [5,4,6,2,null,null,7]
+    Explanation: Given key to delete is 3. So we find the node with value 3 and
+    delete it.  One valid answer is [5,4,6,2,null,null,7], shown in the above
+    BST.  Please notice that another valid answer is [5,2,6,null,4,null,7] and
+    it's also accepted.
+
+
+    --- Example 2 ---
+    Input: root = [5,3,6,2,4,null,7], key = 0
+    Output: [5,3,6,2,4,null,7]
+    Explanation: The tree does not contain a node with value = 0.
+
+
+    --- Example 3 ---
+    Input: root = [], key = 0
+    Output: []
+
+
+    *** Constraints ***
+    The number of nodes in the tree is in the range [0, 10^4].
+    -10^5 <= Node.val <= 10^5
+    Each node has a unique value.
+    root is a valid binary search tree.
+    -10^5 <= key <= 10^5
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/* Time  Beats: 93.89% */
+/* Space Beats: 61.18% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key)
+    {
+        if (!root)
+            return nullptr;
+
+        if (key > root->val)
+            root->right = deleteNode(root->right, key);
+        else if (key < root->val)
+            root->left  = deleteNode(root->left,  key);
+        else
+        {
+            if (!root->left)
+                return root->right;
+            else if (!root->right)
+                return root->left;
+
+            /* Find min from the right Subtree */
+            TreeNode* curr = root->right;
+            while (curr->left)
+                curr = curr->left;
+
+            root->val = curr->val;
+            root->right = deleteNode(root->right, root->val);
+        }
+
+        return root;
+    }
+};
