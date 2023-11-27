632e7453f7dd7b0d88f431d198b9c359c7d92243
Noa Vukasovic <noavukasovic@gmail.com>
2023-11-27 16:09:18 +0100

    Add 2 more Solutions to LeetCode: 1509_Difference_Between_Largest_And_Smallest_Value_In_Three_Moves to Orderless_Problems

--- Orderless_Problems/1509_Minimum_Difference_Between_Largest_And_Smallest_Value_In_Three_Moves/minimum_difference_between_largest_and_smallest_value_in_three_moves.cpp
+++ Orderless_Problems/1509_Minimum_Difference_Between_Largest_And_Smallest_Value_In_Three_Moves/minimum_difference_between_largest_and_smallest_value_in_three_moves.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
+#include <climits>
 
 /*
     ==============
@@ -70,6 +72,25 @@
 
 */
 
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    There are only 4 possible scenarios that can happen once we know what is:
+        1. Smallest value
+        2. Second smallest value
+        3. Second largest value
+        4. Largest value
+
+    And to know these 4 things in O(1), we can sort.
+
+    (Although, we can, indeed, just traverse it multiple times, since that'd
+    still be constant since we won't traverse is more than 3*4 times. That
+    Solution is down below).
+
+*/
+
 /* Time  Beats: 91.16% */
 /* Space Beats: 97.48% */
 
@@ -91,5 +112,131 @@ public:
         int res4 = std::abs(nums[2] - nums[n-2]);
 
         return std::min(std::min(res1, res2), std::min(res3, res4));
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    The same as above, however, implemented using a for loop, instead of
+    writing all 4 cases explicitly.
+
+*/
+
+/* Time  Beats: 80.50% */
+/* Space Beats: 97.48% */
+
+/* Time  Complexity: O(n * logn) */
+/* Space Complexity: O(n) */
+class Solution_For_Loop {
+public:
+    int minDifference(std::vector<int>& nums)
+    {
+        if(nums.size() <= 4)
+            return 0;
+
+        std::sort(nums.begin(), nums.end());
+        int min = INT_MAX;
+
+        int n = nums.size();
+        for(int i = 0; i < 4; i++)
+        {
+            // We need to test 4 cases.
+            int diff = nums[n - 4 + i] - nums[i];
+
+            min = std::min(min, diff);
+        }
+
+        return min;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This one runs in O(n) time since log4 is a Constant, therefore:
+        O(n * log4) becomes just O(n).
+
+    We could've also iterate each of the three times to find all 4 values.
+    That would also be O(n), however, using Heap is a bit more efficient.
+    Not much, though.
+
+*/
+
+/* Time  Beats: 99.30% */
+/* Space Beats: 63.96% */
+
+/* Time  Complexity: O(n)    */ // O(n * log4) -> O(n)
+/* Space Complexity: O(n) */
+class Solution_O_of_N {
+public:
+    int minDifference(std::vector<int>& nums)
+    {
+        if(nums.size() <= 4)
+            return 0;
+
+        // Top 4 smallest elements -> max_heap
+        // Top 4 biggest  elements -> min_heap
+
+        std::priority_queue<int, std::vector<int>> max_heap;
+        std::priority_queue<int, std::vector<int>, std::greater<int>> min_heap;
+
+        for(int i = 0; i < 4; i++)
+        {
+            max_heap.push(nums[i]);
+            min_heap.push(nums[i]);
+        }
+
+        for(int i = 4; i < nums.size(); i++)
+        {
+            if(max_heap.top() > nums[i])
+            {
+               max_heap.pop();
+               max_heap.push(nums[i]);
+            }
+
+            if(min_heap.top() < nums[i])
+            {
+               min_heap.pop();
+               min_heap.push(nums[i]);
+            }
+        }
+
+        // Get small_4 and large_4
+        std::vector<int> small_4(4);
+        std::vector<int> large_4(4);
+
+        for(int i = 0; i < 4; i++)
+        {
+            small_4[i] = max_heap.top();
+            large_4[i] = min_heap.top();
+
+            max_heap.pop();
+            min_heap.pop();
+        }
+
+        int min = INT_MAX;
+
+        for(int i = 0; i < 4; i++)
+        {
+            int small_value = small_4[3-i];
+            int large_value = large_4[i];
+
+            int diff = large_value - small_value;
+
+            min = std::min(min, diff);
+        }
+
+        return min;
     }
 };
