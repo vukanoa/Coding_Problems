72437f011805552e15e38a6533a45e6e3a91e110
Noa Vukasovic <noavukasovic@gmail.com>
2023-12-02 14:30:11 +0100

    Add LeetCode: 1732_Find_The_Highest_Altitude to Orderless_Problems

---
+++ Orderless_Problems/1732_Find_The_Highest_Altitude/find_the_highest_altitude.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <vector>
+
+/*
+    ============
+    === EASY ===
+    ============
+
+    ===============================
+    1732) Find The Highest Altitude
+    ===============================
+
+    ============
+    Description:
+    ============
+
+    There is a biker going on a road trip. The road trip consists of n + 1
+    points at different altitudes. The biker starts his trip on point 0 with
+    altitude equal 0.
+
+    You are given an integer array gain of length n where gain[i] is the net
+    gain in altitude between points i and i + 1 for all (0 <= i < n). Return
+    the highest altitude of a point.
+
+    =================================================
+    FUNCTION: int largestAltitude(vector<int>& gain);
+    =================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: gain = [-5,1,5,0,-7]
+    Output: 1
+    Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
+
+
+    --- Example 2 ---
+    Input: gain = [-4,-3,-2,-1,4,3,2]
+    Output: 0
+    Explanation: The altitudes are [0,-4,-7,-9,-10,-6,-3,-1]. The highest is 0.
+
+
+    *** Constraints ***
+    n == gain.length
+    1 <= n <= 100
+    -100 <= gain[i] <= 100
+
+*/
+
+/* Time  Beats:  100% */
+/* Space Beats: 69.28% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int largestAltitude(std::vector<int>& gain)
+    {
+        if (gain.size() == 1)
+            return gain[0] < 0 ? 0 : gain[0];
+
+        int highest = 0;
+        int curr_height = 0;
+
+        for (int i = 0; i < gain.size(); i++)
+        {
+            curr_height += gain[i];
+
+            highest = std::max(highest, curr_height);
+        }
+
+        return highest;
+    }
+};
