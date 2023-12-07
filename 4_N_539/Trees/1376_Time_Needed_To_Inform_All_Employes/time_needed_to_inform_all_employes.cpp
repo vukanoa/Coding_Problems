c34f8cf6d6afe22eb4116664fd940c8bdfe8ef39
Noa Vukasovic <noavukasovic@gmail.com>
2023-12-07 09:35:36 +0100

    Add LeetCode: 1376_Time_Needed_To_Inform_All_Employes to N_539

---
+++ 4_N_539/Trees/1376_Time_Needed_To_Inform_All_Employes/time_needed_to_inform_all_employes.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <vector>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    =========================================
+    1376) Time Needed to Inform All Employees
+    =========================================
+
+    ============
+    Description:
+    ============
+
+    A company has n employees with a unique ID for each employee from 0 to n -
+    1. The head of the company is the one with headID.
+
+    Each employee has one direct manager given in the manager array where
+    manager[i] is the direct manager of the i-th employee, manager[headID] =
+    -1. Also, it is guaranteed that the subordination relationships have a tree
+    structure.
+
+    The head of the company wants to inform all the company employees of an
+    urgent piece of news. He will inform his direct subordinates, and they will
+    inform their subordinates, and so on until all employees know about the
+    urgent news.
+
+    The i-th employee needs informTime[i] minutes to inform all of his direct
+    subordinates (i.e., After informTime[i] minutes, all his direct
+    subordinates can start spreading the news).
+
+    Return the number of minutes needed to inform all the employees about the
+    urgent news.
+
+    =============================================================================================
+    FUNCTION: int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime);
+    =============================================================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: n = 1, headID = 0, manager = [-1], informTime = [0]
+    Output: 0
+    Explanation: The head of the company is the only employee in the company.
+
+
+
+    --- Example 2 ---
+    Input: n = 6,
+           headID = 2,
+           manager = [2,2,-1,2,2,2],
+           informTime = [0,0,1,0,0,0]
+
+    Output: 1
+    Explanation: The head of the company with id = 2 is the direct manager of
+                 all the employees in the company and needs 1 minute to inform
+                 them all.  The tree structure of the employees in the company
+                 is shown.
+
+
+
+
+    *** Constraints ***
+    1 <= n <= 10^5
+    0 <= headID < n
+    manager.length == n
+    0 <= manager[i] < n
+    manager[headID] == -1
+    informTime.length == n
+    0 <= informTime[i] <= 1000
+    informTime[i] == 0 if employee i has no subordinates.
+    It is guaranteed that all the employees can be informed.
+
+*/
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 28.62% */
+/* Space Beats:  9.49% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_BFS {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime)
+    {
+        std::unordered_map<int, std::vector<int>> adj_list;
+
+        // Make Adjacency List
+        for (int i = 0; i < n; i++)
+            adj_list[manager[i]].push_back(i);
+
+        std::queue<std::pair<int, int>> queue;
+        queue.push( {headID, 0} );
+
+        int result = 0;
+
+        while (!queue.empty())
+        {
+            int id   = queue.front().first;
+            int time = queue.front().second;
+            queue.pop();
+
+            result = std::max(result, time);
+
+            for (int& employee : adj_list[id])
+                queue.push( {employee, time + informTime[id]} );
+        }
+
+        return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+    Implemented using DFS.
+
+*/
+
+/* Time  Beats: 29.12% */
+/* Space Beats: 20.41% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_DFS {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime)
+    {
+        std::unordered_map<int, std::vector<int>> adj_list;
+
+        // Make Adjacency List
+        for (int i = 0; i < n; i++)
+            adj_list[manager[i]].push_back(i);
+
+        int result = 0;
+        dfs(headID, 0, informTime, adj_list, result);
+
+        return result;
+    }
+
+private:
+    void dfs(int id, int time, std::vector<int>& informTime, std::unordered_map<int, std::vector<int>>& adj_list, int &result)
+    {
+        time += informTime[id];
+
+        result = max(result, time);
+
+        for(auto& i : adj_list[id])
+            dfs(i, time, informTime, adj_list, result);
+    }
+};
