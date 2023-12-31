df4e9df0c1287959e528b182385f6c3e80703f6a
Noa Vukasovic <noavukasovic@gmail.com>
2023-12-31 03:54:35 +0100

    Add Explanation(IDEA) to LeetCode: 673_Number_Of_Longest_Increasing_Subsequence in N_539

--- 4_N_539/DP_1-D/673_Number_Of_Longest_Increasing_Subsequence/number_of_longest_increasing_subsequence.cpp
+++ 4_N_539/DP_1-D/673_Number_Of_Longest_Increasing_Subsequence/number_of_longest_increasing_subsequence.cpp
@@ -52,7 +52,89 @@
     --- IDEA ---
     ------------
 
-    TODO
+                       +-----+-----+-----+-----+-----+-----+
+                 dp =  |     |     |     |     |     | 1:1 |
+                       +-----+-----+-----+-----+-----+-----+
+                          0     1     2     3     4     5
+
+                          |     |     |     |     |     |
+                          |     |     |     |     |     |
+                          v     v     v     v     v     v
+
+                       +-----+-----+-----+-----+-----+-----+
+    longest increasing |     |     |     |     |     |  1  |
+                       +-----+-----+-----+-----+-----+-----+
+                 count |     |     |     |     |     |  1  |
+                       +-----+-----+-----+-----+-----+-----+
+                          0     1     2     3     4     5
+
+    You must solve LIS(Longest Increasing Subsequence) Problem first. It's from
+    the Blind75 list.
+
+    This explanation assumes you know know LIS is supposed to be solved.
+
+    Since we're not just asked to find the LIS, but the total number of
+    different sequences that have the same, but maximum, strictly increasing
+    subsequence length.
+
+    So in order to solve this, at each outer iteration, we'll have two inner
+    iterations.
+
+    Why?
+
+    The first one to determine the length of the longest increasing subsequence
+    without including the starting, current, position.
+
+    Then, we want to count how many subsequences, of that length, can be found
+    from i+1th position until the end of the array.
+
+    Since we are told the subsequence must be strictly increasing we always
+    have to check if(nums[i] < nums[j]) is true.
+
+    Once we've found the length of the longest increasing subsequence from i+1
+    position until the end of the array, now we have to count how many such
+    subsequences exist from i+1th position until the end of the array.
+
+    However, it would be a mistake if we increased the counter by one if we
+    stumble upon a position from which the subsequence is equal to the length
+    of the longest subsequence until the end of the array.
+
+    Why?
+
+    Because it may be true that from the position, until the end of the array,
+    it's true that we can have multiple increasing subsequences and we must
+    take into account every single unique subsequence.
+
+    Therefore, instead of doing "count++", we should do count += dp[j].second
+    which represents the number of subsequences to achieve dp[i].first length.
+
+    The "flag" variable is of use for cases where we don't increase the length
+    of the longest subsequence.
+
+    One such example is already given:
+        nums = [2, 2, 2, 2, 2]
+
+    If we were not to have this "flag" variable, we'd make a mistake since we'd
+    count subsequences of "1" multiples times since they are the longest,
+    however that's the default value and we shall not consider it.
+
+    At the end of the outer loop, we must keep track of the total/global max
+    length of the longest increasing subsequence.
+
+    In the very first example, our result happens to be in the dp[0], however
+    that does not necessarily have to be the case, thus, we have to know what
+    that max length is and then, after finisihing the main part of this
+    solution, we shall count how many starting positions are beginnings of
+    unique longest increasing subsequences.
+
+    However, again, we should not only "result++" those starting positions(i.e.
+    we should not just count them), instead we should add all the dp[x].second
+    values since each dp[x].second value represents a unique subsequence.
+
+    if dp[x].first is equal to max_longest_increasing then take dp[x].second
+    into acount by adding it to the the result variable.
+
+    AT the end return result.
 
 */
 
@@ -132,7 +214,7 @@ public:
 
 /* Time  Complexity: O(n^2) */
 /* Space Complexity: O(n)   */
-class Solution {
+class Solution_Neat {
 public:
     int findNumberOfLIS(vector<int>& nums)
     {
