d14f07ce92ed0ee31b254b80202a25c1d991bd2a
Noa Vukasovic <noavukasovic@gmail.com>
2024-01-11 22:09:59 +0100

    Remove old Solution from LeetCode: 219_Contains_Duplicate_II in N_539

--- 4_N_539/Sliding_Window/219_Contains_Duplicate_II/contains_duplicate_ii.cpp
+++ 4_N_539/Sliding_Window/219_Contains_Duplicate_II/contains_duplicate_ii.cpp
@@ -48,64 +48,12 @@
 
 */
 
-
-/* Time  Beats: 70.77% */
-/* Space Beats: 57.79% */
-class Solution_Sliding_Window {
-public:
-    bool containsNearbyDuplicate(std::vector<int>& nums, int k)
-    {
-        if (nums.size() == 1 || k == 0)
-            return false;
-
-        std::unordered_map<int, int> umap;
-
-        for (int i = 0; i < k && i < nums.size(); i++)
-        {
-            if (umap.count(nums[i]))
-                return true;
-
-            umap[nums[i]]++;
-        }
-
-
-        int left  = 0;
-        int right = k;
-
-        while (right < nums.size())
-        {
-            umap[nums[right]]++;
-
-            if (umap[nums[left]] > 1)
-                return true;
-
-            umap[nums[left]]--;
-            left++;
-            right++;
-        }
-
-        while (left < nums.size())
-        {
-            if (umap[nums[left]] > 1)
-                return true;
-
-            umap[nums[left]]--;
-            left++;
-        }
-
-        return false;
-    }
-};
-
-
-
-
 /* Time  Beats: 71.54% */
 /* Space Beats: 38.06% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(n) */
-class Solution_Sliding_Window_Concise {
+class Solution_Hash_Map {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k)
     {
@@ -134,7 +82,10 @@ public:
 
 /* Time  Beats: 72.17% */
 /* Space Beats: 97.00% */
-class Solution_Sliding_Window_USet{
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_Hash_Set_1 {
 public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k)
     {
@@ -176,7 +127,7 @@ public:
     It says:"... array such that ... and abs(i - j) <= k"
 
     Since "abs(i - j)" is a range(window), it is at least possible that this
-    may, in fact, be the Sliding Windows problem, so let's check if we can
+    may, in fact, be the Sliding Windows problem. So let's check if we can
     indeed solve it in an efficient manner using this technique.
 
     What they are asking us to do is to find 2 **distinct** elements, within
@@ -189,38 +140,44 @@ public:
         i = 5
         j = 7
 
-    is considered valid, but you can clearly see that it is consisted of three
-    elements, at indices: 5, 6 and 7.
+    is considered valid, but you can clearly see that it is consisted not of 2,
+    but of 3 elements, at indices: 5, 6 and 7.
     Therefore the length of the window is not 'k', but "k + 1".
 
     So, since we have to "move the window" in order to check all the elements,
-    we only need to consider the elements that are currently in the window.
+    we only need to consider the elements that are currently within the window.
 
     Also, it's important to note that we don't necessarily have to check every
     single element in the array, as we may satisfy the given condition and
-    return true before we check each and every element.
+    return true before we reach the end.
 
     As we've said - We have to "move the window" and check if there are two of
-    the same values within that range(window), if they are, we immediately stop
-    by returning true.
+    the same values within that range(window) and if there are we immediately
+    stop by returning true.
 
     However, if we do not find two of the same values within the current window
-    then we have to add the next element to the windows. But, here is the crux
+    then we have to add the next element to the window. But, here is the crux
     of this problem, we have to check if by adding one more element we'll
     exceed the limit length of our "window" which is, as we've concluded, k+1.
 
-    If adding another element won't exceed the limit of the window(as we may
-    still not have filled the entire window) then just add it to the current
-    window.
+    If adding another element won't exceed the limit length of the window(as we
+    may still not have filled the entire window) then immediately check if
+    current element is present within the current window.
+    If it is:
+        return true;
+    else:
+        just add it to the current window.
 
-    However, it by adding this element we'll exceed the given limit length of
-    window, then we have to pop one. But, which one?
+    However, if by adding this element we'll exceed the given limit length of
+    a window, then we have to pop one. But, which one?
 
     The one that is the "oldest" in the window, meaning the one that got into
-    this current window first. How can we determine that?
+    this current window first.
 
-    If we push element from left to right, one by one, then the element that is
-    "the oldest", or the one that got into this current window first, is the
+    How can we determine that?
+
+    If we push elements from left to right, one by one, then the element that
+    is "the oldest", or the one that got into this current window first, is the
     leftmost one within the given range(window).
 
     Now that we understand the crux of this Solution, we have to ask ourselves
@@ -228,15 +185,16 @@ public:
     current window?
 
     Well, we can, obviously, iterate and check if certain element is present,
-    However that itself has the Time Complexity of O(n), which, in total, would
-    give an O(n^2) which is considered inefficient for this problem.
+    however that itself has the Time Complexity of O(n). That means that in
+    total, this whole Solution would be O(n^2) which is considered inefficient
+    for this problem.
 
     Is there a way to check if a certain element is present within the current
     window in O(1)?
 
     Yes it is! By using a Hash Set.
 
-    We are going to push element, one by one, and if we find out that the one
+    We are going to push elements, one by one, and if we find out that the one
     we're currently on is out of bound of the current window, then at that
     point we evict the leftmost element in the current window and then check
     if this element that we're on is already present within the window(since
@@ -245,7 +203,7 @@ public:
     If it is present, we've satisfied both conditions and thus return true.
 
     If we find out that by adding the current element we would not exceed the
-    limit length of our window and thus can immediately proceed to check if the
+    limit length of our window then we can immediately proceed to check if the
     current element is present within the window.
 
     If it is present, as above, we've satisfied both conditions and thus return
@@ -264,7 +222,7 @@ public:
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(n) */
-class Solution {
+class Solution_Hash_Set_2 {
 public:
     bool containsNearbyDuplicate(std::vector<int>& nums, int k)
     {
