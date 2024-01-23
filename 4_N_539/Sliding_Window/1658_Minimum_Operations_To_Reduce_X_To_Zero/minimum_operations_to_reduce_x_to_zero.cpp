111542424027e9e5b4e314ae9abfdc4fc7329833
Noa Vukasovic <noavukasovic@gmail.com>
2024-01-23 15:24:22 +0100

    Add Explanation(IDEA) & Simulation to LeetCode: 1658_Minimum_Operations_To_Reduce_X_To_Zero to N_539

--- 4_N_539/Sliding_Window/1658_Minimum_Operations_To_Reduce_X_To_Zero/minimum_operations_to_reduce_x_to_zero.cpp
+++ 4_N_539/Sliding_Window/1658_Minimum_Operations_To_Reduce_X_To_Zero/minimum_operations_to_reduce_x_to_zero.cpp
@@ -62,19 +62,221 @@
     --- IDEA ---
     ------------
 
-    Think of it in a reverse sense.
+    This is one of those problems where they are deliberately trying to trick
+    us with the Description.
+
+    If you were to think about this problem in the way they are describing it,
+    you'd quickly realize it's almost impossible to do it in an efficient way.
+
+    What are they asking us to find?
+        1. Minimum Operations(removals)
+        2. Either remove the leftmost or the rightmost element from the array
+
+    Instead, how you should think about this problem:
+        1. MAXIMUM elements that are NOT the result
+        2. Elements that are consecutive and:
+           a) That are NOT on the edges
+            OR
+           b) That are at most connected to one edge
+
+    So we want to get the biggest consecutive window of elements that are NOT
+    the result, so that the remaining elements sum up to x.
+
+    By MAXIMIZING our NON-RESULT elements, we are MINIMIZING the result
+    elements.
+
+    However, instead of finding different length windows that when subtracted
+    from the total sum of the array and taking the biggest one, we can only
+    search for a window that sums up to (total_sum - x) instead.
+
+    Therefore, we'll use a Sliding Window technique.
+
+*******************************************************************************
+********************************** SIMULATION *********************************
+*******************************************************************************
 
     This is a good example to show how difficult it would be if we were to do
     it in a way they're describing.
 
-    nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+    You can try to think of a Solution to solve it in a way they've described,
+    but I'm going to simulate my Solution.
 
-    TODO
+
+                    nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+
+                    total_sum = 29
+                    desired_middle_sum = total_sum - x ==> 29 - 7 => 22
+
+                    curr_sum = 0
+
+-------------------------------------------------------------------------------
+
+    1.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L
+                R
+
+        curr_sum = 3
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            /
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    2.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L  R
+
+        curr_sum = 5
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            /
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    3.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L     R
+
+        curr_sum = 7
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            /
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    4.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L        R
+
+        curr_sum = 16
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            /
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    5.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L           R
+
+        curr_sum = 22
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            /
+
+        if (curr_sum == desired_middle_sum) // IT is INDEED
+            max_window = max(max_window, R - L + 1) ==> max(0, 5) ==> 5;
+
+-------------------------------------------------------------------------------
+
+    6.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L              R
+
+        curr_sum = 23
+
+        if (curr_sum > desired_middle_sum) // IT is INDEED
+            curr_sum -= nums[L]
+            L++
+
+
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                L              R
+
+        curr_sum = 20
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    7.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                   L              R
+
+        curr_sum = 21
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            /
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    8.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                   L                 R
+
+        curr_sum = 23
+
+        if (curr_sum > desired_middle_sum) // It is NOT
+            curr_sum -= nums[L]
+            L++
+
+
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                      L              R
+
+        curr_sum = 21
+
+        if (curr_sum == desired_middle_sum) // It is NOT
+            /
+
+-------------------------------------------------------------------------------
+
+    9.
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                      L                 R
+
+        curr_sum = 24
+
+        if (curr_sum > desired_middle_sum) // IT is INDEED
+            curr_sum -= nums[L]
+            L++
+
+        nums = [3, 2, 2, 9, 6, 1, 1, 2, 3],  x = 7
+                0  1  2  3  4  5  6  7  8
+                      L                 R
+
+
+        curr_sum = 22
+
+        if (curr_sum == desired_middle_sum) // IT is INDEED
+            max_window = max(max_window, R - L + 1) ==> max(5, 7) ==> 7;
+
+
+    return max_window; // 7
+-------------------------------------------------------------------------------
 
 */
 
-/* Time  Beats: 66.58% */
-/* Space Beats: 84.75% */
+/* Time  Beats: 99.23% */
+/* Space Beats: 34.13% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
@@ -82,11 +284,11 @@ class Solution {
 public:
     int minOperations(std::vector<int>& nums, int x)
     {
-        int sum = 0;
+        int total_sum = 0;
         for (int& num : nums)
-            sum += num;
+            total_sum += num;
 
-        int target   = sum - x;
+        int desired_middle_sum = total_sum - x;
         int curr_sum = 0;
 
         int left  = 0;
@@ -97,10 +299,10 @@ public:
         {
             curr_sum += nums[right];
 
-            while (left <= right && curr_sum > target)
+            while (left <= right && curr_sum > desired_middle_sum)
                 curr_sum -= nums[left++];
 
-            if (curr_sum == target)
+            if (curr_sum == desired_middle_sum)
                 max_window = std::max(max_window, right - left + 1);
 
             right++;
