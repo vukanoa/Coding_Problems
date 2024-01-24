bb6a222a79426ba2b45759cc8bca58ddd831e1c2
Noa Vukasovic <noavukasovic@gmail.com>
2024-01-24 11:02:38 +0100

    Add another Solution to LeetCode: 313_Burst_Balloons to N_539

--- 4_N_539/DP_2-D/313_Burst_Balloons/burst_balloons.cpp
+++ 4_N_539/DP_2-D/313_Burst_Balloons/burst_balloons.cpp
@@ -120,6 +120,102 @@ private:
 
 */
 
+/* Time  Beats: 85.70% */
+/* Space Beats: 83.79% */
+
+/* Time  Complexity: O(n^3) */
+/* Space Complexity: O(n^2) */
+class Solution {
+public:
+    int n;
+
+    int maxCoins(std::vector<int>& nums)
+    {
+        int n = nums.size();
+        std::vector<std::vector<int>> dp(n, std::vector<int>(n, -1));
+
+        // Insert two dummy balloons of value 1 to handle the balloons on the corner.
+        std::vector<int> extended_nums;
+
+        extended_nums.push_back(1);
+        extended_nums.insert(extended_nums.end(), nums.begin(), nums.end());
+        extended_nums.push_back(1);
+        n +=2 ;
+
+        //Start from i = 1 and j = arr.size() - 2 since first and last balloons are dummy.
+        return solve(extended_nums, dp, 1, n-2);
+    }
+
+private:
+    int solve(std::vector<int> &nums,
+              std::vector<std::vector<int>>& dp,
+              int i,
+              int j)
+    {
+        if(i > j)
+            return 0;
+
+        if(i == j) // Only one element exists
+        {
+            int tmp = nums[i];
+
+            if(i-1 >= 0)
+                tmp *= nums[i-1];
+
+            if(i+1 < n)
+                tmp *= nums[i+1];
+
+            return tmp;
+        }
+
+        // Check if the solution is already stored for this subproblem
+        if(dp[i][j] != -1)
+            return dp[i][j];
+
+        int result = 0;
+
+        // For all elements in the range i to j, we choose all of them one by
+        // one to make them the last balloon to be burst.
+        for(int k = i; k <= j; k++)
+        {
+            // Burst the kth balloon after bursting (i, k-1) and (k+1, j)
+            // balloons
+            int tmp = nums[k];
+
+            // As balloon j + 1 will become adjacent to k after bursting:
+            // k + 1 to j balloons
+            if(j+1 < n)
+                tmp *= nums[j+1];
+
+            // As balloon i- 1 will become adjacent to k after bursting:
+            // i to k -1 balloons
+            if(i-1 >= 0)
+                tmp *= nums[i-1];
+
+            // Recursively solve the left and right subproblems and add their
+            // contribution
+            tmp += (solve(nums, dp, i, k-1) + solve(nums, dp, k+1, j));
+
+            // If this choice of k yields a better result
+            result = max(result, tmp);
+        }
+
+        return dp[i][j] = result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
 /* Time  Beats: 54.95% */
 /* Space Beats: 53.44% */
 
