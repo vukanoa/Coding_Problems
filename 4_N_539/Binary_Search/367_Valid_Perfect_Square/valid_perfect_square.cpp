e3cadaca9896fc52b6b50bd58609249515ca0e19
Noa Vukasovic <noavukasovic@gmail.com>
2024-01-29 13:16:37 +0100

    Exchange Solutions & Add Explanation(IDEA) to LeetCode: 367_Valid_Perfect_Square to N_539

--- 4_N_539/Binary_Search/367_Valid_Perfect_Square/valid_perfect_square.cpp
+++ 4_N_539/Binary_Search/367_Valid_Perfect_Square/valid_perfect_square.cpp
@@ -50,7 +50,97 @@
     --- IDEA ---
     ------------
 
-    Classic Binary Search problem.
+    "right" pointer is NON-Inclusive.
+
+    Why did we, then, assign it to "num" and not "num + 1"?
+
+    First, it's important to notice that, obviously "num" will never be the
+    answer, meaning if we are finding which number from 1 to "num" when squared
+    gives us "num".
+
+    We cannot square "num" and get "num", right?
+    There is only one edge case where that is possible: "num = 1".
+
+    However we'll take care of that with our final return ternary operator:
+
+        return left == 1 ? true : false;
+
+    Now that "num = 1" is out of the way, ask yourself again - Is it possible
+    to square some number from [2, num](Inclusively) that will give us a result
+    of "num"?
+
+    No, there is not.
+
+
+    Second reason for not initializing our "right" pointer to "num + 1" is
+    because of the "Constraints" of this problem. They clearly say:
+
+        1 <= num <= 2^31 - 1
+
+    So if "num" gets to be exactly 2^31 - 1, then "num + 1" will be an Overflow
+    in an "int" type.
+
+    To overcome that problem, you would think we could write this:
+
+        unsigned left  = 1;
+        unsigned right = num + 1; // This will Overflow again
+
+    Why will this Overflow know when we are assigning it to the "unsigned"
+    which can hold more than "2^31 - 1" POSITIVE values?
+
+    Because the intermediate result is based off of the type you're adding it
+    with. In this case it's type "int" because that is num's type.
+
+    Therfore, we could overcome it like this:
+
+        unsigned left  = 1;
+        unsigned right = num;
+        right++; // Now this will NOT Overflow since "right" is unsigned.
+
+    But to avoid all of this mess, we could just assign it to "num" and not
+    worry since we know that the answer will almost never be even close to
+    "num".(And we've taken care of case "num = 1", remember?)
+
+    Okay, now that is out of the way too.
+    But - Is the Initial value the reason that the "right" pointer is
+    NON-Inclusive?
+
+    Great question!
+    No, that is NOT the reason.
+
+    We could also have a NON-Inclusive value at the beginning for the "left"
+    pointer, but we would still, for this Solution, consider only our "right"
+    pointer that is NON-Inclusive.
+
+    Why?
+
+    Because of the way we're calculating our "mid" pointer.
+
+    There are 2 ways to calculate "mid" pointer:
+        1. Left  leaning:   mid = left + (right - left) / 2;
+        2. Right leaning:   mid = left + std::floor((right - left + 1) / 2);
+
+    You could write "Left leaning" with std::floor as well, it wouldn't make
+    any difference and it's maybe easier to visuallize the difference and
+    remember it:
+        1. Left  leaning:   mid = left + std::floor((right - left    ) / 2);
+        2. Right leaning:   mid = left + std::floor((right - left + 1) / 2);
+
+
+    Since we are using "Left leaning", that means we can decide NOT to include
+    our "right" pointer's value as a possible result.
+
+    That decision is made this way:
+
+            if (mid * mid < num)
+                left  = mid + 1;
+            else
+                right = mid; // Since "mid" is NOT an answer and since "right"
+                             // doesn't point to a value that is a possible
+                             // result, we don't have to move our "right"
+                             // pointer to "mid - 1", but to "mid", since that
+                             // value will not be considered as a potential
+                             // result anymore.
 
 */
 
@@ -63,25 +153,22 @@ class Solution {
 public:
     bool isPerfectSquare(int num)
     {
-        if (num == 1)
-            return true;
-
-        int left  = 0;
+        int left  = 1;
         int right = num;
 
-        while (left < right)
+        while (left < right) // Less than, because "right" is NON-Inclusive
         {
             unsigned long long mid = left + (right - left) / 2;
 
-            if (mid * mid == num) // unsigned long long otherwise overflow
+            if (mid * mid == num) // unsigned long long otherwise Overflow
                 return true;
 
-            if (mid * mid > num)
-                right = mid;
+            if (mid * mid < num)
+                left  = mid + 1;
             else
-                left = mid + 1;
+                right = mid;
         }
 
-        return false;
+        return left == 1 ? true : false;
     }
 };
