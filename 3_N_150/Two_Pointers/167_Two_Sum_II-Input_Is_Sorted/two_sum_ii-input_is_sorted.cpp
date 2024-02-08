acb278b5eda449e69079a6f4c9f9b1a90203b5e4
Noa Vukasovic <noavukasovic@gmail.com>
2024-02-08 14:42:47 +0100

    Exchange Solutions & Add Explanation(IDEA) to LeetCode: 167_Two_Sum_II-Input_Is_Sorted to N_150

--- 3_N_150/Two_Pointers/167_Two_Sum_II-Input_Is_Sorted/two_sum_ii-input_is_sorted.cpp
+++ 3_N_150/Two_Pointers/167_Two_Sum_II-Input_Is_Sorted/two_sum_ii-input_is_sorted.cpp
@@ -2,100 +2,112 @@
 #include <vector>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	=================================
-	167) Two Sum II - Input Is Sorted
-	=================================
+    =================================
+    167) Two Sum II - Input Is Sorted
+    =================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given a 1-indexed array of integers numbers that is already sorted in
-	non-decreasing order, find two numbers such that they add up to a specific
-	target number. Let these two numbers be numbers[index1] and numbers[index2]
-	where 1 <= index1 < index2 < numbers.length.
+    Given a 1-indexed array of integers numbers that is already sorted in
+    non-decreasing order, find two numbers such that they add up to a specific
+    target number. Let these two numbers be numbers[index1] and numbers[index2]
+    where 1 <= index1 < index2 < numbers.length.
 
-	Return the indices of the two numbers, index1 and index2, added by one as
-	an integer array [index1, index2] of length 2.
+    Return the indices of the two numbers, index1 and index2, added by one as
+    an integer array [index1, index2] of length 2.
 
-	The tests are generated such that there is exactly one solution. You may
-	not use the same element twice.
+    The tests are generated such that there is exactly one solution. You may
+    not use the same element twice.
 
-	Your solution must use only constant extra space.
+    Your solution must use only constant extra space.
 
-	===============================================================
-	FUNCTION: vector<int> twoSum(vector<int>& numbers, int target);
-	===============================================================
+    ===============================================================
+    FUNCTION: vector<int> twoSum(vector<int>& numbers, int target);
+    ===============================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: numbers = [2,7,11,15], target = 9
-	Output: [1,2]
-	Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We
-	             return [1, 2].
-
-
-	--- Example 2 ---
-	Input: numbers = [2,3,4], target = 6
-	Output: [1,3]
-	Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We
-	             return [1, 3].
+    --- Example 1 ---
+    Input: numbers = [2,7,11,15], target = 9
+    Output: [1,2]
+    Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We
+                 return [1, 2].
 
 
-	--- Example 3 ---
-	Input: numbers = [-1,0], target = -1
-	Output: [1,2]
-	Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+    --- Example 2 ---
+    Input: numbers = [2,3,4], target = 6
+    Output: [1,3]
+    Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We
+                 return [1, 3].
 
 
-	*** Constraints ***
-	2 <= numbers.length <= 3 * 10^4
-	-1000 <= numbers[i] <= 1000
-	numbers is sorted in non-decreasing order.
-	-1000 <= target <= 1000
-	The tests are generated such that there is exactly one solution.
+    --- Example 3 ---
+    Input: numbers = [-1,0], target = -1
+    Output: [1,2]
+    Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+
+
+    *** Constraints ***
+    2 <= numbers.length <= 3 * 10^4
+    -1000 <= numbers[i] <= 1000
+    numbers is sorted in non-decreasing order.
+    -1000 <= target <= 1000
+    The tests are generated such that there is exactly one solution.
 
 */
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Basic Two Pointer Technique.
+    Since the Input is sorted everytime check:
+        if (nums[left] + nums[right] == target)
+            return {left+1, right+1};
 
+    Otherwise, check if the sum is greater or lower than the current sum.
+
+    If it's greater, then:
+        right--; // Since that will decrease the sum since it's non-decreasing
+                 // order of the Input "numbers"
+    else
+        left++; // This will increase the sum in next iteration since the Input
+                // is sorted in non-decreasing order
 */
 
-/* Time  Beats: 71.26% */
-/* Space Beats: 69.55% */
+/* Time  Beats: 92.91% */
+/* Space Beats: 29.01% */
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
 class Solution {
 public:
-	std::vector<int> twoSum(std::vector<int>& numbers, int target)
-	{
-		int left  = 0;
-		int right = numbers.size() - 1;
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
+        int left  = 0;
+        int right = numbers.size() - 1;
 
-		while (left < right)
-		{
-			if (numbers[left] + numbers[right] == target)
-				return {left+1, right+1}; // Because indexing starts with 1
+        while (left < right)
+        {
+            int sum = numbers[left] + numbers[right];
 
-			if (numbers[left] + numbers[right] > target)
-				right--;
-			else // nums[left] + nums[right] < target
-				left++;
-		}
+            if (sum == target)
+                return {left+1, right+1}; // Because indexing starts with 1
 
-		return {};
-	}
+            if (sum > target)
+                right--;
+            else
+                left++;
+        }
+
+        return {0, 0}; // This will never happen
+    }
 };
