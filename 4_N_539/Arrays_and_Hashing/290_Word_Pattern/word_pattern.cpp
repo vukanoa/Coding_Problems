2008e29e85ccb6563484dc9b2b9a4286926fd9ad
Noa Vukasovic <noavukasovic@gmail.com>
2024-02-17 18:29:39 +0100

    Add another Solution to LeetCode: 290_Word_Pattern to N_539

--- 4_N_539/Arrays_and_Hashing/290_Word_Pattern/word_pattern.cpp
+++ 4_N_539/Arrays_and_Hashing/290_Word_Pattern/word_pattern.cpp
@@ -88,6 +88,7 @@ public:
 
         std::unordered_map<char, std::string> umap;
         std::unordered_set<std::string> uset;
+
         for (int i = 0; i < pattern.length(); i++)
         {
             if (umap.find(pattern[i]) == umap.end()) // Doesn't exist
@@ -102,6 +103,64 @@ public:
             {
                 if (umap.at(pattern[i]) != words[i]) // It would break bijection
                     return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Another way of obtaining a vector of words from a string with single-space
+    separated words.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  32.41% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_2 {
+public:
+    bool wordPattern(string pattern, string s)
+    {
+        std::vector<std::string> words;
+        std::istringstream iss(s);
+
+        /* Tokenize words separated by a single space character */
+        std::string word;
+        while (iss >> word)
+            words.push_back(word);
+
+        /* If they are not of the same length it is certainly not a bijection */
+        if (pattern.length() != words.size())
+            return false;
+
+        std::unordered_map<char, std::string> umap;
+        std::unordered_set<std::string> uset;
+
+        for (int i = 0; i < pattern.length(); i++)
+        {
+            if (umap.find(pattern[i]) != umap.end())
+            {
+                if (umap[pattern[i]] != words[i])
+                    return false;
+            }
+            else
+            {
+                if (uset.count(words[i]))
+                    return false;
+
+                umap.insert({pattern[i], words[i]});
+                uset.insert(words[i]);
             }
         }
 
