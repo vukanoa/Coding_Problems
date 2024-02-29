b0733e4446420f715f289854ed6f3a1be9154a17
Noa Vukasovic <noavukasovic@gmail.com>
2024-02-29 12:41:52 +0100

    Exchange Solutions & Add Detailed Explanation(IDEA) to LeetCode: 456_132_Pattern to N_539

--- 4_N_539/Stack/456_132_Pattern/132_pattern.cpp
+++ 4_N_539/Stack/456_132_Pattern/132_pattern.cpp
@@ -58,9 +58,376 @@
     --- IDEA ---
     ------------
 
-    TODO
+    This one is a bit more difficult than it seems.
 
-    Good Example:
+
+    First of all, DO NOT oversee what is being asked of you here.
+
+            i   <    j    <   k
+
+        nums[i] < nums[k] < nums[j]  // nums[j] is GREATEST!! Not nums[k]!!
+
+
+    So, the element that has the "middle index" is the one that should be the
+    GREATEST.
+
+    Element that has that has the "lowest index" is the one that should be the
+    LOWEST.
+
+    Element that has the the "largest index" is the one that should be the
+    between LOWEST and GREATEST(cannot be equal to either of those).
+
+
+    ---------------------------------------------------------------------------
+
+    If you have never heard of "Monotonic Stack", and to be precise
+    "Monotonicly Decreasing Stack", then you have no chance solving this
+    problem.
+
+    If you have heard of it, it's still unintuitive to come up with this type
+    of solution.
+
+
+    Let's consider one example:
+
+        nums  = [3, 1, 4, 2]
+
+    Since we want it to be DECREASING:
+
+                ------------------------
+        stack = |
+                ------------------------
+
+
+        1. 
+            nums = [3, 1, 4, 2]
+                    i
+
+                    ------------------------
+            stack = | 3                        // Push
+                    ------------------------
+
+        2. 
+            nums = [3, 1, 4, 2]
+                       i
+
+                    ------------------------
+            stack = | 3 | 1                    // Push
+                    ------------------------
+
+
+        3. 
+            nums = [3, 1, 4, 2]
+                          i
+
+                    ------------------------
+            stack = | 3 | 1                     // Pop
+                    ------------------------
+                    ------------------------
+            stack = | 1                         // Pop
+                    ------------------------
+                    ------------------------
+            stack = | 4                         // Push
+                    ------------------------
+
+        4. 
+            nums = [3, 1, 4, 2]
+                             i
+
+                    ------------------------
+            stack = | 4 | 2                     // Push
+                    ------------------------
+
+
+    
+    But, why do we use a "Monotonicly Decreasing Stack"?
+
+    Since we need nums[j](element that has the "middle index" in our
+    subsequence) to be the GREATEST and since we need nums[k](element that has
+    the "largest index" in our subsequence) to be the SECOND largest, then
+    having a "Monotonicly Decreasing Stack" ensures this proparty.
+
+    If there are more than 2 elements on the stack, that means we have
+    certainly have nums[j] and nums[k].
+
+    But how are we going to know if we have found nums[i](element that has the
+    "lowest index" and that is considered LOWEST value out of all three)?
+
+    As you see above, in the 3rd step, we've popped both 1 and 3, because
+    placing 4 on the stack would otherwise violate the "Monotonicly Decreasing
+    Stack" property.
+
+    At the end we do have 2 or more elements on the stack, meaning we have
+    nums[j] and nums[k], however, we DO NOT have nums[i] anymore since we've
+    popped it.
+
+
+    So the absolute crux of this Solution is this:
+
+        If we have nums[j] < nums[k] property fulfilled because we used:
+        "Monotonicly Decreasing Stack", then the only thing we have to know
+        additionally, to be able to solve this, is to know if there are
+        elements LEFT of index j that is LOWER than nums[k].
+
+        [Make sure you read the above paragraph carefully]
+
+        #######################################################################
+            Remember, we are looking for this:
+                i   <    j    <   k
+
+            nums[i] < nums[k] < nums[j]  // nums[j] is GREATEST!! Not nums[k]!!
+        #######################################################################
+
+
+        [?, ?, ...., nums[j], ?, .. ?, nums[k]]
+
+        Our Stack looks like this:
+
+            --------------------------------
+            | nums[j] | nums[k]                 // nums[j] > nums[k]
+            --------------------------------
+
+
+        Before pushing nums[k] on the stack, our Stack looked like this:
+
+            --------------------------------
+            | nums[j]
+            --------------------------------
+
+        Since nums[j] is > nums[k] we have that part done. However, we would
+        like to know if there is an element BEFORE index j, that is LOWER
+        than nums[k].
+
+        Nowhere on our Stack is that information. Even worse, we had that
+        information, but we've popped it to obtain a Decreasing property of our
+        Stack.
+
+        So what should we do?
+        This is the part that is unintuitive:
+
+            We should keep a Stack of pairs! (Or 2 stacks of same size)
+
+        [It's easier to draw two stacks, but consider them a single one with
+         pairs as elements]
+
+                    ----------------   
+              nums  | nums[j]  |
+                    -----------+----   
+           min_left | nums[i]  |     // Min to the left, EXCLUDING current num
+                    ----------------   
+
+
+    Now let's do the same problem, using this Stack with "pairs" as elements:
+
+*******************************************************************************
+********************************** SIMULATION *********************************
+*******************************************************************************
+
+    
+
+        Stack:
+                    ------------------------
+           curr_num |
+                    ------------------------
+           min_left |
+                    ------------------------
+
+                    // minimum value left of index i
+                    curr_min = INT_MAX
+
+-------------------------------------------------------------------------------
+
+        1. 
+            nums = [3, 1, 4, 2]
+                    i
+
+                Before pushing each element onto the Stack, we have to check:
+
+                if (
+                    !stack.empty()               &&
+                    stack.top().first  > nums[i] &&
+                    stack.top().second < nums[i]
+                   )
+
+
+                However, we can omit the 2nd condition:
+
+                    stack.top().first  > nums[i]
+
+                because if we haven't popped it yet, then we are sure it's
+                greater than our nums[i]. That's the power and the very reason
+                we are using "Monotonicly Decreasing Stack".
+
+                Therefore, before pushing each element into a Stack, we have to
+                check:
+
+                if (
+                    !stack.empty()               &&
+                    stack.top().second < nums[i]
+                   )
+
+                This is NOT fullfilled, therefore we are going to Push:
+
+                    ------------------------
+           curr_num | 3                       // Push
+                    ------------------------
+           min_left | INT_MAX                 // Push
+                    --^---------------------
+                      |
+                      ------- curr_min
+
+                    // minimum value left of index i
+                    curr_min = min(curr_min, nums[i])
+                    curr_min = min( INT_MAX,   3    ) = 3     // NEW MIN
+
+-------------------------------------------------------------------------------
+
+        2. 
+            nums = [3, 1, 4, 2]
+                       i
+
+                Before pushing each element onto the Stack, we have to check:
+
+                if (
+                    !stack.empty()               &&
+                    stack.top().first  > nums[i] &&
+                    stack.top().second < nums[i]
+                   )
+
+
+                However, we can omit the 2nd condition:
+
+                    stack.top().first  > nums[i]
+
+                because if we haven't popped it yet, then we are sure it's
+                greater than our nums[i]. That's the power and the very reason
+                we are using "Monotonicly Decreasing Stack".
+
+                Therefore, before pushing each element into a Stack, we have to
+                check:
+
+                if (
+                    !stack.empty()               &&
+                    stack.top().second < nums[i]
+                   )
+
+                This is NOT fullfilled, therefore we are going to Push:
+
+                    ------------------------
+           curr_num |   3     | 1             // Push
+                    ----------+-------------
+           min_left | INT_MAX | 3             // Push
+                    ------------^-----------
+                                |
+                                --- curr_min
+
+                    // minimum value left of index i
+                    curr_min = min(curr_min, nums[i])
+                    curr_min = min(   3    ,   1    ) = 1     // NEW MIN
+
+-------------------------------------------------------------------------------
+
+        3. 
+            nums = [3, 1, 4, 2]
+                          i
+
+                    ------------------------
+           curr_num |   3     | 1             // Pop
+                    ----------+-------------
+           min_left | INT_MAX | 3             // Pop
+                    ------------------------
+
+                    ------------------------
+           curr_num |   3                     // Pop
+                    ------------------------
+           min_left | INT_MAX                 // Pop
+                    ------------------------
+
+                    ------------------------
+           curr_num |
+                    ------------------------
+           min_left |
+                    ------------------------
+
+                Before pushing each element onto the Stack, we have to check:
+
+                if (
+                    !stack.empty()               &&
+                    stack.top().first  > nums[i] &&
+                    stack.top().second < nums[i]
+                   )
+
+
+                However, we can omit the 2nd condition:
+
+                    stack.top().first  > nums[i]
+
+                because if we haven't popped it yet, then we are sure it's
+                greater than our nums[i]. That's the power and the very reason
+                we are using "Monotonicly Decreasing Stack".
+
+                Therefore, before pushing each element into a Stack, we have to
+                check:
+
+                if (
+                    !stack.empty()               &&
+                    stack.top().second < nums[i]
+                   )
+
+                This is NOT fullfilled, therefore we are going to Push:
+
+                    ------------------------
+           curr_num | 4                       // Push
+                    ------------------------
+           min_left | 1                       // Push
+                    --^---------------------
+                      |
+                      --- curr_min
+
+-------------------------------------------------------------------------------
+
+        4. 
+            nums = [3, 1, 4, 2]
+                             i
+
+            Before pushing each element onto the Stack, we have to check:
+
+            if (
+                !stack.empty()               &&
+                stack.top().first  > nums[i] &&
+                stack.top().second < nums[i]
+               )
+
+
+            However, we can omit the 2nd condition:
+
+                stack.top().first  > nums[i]
+
+            because if we haven't popped it yet, then we are sure it's greater
+            than our nums[i]. That's the power and the very reason we are using
+            "Monotonicly Decreasing Stack".
+
+            Therefore, before pushing each element into a Stack, we have to
+            check:
+
+            if (
+                !stack.empty()               &&
+                stack.top().second < nums[i]
+               )
+
+                    ------------------------
+           curr_num | 4
+                    ------------------------
+           min_left | 1
+                    ------------------------
+
+
+            Since stack is NOT empty and stack.top().second < nums[i], that
+            means we have "132" pattern and we immediately return true.
+
+-------------------------------------------------------------------------------
+
+    Good Example to try yourself:
         [5, 8, 1, 4, 5, 3, 7, 6, 3, 4, 9]
 
 */
@@ -70,23 +437,23 @@
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(n) */
-class Solution_1 {
+class Solution {
 public:
     bool find132pattern(std::vector<int>& nums)
     {
-        std::stack<std::pair<int, int>> stack; // {min_to_the_left, num}
+        std::stack<std::pair<int, int>> stack; // {num, min_to_the_left}
         int curr_min = nums[0];
 
         for (int i = 1; i < nums.size(); i++)
         {
-            while (!stack.empty() && stack.top().second <= nums[i])
+            while (!stack.empty() && stack.top().first <= nums[i])
                 stack.pop();
 
-            if (!stack.empty() && stack.top().first < nums[i] && stack.top().second > nums[i])
+            if (!stack.empty() && stack.top().second < nums[i])
                 return true;
 
-            stack.push({curr_min, nums[i]});
-            curr_min = std::min(curr_min, nums[i]);
+            stack.push({nums[i], curr_min});
+            curr_min = std::min(nums[i], curr_min);
         }
 
         return false;
