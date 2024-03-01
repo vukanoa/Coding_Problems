91bcf5c1183b331d269f59426945cd876758884e
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-01 14:05:45 +0100

    Remove Solutions & Add Elegant Solution to LeetCode: 143_Reorder_List to Blind_75

--- 2_LeetCode_Blind_75/Linked_List/143_Reorder_List/reorder_list.cpp
+++ 2_LeetCode_Blind_75/Linked_List/143_Reorder_List/reorder_list.cpp
@@ -1,72 +1,72 @@
 #include <iostream>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	===========================
-	143) Reorder List
-	===========================
+    ===========================
+    143) Reorder List
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	You are given the head of a singly linked-list. The list can be represented
-	as:
-		L0 -> L1 -> ... -> Ln - 1 -> Ln
+    You are given the head of a singly linked-list. The list can be represented
+    as:
+        L0 -> L1 -> ... -> Ln - 1 -> Ln
 
-	Reorder the list to be on the following form:
-		L0 -> Ln -> L1 -> Ln - 1 -> L2 -> Ln - 2 -> ...
+    Reorder the list to be on the following form:
+        L0 -> Ln -> L1 -> Ln - 1 -> L2 -> Ln - 2 -> ...
 
-	You may not modify the values in the list's nodes. Only nodes themselves
-	may be changed.
+    You may not modify the values in the list's nodes. Only nodes themselves
+    may be changed.
 
-	===========================================
-	FUNCTION: void reorderList(ListNode* head); 
-	===========================================
+    ===========================================
+    FUNCTION: void reorderList(ListNode* head);
+    ===========================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	1 --> 2 --> 3 --> 4
+    --- Example 1 ---
+    1 --> 2 --> 3 --> 4
 
-	         |
-	         |
-	         V
+             |
+             |
+             V
 
-	1 --> 4 --> 2 --> 3
+    1 --> 4 --> 2 --> 3
 
-	Input: head = [1,2,3,4]
-	Output: [1,4,2,3]
-
-
-
-
-
-
-	--- Example 2 ---
-	1 --> 2 --> 3 --> 4 --> 5
-
-	         |
-	         |
-	         V
-
-	1 --> 5 --> 2 --> 4 --> 3
-
-	Input: head = [1,2,3,4,5]
-	Output: [1,5,2,4,3]
+    Input: head = [1,2,3,4]
+    Output: [1,4,2,3]
 
 
 
 
 
-	*** Constraints ***
-	The number of nodes in the list is in the range [1, 5 * 10^4].
-	1 <= Node.val <= 1000
+
+    --- Example 2 ---
+    1 --> 2 --> 3 --> 4 --> 5
+
+             |
+             |
+             V
+
+    1 --> 5 --> 2 --> 4 --> 3
+
+    Input: head = [1,2,3,4,5]
+    Output: [1,5,2,4,3]
+
+
+
+
+
+    *** Constraints ***
+    The number of nodes in the list is in the range [1, 5 * 10^4].
+    1 <= Node.val <= 1000
 
 */
 
@@ -82,52 +82,52 @@
  */
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	When you look at this example:
+    When you look at this example:
 
-	    1 --> 2 --> 3 --> 4 --> 5
+        1 --> 2 --> 3 --> 4 --> 5
 
-	             |
-	             |
-	             V
+                 |
+                 |
+                 V
 
-	    1 --> 5 --> 2 --> 4 --> 3
-
-
-	Since we need to alternate, it's like we need two merge two lists. But if
-	we were to just merge two halves, we'd get this:
-
-	    1 --> 2 --> 3 --> 4 --> 5
-
-	             |
-	             |
-	             V
-
-	    1 --> 4 --> 2 --> 5 --> 3
-
-	Which is incorrect. Why? Because we're trying to merge two halves in their
-	given order, but we're told that we need to alternate:
-		front, back, front, back, front, ...
-
-	So when we look at the desired output again:
-	    1 --> 5 --> 2 --> 4 --> 3
+        1 --> 5 --> 2 --> 4 --> 3
 
 
-	We can see that we want to merge two halves with alternating values, but
-	we're not merging the 2nd half from the beginning, but from the back.
-	The 2nd half needs to be merged FROM THE BACK.
+    Since we need to alternate, it's like we need two merge two lists. But if
+    we were to just merge two halves, we'd get this:
 
-	As soon as wee see that, it becomes obvious that we need to reverse the 2nd
-	list and the merge two halves.
+        1 --> 2 --> 3 --> 4 --> 5
 
-	Solution:
-	1. Find the middle node
-	2. Unlink two halves
-	3. Reverse the 2nd half
-	4. Merge two halves
+                 |
+                 |
+                 V
+
+        1 --> 4 --> 2 --> 5 --> 3
+
+    Which is incorrect. Why? Because we're trying to merge two halves in their
+    given order, but we're told that we need to alternate:
+        front, back, front, back, front, ...
+
+    So when we look at the desired output again:
+        1 --> 5 --> 2 --> 4 --> 3
+
+
+    We can see that we want to merge two halves with alternating values, but
+    we're not merging the 2nd half from the beginning, but from the back.
+    The 2nd half needs to be merged FROM THE BACK.
+
+    As soon as wee see that, it becomes obvious that we need to reverse the 2nd
+    list and the merge two halves.
+
+    Solution:
+    1. Find the middle node
+    2. Unlink two halves
+    3. Reverse the 2nd half
+    4. Merge two halves
 
 */
 
@@ -139,87 +139,23 @@
 class Solution {
 public:
     void reorderList(ListNode* head)
-	{
-		ListNode* slow = head;
-		ListNode* fast = head->next;
+    {
+        ListNode* left_tail  = middle(head);
+        ListNode* right_head = left_tail->next;
 
-		while (fast != nullptr && fast->next != nullptr)
-		{
-			slow = slow->next;
-			fast = fast->next->next;
-		}
+        // Unlink
+        left_tail->next = nullptr;
 
-		ListNode* second_half_head = slow->next;
-		
-		// Unlink two halves
-		slow->next = nullptr;
+        // Reverse "right"(second) list
+        right_head = reverse_list(right_head);
 
-		// Reverse 2nd half
-		second_half_head = reverse_list(second_half_head);
-
-		// Merge two lists
-		merge_two_lists(head, second_half_head);
+        // Merge two lists alternating
+        merge_alternating(head, right_head);
     }
 
 private:
-	ListNode* reverse_list(ListNode* head)
-	{
-		ListNode* prev = nullptr;
-		ListNode* curr = head;
-		ListNode* next = head;
-
-		while (curr != nullptr)
-		{
-			next = next->next;
-
-			curr->next = prev;
-			prev = curr;
-			curr = next;
-		}
-		
-		return prev;
-	}
-
-	void merge_two_lists(ListNode* first, ListNode* second)
-	{
-		while (first != nullptr && second != nullptr)
-		{
-			ListNode* first_next  = first->next;
-			ListNode* second_next = second->next;
-
-			first->next  = second;
-			second->next = first_next;
-
-			first  = first_next;
-			second = second_next;
-		}
-	}
-};
-
-
-
-
-/*
-	------------
-	--- IDEA ---
-	------------
-
-	The merge function is different so I wanted to have both Solutions here.
-
-*/
-
-/* Time  Beats: 76.48% */
-/* Space Beats: 60.16% */
-
-/* Time  Complexity: O(n) */
-/* Space Complexity: O(1) */
-class Solution {
-public:
-    void reorderList(ListNode* head)
-	{
-        if (head == nullptr || head->next == nullptr)
-            return;
-        
+    ListNode* middle(ListNode* head)
+    {
         ListNode* slow = head;
         ListNode* fast = head->next;
 
@@ -229,19 +165,9 @@ public:
             fast = fast->next->next;
         }
 
-        ListNode* head2 = slow->next;
-        
-        // Unlink two halves
-        slow->next = nullptr;
-
-        // Reverse second half
-        head2 = reverse_list(head2);
-
-        // Merge alternating between two halves
-        head = merge_alternate_two_lists(head, head2);
+        return slow;
     }
 
-private:
     ListNode* reverse_list(ListNode* head)
     {
         ListNode* prev = nullptr;
@@ -259,34 +185,18 @@ private:
         return prev;
     }
 
-    ListNode* merge_alternate_two_lists(ListNode* list1, ListNode* list2)
+    void merge_alternating(ListNode* list1, ListNode* list2)
     {
-        ListNode* dummy = new ListNode();
-        ListNode* tail = dummy;
-
-        int i = 0;
-        while (list1 && list2)
+        while (list1 != nullptr && list2 != nullptr)
         {
-            if (i % 2 == 0) // Even
-            {
-                tail->next = list1;
-                list1 = list1->next;
-            }
-            else // Odd
-            {
-                tail->next = list2;
-                list2 = list2->next;
-            }
+            ListNode* list1_next = list1->next;
+            ListNode* list2_next = list2->next;
 
-            tail = tail->next;
-            i++;
+            list1->next = list2;
+            list2->next = list1_next;
+
+            list1 = list1_next;
+            list2 = list2_next;
         }
-
-        if (list1)
-            tail->next = list1;
-        else
-            tail->next = list2;
-        
-        return dummy->next;
     }
 };
