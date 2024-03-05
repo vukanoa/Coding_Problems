7e33f60a257611fc499aba9127a58eedbf6bbb98
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-05 10:20:40 +0100

    Add another Implementation to LeetCode: 104_Maximum_Depth_Of_Binary_Tree to Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/056_104__Maximum_Depth_Of_Binary_Tree/maximum_depth_of_binary_tree.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/056_104__Maximum_Depth_Of_Binary_Tree/maximum_depth_of_binary_tree.cpp
@@ -3,42 +3,42 @@
 #include <queue>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	=================================
-	104) Maximum Depth of Binary Tree
-	=================================
+    =================================
+    104) Maximum Depth of Binary Tree
+    =================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given the "root" of a binary tree, return its maximum depth.
+    Given the "root" of a binary tree, return its maximum depth.
 
-	A binary Tree's maximum depth is the nmber of nodes along the longest path
-	from the root node down to the farthest leaf node.
+    A binary Tree's maximum depth is the nmber of nodes along the longest path
+    from the root node down to the farthest leaf node.
 
-	=======================================
-	FUNCTION: int maxDepth(TreeNode* root);
-	=======================================
+    =======================================
+    FUNCTION: int maxDepth(TreeNode* root);
+    =======================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  root = [3, 9, 20, null, null, 15, 7]
-	Output: 3
+    --- Example 1 ---
+    Input:  root = [3, 9, 20, null, null, 15, 7]
+    Output: 3
 
-	--- Example 2 ---
-	Input:  root = [1, null, 2]
-	Output: 2
+    --- Example 2 ---
+    Input:  root = [1, null, 2]
+    Output: 2
 
-	*** Constraints ***
-	The number of nodes in the tree is in the range [0, 10^4]
-	-100 <= Node.val <= 100
+    *** Constraints ***
+    The number of nodes in the tree is in the range [0, 10^4]
+    -100 <= Node.val <= 100
 
 */
 
@@ -54,99 +54,194 @@ struct TreeNode {
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Let's assume that we're already given the maximum depth of root's left and
-	right subtrees by recursion.
+    Let's assume that we're already given the maximum depth of root's left and
+    right subtrees by recursion.
 
-	So to find the maximum depth of this binary tree, we will have to take out
-	the maximum of the 2 depths given to us by recursion and add 1 to that to
-	consider the current level i.e. root's level into our depth.
+    So to find the maximum depth of this binary tree, we will have to take out
+    the maximum of the 2 depths given to us by recursion and add 1 to that to
+    consider the current level i.e. root's level into our depth.
 
-	Base Case:
-		We can easily analyze that if we are at a leaf node as root, then its
-		left and right subtrees will have 0 depth, and consecutively, this leaf
-		node will have max depth of 1.
+    Base Case:
+        We can easily analyze that if we are at a leaf node as root, then its
+        left and right subtrees will have 0 depth, and consecutively, this leaf
+        node will have max depth of 1.
 
-	From Base Case, just follow recursion and it's self-explanatory.
+    From Base Case, just follow recursion and it's self-explanatory.
 
 */
 
 /* Time  Beats: 100% */
 /* Space Beats: 34.31% */
 
-/*	Time  Complexity: O(n) */
+/*    Time  Complexity: O(n) */
 /*
-	Space Complexity: O(h)
-	Where 'h' is the height of the tree, for the recursive stack.
+    Space Complexity: O(h)
+    Where 'h' is the height of the tree, for the recursive stack.
 */
 class Solution{
 public:
-	int maxDepth(TreeNode* root)
-	{
-		if (root == nullptr)
-			return 0;
+    int maxDepth(TreeNode* root)
+    {
+        if (!root)
+            return 0;
 
-		return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
-	}
+        return 1 + std::max(maxDepth(root->left), maxDepth(root->right));
+    }
 };
 
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Almost equivalent idea, but coded a bit differently.
-	
+    Same as above, implemented in a worse way. It's good to be able to see
+    both ways because you get to appreciate the above implementation more.
+
+*/
+
+/* Time  Beats: 61.79% */
+/* Space Beats: 98.60% */
+
+/* Time  Complexity: O(n) */
+/*
+    Space Complexity: O(h)
+    Where 'h' is the height of the tree, for the recursive stack.
+*/
+class Solution_2 {
+public:
+    int maxDepth(TreeNode* root)
+    {
+        return depth(root, 0);
+    }
+
+private:
+    int depth(TreeNode* root, int level)
+    {
+        if (!root)
+            return level;
+
+        return std::max(depth(root->left, level+1), depth(root->right, level+1));
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Almost equivalent idea to both above, but coded in an even worse way than
+    Solution_2.
+
+    Again, it's good to be able to see all the ways of implementing the same
+    idea because that way you get to appreciate the very first Solution in this
+    file.
+
 */
 
 /* Time  Beats: 73.50% */
 /* Space Beats: 19.35% */
 
-/*	Time  Complexity: O(n) */
+/*    Time  Complexity: O(n) */
 /*
-	Space Complexity: O(h)
-	Where 'h' is the height of the tree, for the recursive stack.
+    Space Complexity: O(h)
+    Where 'h' is the height of the tree, for the recursive stack.
 */
-class Solution_DFS {
-private:
-	void dfs(TreeNode* root, int current, int& max)
-	{
-		if (root == nullptr)
-			return;
-
-		// If current is not nullptr, add it to the current path and check max
-		max = std::max(max, current + 1);
-
-		dfs(root->left , current + 1, max);
-		dfs(root->right, current + 1, max);
-	}
-
+class Solution_3 {
 public:
-	int maxDepth(TreeNode* root)
-	{
-		int max = 0;
-		dfs(root, 0, max);
+    int maxDepth(TreeNode* root)
+    {
+        int max = 0;
+        dfs(root, 0, max);
 
-		return max;
-	}
+        return max;
+    }
+
+private:
+    void dfs(TreeNode* root, int current, int& max)
+    {
+        if (!root)
+            return;
+
+        // If current is not nullptr, add it to the current path and check max
+        max = std::max(max, current + 1);
+
+        dfs(root->left , current + 1, max);
+        dfs(root->right, current + 1, max);
+    }
 };
 
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Using BFS(i.e. Level-Order)
-	
+    Same idea, implemented in an Iterative order which is even worse than
+    Solution 3.
+
+    Again, once you see all Four Solutions, you get to appreciate the first one
+    a lot more.
+
+*/
+
+/* Time  Beats: 67.09% */
+/* Space Beats:  9.20% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_Iterative_DFS_Preorder {
+public:
+    int maxDepth(TreeNode* root)
+    {
+        std::stack<std::pair<TreeNode*, int>> stack;
+
+        stack.push({root, 1});
+        int result = 0;
+
+        while (!stack.empty())
+        {
+            std::pair<TreeNode*, int> entry = stack.top();
+            stack.pop();
+
+            TreeNode* node = entry.first;
+            int depth = entry.second;
+
+            if (node)
+            {
+                result = std::max(result, depth);
+
+                // We are adding nullptr as well
+                stack.push({node->left,  depth + 1});
+                stack.push({node->right, depth + 1});
+            }
+        }
+
+        return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Using BFS(i.e. Level-Order)
+
 */
 
 /* Time  Beats: 88.24% */
@@ -156,230 +251,193 @@ public:
 /* Space Complexity: O(n) */
 class Solution_BFS {
 public:
-	int maxDepth(TreeNode* root)
-	{
-		if (root == nullptr)
-			return 0;
+    int maxDepth(TreeNode* root)
+    {
+        if (root == nullptr)
+            return 0;
 
-		int level = 0;
-		std::queue<TreeNode*> queue;
+        int level = 0;
+        std::queue<TreeNode*> queue;
 
-		// Emplace is a bit faster than "push" for custom Data Types
-		queue.emplace(root);
+        // Emplace is a bit faster than "push" for custom Data Types
+        queue.emplace(root);
 
-		while (!queue.empty())
-		{
-			int nodes_at_this_level = queue.size();
+        while (!queue.empty())
+        {
+            int nodes_at_this_level = queue.size();
 
-			for (int i = 0; i < nodes_at_this_level; i++)
-			{
-				TreeNode* node = queue.front();
-				queue.pop(); // Pop from the front because it's a queue
+            for (int i = 0; i < nodes_at_this_level; i++)
+            {
+                TreeNode* node = queue.front();
+                queue.pop(); // Pop from the front because it's a queue
 
-				if (node->left)
-					queue.emplace(node->left);
-				
-				if (node->right)
-					queue.emplace(node->right);
-			}
+                if (node->left)
+                    queue.emplace(node->left);
 
-			level++;
-		}
+                if (node->right)
+                    queue.emplace(node->right);
+            }
 
-		return level;
-	}
-};
+            level++;
+        }
 
-
-
-/* Time  Beats: 67.09% */
-/* Space Beats:  9.20% */
-
-/* Time  Complexity: O(n) */
-/* Space Complexity: O(n) */
-class Solution_Iterative_DFS_Preorder {
-public:
-	int maxDepth(TreeNode* root)
-	{
-		std::stack<std::pair<TreeNode*, int>> stack;
-
-		stack.push({root, 1});
-		int result = 0;
-
-		while (!stack.empty())
-		{
-			std::pair<TreeNode*, int> entry = stack.top();
-			stack.pop();
-
-			TreeNode* node = entry.first;
-			int depth = entry.second;
-
-			if (node)
-			{
-				result = std::max(result, depth);
-				
-				// We are adding nullptr as well
-				stack.push({node->left,  depth + 1});
-				stack.push({node->right, depth + 1});
-			}
-		}
-
-		return result;
-	}
+        return level;
+    }
 };
 
 
 
 
 /*
-	=============================
-	=== This is just printing ===
-	=============================
+    =============================
+    === This is just printing ===
+    =============================
 */
 
 void
 print_array(std::vector<std::string>& nums)
 {
-	bool first = true;
-	std::cout << "\n\t*** Level Order ***";
-	std::cout << "\n\tTree: [";
-	for (auto x: nums)
-	{
-		if (!first)
-			std::cout << ", ";
+    bool first = true;
+    std::cout << "\n\t*** Level Order ***";
+    std::cout << "\n\tTree: [";
+    for (auto x: nums)
+    {
+        if (!first)
+            std::cout << ", ";
 
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n";
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n";
 }
 
 
 void
 print_levelorder(TreeNode* root)
 {
-	if (root == nullptr)
-		return;
-	
-	std::queue<TreeNode*> queue;
-	queue.push(root);
+    if (!root)
+        return;
 
-	std::vector<std::string> vector_print;
+    std::queue<TreeNode*> queue;
+    queue.push(root);
 
-	while (!queue.empty())
-	{
-		int size = queue.size();
+    std::vector<std::string> vector_print;
 
-		for (int i = 0; i < size; i++)
-		{
-			TreeNode* node = queue.front();
-			queue.pop();
+    while (!queue.empty())
+    {
+        int size = queue.size();
 
-			if (node == nullptr)
-			{
-				vector_print.push_back("null");
-				continue;
-			}
-			else
-				vector_print.push_back(std::to_string(node->val));
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode* node = queue.front();
+            queue.pop();
 
-			if (node->left != nullptr)
-				queue.push(node->left);
-			else
-				queue.push(nullptr);
+            if (node == nullptr)
+            {
+                vector_print.push_back("null");
+                continue;
+            }
+            else
+                vector_print.push_back(std::to_string(node->val));
 
-			if (node->right != nullptr)
-				queue.push(node->right);
-			else
-				queue.push(nullptr);
-		}
-	}
+            if (node->left != nullptr)
+                queue.push(node->left);
+            else
+                queue.push(nullptr);
 
-	int x = vector_print.size() - 1;
-	while (vector_print[x] == "null")
-	{
-		vector_print.pop_back();
-		x--;
-	}
+            if (node->right != nullptr)
+                queue.push(node->right);
+            else
+                queue.push(nullptr);
+        }
+    }
 
-	print_array(vector_print);
+    int x = vector_print.size() - 1;
+    while (vector_print[x] == "null")
+    {
+        vector_print.pop_back();
+        x--;
+    }
+
+    print_array(vector_print);
 }
 
 
 int
 main()
 {
-	Solution                        sol;
-	Solution_DFS                    sol_dfs;
-	Solution_BFS                    sol_bfs;
-	Solution_Iterative_DFS_Preorder sol_iter;
+    Solution                        sol;
+    Solution_2                      sol_2;
+    Solution_3                      sol_3;
+    Solution_Iterative_DFS_Preorder sol_iter;
+    Solution_BFS                    sol_bfs;
 
 
-	/* Example 1 */
-	// TreeNode fifteen(15);
-	// TreeNode seven(7);
-	// TreeNode twenty(20, &fifteen, &seven);
-	// TreeNode nine(9);
-	// TreeNode three(3, &nine, &twenty);
+    /* Example 1 */
+    // TreeNode fifteen(15);
+    // TreeNode seven(7);
+    // TreeNode twenty(20, &fifteen, &seven);
+    // TreeNode nine(9);
+    // TreeNode three(3, &nine, &twenty);
 
-	// TreeNode* root = &three;
-
-
-	/* Example 2 */
-	// TreeNode one(1);
-
-	// TreeNode* root = &one;
+    // TreeNode* root = &three;
 
 
-	/* Example 3 */
-	// TreeNode* root = nullptr;
+    /* Example 2 */
+    // TreeNode one(1);
+
+    // TreeNode* root = &one;
 
 
-	/* Example 4 */
-	// TreeNode two(2);
-	// TreeNode one(1, &two, nullptr);
-
-	// TreeNode* root = &one;
+    /* Example 3 */
+    // TreeNode* root = nullptr;
 
 
-	/* Example 5 */
-	// TreeNode five(5);
-	// TreeNode four(4, &five, nullptr);
-	// TreeNode three(3, &four, nullptr);
-	// TreeNode two(2, &three, nullptr);
-	// TreeNode one(1, &two, nullptr);
+    /* Example 4 */
+    // TreeNode two(2);
+    // TreeNode one(1, &two, nullptr);
 
-	// TreeNode* root = &one;
+    // TreeNode* root = &one;
 
 
-	/* Example 6 */
-	TreeNode seven(7);
-	TreeNode five(5, &seven, nullptr);
-	TreeNode four(4);
-	TreeNode two(2, &four, &five);
-	TreeNode six(6);
-	TreeNode three(3, nullptr, &six);
-	TreeNode one(1, &two, &three);
+    /* Example 5 */
+    // TreeNode five(5);
+    // TreeNode four(4, &five, nullptr);
+    // TreeNode three(3, &four, nullptr);
+    // TreeNode two(2, &three, nullptr);
+    // TreeNode one(1, &two, nullptr);
 
-	TreeNode* root = &one;
+    // TreeNode* root = &one;
 
 
-	std::cout << "\n\t======================================";
-	std::cout << "\n\t=== MAXIMUM DEPTH OF A BINARY TREE ===";
-	std::cout << "\n\t======================================\n";
+    /* Example 6 */
+    TreeNode seven(7);
+    TreeNode five(5, &seven, nullptr);
+    TreeNode four(4);
+    TreeNode two(2, &four, &five);
+    TreeNode six(6);
+    TreeNode three(3, nullptr, &six);
+    TreeNode one(1, &two, &three);
 
-	
-	/* Write Input */
-	print_levelorder(root);
-
-
-	/* Solution */
-	int depth = sol.maxDepth(root);
-
-
-	/* Write Output */
-	std::cout << "\n\tMaximum Depth is: " << depth << "\n\n";
+    TreeNode* root = &one;
 
 
-	return 0;
+    std::cout << "\n\t======================================";
+    std::cout << "\n\t=== MAXIMUM DEPTH OF A BINARY TREE ===";
+    std::cout << "\n\t======================================\n";
+
+
+    /* Write Input */
+    print_levelorder(root);
+
+
+    /* Solution */
+    int depth = sol.maxDepth(root);
+
+
+    /* Write Output */
+    std::cout << "\n\tMaximum Depth is: " << depth << "\n\n";
+
+
+    return 0;
 }
