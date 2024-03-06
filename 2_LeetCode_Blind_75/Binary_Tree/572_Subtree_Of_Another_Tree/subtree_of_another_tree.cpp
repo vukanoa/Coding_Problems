5bbff5a49abd9013b9c53097fa6cc46e19ef8fe3
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-06 18:17:05 +0100

    Exchange Solutions in LeetCode: 572_Subtree_Of_Another_Tree to Blind_75

--- 2_LeetCode_Blind_75/Binary_Tree/572_Subtree_Of_Another_Tree/subtree_of_another_tree.cpp
+++ 2_LeetCode_Blind_75/Binary_Tree/572_Subtree_Of_Another_Tree/subtree_of_another_tree.cpp
@@ -1,61 +1,49 @@
 #include <iostream>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	============================
-	572) Subtree of Another Tree
-	============================
+    ============================
+    572) Subtree of Another Tree
+    ============================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given the roots of two binary trees root and subRoot, return true if there
-	is a subtree of root with the same structure and node values of subRoot and
-	false otherwise.
+    Given the roots of two binary trees root and subRoot, return true if there
+    is a subtree of root with the same structure and node values of subRoot and
+    false otherwise.
 
-	A subtree of a binary tree tree is a tree that consists of a node in tree
-	and all of this node's descendants. The tree tree could also be considered
-	as a subtree of itself.
+    A subtree of a binary tree tree is a tree that consists of a node in tree
+    and all of this node's descendants. The tree tree could also be considered
+    as a subtree of itself.
 
-	============================================================
-	FUNCTION: boll isSubtree(TreeNode* root, TreeNode* subRoot);
-	============================================================
+    ============================================================
+    FUNCTION: boll isSubtree(TreeNode* root, TreeNode* subRoot);
+    ============================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: root = [3,4,5,1,2], subRoot = [4,1,2]
-	Output: true
+    --- Example 1 ---
+    Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+    Output: true
 
-	--- Example 2 ---
-	Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
-	Output: false
+    --- Example 2 ---
+    Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+    Output: false
 
 
-	*** Constraints ***
-	The number of nodes in the root tree is in the range [1, 2000].
-	The number of nodes in the subRoot tree is in the range [1, 1000].
-	-10^4 <= root.val <= 10^4
-	-10^4 <= subRoot.val <= 10^4
+    *** Constraints ***
+    The number of nodes in the root tree is in the range [1, 2000].
+    The number of nodes in the subRoot tree is in the range [1, 1000].
+    -10^4 <= root.val <= 10^4
+    -10^4 <= subRoot.val <= 10^4
 
-*/
-
-/*
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
 */
 
 /**
@@ -70,32 +58,50 @@
  * };
  */
 
-/* Time  Beats: 71.25% */
-/* Space Beats: 33.47% */
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    First, you MUST do LeetCode 100 "Same Tree".
+
+    After that, think of what you are being asked and you'll probably come up
+    with this Solution. Of course, only if you genuinely understand reasons
+    behind implementing "SameTree" in that particular way.
+
+    If you don't genuinely understand "SameTree", make sure to understand that
+    one first and then come back. That is an absolutley necessary foundation
+    for solving this problem.
+
+*/
+
+/* Time  Beats: 67.05% */
+/* Space Beats: 99.61% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
 class Solution {
 public:
-	bool isSubtree(TreeNode* root, TreeNode* subRoot)
-	{
-		if (!root || !subRoot)
-			return false;
+    bool isSubtree(TreeNode* root, TreeNode* subRoot)
+    {
+        if (!root && !subRoot) return true;
+        if (!root || !subRoot) return false;
 
-		if (isSameTree(root, subRoot))
-			return true;
+        if (sameTree(root, subRoot))
+            return true;
 
-		return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
-	}
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
 
 private:
-	bool isSameTree(TreeNode* p, TreeNode* q)
-	{
-		if (p == nullptr && q == nullptr)
-			return true;
-		else if (p == nullptr || q == nullptr)
-			return false;
+    bool sameTree(TreeNode* p, TreeNode* q)
+    {
+        if (!p && !q) return true;
+        if (!p || !q) return false;
 
-		if (p->val != q->val)
-			return false;
+        if (p->val != q->val)
+            return false;
 
-		return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-	}
+        return sameTree(p->left, q->left) && sameTree(p->right, q->right);
+    }
 };
