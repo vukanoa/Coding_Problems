1f01b2bf37152a0d5bb25528a849e3c3261cdb5c
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-07 11:42:40 +0100

    Add another Implementation to LeetCode: 606_Construct_String_From_Binary_Tree to N_539

--- 4_N_539/Trees/606_Construct_String_From_Binary_Tree/construct_string_from_binary_tree.cpp
+++ 4_N_539/Trees/606_Construct_String_From_Binary_Tree/construct_string_from_binary_tree.cpp
@@ -31,13 +31,17 @@
     --- Example 1 ---
     Input: root = [1,2,3,4]
     Output: "1(2(4))(3)"
-    Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to omit all the unnecessary empty parenthesis pairs. And it will be "1(2(4))(3)"
+    Explanation: Originally, it needs to be "1(2(4)())(3()())", but you need to
+                 omit all the unnecessary empty parenthesis pairs. And it will
+                 be "1(2(4))(3)"
 
 
     --- Example 2 ---
     Input: root = [1,2,3,null,4]
     Output: "1(2()(4))(3)"
-    Explanation: Almost the same as the first example, except we cannot omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+    Explanation: Almost the same as the first example, except we cannot omit
+                 the first parenthesis pair to break the one-to-one mapping
+                 relationship between the input and the output.
 
 
     *** Constraints ***
@@ -78,26 +82,80 @@ private:
     {
         if (!root)
             return;
-        
+
         /* Root itself */
         out << root->val;
 
         /* Left Subtree */
         if (root->left || root->right)
             out << "(";
-        
+
         preorder(root->left, out);
 
         if (root->left || root->right)
             out << ")";
-        
+
         /* Right Subtree */
         if (root->right)
             out << "(";
-        
+
         preorder(root->right, out);
 
         if (root->right)
             out << ")";
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Another way of impementing it.
+
+*/
+
+/* Time  Beats: 76.35% */
+/* Space Beats: 97.60% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_2 {
+public:
+    string tree2str(TreeNode* root)
+    {
+        std::ostringstream out;
+        dfs(root, out);
+
+        return out.str();
+    }
+
+private:
+    void dfs(TreeNode* root, std::ostringstream& out)
+    {
+        if (!root)
+            return;
+
+        /* Root itself */
+        out << root->val;
+
+        if (!root->left && !root->right)
+            return;
+
+        /* Left Subtree */
+        out << "(";
+        dfs(root->left, out);
+        out << ")";
+
+        /* Right Subtree */
+        if (root->right)
+        {
+            out << "(";
+            dfs(root->right, out);
+            out << ")";
+        }
     }
 };
