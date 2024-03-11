5ca98a4ee3d3dc76be60764df32972faebaef64c
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-11 07:25:27 +0100

    Remove & Exchange Solutions in LeetCode: 102_Binary_Tree_Level_Order_Traversal in Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/055_102__Binary_Tree_Level_Order_Traversal/binary_tree_level_order_traversal.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/055_102__Binary_Tree_Level_Order_Traversal/binary_tree_level_order_traversal.cpp
@@ -3,44 +3,44 @@
 #include <queue>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	======================================
-	102) Binary Tree Level Order Traversal
-	======================================
+    ======================================
+    102) Binary Tree Level Order Traversal
+    ======================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given the "root" of a binary tree, return the level ordder traversal of its
-	nodes' values. (i.e., from left to right, level by level).
+    Given the "root" of a binary tree, return the level ordder traversal of its
+    nodes' values. (i.e., from left to right, level by level).
 
-	=========================================================
-	FUNCTION: vector<vector<int>> levelOrder(TreeNode* root);
-	=========================================================
+    =========================================================
+    FUNCTION: vector<vector<int>> levelOrder(TreeNode* root);
+    =========================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  root = [3, 9, 20, null, null, 15, 7]
-	Output: [[3], [9, 20], [15, 7]]
+    --- Example 1 ---
+    Input:  root = [3, 9, 20, null, null, 15, 7]
+    Output: [[3], [9, 20], [15, 7]]
 
-	--- Example 2 ---
-	Input:  root = [1]
-	Output: [[1]]
+    --- Example 2 ---
+    Input:  root = [1]
+    Output: [[1]]
 
-	--- Example 3 ---
-	Input:  root = []
-	Output: []
+    --- Example 3 ---
+    Input:  root = []
+    Output: []
 
-	*** Constraints ***
-	The number of nodes in the tree is in the range [0, 2000]
-	-1000 <= Node.val <= 1000
+    *** Constraints ***
+    The number of nodes in the tree is in the range [0, 2000]
+    -1000 <= Node.val <= 1000
 
 */
 
@@ -57,297 +57,246 @@ struct TreeNode {
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	It's just BFS. The only "tricky" part is to know when the current level
-	ends and the next one begins.
+    It's just BFS. The only "tricky" part is to know when the current level
+    ends and the next one begins.
 
-	We do that but calculating the number of nodes in the queue before we enter
-	the "for" loop.
+    We do that but calculating the number of nodes in the queue before we enter
+    the "for" loop.
 
-	We don't push null nodes in the queue.
+    We don't push null nodes in the queue.
 
-	Also, we don't push empty vector "level" into the "results" vector.
-	That happens only in the last level.
+    Also, we don't push empty vector "level" into the "results" vector.
+    That happens only in the last level.
 
-	___________
-	_____1_____
-	__2_____3__
-	4___5_6___7
+    ___________
+    _____1_____
+    __2_____3__
+    4___5_6___7
 
 */
 
-
-/* Time  Beats:  100% */
-/* Space Beats: 15.35% */
-
 /*
-	Time  Complexity: O(n)
-*/
-/*
-	Space Complexity: O(n)
+    ------------
+    --- IDEA ---
+    ------------
 
-	The biggest level of the tree could be n/2, that's how Trees work. At least
-	Binary Trees.
-*/
-class Solution{
-public:
-	std::vector<std::vector<int>> levelOrder(TreeNode* root)
-	{
-		std::vector<std::vector<int>> results;
-		std::queue<TreeNode*> queue;
-		
-		queue.push(root);
+    Basic BFS Implementation.
 
-		/* BFS */
-		while (!queue.empty())
-		{
-			int queue_length = queue.size();
-			std::vector<int> level;
+    It's like a "multiplication table", you have to commit it to memory.
+    It's a fundamental technique for Graphs(and Trees, but Trees are Graphs).
 
-			for (int i = 0; i < queue_length; i++)
-			{
-				TreeNode* node = queue.front();
-				queue.pop();
-
-				if (node)
-				{
-					level.push_back(node->val);
-
-					queue.push(node->left);
-					queue.push(node->right);
-				}
-			}
-
-			if (!level.empty())
-				results.push_back(level);
-		}
-
-		return results;
-	}
-};
-
-
-/*
-	------------
-	--- IDEA ---
-	------------
-
-	Implemented on another occasion and I like this one much better.
-	
 */
 
 /* Time  Beats: 88.21% */
 /* Space Beats: 26.52% */
 
 /*
-	Time  Complexity: O(n)
+    Time  Complexity: O(n)
 */
 /*
-	Space Complexity: O(n)
+    Space Complexity: O(n)
 
-	The biggest level of the tree could be n/2, that's how Trees work. At least
-	Binary Trees.
+    The biggest level of the tree could be n/2, that's how Trees work. At least
+    Binary Trees.
 */
-class Solution_Another_Implementation {
+class Solution {
 public:
-	std::vector<std::vector<int>> levelOrder(TreeNode* root)
-	{
-		if (root == nullptr)
-			return {};
+    std::vector<std::vector<int>> levelOrder(TreeNode* root)
+    {
+        if (!root)
+            return {};
 
-		std::vector<std::vector<int>> results;
+        std::vector<std::vector<int>> results;
+        std::queue<TreeNode*> queue;
+        queue.push(root);
 
-		std::queue<TreeNode*> queue;
-		queue.push(root);
+        /* BFS */
+        while (!queue.empty())
+        {
+            std::vector<int> curr_level;
 
-		while (!queue.empty())
-		{
-			int size = queue.size();
-			std::vector<int> level;
+            int size = queue.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode* node = queue.front();
+                queue.pop();
 
-			while (size--)
-			{
-				TreeNode* node = queue.front();
-				queue.pop();
+                if (node->left)
+                    queue.push(node->left);
 
-				level.push_back(node->val);
+                if (node->right)
+                    queue.push(node->right);
 
-				if (node->left)
-					queue.push(node->left);
+                curr_level.push_back(node->val);
+            }
 
-				if (node->right)
-					queue.push(node->right);
-			}
+            results.push_back(curr_level);
+        }
 
-			results.push_back(level);
-		}
-
-		return results;
-	}
+        return results;
+    }
 };
 
 
 
-
 /*
-	=============================
-	=== This is just printing ===
-	=============================
+    =============================
+    === This is just printing ===
+    =============================
 */
 
 void
 print_array(std::vector<std::string>& nums)
 {
-	bool first = true;
-	std::cout << "\n\t*** Level Order ***";
-	std::cout << "\n\tTree: [";
-	for (auto x: nums)
-	{
-		if (!first)
-			std::cout << ", ";
+    bool first = true;
+    std::cout << "\n\t*** Level Order ***";
+    std::cout << "\n\tTree: [";
+    for (auto x: nums)
+    {
+        if (!first)
+            std::cout << ", ";
 
-		std::cout << x;
-		first = false;
-	}
-	std::cout << "]\n";
+        std::cout << x;
+        first = false;
+    }
+    std::cout << "]\n";
 }
 
 
 void
 print_levelorder(TreeNode* root)
 {
-	if (root == nullptr)
-		return;
-	
-	std::queue<TreeNode*> queue;
-	queue.push(root);
+    if (!root)
+        return;
 
-	std::vector<std::string> vector_print;
+    std::queue<TreeNode*> queue;
+    queue.push(root);
 
-	while (!queue.empty())
-	{
-		int size = queue.size();
+    std::vector<std::string> vector_print;
 
-		for (int i = 0; i < size; i++)
-		{
-			TreeNode* node = queue.front();
-			queue.pop();
+    while (!queue.empty())
+    {
+        int size = queue.size();
 
-			if (node == nullptr)
-			{
-				vector_print.push_back("null");
-				continue;
-			}
-			else
-				vector_print.push_back(std::to_string(node->val));
+        for (int i = 0; i < size; i++)
+        {
+            TreeNode* node = queue.front();
+            queue.pop();
 
-			if (node->left != nullptr)
-				queue.push(node->left);
-			else
-				queue.push(nullptr);
+            if (!node)
+            {
+                vector_print.push_back("null");
+                continue;
+            }
+            else
+                vector_print.push_back(std::to_string(node->val));
 
-			if (node->right != nullptr)
-				queue.push(node->right);
-			else
-				queue.push(nullptr);
-		}
-	}
+            if (node->left)
+                queue.push(node->left);
+            else
+                queue.push(nullptr);
 
-	int x = vector_print.size() - 1;
-	while (vector_print[x] == "null")
-	{
-		vector_print.pop_back();
-		x--;
-	}
+            if (node->right)
+                queue.push(node->right);
+            else
+                queue.push(nullptr);
+        }
+    }
 
-	print_array(vector_print);
+    int x = vector_print.size() - 1;
+    while (vector_print[x] == "null")
+    {
+        vector_print.pop_back();
+        x--;
+    }
+
+    print_array(vector_print);
 }
 
 
 int
 main()
 {
-	Solution                        sol;
-	Solution_Another_Implementation sol_another;
+    Solution sol;
 
-	/* Example 1 */
-	TreeNode fifteen(15);
-	TreeNode seven(7);
-	TreeNode twenty(20, &fifteen, &seven);
-	TreeNode nine(9);
-	TreeNode three(3, &nine, &twenty);
+    /* Example 1 */
+    TreeNode fifteen(15);
+    TreeNode seven(7);
+    TreeNode twenty(20, &fifteen, &seven);
+    TreeNode nine(9);
+    TreeNode three(3, &nine, &twenty);
 
-	TreeNode* root = &three;
-
-
-	/* Example 2 */
-	// TreeNode one(1);
-
-	// TreeNode* root = &one;
+    TreeNode* root = &three;
 
 
-	/* Example 3 */
-	// TreeNode* root = nullptr;
+    /* Example 2 */
+    // TreeNode one(1);
+
+    // TreeNode* root = &one;
 
 
-	/* Example 4 */
-	// TreeNode two(2);
-	// TreeNode one(1, &two, nullptr);
-
-	// TreeNode* root = &one;
+    /* Example 3 */
+    // TreeNode* root = nullptr;
 
 
-	/* Example 5 */
-	// TreeNode five(5);
-	// TreeNode four(4, &five, nullptr);
-	// TreeNode three(3, &four, nullptr);
-	// TreeNode two(2, &three, nullptr);
-	// TreeNode one(1, &two, nullptr);
+    /* Example 4 */
+    // TreeNode two(2);
+    // TreeNode one(1, &two, nullptr);
 
-	// TreeNode* root = &one;
+    // TreeNode* root = &one;
 
 
-	std::cout << "\n\t=========================================";
-	std::cout << "\n\t=== BINARY TREE LEVEL ORDER TRAVERSAL ===";
-	std::cout << "\n\t=========================================\n";
+    /* Example 5 */
+    // TreeNode five(5);
+    // TreeNode four(4, &five, nullptr);
+    // TreeNode three(3, &four, nullptr);
+    // TreeNode two(2, &three, nullptr);
+    // TreeNode one(1, &two, nullptr);
+
+    // TreeNode* root = &one;
 
 
-	/* Write Input */
-	print_levelorder(root);
+    std::cout << "\n\t=========================================";
+    std::cout << "\n\t=== BINARY TREE LEVEL ORDER TRAVERSAL ===";
+    std::cout << "\n\t=========================================\n";
 
 
-	/* Solution */
-	std::vector<std::vector<int>> results = sol.levelOrder(root);
-	// std::vector<std::vector<int>> results = sol_another.levelOrder(root);
+    /* Write Input */
+    print_levelorder(root);
 
 
-	/* Write Output */
-	bool first = true;
-	std::cout << "\n\tResults: [";
-	for (auto x: results)
-	{
-		if (!first)
-			std::cout << ", ";
-
-		bool first_first = true;
-		std::cout << "[";
-		for (const auto& xx : x)
-		{
-			if (!first_first)
-				std::cout << ", ";
-
-			std::cout << xx;
-			first_first = false;
-		}
-		std::cout << "]";
-
-		first = false;
-	}
-	std::cout << "]\n\n";
+    /* Solution */
+    std::vector<std::vector<int>> results = sol.levelOrder(root);
 
 
-	return 0;
+    /* Write Output */
+    bool first = true;
+    std::cout << "\n\tResults: [";
+    for (auto x: results)
+    {
+        if (!first)
+            std::cout << ", ";
+
+        bool first_first = true;
+        std::cout << "[";
+        for (const auto& xx : x)
+        {
+            if (!first_first)
+                std::cout << ", ";
+
+            std::cout << xx;
+            first_first = false;
+        }
+        std::cout << "]";
+
+        first = false;
+    }
+    std::cout << "]\n\n";
+
+
+    return 0;
 }
