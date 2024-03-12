58f58832b6e0b2ac1092b790a93c6d9f969e7a74
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-12 07:03:34 +0100

    Remove Solution & Add Explanation(IDEA) to LeetCode: 1448_Count_Good_Nodes_In_Binary_Tree to N_150

--- 3_N_150/Binary_Tree/1448_Count_Good_Nodes_In_Binary_Tree/count_good_nodes_in_binary_tree.cpp
+++ 3_N_150/Binary_Tree/1448_Count_Good_Nodes_In_Binary_Tree/count_good_nodes_in_binary_tree.cpp
@@ -1,66 +1,66 @@
 #include <iostream>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	=====================================
-	1448) Count Good Nodes In Binary Tree
-	=====================================
+    =====================================
+    1448) Count Good Nodes In Binary Tree
+    =====================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given a binary tree root, a node X in the tree is named good if in the path
-	from root to X there are no nodes with a value greater than X.
+    Given a binary tree root, a node X in the tree is named good if in the path
+    from root to X there are no nodes with a value greater than X.
 
-	Return the number of good nodes in the binary tree.
+    Return the number of good nodes in the binary tree.
 
-	========================================
-	FUNCTION: int goodNodes(TreeNode* root); 
-	========================================
+    ========================================
+    FUNCTION: int goodNodes(TreeNode* root);
+    ========================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	        ___________
-	        _____3_____
-	        __1_____4__
-	        3______1__5
+    --- Example 1 ---
+            ___________
+            _____3_____
+            __1_____4__
+            3______1__5
 
-	Input: root = [3,1,4,3,null,1,5]
-	Output: 4
-	Explanation: Nodes in blue are good.
-	Root Node (3) is always a good node.
-	Node 4 -> (3,4) is the maximum value in the path starting from the root.
-	Node 5 -> (3,4,5) is the maximum value in the path
-	Node 3 -> (3,1,3) is the maximum value in the path.
-
-
+    Input: root = [3,1,4,3,null,1,5]
+    Output: 4
+    Explanation: Nodes in blue are good.
+    Root Node (3) is always a good node.
+    Node 4 -> (3,4) is the maximum value in the path starting from the root.
+    Node 5 -> (3,4,5) is the maximum value in the path
+    Node 3 -> (3,1,3) is the maximum value in the path.
 
 
-	--- Example 2 ---
-	        ___________
-	        _____3_____
-	        __3________
-	        4__2_______
-
-	Input: root = [3,3,null,4,2]
-	Output: 3
-	Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
 
 
-	--- Example 3 ---
-	Input: root = [1]
-	Output: 1
-	Explanation: Root is considered as good.
+    --- Example 2 ---
+            ___________
+            _____3_____
+            __3________
+            4__2_______
+
+    Input: root = [3,3,null,4,2]
+    Output: 3
+    Explanation: Node 2 -> (3, 3, 2) is not good, because "3" is higher than it.
 
 
-	*** Constraints ***
+    --- Example 3 ---
+    Input: root = [1]
+    Output: 1
+    Explanation: Root is considered as good.
+
+
+    *** Constraints ***
     The number of nodes in the binary tree is in the range [1, 10^5].
     Each node's value is between [-10^4, 10^4].
 
@@ -78,51 +78,21 @@
  * };
  */
 
-/* Time  Beats: 99.34% */
-/* Space Beats: 7.21% */
+/*
+    ------------
+    --- IDEA ---
+    ------------
 
-/* Time  Complexity: O(n) */
-/* Space Complexity: O(n) */
-class Solution {
-public:
-	int goodNodes(TreeNode* root)
-	{
-		int count = 1;
-		std::stack<int> stack;
+    Keep track of the "max on this path" while doing a DFS.
 
-		stack.push(root->val);
-		
-		dfs(root, count, stack);
-		
-		return count;
-	}
+    If the current node's value is greater than or equals to that
+    "max_on_this_path", then update the value of "max_on_this_path" variable
+    and increment "count" which is essentially what we are going to return at
+    the very end.
 
-private:
-	void dfs(TreeNode* root, int& count, std::stack<int>& stack)
-	{
-		if (root->val >= stack.top())
-			stack.push(root->val);
-		
-		if (root->left != nullptr)
-		{
-			if (root->left->val >= stack.top())
-				count++;
+    That's the whole problem.
 
-			dfs(root->left, count, stack);
-		}
-
-		if (root->right != nullptr)
-		{
-			if (root->right->val >= stack.top())
-				count++;
-			
-			dfs(root->right, count, stack);
-		}
-
-		if (root->val == stack.top())
-			stack.pop();
-	}
-};
+*/
 
 /* Time  Beats: 98.03% */
 /* Space Beats: 67.51% */
@@ -131,27 +101,27 @@ private:
 /* Space Complexity: O(n) */
 class Solution {
 public:
-	int goodNodes(TreeNode* root)
-	{
-		int count = 0;
-		dfs(root, INT_MIN, count);
+    int goodNodes(TreeNode* root)
+    {
+        int count = 0;
+        dfs(root, INT_MIN, count);
 
-		return count;
-	}
+        return count;
+    }
 
 private:
-	void dfs(TreeNode* root, int max_on_this_path, int& count)
-	{
-		if (!root)
-			return;
+    void dfs(TreeNode* root, int max_on_this_path, int& count)
+    {
+        if (!root)
+            return;
 
-		if (root->val >= max_on_this_path)
-		{
-			count++;
-			max_on_this_path = root->val;
-		}
+        if (root->val >= max_on_this_path)
+        {
+            max_on_this_path = root->val;
+            count++;
+        }
 
-		dfs(root->left,  max_on_this_path, count);
-		dfs(root->right, max_on_this_path, count);
-	}
+        dfs(root->left,  max_on_this_path, count);
+        dfs(root->right, max_on_this_path, count);
+    }
 };
