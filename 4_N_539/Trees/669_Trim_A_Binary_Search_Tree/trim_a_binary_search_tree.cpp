85fa18cc97c2a12aed9a701d909b73993aab35da
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-13 08:20:13 +0100

    Add another Solution to LeetCode: 669_Trim_A_Binary_Search_Tree to N_539

--- 4_N_539/Trees/669_Trim_A_Binary_Search_Tree/trim_a_binary_search_tree.cpp
+++ 4_N_539/Trees/669_Trim_A_Binary_Search_Tree/trim_a_binary_search_tree.cpp
@@ -117,3 +117,41 @@ public:
         return root;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same idea as above though implemented entirely recursively. It is much more
+    elegant than the above Solution.
+
+*/
+
+/* Time  Beats: 35.59% */
+/* Space Beats: 51.01% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_Elegant {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high)
+    {
+        if (!root)
+            return nullptr;
+
+        if (root->val < low)
+            return trimBST(root->right, low, high);
+
+        if (root->val > high)
+            return trimBST(root->left, low, high);
+
+        root->left  = trimBST(root->left,  low, high);
+        root->right = trimBST(root->right, low, high);
+
+        return root;
+    }
+};
