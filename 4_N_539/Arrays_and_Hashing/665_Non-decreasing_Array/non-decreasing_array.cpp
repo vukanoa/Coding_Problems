ae1e9e58d6e520facb3e540e908ff03a067d40c8
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-16 08:18:02 +0100

    Add another Solution to LeetCode: 665_Non-decreasing_Array to N_539

--- 4_N_539/Arrays_and_Hashing/665_Non-decreasing_Array/non-decreasing_array.cpp
+++ 4_N_539/Arrays_and_Hashing/665_Non-decreasing_Array/non-decreasing_array.cpp
@@ -170,3 +170,47 @@ public:
         return decreasing <= 1;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 70.06% */
+/* Space Beats: 10.44% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums)
+    {
+        for (int i = 0; i < nums.size()-1; i++)
+        {
+            if (nums[i] > nums[i+1])
+            {
+                if (i-1 >= 0 && nums[i-1] > nums[i+1])
+                    nums[i+1] = nums[i];
+                else
+                    nums[i] = nums[i+1];
+
+                break;
+            }
+        }
+
+        for (int i = 0; i < nums.size()-1; i++)
+        {
+            if (nums[i] > nums[i+1])
+                return false;
+        }
+
+        return true;
+    }
+};
