3bb4cbde9ea44d9ac37d51ddac084686d1452c7d
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-17 08:57:36 +0100

    Exchange for an Elegant Solution in LeetCode: 1094_Car_Pooling in N_539

--- 4_N_539/Heap/1094_Car_Pooling/car_pooling.cpp
+++ 4_N_539/Heap/1094_Car_Pooling/car_pooling.cpp
@@ -53,8 +53,8 @@
 
 */
 
-/* Time  Beats: 19.13% */
-/* Space Beats:  8.87% */
+/* Time  Beats: 99.48% */
+/* Space Beats: 60.01% */
 
 /* Time  Complexity: O(n * logn) */
 /* Space Complexity: O(n) */
@@ -62,75 +62,43 @@ class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity)
     {
-        std::priority_queue<std::vector<int>,
-                            std::vector<std::vector<int>>,
-                            std::greater<std::vector<int>>> currently_driven;
-
-        // O(n)
-        std::vector<std::vector<int>> rearranged_trips;
-        for (auto& trip : trips)
-            rearranged_trips.push_back( {trip[1], trip[0], trip[2]} );
-
-
-        // O(n)
-        std::priority_queue<std::vector<int>,
-                            std::vector<std::vector<int>>,
-                            std::greater<std::vector<int>>> yet_to_be_driven(rearranged_trips.begin(),
-                                                                             rearranged_trips.end());
-
-        if (capacity < yet_to_be_driven.top()[1])
-            return false;
-
-        int from = yet_to_be_driven.top()[0];
-        int num_passengers = yet_to_be_driven.top()[1];
-        int to   = yet_to_be_driven.top()[2];
-        yet_to_be_driven.pop();
-
-        // In this order because we care the most about the "end location"
-        currently_driven.push( {to, from, num_passengers} );
-
-        int current_location = from;
-        capacity -= num_passengers;
-
         // O(n * logn)
-        while (!yet_to_be_driven.empty())
+        // Sort by index 1's value(second element out of 3) in ASCENDING order
+        std::sort(trips.begin(), trips.end(), [](const std::vector<int>& a,
+                                                 const std::vector<int>& b)
+                                                {
+                                                    return a[1] < b[1];
+                                                });
+
+        std::priority_queue<std::pair<int, int>,
+                            std::vector<std::pair<int, int>>,
+                            std::greater<std::pair<int, int>>> min_heap;
+
+        // O(n * logk)
+        for (std::vector<int>& trip : trips)
         {
-            while (!currently_driven.empty())
-            {
-                std::vector<int> driven = currently_driven.top();
+            int to_take_passengers = trip[0];
+            int from = trip[1];
+            int to = trip[2];
 
-                if (current_location == driven[0]) // driven[2] == "end location", i.e. "to"
-                {
-                    capacity += driven[2];
-                    currently_driven.pop();
-                }
-                else
-                    break;
+            /*
+                min_heap.top().first <==> Earliest destination from some trip
+                                          that already started(its passengers
+                                          are currently in the car)
+            */
+            while (!min_heap.empty() && min_heap.top().first <= from)
+            {
+                int to_drop_passengers = min_heap.top().second;
+                min_heap.pop(); // O(logk)
+
+                capacity += to_drop_passengers;
             }
 
-            std::vector<int> not_driven = yet_to_be_driven.top();
-            if (current_location == not_driven[0])
-            {
-                if (capacity < not_driven[1])
-                    return false;
+            if (capacity < to_take_passengers)
+                return false;
 
-                int from = yet_to_be_driven.top()[0];
-                int num_passengers = yet_to_be_driven.top()[1];
-                int to   = yet_to_be_driven.top()[2];
-                yet_to_be_driven.pop();
-
-                capacity -= num_passengers;
-                currently_driven.push( {to, from, num_passengers} );
-            }
-
-            if (currently_driven.empty() && yet_to_be_driven.empty())
-                return true;
-            else if (!currently_driven.empty() && !yet_to_be_driven.empty())
-                current_location = std::min(currently_driven.top()[0], yet_to_be_driven.top()[0]);
-            else if (!currently_driven.empty())
-                current_location = currently_driven.top()[0];
-            else
-                current_location = yet_to_be_driven.top()[0];
+            capacity -= to_take_passengers;
+            min_heap.push( {to, to_take_passengers} ); // O(logk)
         }
 
         return true;
