a379ca4d7ad42f96798c15dfcaf133bfa80e2803
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-23 06:49:59 +0100

    Remove 3 old Solutions from LeetCode: 79_Word_Search from Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/050_79___Word_Search/word_search.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/050_79___Word_Search/word_search.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <functional> // For Lambda function
 
 /*
     ==============
@@ -52,241 +51,6 @@
 
 */
 
-
-
-/*
-    ------------
-    --- IDEA ---
-    ------------
-
-    It's the same idea as in Solution_Efficient(2nd one down below), but
-    unoptimized. It uses another m * n matrix named "flags".
-
-    However, it's a bit easier for reading in my opinion, so I'll leave it.
-
-*/
-
-
-/* Time  Beats: 51.81% */
-/* Space Beats: 85.30% */
-
-/*
-    Time  Complexity: O(M * N * 4^L)
-
-    The Time Complexity of the "backtracking" function is O(4^L), where L is
-    the length of the input "word". This is because for each call to
-    "backtracking", there are up to four recursive calls (up, right, down,
-    left), and this recursion depth can go up to L.  Therefore, the Time
-    Complexity of the "exist" function is O(M * N * 4^L).
-
-    M - Number of Rows    in the Board.
-    N - Number of Columns in the Board.
-*/
-/*
-    Space Complexity: max( O(L), O(M * N) )
-
-    Since the maximum depth of recursion is L, and each recursion depth
-    requires O(1) extra space. Additionally, the "flags" vector has a Space
-    Complexity of O(M * N).
-
-    M - Number of Rows    in the Board.
-    N - Number of Columns in the Board.
-*/
-class Solution_Messy{
-private:
-    bool backtracking(std::vector<std::vector<char>>& board, std::string word,
-                      std::vector<std::vector<bool>>& flags, std::string current,
-                      int i, int j, int w)
-    {
-        int m = board.size();
-        int n = board[0].size();
-
-        if (current == word)
-            return true;
-
-        for (int x = 0; x < 4; x++)
-        {
-            switch(x)
-            {
-                case 0:
-                    if (i-1 >= 0 && board[i-1][j] == word[w] && flags[i-1][j] == true)
-                    {
-                        flags[i][j] = false;
-                        if (backtracking(board, word, flags, current + word[w], i-1, j, w+1))
-                            return true;
-
-                        flags[i][j] = true;
-                    }
-                    break;
-
-                case 1:
-                    if (j + 1 < n && board[i][j+1] == word[w] && flags[i][j+1] == true)
-                    {
-                        flags[i][j] = false;
-                        if (backtracking(board, word, flags, current + word[w], i, j+1, w+1))
-                            return true;
-
-                        flags[i][j] = true;
-                    }
-                    break;
-
-                case 2:
-                    if (i + 1 < m && board[i+1][j] == word[w] && flags[i+1][j] == true)
-                    {
-                        flags[i][j] = false;
-                        if (backtracking(board, word, flags, current + word[w], i+1, j, w+1))
-                            return true;
-
-                        flags[i][j] = true;
-                    }
-                    break;
-
-                case 3:
-                    if (j - 1 >= 0 && board[i][j - 1] == word[w] && flags[i][j-1] == true)
-                    {
-                        flags[i][j] = false;
-                        if (backtracking(board, word, flags, current + word[w], i, j-1, w+1))
-                            return true;
-
-                        flags[i][j] = true;
-                    }
-                    break;
-            }
-        }
-
-        return false;
-    }
-
-public:
-    bool exist(std::vector<std::vector<char>>& board, std::string word)
-    {
-        int m = board.size();
-        int n = board[0].size();
-
-        std::vector<std::vector<bool>> flags(m, std::vector<bool>(n, true));
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (board[i][j] == word[0])
-                {
-                    std::string str = "";
-                    str.push_back(board[i][j]);
-
-                    if (backtracking(board, word, flags, str, i, j, 1))
-                        return true;
-
-                    str.pop_back();
-                }
-            }
-        }
-
-        return false;
-    }
-};
-
-
-
-
-/*
-    ------------
-    --- IDEA ---
-    ------------
-
-    Same Solution as the below one, but written in an inefficient way. It's
-    good to see the difference, so here it is.
-
-*/
-
-/* Time  Beats: 5.02% */
-/* Space Beats: 5.04% */
-
-/*
-    Time  Complexity: O(M * N * 4^L)
-
-    The Time Complexity of the "backtracking" function is O(4^L), where L is
-    the length of the input "word". This is because for each call to
-    "backtracking", there are up to four recursive calls (up, right, down,
-    left), and this recursion depth can go up to L.  Therefore, the Time
-    Complexity of the "exist" function is O(M * N * 4^L).
-
-    M - Number of Rows    in the Board.
-    N - Number of Columns in the Board.
-*/
-/*
-    Space Complexity: max( O(L), O(M * N) )
-
-    Since the maximum depth of recursion is L, and each recursion depth
-    requires O(1) extra space. Additionally, the "flags" vector has a Space
-    Complexity of O(M * N).
-
-    M - Number of Rows    in the Board.
-    N - Number of Columns in the Board.
-*/
-class Solution_Inefficient {
-public:
-    bool exist(std::vector<std::vector<char>>& board, std::string word)
-    {
-        int m = board.size();
-        int n = board[0].size();
-
-        for (int row = 0; row < m; row++)
-        {
-            for (int col = 0; col < n; col++)
-            {
-                if (board[row][col] == word[0]) // If first letter matches
-                {
-                    char letter = word[0];
-                    board[row][col] = '#';
-
-                    std::string curr_str = "";
-                    if (backtracking(board, word, row, col, curr_str + word[0], 1))
-                        return true;
-
-                    board[row][col] = letter;
-                }
-            }
-        }
-
-        return false;
-    }
-
-private:
-    bool backtracking(std::vector<std::vector<char>>& board, std::string& word, int row, int col, std::string curr_str, int i)
-    {
-        if (curr_str == word)
-            return true;
-
-        int m = board.size();
-        int n = board[0].size();
-
-        // Clockwise
-        // Up, Right, Down, Left
-        std::vector<std::pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
-        for (const auto& dir : directions)
-        {
-            if (row + dir.first  < 0 || row + dir.first  >= m || col + dir.second < 0 || col + dir.second >= n)
-                continue;
-
-            if (board[row + dir.first][col + dir.second] == word[i])
-            {
-                char letter = word[i];
-                board[row + dir.first][col + dir.second] = '#';
-
-                if (backtracking(board, word, row + dir.first, col + dir.second, curr_str + word[i], i + 1))
-                    return true;
-
-                board[row + dir.first][col + dir.second] = letter;
-            }
-        }
-
-        return false;
-    }
-};
-
-
-
 /*
     ------------
     --- IDEA ---
@@ -327,7 +91,7 @@ private:
     Where L is the length of the given word. This is because the recursive
     stack will have at most L frames for each call to dfs.
 */
-class Solution_Efficient {
+class Solution {
 public:
     bool exist(std::vector<std::vector<char>>& board, std::string word)
     {
@@ -373,78 +137,6 @@ private:
 };
 
 
-
-
-/*
-    ------------
-    --- IDEA ---
-    ------------
-
-    It's the same as the one above, however this one uses a Lambda Function.
-    I wanted to show that as well.
-
-*/
-
-/* Time  Beats: 49.50% */
-/* Space Beats: 23.98% */
-
-/*
-    Time  Complexity: O(M * N * 4^L)
-
-    Where L is the length of the given word. This is because for each cell, the
-    algorithm can potentially explore 4 adjacent cells(up, down, left, and
-    right) and it explores up to L characters for each starting position.
-*/
-/*
-    Space Complexity: O(L)
-
-    Where L is the length of the given word. This is because the recursive
-    stack will have at most L frames for each call to dfs.
-*/
-class Solution_Efficient_Lambda {
-public:
-    bool exist(std::vector<std::vector<char>>& board, std::string word)
-    {
-        int m = board.size();
-        int n = board[0].size();
-
-        std::function<bool(int, int, int)> dfs;
-
-        dfs = [&](int i, int j, int w)
-        {
-            if (w == word.length())
-                return true;
-
-            if (i < 0 || j < 0 || i == m || j == n || board[i][j] != word[w])
-                return false;
-
-            char letter = board[i][j]; // So that we don't use the same element twice
-            board[i][j] = '#';
-
-            if (dfs(i-1, j  , w+1)) return true;
-            if (dfs(i+1, j  , w+1)) return true;
-            if (dfs(i  , j-1, w+1)) return true;
-            if (dfs(i  , j+1, w+1)) return true;
-
-            board[i][j] = letter; // Return used element so that we can use it again since we haven't found it yet
-
-            return false;
-        };
-
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-            {
-                if (dfs(i, j, 0))
-                    return true;
-            }
-        }
-
-        return false;
-    }
-};
-
-
 void
 print_matrix(std::vector<std::vector<char>>& matrix)
 {
@@ -462,8 +154,7 @@ print_matrix(std::vector<std::vector<char>>& matrix)
 int
 main()
 {
-    Solution_Messy sol_mess;
-    Solution_Efficient sol_eff;
+    Solution sol;
 
     /* Example 1 */
     std::vector<std::vector<char>> board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}, {'A', 'D', 'E', 'E'}};
@@ -490,7 +181,7 @@ main()
 
     /* Solution */
     // bool found = sol_mess.exist(board, word);
-    bool found = sol_eff.exist(board, word);
+    bool found = sol.exist(board, word);
 
 
     /* Write Output */
