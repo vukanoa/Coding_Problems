17fbcef0150b4c76ea49b23edabb4b724bf9be7f
Noa Vukasovic <noavukasovic@gmail.com>
2024-03-25 08:10:06 +0100

    Rename & Reformat LeetCode: 200_Number_Of_Islands in Top_100_Liked

--- 0_LeetCode_Top_100_Liked_Questions/080_200__Number_Of_Islands/number_of_islands.cpp
+++ 0_LeetCode_Top_100_Liked_Questions/080_200__Number_Of_Islands/number_of_islands.cpp
@@ -3,74 +3,74 @@
 #include<queue>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	===========================
-	200) Number of Islands
-	===========================
+    ===========================
+    200) Number of Islands
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given an m x n 2D binary grid "grid" which represents a map of '1's(land)
-	and '0's(water), return the number of islands.
+    Given an m x n 2D binary grid "grid" which represents a map of '1's(land)
+    and '0's(water), return the number of islands.
 
-	An island is surrounded by water and is formed by connecting adjacent lands
-	horizontally or vertically. You may assume all four edges of the grid are
-	all surrounded by water.
+    An island is surrounded by water and is formed by connecting adjacent lands
+    horizontally or vertically. You may assume all four edges of the grid are
+    all surrounded by water.
 
-	=====================================================
-	FUNCTION: int numIslands(vector<vector<char>>& grid);
-	=====================================================
+    =====================================================
+    FUNCTION: int numIslands(vector<vector<char>>& grid);
+    =====================================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input:  grid = [
-		["1", "1", "1", "1", "0"],
-		["1", "1", "0", "1", "0"],
-		["1", "1", "0", "0", "0"],
-		["0", "0", "0", "0", "0"],
-		]
-	Output: 1
+    --- Example 1 ---
+    Input:  grid = [
+        ["1", "1", "1", "1", "0"],
+        ["1", "1", "0", "1", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "0", "0", "0"],
+        ]
+    Output: 1
 
-	--- Example 2 ---
-	Input:  grid = [
-		["1", "1", "0", "0", "0"],
-		["1", "1", "0", "0", "0"],
-		["0", "0", "1", "0", "0"],
-		["0", "0", "0", "1", "1"],
-		]
-	Output: 3
+    --- Example 2 ---
+    Input:  grid = [
+        ["1", "1", "0", "0", "0"],
+        ["1", "1", "0", "0", "0"],
+        ["0", "0", "1", "0", "0"],
+        ["0", "0", "0", "1", "1"],
+        ]
+    Output: 3
 
-	*** Constraints ***
-	m == grid.length
-	n == grid[i].length
-	1 <= m, n <= 300
-	grid[i][j] is '0' or '1'
+    *** Constraints ***
+    m == grid.length
+    n == grid[i].length
+    1 <= m, n <= 300
+    grid[i][j] is '0' or '1'
 
 */
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	A simple self-explanatory DFS.
+    A simple self-explanatory DFS.
 
-	The only important thing here is that we're somehow allowed to modify our
-	"grid" matrix.
+    The only important thing here is that we're somehow allowed to modify our
+    "grid" matrix.
 
-	If that wasn't the case then we would've had to use an unordered_set and/or
-	we could implement a BFS Solution instead of DFS.
+    If that wasn't the case then we would've had to use an unordered_set and/or
+    we could implement a BFS Solution instead of DFS.
 
-	There is also an elegant Find&Union Solution, but I won't implement it yet.
+    There is also an elegant Find&Union Solution, but I won't implement it yet.
 
 */
 
@@ -78,67 +78,71 @@
 /* Space Beats: 85.21% */
 
 /* Time  Complexity:
-	O(n) or O(row * columns) or O(V) 'V' for Vertices in Graph
+    O(n) or O(row * columns) or O(V) 'V' for Vertices in Graph
 */
 /* Space Complexity: O(n)
-	Recursion can go up to N levels deep since we can have all 1's in "grid".
+    Recursion can go up to N levels deep since we can have all 1's in "grid".
 */
 class Solution_DFS {
 public:
-	int numIslands(std::vector<std::vector<char>>& grid)
-	{
-		int islands = 0;
-		int m = grid.size();
-		int n = grid[0].size();
+    int numIslands(std::vector<std::vector<char>>& grid)
+    {
+        int islands = 0;
 
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (grid[i][j] == '1')
-				{
-					dfs(grid, i, j);
-					islands++;
-				}
-			}
-		}
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
 
-		return islands;
-	}
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    dfs(grid, i, j);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
 
 private:
-	void dfs(std::vector<std::vector<char>>& grid, int i, int j)
-	{
-		if (i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size())
-			return;
+    void dfs(std::vector<std::vector<char>>& grid, int i, int j)
+    {
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
 
-		if (grid[i][j] != '1')
-			return;
+        if (i < 0 || j < 0 || i == ROWS || j >= COLS)
+            return;
 
-		grid[i][j] = 'x'; // visited
+        if (grid[i][j] != '1')
+            return;
 
-		/* Clockwise */
-		dfs(grid, i - 1, j    ); // Up
-		dfs(grid, i    , j + 1); // Right
-		dfs(grid, i + 1, j    ); // Down
-		dfs(grid, i    , j - 1); // Left
-	}
+        grid[i][j] = 'x'; // visited
+
+        /* Signing Cross */
+        dfs(grid, i-1, j  ); // Up
+        dfs(grid, i+1, j  ); // Down
+        dfs(grid, i  , j+1); // Right
+        dfs(grid, i  , j-1); // Left
+    }
 };
 
 
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	It's a similar IDEA, just implemented using BFS.
+    It's a similar IDEA, just implemented using BFS.
 
-	I wanted to implement it in as different way as possible, that's why I've
-	used: lambda function, directions, pairs, etc.
+    I wanted to implement it in as different way as possible, that's why I've
+    used: lambda function, directions, pairs, etc.
 
-	BFS does NOT use recursion. BFS uses a Queue, instead.
+    BFS does NOT use recursion. BFS uses a Queue, instead.
 
 */
 
@@ -149,127 +153,127 @@ private:
 /* Space Complexity: O(n) */
 class Solution_BFS{
 public:
-	int numIslands(std::vector<std::vector<char>>& grid)
-	{
-		if (grid.size() == 0 || grid[0].size() == 0)
-			return 0;
+    int numIslands(std::vector<std::vector<char>>& grid)
+    {
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
 
-		int m = grid.size();
-		int n = grid[0].size();
+        /* Signing Cross */
+        // (Up, Down, Left, Right)
+        std::vector<std::pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-		std::vector<std::pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-		int islands = 0;
+        int islands = 0;
 
-		/* Lambda */
-		auto inside = [&](int i, int j)
-		{
-			return i >= 0 && j >= 0 && i < m && j < n;
-		};
+        /* Lambda */
+        auto inside = [&](int i, int j)
+        {
+            return i >= 0 && j >= 0 && i < ROWS && j < COLS;
+        };
 
-		for (int i = 0; i < m; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (inside(i, j) && grid[i][j] == '1')
-				{
-					islands++;
-					/* If we are not allowed to modify original given "grid",
-					   then use vector<vector<bool>> visisted.
-					*/
-					grid[i][j] = 'x'; // Mark as processed
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                if (inside(i, j) && grid[i][j] == '1')
+                {
+                    islands++;
+                    /* If we are not allowed to modify original given "grid",
+                       then use vector<vector<bool>> visisted.
+                    */
+                    grid[i][j] = 'x'; // Mark as processed
 
-					/* BFS */
-					std::queue<std::pair<int, int>> queue;
-					queue.push({i, j});
+                    /* BFS */
+                    std::queue<std::pair<int, int>> queue;
+                    queue.push({i, j});
 
-					while (!queue.empty())
-					{
-						std::pair<int, int> curr = queue.front();
-						queue.pop();
+                    while (!queue.empty())
+                    {
+                        std::pair<int, int> curr = queue.front();
+                        queue.pop();
 
-						for (const std::pair<int, int>& dir : directions)
-						{
-							int row = curr.first  + dir.first;
-							int col = curr.second + dir.second; 
+                        for (const std::pair<int, int>& dir : directions)
+                        {
+                            int row = curr.first  + dir.first;
+                            int col = curr.second + dir.second;
 
-							if (inside(row, col) && grid[row][col] == '1')
-							{
-								grid[row][col] = 'x';
-								queue.push({row, col}); // Mark as processed
-							}
-						}
-					}
-				}
-			}
-		}
+                            if (inside(row, col) && grid[row][col] == '1')
+                            {
+                                grid[row][col] = 'x'; // Mark as processed
+                                queue.push({row, col});
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		return islands;
-	}
+        return islands;
+    }
 };
 
 
 int
 main()
 {
-	Solution_DFS sol_dfs;
-	Solution_BFS sol_bfs;
+    Solution_DFS sol_dfs;
+    Solution_BFS sol_bfs;
 
 
-	/* Example 1 */
-	// std::vector<std::vector<char>> grid {
-	// 	{'1', '1', '1', '1', '0'},
-	// 	{'1', '1', '0', '1', '0'},
-	// 	{'1', '1', '0', '0', '0'},
-	// 	{'0', '0', '0', '0', '0'}
-	// };
+    /* Example 1 */
+    // std::vector<std::vector<char>> grid {
+    //     {'1', '1', '1', '1', '0'},
+    //     {'1', '1', '0', '1', '0'},
+    //     {'1', '1', '0', '0', '0'},
+    //     {'0', '0', '0', '0', '0'}
+    // };
 
-	/* Example 2 */
-	std::vector<std::vector<char>> grid {
-		{'1', '1', '0', '0', '0'},
-		{'1', '1', '0', '0', '0'},
-		{'0', '0', '1', '0', '0'},
-		{'0', '0', '0', '1', '1'}
-	};
-
-
-	std::cout << "\n\t=========================";
-	std::cout << "\n\t=== NUMBER OF ISLANDS ===";
-	std::cout << "\n\t=========================\n";
+    /* Example 2 */
+    std::vector<std::vector<char>> grid {
+        {'1', '1', '0', '0', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '1', '0', '0'},
+        {'0', '0', '0', '1', '1'}
+    };
 
 
-	/* Write Input */
-	bool first = true;
-	std::cout << "\n\tGrid: \n\t[\n\t\t";
-	for (auto x: grid)
-	{
-		if (!first)
-			std::cout << ", \n\t\t";
-
-		bool first_first = true;
-		std::cout << "[";
-		for (const auto& xx : x)
-		{
-			if (!first_first)
-				std::cout << ", ";
-
-			std::cout << "\"" << xx << "\"";
-			first_first = false;
-		}
-		std::cout << "]";
-
-		first = false;
-	}
-	std::cout << "\n\t]\n";
+    std::cout << "\n\t=========================";
+    std::cout << "\n\t=== NUMBER OF ISLANDS ===";
+    std::cout << "\n\t=========================\n";
 
 
-	/* Solution */
-	int islands = sol_dfs.numIslands(grid);
-	// int islands = sol_bfs.numIslands(grid);
+    /* Write Input */
+    bool first = true;
+    std::cout << "\n\tGrid: \n\t[\n\t\t";
+    for (auto x: grid)
+    {
+        if (!first)
+            std::cout << ", \n\t\t";
+
+        bool first_first = true;
+        std::cout << "[";
+        for (const auto& xx : x)
+        {
+            if (!first_first)
+                std::cout << ", ";
+
+            std::cout << "\"" << xx << "\"";
+            first_first = false;
+        }
+        std::cout << "]";
+
+        first = false;
+    }
+    std::cout << "\n\t]\n";
 
 
-	/* Write Output */
-	std::cout << "\n\tNumber of Islands: " << islands << "\n\n";
+    /* Solution */
+    int islands = sol_dfs.numIslands(grid);
+    // int islands = sol_bfs.numIslands(grid);
 
 
-	return 0;
+    /* Write Output */
+    std::cout << "\n\tNumber of Islands: " << islands << "\n\n";
+
+
+    return 0;
 }
