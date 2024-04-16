2ff0d070e03490e57f3d0dd4ec8d4364ee378e2b
Noa Vukasovic <noavukasovic@gmail.com>
2024-04-16 06:26:37 +0200

    Add another Solution to LeetCode: 82_Remove_Duplicates_From_Soted_List_II to Orderless_Problems

--- Orderless_Problems/82_Remove_Duplicates_From_Sorted_List_II/remove_duplicates_from_sorted_list_ii.cpp
+++ Orderless_Problems/82_Remove_Duplicates_From_Sorted_List_II/remove_duplicates_from_sorted_list_ii.cpp
@@ -52,6 +52,24 @@
  * };
  */
 
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    The only thing that is difficult here is to come up with a "dummy node".
+    However, once you know about the general trick for Linked List problems,
+    i.e. using a "dummy node", then it becomes straightforward.
+
+    The rest of the Solution is intuitive. It's not a trick or technique, it's
+    just logic.
+
+    Therefore, once you start using a "dummy node", you should be able to come
+    up with this Solution. (Or the 2nd one down below).
+
+*/
+
 /* Time  Beats: 88.66% */
 /* Space Beats: 72.20% */
 
@@ -87,6 +105,52 @@ public:
 
             if (!duplicate)
                 prev->next = nullptr; // Unlink from the original list
+        }
+
+        return dummy.next;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Here, again, the whole point is to know about the "dummy node" trick. Once
+    you do that, then there are myriad of ways to solve it.
+
+    This is another way. It's more concise.
+
+*/
+
+/* Time  Beats: 88.66% */
+/* Space Beats: 22.26% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_2 {
+public:
+    ListNode* deleteDuplicates(ListNode* head)
+    {
+       ListNode dummy(-101, head);
+       ListNode *prev = &dummy;
+
+        while (head)
+        {
+            if(head->next && head->next->val == head->val)
+            {
+                while (head->next && head->next->val == head->val)
+                    head = head->next;
+
+                prev->next=head->next;
+            }
+            else
+                prev = prev->next;
+
+            head = head->next;
         }
 
         return dummy.next;
