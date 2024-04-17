feea3a9ed162d19126ae609ee2bfa5118073221d
Noa Vukasovic <noavukasovic@gmail.com>
2024-04-17 07:39:05 +0200

    Add LeetCode: 111_Minimum_Depth_Of_Binary_Tree to Orderless_Problems

---
+++ Orderless_Problems/111_Minimum_Depth_Of_Binary_Tree/minimum_death_of_binary_tree.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    =================================
+    111) Minimum Death of Binary Tree
+    =================================
+
+    ============
+    Description:
+    ============
+
+    Given a binary tree, find its minimum depth.
+
+    The minimum depth is the number of nodes along the shortest path from the
+    root node down to the nearest leaf node.
+
+    Note: A leaf is a node with no children.
+
+    =======================================
+    FUNCTION: int minDepth(TreeNode* root);
+    =======================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+
+                                    3
+                                  /   \
+                                 /     \
+                                /       \
+                               /         \
+                              /           \
+                             9            20
+                                          / \
+                                         /   \
+                                        /     \
+                                      15       7
+
+    Input: root = [3,9,20,null,null,15,7]
+    Output: 2
+
+
+    --- Example 2 ---
+    Input: root = [2,null,3,null,4,null,5,null,6]
+    Output: 5
+
+
+    *** Constraints ***
+    The number of nodes in the tree is in the range [0, 10^5].
+    -1000 <= Node.val <= 1000
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-Explnatory. This is one of the most fundamental techniques. It uses
+    the most basic form of DFS traversal along with findind the minimum within
+    the function of DFS.
+
+    That's it.
+
+*/
+
+/* Time  Beats: 82.02% */
+/* Space Beats: 93.27% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution {
+public:
+    int minDepth(TreeNode* root)
+    {
+        int min_depth = INT_MAX;
+        dfs(root, 1, min_depth);
+
+        return min_depth == INT_MAX ? 0 : min_depth;
+    }
+
+private:
+    void dfs(TreeNode* root, int curr_depth, int& min_depth)
+    {
+        if (!root)
+            return;
+
+        if (!root->left && !root->right)
+        {
+            min_depth = std::min(min_depth, curr_depth);
+            return;
+        }
+
+        dfs(root->left,  curr_depth+1, min_depth);
+        dfs(root->right, curr_depth+1, min_depth);
+    }
+};
