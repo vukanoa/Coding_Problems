11b6678f039b54301d43b95dc93ec4b9ad088567
Noa Vukasovic <noavukasovic@gmail.com>
2024-04-17 07:15:42 +0200

    Add an Efficient Solution to LeetCode: 99_Recover_Binary_Search_Tree to Orderless_Problems

--- Orderless_Problems/99_Recover_Binary_Search_Tree/recover_binary_search_tree.cpp
+++ Orderless_Problems/99_Recover_Binary_Search_Tree/recover_binary_search_tree.cpp
@@ -158,3 +158,115 @@ private:
         inorder(root->right, nodes);
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This is essentially a similar approach, however this one doesn't use any
+    extra space. It doesn't even use a recursion. Instead, what it does is it
+    implements a "Morris inorder traversal" after which it just swaps elements
+    pointed to by "first" and "second".
+
+
+    The intuition behind this code is to use a modified Morris traversal to
+    find the two nodes in a BST that have been swapped. In a Morris traversal,
+    we thread the right pointer of each node to its predecessor in the inorder
+    traversal. This allows us to traverse the tree without using a stack.
+
+    Initialize two pointers, prev and current, to null. While current is NOT
+    null:
+
+        If current->left is NOT null:
+            Find the predecessor of current in the inorder traversal.
+
+            If the predecessor is nullptr:
+                current is the first node in the inorder traversal.
+            else
+                thread the predecessor's right pointer to current.
+
+        Set current to current->left.
+
+        Otherwise, if current->right is NOT nullptr and current->right is NOT
+        the predecessor of current:
+
+            Set current to current->right.
+
+        Otherwise, if prev is NOT nullptr and prev->val > current->val:
+            If first is nullptr, then prev is the first node in the inorder
+            traversal.
+
+        Set second  to current.
+        Set prev    to current.
+        Set current to current->right.
+
+
+    If first and second are not null, then swap their values.
+
+*/
+
+/* Time  Beats: 56.11% */
+/* Space Beats: 99.02% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution_Morris {
+public:
+    void recoverTree(TreeNode* root)
+    {
+        TreeNode* prev    = nullptr;
+        TreeNode* current = root;
+        TreeNode* first   = nullptr;
+        TreeNode* second  = nullptr;
+
+        while (current)
+        {
+            if (current->left)
+            {
+                TreeNode* tmp = current->left;
+
+                while (tmp->right && tmp->right != current)
+                    tmp = tmp->right;
+
+                if (tmp->right == nullptr)
+                {
+                    tmp->right = current;
+                    current = current->left;
+                }
+                else
+                {
+                    tmp->right = nullptr;
+                    if (prev && prev->val > current->val)
+                    {
+                        if (first == nullptr)
+                            first = prev;
+
+                        second = current;
+                    }
+
+                    prev    = current;
+                    current = current->right;
+                }
+            }
+            else
+            {
+                if (prev && prev->val > current->val)
+                {
+                    if (first == nullptr)
+                        first = prev;
+
+                    second = current;
+                }
+
+                prev = current;
+                current = current->right;
+            }
+        }
+
+        std::swap(first->val, second->val);
+    }
+};
