5e58b22f9967c98c39f4c878ab128c498898d885
Noa Vukasovic <noavukasovic@gmail.com>
2024-04-24 07:48:52 +0200

    Add Follow_Up Solution to LeetCode: 231_Power_Of_Two to Orderless_Problems

--- Orderless_Problems/231_Power_Of_Two/power_of_twoc.cpp
+++ Orderless_Problems/231_Power_Of_Two/power_of_twoc.cpp
@@ -200,3 +200,104 @@ public:
         return false;
     }
 };
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This one is not easy to come up with, but it's not too difficult.
+
+    First, as we've concluded above: NEGATIVE NUMBER ARE NOT CONSIDERED A POWER
+    OF TWO.
+
+    Therefore we only need to check if n > 0.
+
+    Let's take a look at some examples:
+
+        8 = 0000 0000 0000 0000 0000 0000 0000 1000
+       16 = 0000 0000 0000 0000 0000 0000 0001 0000
+       32 = 0000 0000 0000 0000 0000 0000 0010 0000
+       64 = 0000 0000 0000 0000 0000 0000 0100 0000
+
+    Let's consider, for example, number 64:
+
+       64 = 0000 0000 0000 0000 0000 0000 0100 0000
+
+    If we can somehow get this number:
+
+       ?? = 0000 0000 0000 0000 0000 0000 0011 1111
+
+    and &&(bitiwse AND) it with 64, then and only then would we get a result 0.
+    If we get a 0, only then are we sure n is indeed a power of two.
+
+    But how can we get this other number?
+
+    It's surprisingly simple. That number is simply n-1.
+
+       n = 64
+
+       64 = 0000 0000 0000 0000 0000 0000 0100 0000
+       63 = 0000 0000 0000 0000 0000 0000 0011 1111  // 63 = n-1
+
+    If we now bitise AND them:
+        0000 0000 0000 0000 0000 0000 0100 0000
+      & 0000 0000 0000 0000 0000 0000 0011 1111
+        ---------------------------------------
+        0000 0000 0000 0000 0000 0000 0000 0000
+
+
+
+    Now let's consider some number that WASN'T a power of 2.
+    For example:
+
+       72 = 0000 0000 0000 0000 0000 0000 0100 1000
+
+    n   = 72
+    n-1 = 71
+
+
+       72 = 0000 0000 0000 0000 0000 0000 0100 1000
+       71 = 0000 0000 0000 0000 0000 0000 0100 0111
+
+    If we now bitise AND them:
+
+        0000 0000 0000 0000 0000 0000 0100 1000
+      & 0000 0000 0000 0000 0000 0000 0100 0111
+        ---------------------------------------
+        0000 0000 0000 0000 0000 0000 0100 0000 // 64
+
+    Since the result is NOT equal to 0, we conclude that n WASN'T a power of 2.
+
+
+    Binary representation of a number-1 is found if you take all the 0's to the
+    right of the rightmost 1 and flip them to 0's, while you flip only that
+    rightmost 1 to 0.
+
+                                               0's to the right of rightmost 1
+                                                |||
+                                                |||
+                                                vvv
+       72 = 0000 0000 0000 0000 0000 0000 0100 1000
+                                           ^   ^
+                              _____________|   |
+                              |                |
+             We don't touch this one         rightmost 1
+
+
+       71 = 0000 0000 0000 0000 0000 0000 0100 0111
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  36.83% */
+
+/* Time  Complexity: O(1) */
+/* Space Complexity: O(1) */
+class Solution_Follow_Up {
+public:
+    bool isPowerOfTwo(int n)
+    {
+        return n > 0 && (n & (n-1)) == 0;
+    }
+};
