7edfc666dc65ac1a271d020787c5d69d1a1e5604
Noa Vukasovic <noavukasovic@gmail.com>
2024-05-14 13:42:10 +0200

    Add Efficient Solution to LeetCode: 1219_Path_With_Maximum_Gold to Orderless_Problems

--- Orderless_Problems/1219_Path_With_Maximum_Gold/path_with_maximum_gold.cpp
+++ Orderless_Problems/1219_Path_With_Maximum_Gold/path_with_maximum_gold.cpp
@@ -138,3 +138,130 @@ private:
         return curr_cell_gold + std::max( {up, down, left, right} );
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    We've optimized the above Solution by using the "grid_with_no_empty_cells"
+    function.
+
+    What that function does is - It it adding up all the cells, if at any point
+    we stumble upon an empty cell(a cell with 0 gold) we immediately return
+    false, because the grid does INDEED contain a cell that is empty.
+
+    However, if NO cell is empty, i.e. each cell contain some amount of gold,
+    then we don't care about the "path". We can simply return the sum of all
+    the cells. That will be our answer no matters what. It doesn't matter where
+    we are starting.
+
+    Had we not have this optimization(as in the above Solution) what we would
+    do if there were no empty cells is: We would do THE SAME thing for each and
+    every cell in the grid. Therefore, that's a ROWS * COLS - 1 redundant
+    calculations.
+
+    Consider this example:
+
+    We can start at (0,0) and the result would be 39.
+    We can start at (0,1) and the result would be 39.
+    ...
+
+    We can start at (2,2) and the result would be 39.
+
+                          0     1     2
+                       +-----+-----+-----+
+                    0  |  1  |  6  |  1  |
+                       +-----+-----+-----+
+                    1  |  5  |  8  |  7  |
+                       +-----+-----+-----+
+                    2  |  1  |  9  |  1  |
+                       +-----+-----+-----+
+
+
+    No matter where we start, if there are NO empty cells, the path is
+    irrelevant. We'll always have the same result.
+
+    That's why, if there are no empty cells, we return the sum of the entire
+    grid.
+
+    That way we've optimized the above Solution.
+
+*/
+
+/* Time  Beats: 99.34% */
+/* Space Beats: 36.97% */
+
+/* Time  Complexity: O(m * n * 4^(m * n)) */
+/* Space Complexity: O(4^(m * n))         */
+class Solution_Efficient {
+public:
+    int getMaximumGold(std::vector<std::vector<int>>& grid)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        int result = 0;
+
+        /* Optimization */
+        if (grid_with_no_empty_cells(grid, result))
+            return result;
+
+        // If we haven't returned "result" up above, then result is certainly 0
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                if (grid[i][j] > 0)
+                    result = std::max(result, dfs(grid, i, j));
+            }
+        }
+
+        return result;
+    }
+
+private:
+    int dfs(std::vector<std::vector<int>>& grid, int i, int j)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        if (i < 0 || j < 0 || i == ROWS || j == COLS || grid[i][j] == 0)
+            return 0;
+
+        int curr_cell_gold = grid[i][j];
+        grid[i][j] = 0; // To prevent visiting the same cell more than once
+
+        int up    = dfs(grid, i-1, j  );
+        int down  = dfs(grid, i+1, j  );
+        int left  = dfs(grid, i  , j-1);
+        int right = dfs(grid, i  , j+1);
+
+        grid[i][j] = curr_cell_gold; // Return original gold from that cell
+
+        return curr_cell_gold + std::max( {up, down, left, right} );
+    }
+
+    bool grid_with_no_empty_cells(vector<vector<int>>& grid, int& result)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        for(int i = 0; i < ROWS; i++)
+        {
+            for(int j = 0; j < COLS; j++)
+            {
+                if (grid[i][j] == 0)
+                    return false;
+
+                result += grid[i][j];
+            }
+        }
+
+        return true;
+    }
+};
