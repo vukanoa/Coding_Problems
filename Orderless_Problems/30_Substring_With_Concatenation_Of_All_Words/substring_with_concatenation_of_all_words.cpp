8f2213a03b4ade8b94002de0d192e12b2ddc94b3
Noa Vukasovic <noavukasovic@gmail.com>
2024-05-21 06:25:10 +0200

    Add TLE Solution to LeetCode: 30_Substring_With_Concatenation_Of_All_Words to Orderless_Problems

--- Orderless_Problems/30_Substring_With_Concatenation_Of_All_Words/substring_with_concatenation_of_all_words.cpp
+++ Orderless_Problems/30_Substring_With_Concatenation_Of_All_Words/substring_with_concatenation_of_all_words.cpp
@@ -77,6 +77,105 @@
 */
 
 /*
+    ===========================
+    === THIS CODE GIVES TLE ===
+    ===========================
+
+    But I wanted to include it in this file anyway. It's nice to see the Brute
+    force Solution and an Optimized one afterwards.
+
+    This is WAAAAAAAAAY to inefficient, but it is composed of different parts
+    that are or could be useful for other problems, that's why I chose to
+    indlude it in this file.
+
+*/
+
+/* Time  Complexity: O(n * n!) */
+/* Space Complexity: O(n!) */
+class Solution_TLE {
+public:
+    vector<int> findSubstring(string s, vector<string>& words)
+    {
+        const int WORD_LENGTH = words[0].length();
+        const int WORDS = words.size();
+        const int ONE_PERMUTATION_LENGTH = WORDS * WORD_LENGTH;
+
+        if (s.length() < ONE_PERMUTATION_LENGTH)
+            return {};
+
+        std::vector<int> results;
+
+        std::vector<int> words_indices(WORDS);
+        std::iota(words_indices.begin(), words_indices.end(), 0); // words_indices = [0, 1, 2, 3, 4]
+
+        std::vector<std::vector<int>> permutations = permute(words_indices);
+        std::unordered_set<std::string> uset_string_permutations;
+
+        std::vector<int> vec = {1,3,2,0};
+        for (int i = 0; i < permutations.size(); i++)
+        {
+            std::ostringstream curr_string_permutation;
+            for (int j = 0; j < permutations[0].size(); j++)
+            {
+                curr_string_permutation << words[permutations[i][j]];
+            }
+
+            uset_string_permutations.insert(curr_string_permutation.str());
+        }
+
+        for (int i = 0; i <= s.length()-ONE_PERMUTATION_LENGTH; i++)
+        {
+            std::string substr = s.substr(i, ONE_PERMUTATION_LENGTH);
+
+            if (uset_string_permutations.find(substr) != uset_string_permutations.end())
+                results.push_back(i);
+        }
+
+        return results;
+    }
+
+private:
+    std::vector<std::vector<int>> permute(std::vector<int>& nums)
+    {
+        std::vector<std::vector<int>> results;
+        std::unordered_set<int> uset_permutations; // Unordered set of permutations
+
+        backtracking(nums, {}, uset_permutations, results);
+
+        return results;
+    }
+
+    void backtracking(std::vector<int>& nums,
+                      std::vector<int> curr_permutation,
+                      std::unordered_set<int>& uset_permutations,
+                      std::vector<std::vector<int>>& results)
+    {
+        if (curr_permutation.size() == nums.size())
+        {
+            results.push_back(curr_permutation);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (uset_permutations.count(nums[i])) // If it exists in the Set
+                continue;
+
+            curr_permutation.push_back(nums[i]);
+            uset_permutations.insert(nums[i]);
+
+            backtracking(nums, curr_permutation, uset_permutations, results);
+
+            uset_permutations.erase(nums[i]);
+            curr_permutation.pop_back();
+        }
+    }
+};
+
+
+
+
+/*
     ------------
     --- IDEA ---
     ------------
