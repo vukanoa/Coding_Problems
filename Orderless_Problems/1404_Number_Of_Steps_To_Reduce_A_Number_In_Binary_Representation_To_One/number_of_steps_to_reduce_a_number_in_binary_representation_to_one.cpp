c44f999a6910cbfda8069c77138de2f7d399542e
Noa Vukasovic <noavukasovic@gmail.com>
2024-05-29 13:22:45 +0200

    Add LeetCode: 1404_Number_Of_Steps_To_Reduce_A_Number_In_Binary_Representation_To_One to Orderless_Problems

---
+++ Orderless_Problems/1404_Number_Of_Steps_To_Reduce_A_Number_In_Binary_Representation_To_One/number_of_steps_to_reduce_a_number_in_binary_representation_to_one.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ========================================================================
+    1404) Number of Steps to Reduce a Number in Binary Representation to One
+    ========================================================================
+
+    ============
+    Description:
+    ============
+
+    Given the binary representation of an integer as a string s, return the
+    number of steps to reduce it to 1 under the following rules:
+
+        + If the current number is even, you have to divide it by 2.
+
+        + If the current number is odd, you have to add 1 to it.
+
+    It is guaranteed that you can always reach one for all test cases.
+
+    =================================
+    FUNCTION: int numSteps(string s);
+    =================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: s = "1101"
+    Output: 6
+    Explanation: "1101" corressponds to number 13 in their decimal
+                 representation.
+    Step 1) 13 is odd, add 1 and obtain 14.
+    Step 2) 14 is even, divide by 2 and obtain 7.
+    Step 3) 7 is odd, add 1 and obtain 8.
+    Step 4) 8 is even, divide by 2 and obtain 4.
+    Step 5) 4 is even, divide by 2 and obtain 2.
+    Step 6) 2 is even, divide by 2 and obtain 1.
+
+
+    --- Example 2 ---
+    Input: s = "10"
+    Output: 1
+    Explanation: "10" corressponds to number 2 in their decimal representation.
+    Step 1) 2 is even, divide by 2 and obtain 1.
+
+
+    --- Example 3 ---
+    Input: s = "1"
+    Output: 0
+
+
+    *** Constraints ***
+    1 <= s.length <= 500
+    s consists of characters '0' or '1'
+    s[0] == '1'
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Intuitive.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats: 76.47% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int numSteps(string s)
+    {
+        const int N = s.length();
+
+        int result = N-1;
+        int carry  = 0;
+
+        for (int i = N-1; i > 0; i--)
+        {
+            // (s[i] - '0') is an integer, either 0 or 1
+            if ((s[i] - '0') + carry == 1)
+            {
+                result++;
+                carry = 1;
+            }
+        }
+
+        return result + carry;
+    }
+};
