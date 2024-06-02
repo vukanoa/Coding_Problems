a8200a2b0031443bf75898c1ba7e8ed4c5f9c6f2
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-02 21:14:48 +0200

    Reformat LeetCode: 344_Reverse_String in Top_150_Interview

--- 1_LeetCode_Top_150_Interview_Questions/18_344_Reverse_String/reverse_string.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/18_344_Reverse_String/reverse_string.cpp
@@ -1,54 +1,54 @@
 #include <iostream>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	===========================
-	344) Reverse String
-	===========================
+    ===========================
+    344) Reverse String
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Write a function that reverses a string. The input string is given as an
-	array of characters s.
+    Write a function that reverses a string. The input string is given as an
+    array of characters s.
 
-	You must do this by modifying the input array in-place with O(1) extra
-	memory.
+    You must do this by modifying the input array in-place with O(1) extra
+    memory.
 
-	==============================================
-	FUNCTION: void reverseString(vector<char>& s);
-	==============================================
+    ==============================================
+    FUNCTION: void reverseString(vector<char>& s);
+    ==============================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: s = ["h","e","l","l","o"]
-	Output: ["o","l","l","e","h"]
+    --- Example 1 ---
+    Input: s = ["h","e","l","l","o"]
+    Output: ["o","l","l","e","h"]
 
-	--- Example 2 ---
-	Input: s = ["H","a","n","n","a","h"]
-	Output: ["h","a","n","n","a","H"]
+    --- Example 2 ---
+    Input: s = ["H","a","n","n","a","h"]
+    Output: ["h","a","n","n","a","H"]
 
 
-	*** Constraints ***
-	1 <= s.length <= 105
-	s[i] is a printable ascii character.
+    *** Constraints ***
+    1 <= s.length <= 105
+    s[i] is a printable ascii character.
 
 */
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Self Explanatory.
-	
+    Self Explanatory.
+
 */
 
 /* Time  Beats: 87.20% */
@@ -58,12 +58,12 @@
 /* Space Complexity: O(1) */
 class Solution{
 public:
-	void reverseString(std::vector<char>& s)
-	{
-		int left  = 0;
-		int right = s.size() - 1;
+    void reverseString(std::vector<char>& s)
+    {
+        int left  = 0;
+        int right = s.size() - 1;
 
-		while (left < right)
-			std::swap(s[left++], s[right--]);
-	}
+        while (left < right)
+            std::swap(s[left++], s[right--]);
+    }
 };
