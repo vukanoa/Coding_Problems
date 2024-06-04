1929f591b78ac457a991ac26e7f801ff1da23211
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-04 21:12:38 +0200

    Add LeetCode: 342_Power_Of_Four to Orderless_Problems

---
+++ Orderless_Problems/342_Power_Of_Four/power_of_four.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+/*
+    ============
+    === EASY ===
+    ============
+
+    ===========================
+    342) Power Of Four
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    Given an integer n, return true if it is a power of four. Otherwise, return
+    false.
+
+    An integer n is a power of four, if there exists an integer x such that n
+    == 4x.
+
+
+    ---------------
+    -- Follow Up --
+    ---------------
+
+        Could you solve it without loops/recursion?
+
+
+    ====================================
+    FUNCTION: bool isPowerOfFour(int n);
+    ====================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: n = 16
+    Output: true
+
+    --- Example 2 ---
+    Input: n = 5
+    Output: false
+
+    --- Example 3 ---
+    Input: n = 1
+    Output: true
+
+
+    *** Constraints ***
+    -2^31 <= n <= 2^31 - 1
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Simple math.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  10.78% */
+
+/* Time  Complexity: O(1) */
+/* Space Complexity: O(1) */
+class Solution_Follow_Up {
+public:
+    bool isPowerOfFour(int n)
+    {
+        if (n == 0)
+            return false;
+
+        return is_integer(log_base_b(n, 4));
+    }
+
+private:
+    double log_base_b(int num, int b)
+    {
+        return std::log2(num) / std::log2(b);
+    }
+
+    bool is_integer(double number)
+    {
+        return std::floor(number) == number;
+    }
+};
