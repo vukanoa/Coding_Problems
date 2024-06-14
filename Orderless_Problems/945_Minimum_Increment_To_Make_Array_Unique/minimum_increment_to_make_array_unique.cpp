a46b3f785f37f044543ad8708298c319289f9b2a
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-14 14:40:19 +0200

    Add another Solution to LeetCode: 945_Minimum_Increment_To_Make_Array_Unique to Orderless_Problems

--- Orderless_Problems/945_Minimum_Increment_To_Make_Array_Unique/minimum_increment_to_make_array_unique.cpp
+++ Orderless_Problems/945_Minimum_Increment_To_Make_Array_Unique/minimum_increment_to_make_array_unique.cpp
@@ -75,3 +75,55 @@ public:
         return result;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    1. Sorting:
+        Sort the input array to simplify sequential processing.
+
+    2. Initialization:
+        Set up variables for the target unique value and the total increments
+        counter.
+
+    3. Iteration and Adjustment:
+        For each number, adjust it to at least match the last unique value,
+        increasing the total increments as needed.
+
+    4. Result:
+        Return the cumulative number of increments required to ensure all
+        numbers are unique.
+
+*/
+
+/* Time  Beats: 84.45% */
+/* Space Beats: 21.06% */
+
+/* Time  Complexity: O(n * logn) */
+/* Space Complexity: O(1)        */
+class Solution_2 {
+public:
+    int minIncrementForUnique(std::vector<int>& nums)
+    {
+        /* Sort */
+        std::sort(nums.begin(), nums.end());
+
+        int num_tracker   = 0; // Tracks next unique number that should be set.
+        int min_increment = 0; // Counts the total increments required.
+
+        for (const int& num : nums)
+        {
+            num_tracker    = std::max(num_tracker, num);
+            min_increment += num_tracker - num;
+
+            num_tracker   += 1; // Increment the tracker for the next number.
+        }
+
+        return min_increment;
+    }
+};
