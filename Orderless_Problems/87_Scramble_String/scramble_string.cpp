1692e49c02228e68d5acce1af78e946814ad713b
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-15 22:26:48 +0200

    Add Top_Down Memoization to LeetCode: 87_Scramble_String to Orderless_Problems

--- Orderless_Problems/87_Scramble_String/scramble_string.cpp
+++ Orderless_Problems/87_Scramble_String/scramble_string.cpp
@@ -134,3 +134,124 @@ public:
         return umap[s1 + " " + s2] = false;
     }
 };
+
+
+
+
+// Recursive approach - gives TLE
+class Solution_TLE {
+public:
+    bool scramble(string s, string t)
+    {
+        const int n = s.size();
+
+        if(s == t)
+            return true;
+
+        std::vector<int> letters_freq(26, 0);
+        for(int i = 0; i < n; i++)
+        {
+            letters_freq[s[i] - 'a']++;
+            letters_freq[t[i] - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++)
+        {
+            if (letters_freq[i])
+                return false;
+        }
+
+        // s=gr|eat  t=gr|eat   (not scrambled)
+        // s=gr|eat  t=ate|gr   (scrambled)
+        for (int len = 1; len < n; len++)
+        {
+            if (scramble(s.substr(0,len), t.substr(0,len)) &&
+                scramble(s.substr(len), t.substr(len)))
+            {
+                return true;
+            }
+
+            if (scramble(s.substr(0,len), t.substr(n-len)) &&
+                scramble(s.substr(len), t.substr(0,n-len)))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    bool isScramble(std::string s, std::string t)
+    {
+        return scramble(s, t);
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 70.77% */
+/* Space Beats: 56.09% */
+
+/* Time  Complexity: O(n^4) */
+/* Space Complexity: O(n^3) */
+class Solution_DP_plus_Top_Down {
+private:
+   std::map<std::pair<std::string, std::string>, bool> dp;
+
+public:
+    bool isScramble(std::string s, std::string t)
+    {
+        return scramble(s, t);
+    }
+
+private:
+    bool scramble(std::string s, std::string t)
+    {
+        const int n = s.size();
+        std::pair<std::string, std::string> combo = std::make_pair(s,t);
+
+        if (dp.count(combo) != 0)
+            return dp[combo];
+
+        if (s == t)
+            return true;
+
+        std::vector<int> letters_freq(26, 0);
+
+        for(int i = 0; i < n; i++)
+            letters_freq[s[i] - 'a']++, letters_freq[t[i]-'a']--;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (letters_freq[i])
+                return false;
+        }
+
+        // s=gr|eat  t=gr|eat   (not scrambled)
+        // s=gr|eat  t=ate|gr   (scrambled)
+
+        for(int len = 1; len < n; len++)
+        {
+            if (scramble(s.substr(0,len), t.substr(0,len))     &&
+                scramble(s.substr(len)  , t.substr(len))       ||
+                scramble(s.substr(0,len), t.substr(n-len))     &&
+                scramble(s.substr(len)  , t.substr(0, n-len)))
+            {
+                return dp[combo] = true;
+            }
+
+        }
+
+        return dp[combo] = false;
+    }
+};
