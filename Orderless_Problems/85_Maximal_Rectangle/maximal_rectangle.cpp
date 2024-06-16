6d436485c6e58771f19b99477acaa89a3f626025
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-16 22:13:08 +0200

    Add Optimized Solution to LeetCode: 85_Maximal_Rectangle to Orderless_Problems

--- Orderless_Problems/85_Maximal_Rectangle/maximal_rectangle.cpp
+++ Orderless_Problems/85_Maximal_Rectangle/maximal_rectangle.cpp
@@ -114,3 +114,64 @@ public:
         return max_area;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    To optimize, identify and address inefficiencies in the brute-force method.
+    This involves recognizing repetitive calculations, such as repeatedly
+    finding the minimum height between pairs of bars. We can use techniques
+    like dynamic programming (DP) or stack-based methods to streamline the
+    process and reduce unnecessary recalculations, ultimately improving
+    efficiency and scalability for larger datasets.
+
+*/
+
+/* Time  Beats: 84.87% */
+/* Space Beats: 81.27% */
+
+/* Time  Complexity: O(N * M) */
+/* Space Complexity: O(M)     */
+class Solution_Optimization {
+public:
+    int maximalRectangle(std::vector<std::vector<char>>& matrix)
+    {
+        if (matrix.empty() || matrix[0].empty())
+            return 0;
+
+        int ROWS = matrix.size();
+        int COLS = matrix[0].size();
+        std::vector<int> heights(COLS + 1, 0); // Include an extra element for easier calculation
+        int max_area = 0;
+
+        for (const auto& row : matrix)
+        {
+            for (int i = 0; i < COLS; i++)
+            {
+                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+            }
+
+            // Calculate max area using stack-based method
+            stack<int> stack;
+            for (int i = 0; i < heights.size(); i++)
+            {
+                while (!stack.empty() && heights[i] < heights[stack.top()])
+                {
+                    int h = heights[stack.top()];
+                    stack.pop();
+                    int w = stack.empty() ? i : i - stack.top() - 1;
+                    max_area = max(max_area, h * w);
+                }
+
+                stack.push(i);
+            }
+        }
+
+        return max_area;
+    }
+};
