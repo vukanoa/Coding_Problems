02f79fe92f45ee39cf3833716af10c50a5de63b5
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-16 22:11:53 +0200

    Add LeetCode: 85_Maximal_Rectangle to Orderless_Problems

---
+++ Orderless_Problems/85_Maximal_Rectangle/maximal_rectangle.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+/*
+    ============
+    === HARD ===
+    ============
+
+    =====================
+    85) Maximal Rectangle
+    =====================
+
+    ============
+    Description:
+    ============
+
+    Given a rows x cols binary matrix filled with 0's and 1's, find the largest
+    rectangle containing only 1's and return its area.
+
+    =============================================================
+    FUNCTION: int maximalRectangle(vector<vector<char>>& matrix);
+    =============================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    +-----+-----+-----+-----+-----+
+    |  1  |  0  |  1  |  0  |  0  |
+    +-----+-----+-----+-----+-----+
+    |  1  |  0  |  1  |  1  |  1  |
+    +-----+-----+-----+-----+-----+
+    |  1  |  1  |  1  |  1  |  1  |
+    +-----+-----+-----+-----+-----+
+    |  1  |  0  |  0  |  1  |  0  |
+    +-----+-----+-----+-----+-----+
+    Input: matrix =
+    [["1","0","1","0","0"],
+     ["1","0","1","1","1"],
+     ["1","1","1","1","1"],
+     ["1","0","0","1","0"]]
+    Output: 6
+    Explanation: The maximal rectangle is shown in the above picture.
+
+
+    --- Example 2 ---
+    Input: matrix = [["0"]]
+    Output: 0
+
+
+    --- Example 3 ---
+    Input: matrix = [["1"]]
+    Output: 1
+
+
+    *** Constraints ***
+    rows == matrix.length
+    cols == matrix[i].length
+    1 <= row, cols <= 200
+    matrix[i][j] is '0' or '1'.
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 19.25% */
+/* Space Beats: 98.29% */
+
+/* Time  Complexity: O(n^3) */
+/* Space Complexity: O(n^2) */
+class Solution_Brute_Force {
+public:
+    int maximalRectangle(std::vector<std::vector<char>>& matrix)
+    {
+        if (matrix.empty() || matrix[0].empty())
+            return 0;
+
+        const int ROWS = matrix.size();
+        const int COLS = matrix[0].size();
+
+        // Include an extra element for easier calculation
+        std::vector<int> heights(COLS + 1, 0);
+        int max_area = 0;
+
+        for (const auto& row : matrix)
+        {
+            for (int i = 0; i < COLS; i++)
+                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+
+            // Calculate max area using histogram method
+            int n = heights.size(); // Number of bars in the histogram
+
+            for (int i = 0; i < n; i++)
+            {
+                int min_height = INT_MAX;
+
+                for (int j = i; j < n; j++)
+                {
+                    min_height = std::min(min_height, heights[j]);
+
+                    int area = min_height * (j - i + 1);
+                    max_area = std::max(max_area, area);
+                }
+            }
+        }
+
+        return max_area;
+    }
+};
