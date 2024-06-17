caa48183ae9d7eadb3f077bfc62ae546c6034e68
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-17 23:20:49 +0200

    Add another Solution to LeetCode: 633_Sum_Of_Square_Numbers to Orderless_Problems

--- Orderless_Problems/633_Sum_Of_Square_Numbers/sum_of_square_numbers.cpp
+++ Orderless_Problems/633_Sum_Of_Square_Numbers/sum_of_square_numbers.cpp
@@ -73,3 +73,37 @@ public:
         return false;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 27.80% */
+/* Space Beats: 91.90% */
+
+/* Time  Complexity: O(sqrt(c)) */
+/* Space Complexity: O(1)       */
+class Solution_2 {
+public:
+    bool judgeSquareSum(int c)
+    {
+        for (long a = 0; a * a <= c; a++) // Iterate through all possible values of `a`
+        {
+            double b = sqrt(c - a * a);   // Compute `b` as the square root of `c - a^2`
+            if (b == (int)b)              // Check if `b` is an integer
+            {
+                return true;              // If `b` is an integer, return true
+            }
+        }
+
+        return false;                     // If no such pair `(a, b)` is found, return false
+    }
+};
