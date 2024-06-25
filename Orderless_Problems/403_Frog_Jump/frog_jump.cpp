f11c8612bf5cf25cb9d2a26828ed514b26edeb64
Noa Vukasovic <noavukasovic@gmail.com>
2024-06-25 20:16:18 +0200

    Add Tabulation_Plus_Binary_Search Solution to LeetCode: 403_Frog_Jump to Orderless_Problems

--- Orderless_Problems/403_Frog_Jump/frog_jump.cpp
+++ Orderless_Problems/403_Frog_Jump/frog_jump.cpp
@@ -157,3 +157,65 @@ public:
         return umap[stones.back()].size() != 0;    
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 87.88% */
+/* Space Beats: 76.55% */
+
+/* Time  Complexity: O(n^3 * logn) */
+/* Space Complexity: O(n^3) */
+class Solution_Tabulation_Plus_Binary_Search {
+public:
+    int dp[2001][2001];
+    bool canCross(vector<int>& stones) 
+    {
+        memset(dp, -1, sizeof(dp));
+
+        return find(stones, 0, 0);
+    }
+
+private:
+    bool find(std::vector<int>& stones, int i, int k)
+    {
+        if (i == stones.size()-1)
+            return true;
+
+        if (i >= stones.size()) 
+            return false;
+
+        if (dp[i][k] != -1) 
+            return dp[i][k]; 
+
+        bool result = false;
+
+        for(int j = -1; j <= 1; j++)
+        {
+            int next_jump = k+j;
+
+            if(next_jump > 0)
+            {
+                int in= lower_bound(stones.begin(),stones.end(),stones[i]+next_jump)-stones.begin();
+
+                if (in == stones.size() || stones[in] != stones[i]+next_jump) 
+                    continue;
+
+                result = result || find(stones,in,next_jump);
+            }
+        }
+
+        return dp[i][k] = result;
+
+    }
+
+};
