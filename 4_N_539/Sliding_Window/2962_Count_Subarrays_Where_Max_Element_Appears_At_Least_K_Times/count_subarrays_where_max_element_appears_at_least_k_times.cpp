128de3a07afaff05ba5634a875b63378571d6008
Noa Vukasovic <noavukasovic@gmail.com>
2024-08-10 15:27:59 +0200

    Add LeetCode: 2962_Count_Subarrays_Where_Max_Element_Appears_At_Least_K_Times to N_539

---
+++ 4_N_539/Sliding_Window/2962_Count_Subarrays_Where_Max_Element_Appears_At_Least_K_Times/count_subarrays_where_max_element_appears_at_least_k_times.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ================================================================
+    2962) Count Subarrays Where Max Element Appears at Least K Times
+    ================================================================
+
+    ============
+    Description:
+    ============
+
+    You are given an integer array nums and a positive integer k.
+
+    Return the number of subarrays where the maximum element of nums appears at
+    least k times in that subarray.
+
+    A subarray is a contiguous sequence of elements within an array.
+
+    =============================================================
+    FUNCTION: long long countSubarrays(vector<int>& nums, int k);
+    =============================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [1,3,2,3,3], k = 2
+    Output: 6
+    Explanation: The subarrays that contain the element 3 at least 2 times are:
+                 [1,3,2,3], [1,3,2,3,3], [3,2,3], [3,2,3,3], [2,3,3] and [3,3].
+
+
+    --- Example 2 ---
+    Input: nums = [1,4,2,1], k = 3
+    Output: 0
+    Explanation: No subarray contains the element 4 at least 3 times.
+
+
+    *** Constraints ***
+    1 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^6
+    1 <= k <= 10^5
+
+*/
+
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 57.70% */
+/* Space Beats: 47.85% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k)
+    {
+        const int n = nums.size();
+        int max_elem = *std::max_element(nums.begin(), nums.end());
+        int max_cnt  = 0;
+
+        long long result = 0;
+
+        int left  = 0;
+        int right = 0;
+
+        while (right < n)
+        {
+            if (nums[right] == max_elem)
+                max_cnt++;
+
+            while (max_cnt > k || (left <= right && max_cnt == k && nums[left] != max_elem))
+            {
+                if (nums[left] == max_elem)
+                    max_cnt--;
+
+                left++;
+            }
+
+            if (max_cnt == k)
+                result += left + 1;
+
+            right++;
+        }
+
+        return result;
+    }
+};
