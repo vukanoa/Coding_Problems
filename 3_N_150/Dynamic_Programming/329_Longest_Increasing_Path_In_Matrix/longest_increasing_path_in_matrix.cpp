15f26c7cba568bde57413641af75e917fdd3bd82
Noa Vukasovic <noavukasovic@gmail.com>
2024-08-14 21:15:08 +0200

    Add LeetCode: 329_Longest_Increasing_Path_In_Matrix to N_150

---
+++ 3_N_150/Dynamic_Programming/329_Longest_Increasing_Path_In_Matrix/longest_increasing_path_in_matrix.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <vector>
+
+/*
+    ============
+    === HARD ===
+    ============
+
+    ======================================
+    329) Longest Increasing Path In Matrix
+    ======================================
+
+    ============
+    Description:
+    ============
+
+    Given an m x n integers matrix, return the length of the longest increasing
+    path in matrix.
+
+    From each cell, you can either move in four directions: left, right, up, or
+    down. You may not move diagonally or move outside the boundary (i.e.,
+    wrap-around is not allowed).
+
+    ===============================
+    FUNCTION:
+    ===============================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: matrix = [[9,9,4],
+                     [6,6,8],
+                     [2,1,1]]
+    Output: 4
+    Explanation: The longest increasing path is [1, 2, 6, 9].
+
+
+    --- Example 2 ---
+    Input: matrix = [[3,4,5],
+                     [3,2,6],
+                     [2,2,1]]
+    Output: 4
+    Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally
+                 is not allowed.
+
+
+    --- Example 3 ---
+    Input: matrix = [[1]]
+    Output: 1
+
+
+    *** Constraints ***
+    m == matrix.length
+    n == matrix[i].length
+    1 <= m, n <= 200
+    0 <= matrix[i][j] <= 2^31 - 1
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+    (Classic 2D Dynamic Programming)
+
+*/
+
+/* Time  Beats: 32.85% */
+/* Space Beats: 27.49% */
+
+/* Time  Complexity: O(ROWS * COLS) */
+/* Space Complexity: O(ROWS * COLS) */
+class Solution {
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix)
+    {
+        const int ROWS = matrix.size();
+        const int COLS = matrix[0].size();
+
+        vector<vector<int>> dp(ROWS, vector(COLS, 0));
+        vector<vector<bool>> visited(ROWS, vector(COLS, false));
+
+        int absolute_longest = 0;
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                if (dp[i][j] > 0)
+                    continue;
+
+                int local_longest = 0;
+
+                visited[i][j] = true;
+
+                int up    = dfs(matrix, i-1, j  , visited, dp, matrix[i][j]);
+                int down  = dfs(matrix, i+1, j  , visited, dp, matrix[i][j]);
+                int left  = dfs(matrix, i  , j-1, visited, dp, matrix[i][j]);
+                int right = dfs(matrix, i  , j+1, visited, dp, matrix[i][j]);
+
+                visited[i][j] = false;
+
+                up    =   up  == 0 ? 0 : up+1;
+                down  = down  == 0 ? 0 : down+1;
+                left  = left  == 0 ? 0 : left+1;
+                right = right == 0 ? 0 : right+1;
+
+                local_longest = max( {1, up, down, left, right} );
+
+                dp[i][j] = local_longest;
+
+                absolute_longest = max(absolute_longest, dp[i][j]);
+            }
+        }
+
+        return absolute_longest;
+    }
+
+private:
+    int dfs(vector<vector<int>>& matrix,
+            int i,
+            int j,
+            vector<vector<bool>>& visited,
+            vector<vector<int>>& dp,
+            int prev)
+    {
+        const int ROWS = matrix.size();
+        const int COLS = matrix[0].size();
+
+        if (i < 0 || j < 0 || i == ROWS || j == COLS || visited[i][j] || prev <= matrix[i][j])
+            return 0;
+
+        if (dp[i][j] > 0)
+            return dp[i][j];
+
+        int local_longest = 0;
+
+        visited[i][j] = true;
+
+        int up    = dfs(matrix, i-1, j  , visited, dp, matrix[i][j]);
+        int down  = dfs(matrix, i+1, j  , visited, dp, matrix[i][j]);
+        int left  = dfs(matrix, i  , j-1, visited, dp, matrix[i][j]);
+        int right = dfs(matrix, i  , j+1, visited, dp, matrix[i][j]);
+
+        visited[i][j] = false;
+
+        up    =   up  == 0 ? 0 : up+1;
+        down  = down  == 0 ? 0 : down+1;
+        left  = left  == 0 ? 0 : left+1;
+        right = right == 0 ? 0 : right+1;
+
+        local_longest = max( {1, up, down, left, right} );
+
+        dp[i][j] = local_longest;
+
+        return dp[i][j];
+    }
+};
