78c0c679e77415a40bff1abdc5ab11e2e5418a79
Noa Vukasovic <noavukasovic@gmail.com>
2024-08-24 22:15:14 +0200

    Add LeetCode: 1614_Maximum_Nesting_Depth_Of_The_Parentheses to N_539

---
+++ 4_N_539/Greedy/1614_Maximum_Nesting_Depth_Of_The_Parentheses/maximum_nesting_depth_of_the_parentheses.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+/*
+    ============
+    === EASY ===
+    ============
+
+    ==============================================
+    1614) Maximum Nesting Depth of the Parentheses
+    ==============================================
+
+    ============
+    Description:
+    ============
+
+    Given a valid parentheses string s, return the nesting depth of s. The
+    nesting depth is the maximum number of nested parentheses.
+
+    =================================
+    FUNCTION: int maxDepth(string s);
+    =================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: s = "(1+(2*3)+((8)/4))+1"
+    Output: 3
+    Explanation: Digit 8 is inside of 3 nested parentheses in the string.
+
+
+    --- Example 2 ---
+    Input: s = "(1)+((2))+(((3)))"
+    Output: 3
+    Explanation: Digit 3 is inside of 3 nested parentheses in the string.
+
+
+    --- Example 3 ---
+    Input: s = "()(())((()()))"
+    Output: 3
+
+
+    *** Constraints ***
+    1 <= s.length <= 100
+    s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
+    It is guaranteed that parentheses expression s is a VPS.
+
+*/
+
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  58.83% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int maxDepth(string s)
+    {
+        int max_depth  = 0;
+        int curr_depth = 0;
+
+        for (const char& chr : s)
+        {
+            if (chr != '(' && chr != ')')
+                continue;
+
+            if (chr == '(')
+            {
+                curr_depth++;
+                max_depth = max(max_depth, curr_depth);
+            }
+            else
+                curr_depth--;
+        }
+
+        return max_depth;
+    }
+};
