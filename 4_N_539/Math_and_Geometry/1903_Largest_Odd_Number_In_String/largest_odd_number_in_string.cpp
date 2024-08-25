85458fb9b0e4c76360f846e97d011d72ff620096
Noa Vukasovic <noavukasovic@gmail.com>
2024-08-25 22:42:13 +0200

    Add another Solution to LeetCode: 1903_Largest_Odd_Number_In_String to N_539

--- 4_N_539/Math_and_Geometry/1903_Largest_Odd_Number_In_String/largest_odd_number_in_string.cpp
+++ 4_N_539/Math_and_Geometry/1903_Largest_Odd_Number_In_String/largest_odd_number_in_string.cpp
@@ -95,3 +95,36 @@ public:
         return num.substr(0, e+1);
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 21.23% */
+/* Space Beats: 56.56% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    string largestOddNumber(string num)
+    {
+        int idx = num.length() - 1;
+
+        // num[idx] - '0' converts the number character into an integer
+        // & 1 checks whether or not the number is odd
+        while (idx >= 0 && ((num[idx] - '0') & 1) == 0)
+            idx--;
+
+        // return the substring we found based on the index we stopped at
+        return num.substr(0, idx + 1);
+    }
+};
