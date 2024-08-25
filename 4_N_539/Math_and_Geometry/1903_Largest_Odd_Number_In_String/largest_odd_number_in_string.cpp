77073a9606efdf35c9aabd4866bb21c86993367d
Noa Vukasovic <noavukasovic@gmail.com>
2024-08-25 22:39:58 +0200

    Add LeetCode: 1903_Largest_Odd_Number_In_String to N_539

---
+++ 4_N_539/Math_and_Geometry/1903_Largest_Odd_Number_In_String/largest_odd_number_in_string.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+
+/*
+    ============
+    === EASY ===
+    ============
+
+    ==================================
+    1903) Largest Odd Number in String
+    ==================================
+
+    ============
+    Description:
+    ============
+
+    You are given a string num, representing a large integer. Return the
+    largest-valued odd integer (as a string) that is a non-empty substring of
+    num, or an empty string "" if no odd integer exists.
+
+    A substring is a contiguous sequence of characters within a string.
+
+    ==============================================
+    FUNCTION: string largestOddNumber(string num);
+    ==============================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: num = "52"
+    Output: "5"
+    Explanation: The only non-empty substrings are "5", "2", and "52". "5" is
+                 the only odd number.
+
+
+    --- Example 2 ---
+    Input: num = "4206"
+    Output: ""
+    Explanation: There are no odd numbers in "4206".
+
+
+    --- Example 3 ---
+    Input: num = "35427"
+    Output: "35427"
+    Explanation: "35427" is already an odd number.
+
+
+    *** Constraints ***
+    1 <= num.length <= 10^5
+    num only consists of digits and does not contain any leading zeros.
+
+*/
+
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 51.31% */
+/* Space Beats: 87.98% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    string largestOddNumber(string num)
+    {
+        if (static_cast<int>((num[num.size()-1])) % 2 != 0)
+            return num;
+
+        const int n = num.size();
+        int e = 0;
+
+        for (; e < n; e++)
+        {
+            if( static_cast<int>((num[e])) % 2 != 0)
+                break;
+        }
+
+        if (e == n)
+            return "";
+
+        e = n-2;
+
+        while (e >= 0 && static_cast<int>((num[e])) % 2 == 0)
+            e--;
+
+        return num.substr(0, e+1);
+    }
+};
