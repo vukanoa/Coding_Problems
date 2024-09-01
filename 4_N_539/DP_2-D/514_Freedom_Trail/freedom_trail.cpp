03ac915a3b27363583fe003dadaf69096271c7e0
Noa Vukasovic <noavukasovic@gmail.com>
2024-09-01 21:27:41 +0200

    Add Iterative DP Solution to LeetCode: 514_Freedom_Trail to N_539

--- 4_N_539/DP_2-D/514_Freedom_Trail/freedom_trail.cpp
+++ 4_N_539/DP_2-D/514_Freedom_Trail/freedom_trail.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 /*
     ============
@@ -74,6 +75,8 @@
 
 */
 
+using namespace std;
+
 /*
     ------------
     --- IDEA ---
@@ -147,5 +150,59 @@ private:
         }
 
         return dp[i][j] = result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 97.86% */
+/* Space Beats: 81.89% */
+
+/* Time  Complexity: O(??) */
+/* Space Complexity: O(??) */
+class Solution_Bottom_Up {
+public:
+    int findRotateSteps(string& ring, string& key)
+    {
+        vector<int> pos[26];
+        const int r = ring.size();
+        const int k = key.size();
+
+        for (int i = 0; i < r; i++)
+            pos[ring[i] - 'a'].push_back(i);
+
+        int dp[2][200];
+        fill(dp[0], begin(dp[0]) + r, INT_MAX);
+
+        for (int p : pos[key[0] - 'a'])
+            dp[0][p] = min(p, r-p) + 1;
+
+        for (int i = 1; i < k; i++)
+        {
+            fill(dp[i&1], begin(dp[i&1]) + r, INT_MAX); //Reset the current row
+
+            for (int j: pos[key[i-1] - 'a'])
+            {
+                for (int p : pos[key[i] - 'a'])
+                {
+                    int step = abs(p-j);
+                    step = min(step, r-step);
+
+                    dp[i&1][p] = min(dp[i&1][p], dp[(i-1)&1][j] + step+1);
+                }
+            }
+        }
+
+        return *min_element(dp[(k-1) & 1], begin(dp[(k-1) & 1]) + r);
     }
 };
