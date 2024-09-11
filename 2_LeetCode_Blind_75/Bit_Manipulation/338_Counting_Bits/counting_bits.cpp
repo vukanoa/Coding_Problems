242d83f6e499be547ff03af600ea71d34dec31b6
Noa Vukasovic <noavukasovic@gmail.com>
2024-09-11 21:41:12 +0200

    Reformat & Improve Explanation(IDEA) to LeetCode: 338_Counting_Bits in Blind_75

--- 2_LeetCode_Blind_75/Bit_Manipulation/338_Counting_Bits/counting_bits.cpp
+++ 2_LeetCode_Blind_75/Bit_Manipulation/338_Counting_Bits/counting_bits.cpp
@@ -1,71 +1,111 @@
 #include <iostream>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	===========================
-	338) Counting Bits
-	===========================
+    ===========================
+    338) Counting Bits
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given an integer n, return an array ans of length n + 1 such that for each
-	i (0 <= i <= n), ans[i] is the number of 1's in the binary representation
-	of i.
+    Given an integer n, return an array ans of length n + 1 such that for each
+    i (0 <= i <= n), ans[i] is the number of 1's in the binary representation
+    of i.
 
-	~~~ Follow Up ~~~
-	It is very easy to come up with a solution with a runtime of O(n log n).
+    ~~~ Follow Up ~~~
+    It is very easy to come up with a solution with a runtime of O(n log n).
 
-	Can you do it in linear time O(n) and possibly in a single pass?
-	Can you do it without using any built-in function (i.e., like
-	__builtin_popcount in C++)?
-
-
-	=======================================
-	FUNCTION: vector<int> countBits(int n);
-	=======================================
-
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
-
-	--- Example 1 ---
-	Input: n = 2
-	Output: [0,1,1]
-	Explanation:
-	0 --> 0
-	1 --> 1
-	2 --> 10
+    Can you do it in linear time O(n) and possibly in a single pass?
+    Can you do it without using any built-in function (i.e., like
+    __builtin_popcount in C++)?
 
 
-	--- Example 2 ---
-	Input: n = 5
-	Output: [0,1,1,2,1,2]
-	Explanation:
-	0 --> 0
-	1 --> 1
-	2 --> 10
-	3 --> 11
-	4 --> 100
-	5 --> 101
+    =======================================
+    FUNCTION: vector<int> countBits(int n);
+    =======================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: n = 2
+    Output: [0,1,1]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
 
 
-	*** Constraints ***
-	0 <= n <= 105
+    --- Example 2 ---
+    Input: n = 5
+    Output: [0,1,1,2,1,2]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+    3 --> 11
+    4 --> 100
+    5 --> 101
+
+
+    *** Constraints ***
+    0 <= n <= 105
 */
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Self-Explanatory.
-	
+    If we do a "bitwise AND" with 0x1 on any number, we get to see if it's an
+        1) Odd, or
+        2) Even
+    number.
+
+    Odd numbers in binary representation have 1 at the very end.
+    Examples:                                ~~~~~~~~~~~~~~~~~~
+                                                   |
+              _____________________________________|
+              |
+              v
+        3: 0011
+        5: 0101
+        7: 0111
+        9: 1001
+
+
+    Even number, on the contrary, have 0 at the very end.
+    Examples:                         ~~~~~~~~~~~~~~~~~~
+                                                   |
+              _____________________________________|
+              |
+              v
+        2: 0010
+        4: 0100
+        6: 0110
+        8: 1000
+
+
+    Therefore, if we do a "bitwise AND" with 0x1:
+
+        4: 0100
+        1: 0001  AND
+        ________
+           0000 // 0 means 4 is an EVEN number
+                // 1 would mean it is an ODD number
+
+    Based on that we are checking each bit and after that we do a Logical Shift
+    Right.
+
+    That's it.
+
 */
 
 /* Time  Beats: 41.06% */
@@ -75,28 +115,28 @@
 /* Space Complexity: O(n) */
 class Solution {
 public:
-	vector<int> countBits(int n)
-	{
-		std::vector<int> ans;
+    vector<int> countBits(int n)
+    {
+        std::vector<int> ans;
 
-		for (int i = 0; i <= n; i++)
-		{
-			int num = i;
-			int count = 0;
+        for (int i = 0; i <= n; i++)
+        {
+            int num = i;
+            int count = 0;
 
-			for (int j = 0; j < 32; j++) // O(32) => constant => O(1)
-			{
-				if (num & 1)
-				count++;
+            for (int j = 0; j < 32; j++) // O(32) => constant => O(1)
+            {
+                if (num & 1)
+                    count++;
 
-				num >>= 1;
-			}
+                num >>= 1;
+            }
 
-			ans.push_back(count);
-		}
+            ans.push_back(count);
+        }
 
-		return ans;
-	}
+        return ans;
+    }
 };
 
 
@@ -109,17 +149,17 @@ public:
 /* Space Complexity: O(n) */
 class Solution {
 public:
-	std::vector<int> countBits(int n)
-	{
-		
-		vector<int> ans(n+1);
-		ans[0] = 0;
+    std::vector<int> countBits(int n)
+    {
 
-		for(int i = 1; i <= n; ++i)
-			ans[i] = ans[i/2] + i%2;
+        vector<int> res(n+1);
+        res[0] = 0;
 
-		return ans;
-	}
+        for (int i = 1; i <= n; ++i)
+            res[i] = res[i/2] + i%2;
+
+        return res;
+    }
 };
 
 
@@ -132,13 +172,13 @@ public:
 /* Space Complexity: O(n) */
 class Solution{
 public:
-	std::vector<int> countBits(int num)
-	{
-		std::vector<int> ans(num+1, 0);
+    std::vector<int> countBits(int num)
+    {
+        std::vector<int> res(num+1, 0);
 
-		for (int i = 1; i <= num; ++i)
-			ans[i] = ans[i & (i-1)] + 1;
+        for (int i = 1; i <= num; ++i)
+            res[i] = res[i & (i-1)] + 1;
 
-		return ans;
-	}
+        return res;
+    }
 };
