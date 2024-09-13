efdd5880d8ee6077b6715a7d14cd154dd91cc7f7
Noa Vukasovic <noavukasovic@gmail.com>
2024-09-13 20:48:49 +0200

    Reformat LeetCode: 190_Reverse_Bits to Top_150_Interview_Questions

--- 1_LeetCode_Top_150_Interview_Questions/11_190_Reverse_Bits/reverse_bits.cpp
+++ 1_LeetCode_Top_150_Interview_Questions/11_190_Reverse_Bits/reverse_bits.cpp
@@ -4,52 +4,52 @@
 #include <cmath>
 
 /*
-	============
-	=== EASY ===
-	============
+    ============
+    === EASY ===
+    ============
 
-	===========================
-	190) Reverse Bits
-	===========================
+    ===========================
+    190) Reverse Bits
+    ===========================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Reverse bits of a given 32 bits unsigned integer.
+    Reverse bits of a given 32 bits unsigned integer.
 
-	Follow up:
-	If this function is called many times, how would you optimize it?
+    Follow up:
+    If this function is called many times, how would you optimize it?
 
-	===========================================
-	FUNCTION: uint32_t reverseBits(uint32_t n);
-	===========================================
+    ===========================================
+    FUNCTION: uint32_t reverseBits(uint32_t n);
+    ===========================================
 
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
 
-	--- Example 1 ---
-	Input: n = 00000010100101000001111010011100
-	Output:    964176192 (00111001011110000010100101000000)
-	Explanation: The input binary string 00000010100101000001111010011100
-	represents the unsigned integer 43261596, so return 964176192 which its
-	binary representation is 00111001011110000010100101000000
-
-
-	--- Example 2 ---
-	Input: n = 11111111111111111111111111111101
-	Output:   3221225471 (10111111111111111111111111111111)
-	Explanation: The input binary string 11111111111111111111111111111101
-	represents the unsigned integer 4294967293, so return 3221225471 which its
-	binary representation is 10111111111111111111111111111111
+    --- Example 1 ---
+    Input: n = 00000010100101000001111010011100
+    Output:    964176192 (00111001011110000010100101000000)
+    Explanation: The input binary string 00000010100101000001111010011100
+    represents the unsigned integer 43261596, so return 964176192 which its
+    binary representation is 00111001011110000010100101000000
 
 
-	--- Example 3 ---
-	
+    --- Example 2 ---
+    Input: n = 11111111111111111111111111111101
+    Output:   3221225471 (10111111111111111111111111111111)
+    Explanation: The input binary string 11111111111111111111111111111101
+    represents the unsigned integer 4294967293, so return 3221225471 which its
+    binary representation is 10111111111111111111111111111111
 
-	*** Constraints ***
-	The input must be a binary string of length 32
+
+    --- Example 3 ---
+
+
+    *** Constraints ***
+    The input must be a binary string of length 32
 
 */
 
@@ -58,10 +58,10 @@
 /* Space Beats:  5.73% */
 
 /*
-	Time  Complexity: O(1)
-	Since it's always 32 bits. We always have to go through the 32 bits no
-	matter how big is the input, since we are told it is within the bounds of
-	an unsigned integer.
+    Time  Complexity: O(1)
+    Since it's always 32 bits. We always have to go through the 32 bits no
+    matter how big is the input, since we are told it is within the bounds of
+    an unsigned integer.
 */
 /* Space Complexity: O(n) */
 class Solution {
@@ -82,22 +82,22 @@ public:
             }
             else
                 bits.push_back(0);
-            
+
             degree--;
         }
 
-        
+
         std::reverse(bits.begin(), bits.end());
 
 
         degree = 31;
         uint32_t value = 0;
-        
+
         for (int i = 0; i < 32; i++)
         {
             if (bits[i] == 1)
                 value += bits[i] * std::pow(2, degree);
-            
+
             degree--;
         }
 
@@ -109,13 +109,13 @@ public:
 
 
 /*
-	------------
-	--- IDEA ---
-	------------
+    ------------
+    --- IDEA ---
+    ------------
 
-	Idea is pretty simple, but you have to go through the code and you'll get
-	it. No amount of explanation will explain it better.
-	
+    Idea is pretty simple, but you have to go through the code and you'll get
+    it. No amount of explanation will explain it better.
+
 */
 
 /* Time  Beats: 100% */
@@ -125,18 +125,18 @@ public:
 /* Space Complexity: O(1) */
 class Solution_Efficient {
 public:
-	uint32_t reverseBits(uint32_t n)
-	{
-		uint32_t rev = 0;
+    uint32_t reverseBits(uint32_t n)
+    {
+        uint32_t rev = 0;
 
-		for (int i = 0; i < 32; i++)
-		{
-			rev <<= 1;
-			rev += n % 2;
+        for (int i = 0; i < 32; i++)
+        {
+            rev <<= 1;
+            rev += n % 2;
 
-			n >>= 1;
-		}
+            n >>= 1;
+        }
 
-		return rev;
-	}
+        return rev;
+    }
 };
