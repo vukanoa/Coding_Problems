ae280015abc7b78d6fa069532f1eac985f415190
Noa Vukasovic <noavukasovic@gmail.com>
2024-09-14 22:06:27 +0200

    Reformat LeetCode: 1249_Minimum_Remove_To_Make_Valid_Parentheses to Orderless_Problems

--- Orderless_Problems/1249_Minimum_Remove_To_Make_Valid_Parentheses/minimum_remove_to_make_valid_parentheses.cpp
+++ Orderless_Problems/1249_Minimum_Remove_To_Make_Valid_Parentheses/minimum_remove_to_make_valid_parentheses.cpp
@@ -3,63 +3,63 @@
 #include <string>
 
 /*
-	==============
-	=== MEDIUM ===
-	==============
+    ==============
+    === MEDIUM ===
+    ==============
 
-	==============================================
-	1249) Minimum Remove To Make Valid Parentheses
-	==============================================
+    ==============================================
+    1249) Minimum Remove To Make Valid Parentheses
+    ==============================================
 
-	============
-	Description:
-	============
+    ============
+    Description:
+    ============
 
-	Given a string s of '(' , ')' and lowercase English characters.
+    Given a string s of '(' , ')' and lowercase English characters.
 
-	Your task is to remove the minimum number of parentheses ( '(' or ')', in
-	any positions ) so that the resulting parentheses string is valid and
-	return any valid string.
+    Your task is to remove the minimum number of parentheses ( '(' or ')', in
+    any positions ) so that the resulting parentheses string is valid and
+    return any valid string.
 
-	Formally, a parentheses string is valid if and only if:
+    Formally, a parentheses string is valid if and only if:
 
-	- It is the empty string, contains only lowercase characters, or
-	- It can be written as AB (A concatenated with B), where A and B are valid
-	  strings, or
-	- It can be written as (A), where A is a valid string.
-
-
-	================================================
-	FUNCTION: string minRemoveToMakeValid(string s); 
-	================================================
-
-	==========================================================================
-	================================ EXAMPLES ================================
-	==========================================================================
-
-	--- Example 1 ---
-	Input: s = "lee(t(c)o)de)"
-	Output: "lee(t(c)o)de"
-	Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
-
-	--- Example 2 ---
-	Input: s = "a)b(c)d"
-	Output: "ab(c)d"
-
-	--- Example 3 ---
-	Input: s = "))(("
-	Output: ""
-	Explanation: An empty string is also valid.
+    - It is the empty string, contains only lowercase characters, or
+    - It can be written as AB (A concatenated with B), where A and B are valid
+      strings, or
+    - It can be written as (A), where A is a valid string.
 
 
-	*** Constraints ***
-	1 <= s.length <= 105
-	s[i] is either'(' , ')', or lowercase English letter.
+    ================================================
+    FUNCTION: string minRemoveToMakeValid(string s);
+    ================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: s = "lee(t(c)o)de)"
+    Output: "lee(t(c)o)de"
+    Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+
+    --- Example 2 ---
+    Input: s = "a)b(c)d"
+    Output: "ab(c)d"
+
+    --- Example 3 ---
+    Input: s = "))(("
+    Output: ""
+    Explanation: An empty string is also valid.
+
+
+    *** Constraints ***
+    1 <= s.length <= 105
+    s[i] is either'(' , ')', or lowercase English letter.
 
 */
 
 /*
-	There are 3 Solutions in here. The 3rd one(down most one) is the best.
+    There are 3 Solutions in here. The 3rd one(down most one) is the best.
 */
 
 
@@ -72,63 +72,63 @@
 /* Space Complexity: O(n) */
 class Solution {
 public:
-	string minRemoveToMakeValid(string s)
-	{
-		int opened = 0;
-		std::ostringstream out;
+    string minRemoveToMakeValid(string s)
+    {
+        int opened = 0;
+        std::ostringstream out;
 
-		for (auto& character : s)
-		{
-			if (character == ')')
-			{
-				if (opened == 0)
-					continue;
-				else
-				{
-					opened--;
-					out << character;
-				}
-			}
-			else if (character == '(')
-			{
-				opened++;
-				out << character;
-			}
-			else
-				out << character;
-		}
+        for (auto& character : s)
+        {
+            if (character == ')')
+            {
+                if (opened == 0)
+                    continue;
+                else
+                {
+                    opened--;
+                    out << character;
+                }
+            }
+            else if (character == '(')
+            {
+                opened++;
+                out << character;
+            }
+            else
+                out << character;
+        }
 
-		std::string str = out.str();
-		std::ostringstream result;
+        std::string str = out.str();
+        std::ostringstream result;
 
-		opened = 0;
+        opened = 0;
 
-		for (int i = str.length() - 1; i >= 0; i--)
-		{
-			if (str[i] == '(')
-			{
-				if (opened == 0)
-					continue;
-				else
-				{
-					opened--;
-					result << str[i];
-				}
-			}
-			else if (str[i] == ')')
-			{
-				opened++;
-				result << str[i];
-			}
-			else
-				result << str[i];
-		}
+        for (int i = str.length() - 1; i >= 0; i--)
+        {
+            if (str[i] == '(')
+            {
+                if (opened == 0)
+                    continue;
+                else
+                {
+                    opened--;
+                    result << str[i];
+                }
+            }
+            else if (str[i] == ')')
+            {
+                opened++;
+                result << str[i];
+            }
+            else
+                result << str[i];
+        }
 
-		std::string final_str = result.str();
-		std::reverse(final_str.begin(), final_str.end());
+        std::string final_str = result.str();
+        std::reverse(final_str.begin(), final_str.end());
 
-		return final_str;
-	}
+        return final_str;
+    }
 };
 
 
@@ -142,34 +142,34 @@ public:
 class Solution {
 public:
     std::string minRemoveToMakeValid(std::string s)
-	{
+    {
         int open = 0;
-		int position = 0;
+        int position = 0;
 
-		std::string str(s.size(), '#');
+        std::string str(s.size(), '#');
 
         for (char character : s)
-		{
-			if (character == '(')
-				open++;
-			else if (character == ')')
-			{
-				if (open > 0)
-					open--;
-				else
-					continue;
-			}
+        {
+            if (character == '(')
+                open++;
+            else if (character == ')')
+            {
+                if (open > 0)
+                    open--;
+                else
+                    continue;
+            }
 
-			str[position++] = character;
+            str[position++] = character;
         }
 
         // Properly sizing res
         std::string final_str(position - open, '*');
 
         for (int i = position - 1, j = position - open - 1; i >= 0; i--)
-		{
-			if (str[i] != '(' || open-- <= 0)
-				final_str[j--] = str[i];
+        {
+            if (str[i] != '(' || open-- <= 0)
+                final_str[j--] = str[i];
         }
 
         return final_str;
@@ -187,34 +187,34 @@ public:
 class Solution {
 public:
     std::string minRemoveToMakeValid(std::string s)
-	{
+    {
         int open = 0;
-		int position = 0;
+        int position = 0;
 
-		std::string str(s.size(), '#');
+        std::string str(s.size(), '#');
 
         for (char character : s)
-		{
-			if (character == '(')
-				open++;
-			else if (character == ')')
-			{
-				if (open > 0)
-					open--;
-				else
-					continue;
-			}
+        {
+            if (character == '(')
+                open++;
+            else if (character == ')')
+            {
+                if (open > 0)
+                    open--;
+                else
+                    continue;
+            }
 
-			str[position++] = character;
+            str[position++] = character;
         }
 
         // Properly sizing res
-		s.resize(position - open);
+        s.resize(position - open);
 
         for (int i = position - 1, j = position - open - 1; i >= 0; i--)
-		{
-			if (str[i] != '(' || open-- <= 0)
-				s[j--] = str[i];
+        {
+            if (str[i] != '(' || open-- <= 0)
+                s[j--] = str[i];
         }
 
         return s;
