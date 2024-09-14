afdf41fa060b1f6fe65f7e9cd8190be0c662a612
Noa Vukasovic <noavukasovic@gmail.com>
2024-09-14 21:57:50 +0200

    Add LeetCode: 2419_Longest_Subarray_With_Maximum_Bitwise_AND to Orderless_Problems

---
+++ Orderless_Problems/2419_Longest_Subarray_With_Maximum_Bitwise_AND/longest_subarray_with_maximum_bitwise_and.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <vector>
+
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===============================================
+    2419) Longest Subarray With Maximum Bitwise AND
+    ===============================================
+
+    ============
+    Description:
+    ============
+
+    You are given an integer array nums of size n.
+
+    Consider a non-empty subarray from nums that has the maximum possible
+    bitwise AND.
+
+        In other words, let k be the maximum value of the bitwise AND of any
+        subarray of nums. Then, only subarrays with a bitwise AND equal to k
+        should be considered.
+
+    Return the length of the longest such subarray.
+
+    The bitwise AND of an array is the bitwise AND of all the numbers in it.
+
+    A subarray is a contiguous sequence of elements within an array.
+
+    =================================================
+    FUNCTION: int longestSubarray(vector<int>& nums);
+    =================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [1,2,3,3,2,2]
+    Output: 2
+    Explanation:
+    The maximum possible bitwise AND of a subarray is 3.
+    The longest subarray with that value is [3,3], so we return 2.
+
+    --- Example 2 ---
+    Input: nums = [1,2,3,4]
+    Output: 1
+    Explanation:
+    The maximum possible bitwise AND of a subarray is 4.
+    The longest subarray with that value is [4], so we return 1.
+
+
+    *** Constraints ***
+    1 <= nums.length <= 10^5
+    1 <= nums[i] <= 10^6
+
+*/
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 26.16% */
+/* Space Beats: 34.15% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums)
+    {
+        int len = 1;
+
+        // step 01
+        int mx = *max_element(nums.begin(), nums.end());
+
+        // step 02
+        int tmp_len = 0;
+
+        for (int i=0;i<nums.size();i++)
+        {
+            if (nums[i] == mx)
+                tmp_len++;
+            else
+            {
+                len = max(len, tmp_len);
+                tmp_len = 0;
+            }
+        }
+
+        len = max(len, tmp_len);
+
+        return len;
+    }
+};
