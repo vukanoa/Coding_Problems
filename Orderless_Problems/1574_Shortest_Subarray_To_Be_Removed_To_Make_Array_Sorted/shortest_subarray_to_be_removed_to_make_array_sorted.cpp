a15f69e2e6cd3d535b4d6463d90e6d171e6a8e66
Noa Vukasovic <noavukasovic@gmail.com>
2024-11-15 19:06:24 +0100

    Add Optimized Solution to LeetCode: 1574_Shortest_Subarray_To_Be_Removed_To_Make_Array_Sorted to Orderless_Problems

--- Orderless_Problems/1574_Shortest_Subarray_To_Be_Removed_To_Make_Array_Sorted/shortest_subarray_to_be_removed_to_make_array_sorted.cpp
+++ Orderless_Problems/1574_Shortest_Subarray_To_Be_Removed_To_Make_Array_Sorted/shortest_subarray_to_be_removed_to_make_array_sorted.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits>
 
 /*
     ==============
@@ -22,9 +23,9 @@
 
     A subarray is a contiguous subsequence of the array.
 
-    ===============================
-    FUNCTION:  
-    ===============================
+    =============================================================
+    FUNCTION: int findLengthOfShortestSubarray(vector<int>& arr);
+    =============================================================
 
     ==========================================================================
     ================================ EXAMPLES ================================
@@ -148,12 +149,6 @@ private:
     // ***************************
     // *** STRICTLY DECREASING ***
     // ***************************
-
-    // bool strictly_decreasing(const vector<int>& arr)
-    // {
-    //     return std::is_sorted(arr.begin(), arr.end(), std::greater<int>());
-    // }
-
     bool strictly_decreasing(vector<int>& arr)
     {
         for (int i = 1; i < arr.size(); i++)
@@ -166,17 +161,117 @@ private:
     }
 
 
+    // **********************
+    // *** NON-DECREASING ***
+    // **********************
+    bool non_decreasing(vector<int>& arr)
+    {
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i-1] > arr[i])
+                return false;
+        }
+
+        return true;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+    Slight, but VERY important, optimization on the above Solution.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  25.81% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution_Optimized {
+class Solution {
+public:
+    int findLengthOfShortestSubarray(vector<int>& arr)
+    {
+        const int n = arr.size();
+
+        if (strictly_decreasing(arr)) // O(n)
+            return n-1;
+        else if (non_decreasing(arr)) // O(n)
+            return 0;
+
+        int result = INT_MAX;
+
+        // O(n)
+        /* From which Idx from the back, is the rest of the array sorted? */
+        int sorted_from_idx = n; // Out of bounds currently
+        for (int i = n-2; i >= 0; i--)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                sorted_from_idx = i+1;
+                break;
+            }
+        }
+        result = min(result, sorted_from_idx); // i.e. we removed elements from [0 to sorted_from_idx-1]
+        cout << "\n\t\t\t---Result: " << result;
+
+        // O(n)
+        /* Up to which idx is the array in non-decreasing order? */
+        int sorted_up_to_idx = -1; // Out of bounds currently
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i-1] > arr[i])
+            {
+                sorted_up_to_idx = i;
+                break;
+            }
+        }
+        result = min(result, n - sorted_up_to_idx);
+
+        // O(n)
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && arr[i-1] > arr[i])
+                break;
+
+            while (sorted_from_idx < n && arr[i] > arr[sorted_from_idx])
+                sorted_from_idx++;
+
+            if (sorted_from_idx == n)
+                break;
+
+            result = min(result, sorted_from_idx - (i+1));
+        }
+
+        return result;
+    }
+
+private:
+    // ***************************
+    // *** STRICTLY DECREASING ***
+    // ***************************
+    bool strictly_decreasing(vector<int>& arr)
+    {
+        for (int i = 1; i < arr.size(); i++)
+        {
+            if (arr[i-1] <= arr[i])
+                return false;
+        }
+
+        return true;
+    }
 
 
     // **********************
     // *** NON-DECREASING ***
     // **********************
-
-    // bool non_decreasing(const vector<int>& arr)
-    // {
-    //     return std::is_sorted(arr.begin(), arr.end());
-    // }
-
     bool non_decreasing(vector<int>& arr)
     {
         for (int i = 1; i < arr.size(); i++)
