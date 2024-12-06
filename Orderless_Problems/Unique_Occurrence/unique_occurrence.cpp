bb315bd4d0408333af6af57c43870ed71db6c7da
Noa Vukasovic <noavukasovic@gmail.com>
2024-12-06 09:56:47 +0100

    Add Verbose Solution to: Unique_Occurrence in Orderless_Problems

--- Orderless_Problems/Unique_Occurrence/unique_occurrence.cpp
+++ Orderless_Problems/Unique_Occurrence/unique_occurrence.cpp
@@ -47,6 +47,70 @@ public:
 };
 
 
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */ // Usually it's going to be much less than n
+class Solution_2 {
+public:
+    int solution(vector<int>& A)
+    {
+        int result = 0;
+
+        int highest_occurrence_times = 0;
+        int lowest__occurrence_times = INT_MAX;
+
+        // O(n)
+        unordered_map<int, int> umap_occurrences;
+        for (const int& num : A)
+            umap_occurrences[num]++;
+
+        // O(n)
+        for (const auto& entry : umap_occurrences)
+        {
+            highest_occurrence_times = max(highest_occurrence_times, entry.second);
+            lowest__occurrence_times = min(lowest__occurrence_times, entry.second);
+        }
+
+        // O(n)
+        vector<vector<int>> occurrences(highest_occurrence_times+1);
+        // occurrences[0] is NOT going to be used
+
+        // O(n) at worst, but usually much smaller
+        for (const auto& entry : umap_occurrences)
+            occurrences[entry.second].push_back(entry.first);
+
+        int curr_frequency  = highest_occurrence_times;
+        int first_available = lowest__occurrence_times - 1;
+
+        // O(n), even if there is a nested while-loop, we only go through n elements at most
+        while (curr_frequency >= lowest__occurrence_times)
+        {
+            while (occurrences[curr_frequency].size() > 1)
+            {
+                if (first_available == 0)
+                {
+                    result += (occurrences[curr_frequency].size()-1) * (curr_frequency - 0);
+
+                    occurrences[curr_frequency] = {occurrences[curr_frequency][0]};
+                    break;
+                }
+                else
+                {
+                    result += curr_frequency - first_available;
+
+                    occurrences[curr_frequency].pop_back();
+                    first_available--;
+                }
+            }
+
+            curr_frequency--;
+        }
+
+        return result;
+    }
+};
+
+
 int
 main()
 {
