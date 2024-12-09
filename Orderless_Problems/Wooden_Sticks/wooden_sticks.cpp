49a9dc728a0de5d6119d212947ab68bdd9a9d0b0
Noa Vukasovic <noavukasovic@gmail.com>
2024-12-09 14:08:16 +0100

    Add another Solution to: Wooden_Sticks to Orderless_Problems

--- Orderless_Problems/Wooden_Sticks/wooden_sticks.cpp
+++ Orderless_Problems/Wooden_Sticks/wooden_sticks.cpp
@@ -2,74 +2,89 @@
 
 int solution(int A, int B)
 {
-	if (A+B <= 3)
-		return 0;
+    if (A+B <= 3)
+        return 0;
 
-	// Make sure A is always longer so that we don't have to "duplicate" code
-	if(A < B)
-		std::swap(A, B);
+    // Make sure A is always longer so that we don't have to "duplicate" code
+    if(A < B)
+        std::swap(A, B);
 
-	if (A/4 > B)
-		return A/4;
-	else if (A/3 >= B)
-		return B;
-	else if (A/3 < B)
-		return std::max(A/3, B/2);
+    if (A/4 > B)
+        return A/4;
+    else if (A/3 >= B)
+        return B;
+    else if (A/3 < B)
+        return std::max(A/3, B/2);
 
-	return 0;
+    return 0;
+}
+
+
+int solution_2(int A, int B)
+{
+    if (A + B < 4)
+        return 0;
+
+    if (A < B)
+        std::swap(A, B); // So that 'A' always holds GREATER value
+
+    if (A/4 >= B)
+        return A/4;
+
+    return std::max(B/2, std::min(A/3, B));
 }
 
 int
 main()
 {
-	/* Example 1 */
-	// int a = 10;
-	// int b = 21;
+    /* Example 1 */
+    // int a = 10;
+    // int b = 21;
 
-	/* Example 2 */
-	// int a = 21;
-	// int b = 10;
+    /* Example 2 */
+    // int a = 21;
+    // int b = 10;
 
-	/* Example 3 */
-	// int a = 5;
-	// int b = 21;
+    /* Example 3 */
+    // int a = 5;
+    // int b = 21;
 
-	/* Example 4 */
-	// int a = 4;
-	// int b = 21;
+    /* Example 4 */
+    // int a = 4;
+    // int b = 21;
 
-	/* Example 5 */
-	// int a = 2;
-	// int b = 1;
+    /* Example 5 */
+    // int a = 2;
+    // int b = 1;
 
-	/* Example 6 */
-	// int a = 1;
-	// int b = 8;
+    /* Example 6 */
+    // int a = 1;
+    // int b = 8;
 
-	/* Example 7 */
-	// int a = 15;
-	// int b = 18;
+    /* Example 7 */
+    // int a = 15;
+    // int b = 18;
 
-	/* Example 8 */
-	// int a = 13;
-	// int b = 31;
+    /* Example 8 */
+    // int a = 13;
+    // int b = 31;
 
-	/* Example 9 */
-	int a = 13;
-	int b = 13;
+    /* Example 9 */
+    int a = 13;
+    int b = 13;
 
-	std::cout << "\n\t=====================";
-	std::cout << "\n\t=== WOODEN STICKS ===";
-	std::cout << "\n\t=====================\n";
+    std::cout << "\n\t=====================";
+    std::cout << "\n\t=== WOODEN STICKS ===";
+    std::cout << "\n\t=====================\n";
 
-	/* Write Input */
-	std::cout << "\n\tInput: A = " << a << ", B = " << b << "\n";
+    /* Write Input */
+    std::cout << "\n\tInput: A = " << a << ", B = " << b << "\n";
 
-	/* Solution */
-	int result = solution(a, b);
+    /* Solution */
+    int result = solution(a, b);
 
-	/* Write Onput */
-	std::cout << "\n\tSolution is: " << result << std::endl << std::endl;
+    /* Write Onput */
+    std::cout << "\n\tSolution is: " << result << std::endl << std::endl;
 
-	return 0;
+    return 0;
 }
