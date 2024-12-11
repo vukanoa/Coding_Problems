3a95abeaae251c00243cec28bd3c2639a9979da5
Noa Vukasovic <noavukasovic@gmail.com>
2024-12-11 08:58:08 +0100

    Add Detailed Explanation(IDEA) to LetCode: 2779_Maximum_Beauty_Of_An_Array_After_Applying_Operation to Orderless_Problems

--- Orderless_Problems/2779_Maximum_Beauty_Of_An_Array_After_Applying_Operation/maximum_beauty_of_an_array_after_applying_operation.cpp
+++ Orderless_Problems/2779_Maximum_Beauty_Of_An_Array_After_Applying_Operation/maximum_beauty_of_an_array_after_applying_operation.cpp
@@ -77,7 +77,160 @@ using namespace std;
     --- IDEA ---
     ------------
 
-    TODO
+    Solution to this problem is most certainly much easier than you would
+    imagine, however, it's not that easy to come up with it. It's not inuitive.
+
+    After reading the problem, one of the first thing you would probably try is
+    for each element in vector "nums", try to put every number from:
+
+        [nums[i] - k, nums[i] + k]
+
+    in a "frequency counter" HashMap.
+
+    Example:
+        nums = [1,2,3],  k = 2
+
+        elem = 1:   [-1, 3]
+            umap[-1]++;
+            umap[0]++;
+            umap[1]++;
+            umap[2]++;
+            umap[3]++;
+
+        elem = 2:   [0, 4]
+            umap[0]++;
+            umap[1]++;
+            umap[2]++;
+            umap[3]++;
+            umap[4]++;
+
+        elem = 3:   [1, 5]
+            umap[1]++;
+            umap[2]++;
+            umap[3]++;
+            umap[4]++;
+            umap[5]++;
+
+    And then take the one element that occurs most amount of time and return
+    its frequency as a result.
+
+    This works, but is very inefficient.
+    Why?
+
+    Because in the Constraints of this problem it is written:
+
+        1 <= nums.length <= 10^5
+        0 <= nums[i], k <= 10^5
+
+    Which means that for every element(that is O(N)) we need to go through all
+    the elements in the range. But the range for each element is 2*k.
+
+    Why?
+    Because in range [nums[i] - k, num[i] + k] there are 2*k different elements
+
+    That means that for every element we would have to scan the entire 2*k
+    range, but we also know that in the worst case:
+
+        k == nums.size() == 10^5
+
+    Which means that 2*k is 2*N or just O(N). That further means the overall
+    Time Complexity would be O(N * N) which is O(N^2).
+
+    You would get a TLE(Time Limit Exceeded) on LeetCode.
+
+
+    So how can we optimize this?
+    Upon reviewing this Solution you will quickly find out that it's impossible
+    to improve this Solution and that you have to come up with a completely
+    different idea.
+
+    So what is the idea?
+    Here is the part that makes this Solution difficult to come up with, even
+    if the Solution itself is incredibly easy.
+
+    When reading a problem, they told us this piece of information:
+
+        "The beauty of the array is the length of the longest SUBSEQUENCE
+         consisting of equal elements."
+
+    They've also told us what is SUBSEQUNCE:
+
+        "A subsequence of an array is a new array generated from the original
+         array by deleting some elements (possibly none) without changing the
+         order of the remaining elements."
+
+    This piece of information is probably what drove you away from the correct
+    approach.
+
+    In "Subsequence" kind-of problems, we must NOT sort or re-order in any way,
+    shape or form.
+
+    However, that is not ALWAYS true. That is true "only" for most subsequence
+    problems.
+
+    In this Problem, as you've tried yourself with a Brute-Force, you could
+    see that the order is not really important, right?
+
+    We only cared about the number that appear in most ranges. Even though the
+    "longest subsequence" is what we were looking for, we didn't tweak our
+    approach to suit general subsequence problems in any way.
+
+    That should tell you that the order does NOT matter. This is very
+    un-intuitive for "subsequence" problems, but bear with me.
+
+    Consider this example:
+        nums = [4, 1, 6]     k = 2,    Let's draw them on the X-axis:
+                |  |  | 
+----------------   |  |
+|  -----------------  | 
+|  |   ----------------
+|  |   |
+|  |   v
+|  |   6:                   <===========>
+|  - > 1:    <===========>
+-----> 4:             <===========>
+             |--|--X--|--|--X--|--X--|--|--|
+            -1  0  1  2  3  4  5  6  7  8  9
+
+
+    However, if we re-ordered the input like this: 
+
+        nums = [1, 4, 6]     k = 2
+
+    And draw it again:
+       6:                   <===========>
+       4:             <===========>
+       1:    <===========>
+             |--|--X--|--|--X--|--X--|--|--|
+            -1  0  1  2  3  4  5  6  7  8  9
+
+    Would anything change?
+
+    We'll the order did change of the drawing, but the answer would still be
+    the same. There are no more than 2 numbers, in any range, that appear more
+    than twice, thus that is the answer.
+
+    However, when you look at this sorted "picture", does anything comes to
+    mind?
+
+    If you have a FIXED part to the left and FIXED part to the right and
+    therefore that range itself is FIXED, and it's FIXED for all the other
+    elements as well, does anything comes to mind now?
+
+    Yes, it's Sliding Window!
+
+    Now the problem becomes trivial. It's the most fundamental Sliding Window
+    approach.
+
+    (If you aren't familiar with "Sliding Window" technique, check-out problems
+     such as: 
+        1. Best Time to Buy and Sell a Stock
+        2. Longest Substring Without Repeating Characters
+        3. Longest Repeating Character Replacement       
+    )
+
+    If you are indeed familiar with this technique, the code down below is
+    trivial and self-explanatory. The most fundamental Sliding Window problem.
 
 */
 
