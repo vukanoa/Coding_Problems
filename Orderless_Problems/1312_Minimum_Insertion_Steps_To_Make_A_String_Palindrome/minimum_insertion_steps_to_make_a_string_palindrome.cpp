c8d4e7c354987e28952b1ca4ac0d32e48061c776
Noa Vukasovic <noavukasovic@gmail.com>
2024-12-13 11:33:39 +0100

    Add Iterative_DP Solution to LeetCode: 1312_Minimum_Insertion_Steps_To_Make_A_String_Palindrome to Orderless_Problems

--- Orderless_Problems/1312_Minimum_Insertion_Steps_To_Make_A_String_Palindrome/minimum_insertion_steps_to_make_a_string_palindrome.cpp
+++ Orderless_Problems/1312_Minimum_Insertion_Steps_To_Make_A_String_Palindrome/minimum_insertion_steps_to_make_a_string_palindrome.cpp
@@ -65,6 +65,12 @@ using namespace std;
 
 */
 
+/* Time  Beats: 7.40% */
+/* Space Beats: 9.03% */
+
+/* Time  Complexity: O(n^2) */
+/* Space Complexity: O(n)   */
+class Solution_Memoization {
 class Solution {
 public:
     int minInsertions(string s)
@@ -88,9 +94,69 @@ private:
         if (memo[m][n] != -1)
             return memo[m][n];
 
-        if (s1[m - 1] == s2[n - 1])
-            return memo[m][n] = 1 + LCS(s1, s2, m - 1, n - 1, memo);
+        if (s1[m-1] == s2[n-1])
+            return memo[m][n] = 1 + LCS(s1, s2, m-1, n-1, memo);
 
-        return memo[m][n] = max(LCS(s1, s2, m - 1, n, memo), LCS(s1, s2, m, n - 1, memo));
+        return memo[m][n] = max(LCS(s1, s2, m-1, n  , memo),
+                                LCS(s1, s2, m  , n-1, memo));
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Iterative Dynamic Programming approach.
+
+*/
+
+/* Time  Beats: 25.70% */
+/* Space Beats:  9.03% */
+
+/* Time  Complexity: O(n^2) */
+/* Space Complexity: O(n)   */
+class Solution_DP_Iterative {
+class Solution {
+public:
+    int minInsertions(string s)
+    {
+        const int n = s.length();
+
+        string s_reverse = s;
+        reverse(s_reverse.begin(), s_reverse.end());
+
+        return n - LCS(s, s_reverse, n, n);
+    }
+
+private:
+    int LCS(string& s1, string& s2, int M, int n)
+    {
+        vector<vector<int>> dp(n+1, vector<int>(n+1));
+
+        for (int row = 0; row <= M; row++)
+        {
+            for (int col = 0; col <= n; col++)
+            {
+                if (row == 0 || col == 0)
+                {
+                    dp[row][col] = 0; // One of the two strings is empty
+                }
+                else if (s1[row-1] == s2[col-1])
+                {
+                    dp[row][col] = 1 + dp[row-1][col-1];
+                }
+                else
+                {
+                    dp[row][col] = max(dp[row-1][col  ],
+                                       dp[row  ][col-1]);
+                }
+            }
+        }
+
+        return dp[M][n];
     }
 };
