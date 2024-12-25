52ca5ad2ab834f9ab97bdb4bc8b847209b3c6a1d
Noa Vukasovic <noavukasovic@gmail.com>
2024-12-25 14:17:27 +0100

    Fix LeetCode: Find_Largest_Value_In_Each_Tree_Row in Orderless_Problems

--- Orderless_Problems/515_Find_Largest_Value_In_Each_Tree_Row/find_largest_value_in_each_tree_row.cpp
+++ Orderless_Problems/515_Find_Largest_Value_In_Each_Tree_Row/find_largest_value_in_each_tree_row.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 /*
     ==============
     === MEDIUM ===
@@ -61,17 +59,22 @@
 
 */
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+// Definition for a binary tree node.
+#include <climits>
+#include <queue>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+ 
+
+#include <vector>
+using namespace std;
 
 /*
     ------------
