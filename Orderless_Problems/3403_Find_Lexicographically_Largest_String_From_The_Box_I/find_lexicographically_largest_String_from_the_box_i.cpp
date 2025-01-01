795c05a019c829326b84f5512e091d7a4e397063
Noa Vukasovic <noavukasovic@gmail.com>
2025-01-01 21:03:15 +0100

    Add LeetCode: 3403_Find_Lexicographically_Largest_String_From_The_Box_I to Orderless_Problems

---
+++ Orderless_Problems/3403_Find_Lexicographically_Largest_String_From_The_Box_I/find_lexicographically_largest_String_from_the_box_i.cpp
@@ -0,0 +1,110 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ==============================================================
+    3403) Find the Lexicographically Largest String From the Box I
+    ==============================================================
+
+    ============
+    Description:
+    ============
+
+    You are given a string word, and an integer numFriends.
+
+    Alice is organizing a game for her numFriends friends. There are multiple
+    rounds in the game, where in each round:
+
+    word is split into numFriends non-empty strings, such that no previous
+    round has had the exact same split. All the split words are put into a box.
+
+    Find the lexicographically largest string from the box after all the rounds
+    are finished.
+
+    A string a is lexicographically smaller than a string b if in the first
+    position where a and b differ, string a has a letter that appears earlier
+    in the alphabet than the corresponding letter in b. If the first
+    min(a.length, b.length) characters do not differ, then the shorter string
+    is the lexicographically smaller one.
+
+    ===========================================================
+    FUNCTION: string answerString(string word, int numFriends);
+    ===========================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: word = "dbca", numFriends = 2
+    Output: "dbc"
+    Explanation: 
+    All possible splits are:
+        "d" and "bca".
+        "db" and "ca".
+        "dbc" and "a".
+
+
+    --- Example 2 ---
+    Input: word = "gggg", numFriends = 4
+    Output: "g"
+    Explanation: 
+    The only possible split is: "g", "g", "g", and "g".
+
+
+    *** Constraints ***
+    1 <= word.length <= 5 * 10^3
+    word consists only of lowercase English letters.
+    1 <= numFriends <= word.length
+
+*/
+
+#include <string>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    There's not much to "not know" here. Literally just code-up what you're
+    told.
+
+*/
+
+/* Time  Beats: 7.58% */
+/* Space Beats: 5.89% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    string answerString(string word, int numFriends)
+    {
+        if (numFriends == 1)
+            return word;
+
+        const int N = word.length();
+        string result = "";
+
+        int len = N - (numFriends - 1);
+        if (N == numFriends)
+            len = 1;
+
+        for (int i = 0; i < N; i++)
+        {
+            string prev;
+            prev.append(1, word[i]);
+
+            string curr = prev;
+            for (int j = i+1; j < N && j-i+1 <= len; j++)
+                curr += word[j];
+
+            if (result.compare(curr) < 0)
+                result = curr;
+        }
+
+        return result;
+    }
+};
