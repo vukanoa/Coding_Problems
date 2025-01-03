f36288eb625761c1a6ccd95a3c9f658e82fd448b
Noa Vukasovic <noavukasovic@gmail.com>
2025-01-03 19:36:06 +0100

    Add Another Solution & Explanations(IDEAs) to LeetCode: 1422_Maximum_Score_After_Splitting_A_String to Orderless_Problems

--- 4_N_539/Arrays_and_Hashing/1422_Maximum_Score_After_Splitting_A_String/maximum_score_after_splitting_a_string.cpp
+++ 4_N_539/Arrays_and_Hashing/1422_Maximum_Score_After_Splitting_A_String/maximum_score_after_splitting_a_string.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 /*
     ============
     === EASY ===
@@ -56,12 +54,93 @@
 
 */
 
+#include <climits>
+#include <string>
+#include <vector>
+using namespace std;
+
 /*
     ------------
     --- IDEA ---
     ------------
 
-    TODO
+    The only important thing here is to "notice" that you need to "add":
+
+        left_zeroes[i] and right_ones[i+1]
+
+    that is--Index of right_ones has to be larger by 1. If you are going to
+    split it on index i, then you're not allow to count both 0s to the left
+    from that index and number of 1s to the right, therefore, number of 1s is
+    counted from the next elementi, i.e. i+1.
+
+    Other than that, it's obvious that we have to precalculate number of 0s to
+    the left and number of 1s to the right.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  15.33% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution {
+public:
+    int maxScore(string s)
+    {
+        const int N = s.length();
+        int result = INT_MIN;
+
+        vector<int> left_zeroes(N, 0);
+        vector<int> right_ones(N, 0);
+
+        left_zeroes[0] = s[0] == '0' ? 1 : 0;
+        for (int i = 1; i < N; i++)
+        {
+            left_zeroes[i] = left_zeroes[i-1];
+
+            if (s[i] == '0')
+                left_zeroes[i]++;
+        }
+
+        right_ones[N-1] = s[N-1] == '1' ? 1 : 0;
+        for (int i = N-2; i >= 0; i--)
+        {
+            right_ones[i] = right_ones[i+1];
+
+            if (s[i] == '1')
+                right_ones[i]++;
+        }
+
+        for (int i = 0; i < N-1; i++)
+            result = max(result, left_zeroes[i] + right_ones[i+1]);
+
+        return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Here, we can see that we don't actually need to allocate an entire vector,
+    instead we can simply use one variable per character(i.e. one for 0s and
+    one for 1s).
+
+    We count the total number of 1s and then--In another loop--we begin from 0
+    and iterate up until N-1 for the same reason as above.
+
+    However, because we do NOT have a vector now, we must use our variables to
+    have a correct idea how many 0s is from this index to the left and how many
+    is from the index right to this, to the right.
+
+    We check if current number of left_zeroes and current number of right_ones,
+    AFTER the addition/subtraction is the maximum.
+
+    That's it.
 
 */
 
@@ -70,7 +149,7 @@
 
 /* Time  Complexity: O(n) */
 /* Space Complexity: O(1) */
-class Solution {
+class Solution_Optimized {
 public:
     int maxScore(string s)
     {
