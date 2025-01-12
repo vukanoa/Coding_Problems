0f3dedb9a65212b70eefaea438bfa7797593c396
Noa Vukasovic <noavukasovic@gmail.com>
2025-01-12 13:20:22 +0100

    Add LeetCode: 2116_Check_If_A_Parentheses_String_Can_Be_Valid to Orderless_Problems

---
+++ Orderless_Problems/2116_Check_If_A_Parentheses_String_Can_Be_Valid/check_if_a_parentheses_string_can_be_valid.cpp
@@ -0,0 +1,138 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ================================================
+    2116) Check if a Parentheses String Can Be Valid
+    ================================================
+
+    ============
+    Description:
+    ============
+
+    A parentheses string is a non-empty string consisting only of '(' and ')'.
+    It is valid if any of the following conditions is true:
+
+        + It is ().
+
+        + It can be written as AB (A concatenated with B), where A and B are
+          valid parentheses strings.
+
+        + It can be written as (A), where A is a valid parentheses string.
+
+    You are given a parentheses string s and a string locked, both of length n.
+    locked is a binary string consisting only of '0's and '1's. For each index
+    i of locked,
+
+        + If locked[i] is '1', you cannot change s[i].
+
+        + But if locked[i] is '0', you can change s[i] to either '(' or ')'.
+
+    Return true if you can make s a valid parentheses string. Otherwise, return
+    false.
+
+    ===================================================
+    FUNCTION: bool canBeValid(string s, string locked);
+    ===================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: s = "))()))", locked = "010100"
+    Output: true
+    Explanation: locked[1] == '1' and locked[3] == '1', so we cannot change s[1] or s[3].
+    We change s[0] and s[4] to '(' while leaving s[2] and s[5] unchanged to make s valid.
+
+    --- Example 2 ---
+    Input: s = "()()", locked = "0000"
+    Output: true
+    Explanation: We do not need to make any changes because s is already valid.
+
+    --- Example 3 ---
+    Input: s = ")", locked = "0"
+    Output: false
+    Explanation: locked permits us to change s[0].
+    Changing s[0] to either '(' or ')' will not make s valid.
+
+
+    *** Constraints ***
+    n == s.length == locked.length
+    1 <= n <= 10^5
+    s[i] is either '(' or ')'.
+    locked[i] is either '0' or '1'.
+
+*/
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 81.22% */
+/* Space Beats: 16.75% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution {
+public:
+    bool canBeValid(string s, string lockedStatus)
+    {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0); // Accelerates
+        
+        int N = s.size();
+
+        if (N % 2 == 1)
+            return false;
+
+        stack<int> open_indices;
+        stack<int> unlocked_indices;
+
+        for (int i = 0; i < N; i++)
+        {
+            if (lockedStatus[i] == '0')
+            {
+                unlocked_indices.push(i);
+            }
+            else if (s[i] == '(')
+            {
+                open_indices.push(i);
+            }
+            else if (s[i] == ')')
+            {
+                if ( ! open_indices.empty())
+                {
+                    open_indices.pop();
+                }
+                else if ( ! unlocked_indices.empty())
+                {
+                    unlocked_indices.pop();
+                }
+                else
+                    return false;
+            }
+        }
+
+        while ( ! open_indices.empty() && ! unlocked_indices.empty() && open_indices.top() < unlocked_indices.top())
+        {
+            open_indices.pop();
+            unlocked_indices.pop();
+        }
+
+        if (open_indices.empty() && ! unlocked_indices.empty())
+            return unlocked_indices.size() % 2 == 0;
+
+        return open_indices.empty();
+    }
+};
