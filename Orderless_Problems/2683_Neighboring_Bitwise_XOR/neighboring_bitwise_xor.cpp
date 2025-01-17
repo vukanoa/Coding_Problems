8c20e6c13bd11ca6b4d54d82460fe7e24eefe91c
Noa Vukasovic <noavukasovic@gmail.com>
2025-01-17 14:34:43 +0100

    Add Elegant Solution to LeetCode: 2683_Neighoring_Bitwise_XOR to Orderless_Problems

--- Orderless_Problems/2683_Neighboring_Bitwise_XOR/neighboring_bitwise_xor.cpp
+++ Orderless_Problems/2683_Neighboring_Bitwise_XOR/neighboring_bitwise_xor.cpp
@@ -40,7 +40,7 @@
     Input: derived = [1,1,0]
     Output: true
     Explanation: A valid original array that gives derived is [0,1,0].
-    derived[0] = original[0] ⊕ original[1] = 0 ⊕ 1 = 1 
+    derived[0] = original[0] ⊕ original[1] = 0 ⊕ 1 = 1
     derived[1] = original[1] ⊕ original[2] = 1 ⊕ 0 = 1
     derived[2] = original[2] ⊕ original[0] = 0 ⊕ 0 = 0
 
@@ -198,5 +198,79 @@ private:
         }
 
         return false; // Unreachable
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+
+    Similar to the above approach, but written in a much more elegant way.
+
+    To determine whether a valid original array can be constructed from the
+    given derived array, we can carefully simulate how the original array would
+    be built.
+
+    From the problem, we know:
+
+     derived[i] = original[i] XOR original[i+1]
+
+
+    Using the inversion property of XOR, we can rewrite this as:
+
+        original[i+1] = derived[i] XOR original[i]
+
+    This means that if we know the value of the original[i], we can calculate
+    the next element, original[i+1], using the corresponding value from derived.
+
+    The first element of original, original[0], can be either 0 or 1 (since
+    it’s binary).
+
+        + If we assume original[0] = 0, we can calculate the rest of the array.
+
+        + Similarly, we can repeat the process assuming original[0] = 1.
+
+    Once we compute all the elements of the original for both starting points,
+    we need to check if they satisfy the circular condition:
+
+        derived[N-1] = original[N-1] XOR original[0]
+
+
+    This ensures that the last element in derived matches the XOR of the first
+    and last elements of original.
+
+    If the circular condition is satisfied for either of the two cases
+    (original[0] = 0 or original[0] = 1), then a valid original array exists,
+    and we return true. Otherwise, we return false.
+
+*/
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_2 {
+public:
+    bool doesValidArrayExist(vector<int>& derived)
+    {
+        const int N = derived.size();
+        vector<int> original = {0};
+
+        for (int i = 0; i < N; i++)
+            original.push_back((derived[i] ^ original[i]));
+
+        bool check_for_zero = (original[0] == original[N-1]);
+
+        original = {1};
+
+        for (int i = 0; i < derived.size(); i++)
+            original.push_back((derived[i] ^ original[i]));
+
+        bool check_for_one = (original[0] == original[N-1]);
+
+        return check_for_zero | check_for_one;
     }
 };
