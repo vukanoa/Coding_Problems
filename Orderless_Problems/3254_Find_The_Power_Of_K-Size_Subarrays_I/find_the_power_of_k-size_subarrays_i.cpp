a63f1d2a74f23891b1565034840184f9818c3c7f
Noa Vukasovic <noavukasovic@gmail.com>
2025-02-04 18:07:08 +0100

    Add another Solution to LeetCodE: 3254_Find_The_Power_Of_K-Size_Subarrays_I to ORderless_Problems

--- Orderless_Problems/3254_Find_The_Power_Of_K-Size_Subarrays_I/find_the_power_of_k-size_subarrays_i.cpp
+++ Orderless_Problems/3254_Find_The_Power_Of_K-Size_Subarrays_I/find_the_power_of_k-size_subarrays_i.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <vector>
-
 /*
     ==============
     === MEDIUM ===
@@ -66,6 +63,9 @@
 
 */
 
+#include <vector>
+using namespace std;
+
 /*
     ------------
     --- IDEA ---
@@ -86,11 +86,11 @@
     (I've devised a longer example so that the optimization is more obvious)
 
     Since k=5, we have to make sure that all the elements:
-        
+
         from left=0 to right=k-1, inclusively, are sorted in CONSECUTIVE
         ascending order.
-    
-    Brute-Forcing is would look like this:
+
+    Brute-Forcing it would look like this:
 
                start
                 |
@@ -141,9 +141,10 @@
         nums = [6, 7, 8, 9, 5, 6, 7, 8, 9, 10, 11, 4, 7, 2, 17, 18, 19, 20, 21]
                 0  1  2  3  4  5  6  7  8   9  10 11 12 13  14  15  16  17  18
                             i
-                (At this point, i=4) we would find out that this is NOT sorted
+                (At this point, for i=4, we could know that this is NOT sorted,
+                 however we're going to continue until we process all k)
 
-        
+
         nums = [6, 7, 8, 9, 5, 6, 7, 8, 9, 10, 11, 4, 7, 2, 17, 18, 19, 20, 21]
                 0  1  2  3  4  5  6  7  8   9  10 11 12 13  14  15  16  17  18
                                i
@@ -162,7 +163,7 @@
 
 
     We're going to solve this by using a "Sliding Window" technique, or the
-    "Two Pointers", you could call it "Two Pointers" as well.
+    "Two Pointers". I guess you could call it "Two Pointers" as well.
 
     We're going to have "left" and "right" pointers, but we will need another
     pointer to take care of the "sortedness" in cases k > 2.
@@ -242,22 +243,22 @@
             be sorted in CONSECUTIVE ascending order.
 
             Let's consider all subarrays that contains nums[3]=9 and nums[4]=5:
-                
+
                 1)          |             |
                     nums = [|6, 7, 8, 9, 5| 6, 7, 8, 9, 10, 11, 4, 7, 2]
                             |0  1  2  3  4| 5  6  7  8   9  10 11 12 13
                             |             |
-                
+
                 2)             |             |
                     nums = [ 6,|7, 8, 9, 5  6| 7, 8, 9, 10, 11, 4, 7, 2]
                              0 |1  2  3  4  5| 6  7  8   9  10 11 12 13
                                |             |
-                
+
                 3)                |             |
                     nums = [ 6, 7,|8, 9, 5  6, 7| 8, 9, 10, 11, 4, 7, 2]
                              0  1 |2  3  4  5  6| 7  8   9  10 11 12 13
                                   |             |
-                
+
                 4)                   |              |
                     nums = [ 6, 7, 8,|9, 5  6, 7, 8,|9, 10, 11, 4, 7, 2]
                              0  1  2 |3  4  5  6  7 |8   9  10 11 12 13
@@ -304,7 +305,7 @@
                nums = [1, 2, 3, 4, 3, 2, 5],   k = 3
                        0  1  2  3  4  5  6
                        M  L     R
-                
+
             But if we don't do anything, then our "mid" pointer is deprecated.
             Then we would need to, again, iterate over new subarray, which
             we've concluded was inefficient.
@@ -406,5 +407,139 @@ public:
         }
 
         return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Another way of solving it. This one is much more concise and
+    striaghtforward.
+
+    First, note that if N == 1, then k MUST be 1 as well, due to the given
+    Constraints.
+
+    Also, if k == 1, then we should just return our Input "nums" as a result,
+    since longest subarray starting at each index is going to be, both
+    minimally and maximally, of size 1.
+
+    Variable 'L' represents the beginning of a subarray that is consecutive
+    and sorted in ascending order. Also L must update if either:
+
+        1) Current window, i.e. (R - L) gets bigger than k, or
+
+        2) Current element is NOT a consecutive ascending successor of the
+           previous one.
+
+
+    It's much easier if you take a look at an example:
+
+        Input: nums = [1,2,3,4,3,2,5], k = 3
+
+    Until the window becomes size k, only check if the current element is the
+    consectuive sorted successor of the previous.
+
+    If it is INDEED, then do nothing.
+    It it is NOT   , then say L = R. Why? Because we certainly will NOT have
+    a window of size k BEFORE the one that starts at the current index.
+
+    Look at this example:
+
+        Input: nums = [7,8,9,2,3,4,5,6,7], k = 6
+                       0 1 2 3 4 5 6 7 8
+                       L     R
+
+    Once R gets to be 3, we still don't have a window of size k, right?
+    (Because R - L + 1 is (3 - 0 + 1) = 4)
+
+    However, we can see that the current element is NOT the successor of the
+    one before it, therefore there's not POINT in checking these next few
+    windows:
+
+        Input: nums = [7,8,9,2,3,4,5,6,7], k = 6
+                       0 1 2 3 4 5 6 7 8
+                         L         R                // This one [1,6]
+
+        Input: nums = [7,8,9,2,3,4,5,6,7], k = 6
+                       0 1 2 3 4 5 6 7 8
+                           L         R              // And this one [2,7]
+
+    Why is there no point in checking these?
+    Because we know that somewhere in the middle we will 100% have an
+    infringement of the required conditions--We won't have consecutive
+    sortedness because we saw that element 2 at index 3 is NOT the consecutive
+    successor of 9 at index 2, THEREFORE ANY WINDOW THAT CONTAINTS BOTH
+    9 and 2 WILL CERTAINLY NOT FULFILL OUR REQUIRED CONDITIONS!
+
+    The FIRST window that is NOT going to contain BOTH 9 and 2 is the window
+    that BEGINS at element 2(index 3).
+
+    That's why we're IMMEDIATELY setting our 'L' pointer to point at element 2
+    at index 3.
+
+    We know that until we have a window of size k, beginning at index 3, we
+    CERTAINLY won't be able to fulfill our requirements.
+
+    Maybe we won't fulfill it even if we begin at index 3, but we just don't
+    know yet.
+
+    However, we DO know that we CERTAINLY will NOT be able to fulfill all of
+    the requirements if both 9 and 2 are included.
+
+    This is the absolute crux. Once you understand this, the code becomes
+    painfully obvious.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  72.43% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution_2 {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k)
+    {
+        const int N = nums.size();
+        vector<int> results;
+
+        if (k == 1)
+            return nums;
+
+        int L = 0;
+        int R = 1;
+        while (R < N)
+        {
+            if (R < k-1)
+            {
+                if ((nums[R-1] + 1) != nums[R])
+                    L = R;
+            }
+            else
+            {
+                if ((L == (R - k+1)) && ((nums[R-1] + 1) == nums[R]))
+                {
+                    L++;
+                    results.push_back(nums[R]);
+                }
+                else
+                {
+                    if (((nums[R-1] + 1 != nums[R])))
+                        L = R;
+
+                    results.push_back(-1);
+                }
+            }
+
+            // Increment
+            R++;
+        }
+
+        return results;
     }
 };
