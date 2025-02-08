3037e02718247bb2682eda3dc18925551c17e3e4
Noa Vukasovic <noavukasovic@gmail.com>
2025-02-08 16:13:08 +0100

    Add TreeSet Solution to LeetCode: 2349_Design_A_Number_Container_System to Orderless_Problems

--- Orderless_Problems/2349_Design_A_Number_Container_System/design_a_number_container_system.cpp
+++ Orderless_Problems/2349_Design_A_Number_Container_System/design_a_number_container_system.cpp
@@ -80,10 +80,10 @@
 */
 
 #include <queue>
+#include <set>
 #include <unordered_map>
 #include <vector>
 using namespace std;
-
 
 /*
     ------------
@@ -91,15 +91,15 @@ using namespace std;
     ------------
 
     The code is more or less self-explanatory. The only thing that is important
-    to note here is how "number_heap" HashMap works.
+    to note here is how "number_to_heap" HashMap works.
 
     We'll have a total of 3 HashMaps:
-        1. idx_number
-        2. number_heap
-        3. number_removed_idx
+        1. idx_to_number
+        2. number_to_heap
+        3. number_to_removed_indices
 
-    the first one, i.e. "idx_number", is just a basic helper HashMap, nothing
-    fancy about it.
+    the first one, i.e. "idx_to_number", is just a basic helper HashMap,
+    nothing fancy about it.
 
     The second and third HashMaps are working together.
     Since for every number we need the SMALLEST index, we can't just assign
@@ -117,7 +117,7 @@ using namespace std;
 
     But there's one problem with it.
 
-    Imagine that we had this situation in "number_heap":
+    Imagine that we had this situation in "number_to_heap":
 
              Hash Map
         | Number :  MinHeap  |
@@ -135,8 +135,8 @@ using namespace std;
 
         change(3, 50) ? // It's THREE(3), not TWO(2), it's not a mistake
 
-    We would certainly push index 3 to number_heap[50], however there' one
-    problem -- What would happen with "index 3" that is in number_heap[10]?
+    We would certainly push index 3 to number_to_heap[50], however there' one
+    problem -- What would happen with "index 3" that is in number_to_heap[10]?
 
     If after this we've had this change:
 
@@ -168,25 +168,26 @@ using namespace std;
     This seems impossible or very challenging.
 
     The solution is -- another HashMap.
-    This Hashmap will be called "number_removed_idx" and will track all the
-    indices that are REMOVED from that number.
+    This Hashmap will be called "number_to_removed_indices" and will track all
+    the indices that are REMOVED from that number.
 
     In this case we'll have index 3 at number 10.
     But there's yet another problem.
 
     What if we had removed 3 and thus have put it in HashMap
-    "number_removed_idx", but then afterwards we add 3 to number 10?
+    "number_to_removed_indices", but then afterwards we add 3 to number 10?
 
     In that case we'll only have ONE removal of "index 3" from number 10 which
     would be correct for this case, but what if we, then, removed "index 3"
     from 10 again?
 
-    Then our "number_removed_idx" would have ONLY ONE "index 3" in it, but we'd
-    have two 3's in min_heap of number 10.
+    Then our "number_to_removed_indices" would have ONLY ONE "index 3" in it,
+    but we'd have two 3's in min_heap of number 10.
 
-    That's why, for every number in "number_removed_idx" we'll not only have an
-    index, but a whole other unordered_map of itself where we'll keep the track
-    of the amount of removals of some specific index from some "number".
+    That's why, for every number in "number_to_removed_indices" we'll not only
+    have an index, but a whole other unordered_map of itself where we'll keep
+    the track of the amount of removals of some specific index from some
+    "number".
 
     It's a little confusing at first, but once you read the code CAREFULLY and
     reason every line, it's not too bad.
@@ -205,36 +206,36 @@ public:
     /* Time  Complexity: O(1) */
     void change(int index, int number)
     {
-        if (idx_number.count(index))
+        if (idx_to_number.count(index))
         {
-            int old_number = idx_number[index];
+            int old_number = idx_to_number[index];
 
-            if ( ! number_removed_idx.count(old_number))
-                number_removed_idx.insert( {old_number, {}} );
+            if ( ! number_to_removed_indices.count(old_number))
+                number_to_removed_indices.insert( {old_number, {}} );
 
-            number_removed_idx[old_number][index]++;
+            number_to_removed_indices[old_number][index]++;
         }
 
         // if (number_heap[number].empty())
-        number_heap[number].push(index);
-        idx_number[index] = number;
+        number_to_heap[number].push(index);
+        idx_to_number[index] = number;
     }
 
     /* Time  Complexity: O(logN) */
     int find(int number)
     {
-        while ( ! number_heap[number].empty())
+        while ( ! number_to_heap[number].empty())
         {
-            int smallest_idx = number_heap[number].top();
+            int smallest_idx = number_to_heap[number].top();
 
-            if (number_removed_idx[number].count(smallest_idx))
+            if (number_to_removed_indices[number].count(smallest_idx))
             {
-                number_removed_idx[number][smallest_idx]--;
+                number_to_removed_indices[number][smallest_idx]--;
 
-                if (number_removed_idx[number][smallest_idx] == 0)
-                    number_removed_idx[number].erase(smallest_idx);
+                if (number_to_removed_indices[number][smallest_idx] == 0)
+                    number_to_removed_indices[number].erase(smallest_idx);
 
-                number_heap[number].pop();
+                number_to_heap[number].pop();
             }
             else
                 return smallest_idx;
@@ -244,9 +245,9 @@ public:
     }
 
 private:
-    unordered_map<int,int> idx_number;
-    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> number_heap;
-    unordered_map<int,unordered_map<int,int>> number_removed_idx;
+    unordered_map<int,int> idx_to_number;
+    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> number_to_heap;
+    unordered_map<int,unordered_map<int,int>> number_to_removed_indices;
 };
 
 
@@ -261,12 +262,20 @@ private:
     "number_removed_idx" HashMap, instead it uses "Lazy Update" technique in
     MinHeap.
 
+    We can do a "Lazy Update" and not have an additional HashMap because we can
+    now in O(1) in which "number" does some index belong to.
+
+    If it DOESN'T belong to the current number, then we should've removed it
+    already, but since this is "Lazy Update" we're doing it here.
+
+    That's the whole difference.
+
     It's good to see both Solutions. Sometimes "Lazy Update" is difficult to
     spot.
 
 */
 
-/* Time  Beats: 89.06% */
+/* Time  Beats: 95.63% */
 /* Space Beats: 93.13% */
 
 /* Space Complexity: O(N) */
@@ -306,3 +315,65 @@ private:
     unordered_map<int, priority_queue<int, vector<int>, greater<int>>> number_to_heap;
     unordered_map<int, int> idx_to_number;
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This one uses a HashSet, but ORDERED HashSet(sometimes referred to as
+    "Tree Set" because it's usually impemented using Red-Black Trees)
+
+    Idea is very similar to two above. If you've understood MinHeap ones, then
+    reading the code of this one we'll invoke an "AHA" moment.
+
+*/
+
+/* Time  Beats: 36.56% */
+/* Space Beats: 45.63% */
+
+/* Space Complexity: O(N) */
+class NumberContainers_Tree_Set {
+public:
+    NumberContainers_Tree_Set() {}
+
+    /* Time  Complexity: O(logN) */
+    void change(int index, int number)
+    {
+        if (idx_to_number.find(index) != idx_to_number.end()) // If exists
+        {
+            int old_number = idx_to_number[index];
+
+            number_to_indices[old_number].erase(index);
+
+            if (number_to_indices[old_number].empty())
+                number_to_indices.erase(old_number);
+        }
+
+        idx_to_number[index] = number;
+        number_to_indices[number].insert(index);
+    }
+
+    /* Time  Complexity: O(1) */
+    int find(int number)
+    {
+        if (number_to_indices.find(number) != number_to_indices.end())
+            return *number_to_indices[number].begin();
+
+        return -1;
+    }
+
+private:
+    unordered_map<int, set<int>> number_to_indices;
+    unordered_map<int, int> idx_to_number;
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
