f4883b6433a4dd8425282df650673d2df8884abf
Noa Vukasovic <noavukasovic@gmail.com>
2025-02-11 16:19:58 +0100

    Add LeetCode: 1329_Sort_The_Matrix_Diagonally to Orderless_Problems

---
+++ Orderless_Problems/1329_Sort_The_Matrix_Diagonally/sort_the_matrix_diagonally.cpp
@@ -0,0 +1,162 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ================================
+    1329) Sort the Matrix Diagonally
+    ================================
+
+    ============
+    Description:
+    ============
+
+    A matrix diagonal is a diagonal line of cells starting from some cell in
+    either the topmost row or leftmost column and going in the bottom-right
+    direction until reaching the matrix's end. For example, the matrix diagonal
+    starting from mat[2][0], where mat is a 6 x 3 matrix, includes cells
+    mat[2][0], mat[3][1], and mat[4][2].
+
+    Given an m x n matrix mat of integers, sort each matrix diagonal in
+    ascending order and return the resulting matrix.
+
+    =====================================================================
+    FUNCTION: vector<vector<int>> diagonalSort(vector<vector<int>>& mat);
+    =====================================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: mat = [[3,3,1,1],[2,2,1,2],[1,1,1,2]]
+    Output: [[1,1,1,1],[1,2,2,2],[1,2,3,3]]
+
+    --- Example 2 ---
+    Input: mat = [[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]]
+    Output: [[5,17,4,1,52,7],[11,11,25,45,8,69],[14,23,25,44,58,15],[22,27,31,36,50,66],[84,28,75,33,55,68]]
+
+
+    *** Constraints ***
+    m == mat.length
+    n == mat[i].length
+    1 <= m, n <= 100
+    1 <= mat[i][j] <= 100
+
+*/
+
+#include <algorithm>
+#include <queue>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+
+
+/* Time  Beats: 25.23% */
+/* Space Beats: 78.96% */
+
+/* Time  Complexity: O(N^2 * logN) */
+/* Space Complexity: O(N^2)        */
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat)
+    {
+        const int ROWS = mat.size();
+        const int COLS = mat[0].size();
+
+        vector<vector<int>> result(ROWS, vector<int>(COLS, 0));
+
+        /* Top-Right */
+        for (int col = 1; col < COLS; col++)
+        {
+            int i = 0;
+            int j = col;
+            vector<int> diagonal;
+
+            while (i < ROWS && j < COLS)
+            {
+                diagonal.push_back(mat[i][j]);
+                i++;
+                j++;
+            }
+
+            sort(diagonal.begin(), diagonal.end(), greater<int>());
+
+            i = 0;
+            j = col;
+            while (i < ROWS && j < COLS)
+            {
+                result[i][j] = diagonal.back();
+                diagonal.pop_back();
+
+                // Increment
+                i++;
+                j++;
+            }
+        }
+
+        /* Bottom-Left */
+        for (int row = 0; row < ROWS; row++)
+        {
+            int i = row;
+            int j = 0;
+
+            vector<int> diagonal;
+
+            while (i < ROWS && j < COLS)
+            {
+                diagonal.push_back(mat[i][j]);
+                i++;
+                j++;
+            }
+
+            sort(diagonal.begin(), diagonal.end(), greater<int>());
+
+            i = row;
+            j = 0;
+            while (i < ROWS && j < COLS)
+            {
+                result[i][j] = diagonal.back();
+                diagonal.pop_back();
+
+                // Increment
+                i++;
+                j++;
+            }
+        }
+
+        return result;
+    }
+};
+
+
+class Solution_Concise {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat)
+    {
+        int ROWS = mat.size();
+        int COLS = mat[0].size();
+
+        unordered_map<int, priority_queue<int, vector<int>, greater<int>>> diagonal;
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                diagonal[i - j].push(mat[i][j]);
+            }
+        }
+
+        for (int i = 0; i < ROWS; i++)
+        {
+            for (int j = 0; j < COLS; j++)
+            {
+                mat[i][j] = diagonal[i - j].top();
+                diagonal[i - j].pop();
+            }
+        }
+
+        return mat;
+    }
+};
