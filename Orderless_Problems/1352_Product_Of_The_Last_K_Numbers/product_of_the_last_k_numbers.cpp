73949af881fb201045a0ab1e4b7e07affd75f158
Noa Vukasovic <noavukasovic@gmail.com>
2025-02-14 15:03:33 +0100

    Add Follow-Up Solution to LeetCode: 1352_Product_Of_The_Last_K_Numbers to Orderless_Problems

--- Orderless_Problems/1352_Product_Of_The_Last_K_Numbers/product_of_the_last_k_numbers.cpp
+++ Orderless_Problems/1352_Product_Of_The_Last_K_Numbers/product_of_the_last_k_numbers.cpp
@@ -136,3 +136,106 @@ public:
 private:
     vector<int> vec;
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    The only tricky part here is how we handle number 0. In this case we're
+    resetting our prefix_product, however that leaves us with a potential
+    problem which appears once k is GREATER than the number of last non-zero
+    elements.
+
+    For example:
+
+        4, 8, 9, 2, 0, 4, 2, 3
+                             ^
+                             |
+                       first from the back
+
+        k = 5 or k = 6
+
+
+    We're told one important thing for the getProduct function:
+
+        "You can assume that always the current list has at least k numbers."
+
+    However, since we're RESETTING our "size" variable, that will NOT be the
+    case anymore. Therefore, we have to be careful with it.
+
+    We're going to handle it by always checking if there are less last non-zero
+    elements than k and if that's true, we just return 0.
+
+    If this confuses you, look at the example:
+
+    For example:
+
+        4, 8, 9, 2, 0, 4, 2, 3
+                             ^
+                             |
+                       first from the back
+
+
+        For k = 4 ork = 5 or k = 6, our result SHOULD be 0.
+
+        For k = 6: 9 * 2 * 0 * 4 * 2 * 3 = 0
+        For k = 5:     2 * 0 * 4 * 2 * 3 = 0
+        For k = 4:         0 * 4 * 2 * 3 = 0
+
+        However, for k = 3 or k = 2 or k = 1, we should return: 24, 6, 3
+        respectively.
+
+        For k = 3: 4 * 2 * 3 = 24
+        For k = 2:     2 * 3 =  6
+        For k = 1:         3 =  3
+
+    If k is NOT greater than our size of last non-zero elements, then simply
+    return:
+
+        return prefix_product[size] / prefix_product[size - k];
+
+*/
+
+/* Time  Beats: 38.79% */
+/* Space Beats: 93.67% */
+class ProductOfNumbers_Follow_Up {
+public:
+    ProductOfNumbers_Follow_Up ()
+    {
+        prefix_product.push_back(1);
+        size = 0;
+    }
+
+    // O(1)
+    void add(int num)
+    {
+        if (num == 0)
+        {
+            prefix_product = {1};
+            size = 0;
+        }
+        else
+        {
+            prefix_product.push_back(prefix_product[size] * num);
+            size++;
+        }
+    }
+
+    // O(1)
+    int getProduct(int k)
+    {
+        // This never happens according to Constraints, but we reset "size"
+        if (k > size)
+            return 0;
+
+        return prefix_product[size] / prefix_product[size - k];
+    }
+
+private:
+    vector<int> prefix_product;
+    int size = 0;
+};
