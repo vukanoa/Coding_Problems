83773aaef12a907c9058e0a7c6eddaddf74f6678
Noa Vukasovic <noavukasovic@gmail.com>
2025-03-14 15:17:08 +0100

    Add Detailed Explanation(IDEA) to LeetCode: 2226_Maximum_Candles_Allocated_To_K_Children to Orderless_Problems

--- Orderless_Problems/2226_Maximum_Candies_Allocated_To_K_Children/maximum_candies_allocated_to_k_children.cpp
+++ Orderless_Problems/2226_Maximum_Candies_Allocated_To_K_Children/maximum_candies_allocated_to_k_children.cpp
@@ -64,7 +64,159 @@ using namespace std;
     --- IDEA ---
     ------------
 
-    TODO
+    This is a classic Binary Search problem. It may not be obvious to people
+    who just started using Binary Search, but this is Binary Search by-the-book
+
+    (It's almost equivalent problem to the famous "875. Koko Eating Bananas")
+
+    The initial Binary Search people know of is when you have a sorted array
+    through which you have to find some element using it.
+
+    But that's way too trivial. No interviewer will ever ask such a question.
+    Instead, Binary Search problems look exactly like this.
+
+    You don't have a sorted vector, but you can perform it.
+    How?
+
+    Anytime you don't know what to do, ask yourself this question:
+
+        "Is there a MINIMAL and MAXIMAL possible answer to this problem?"
+
+    In other words, we're asked what is THE MAXIMUM number of candies we can
+    allocate given that there are k children.
+
+    If we sum all the candies and the total number of candies is LESS than k,
+    then we are certain that we cannot allocate even 1 candies to all children.
+
+
+        ******************
+        *** Digression ***
+        ******************
+
+        Consider this example:
+
+            Input: candies = [1,8,6], k = 7
+
+        To allocate candies for k=7 children, we need AT LEAST 7 candies in
+        total, right?
+
+        However we don't really need to use every pile. Here we're able to
+        obtain candies from only index 1 and index 2.
+
+
+        Index 1 will gives us two candies for 4 children.
+        Index 2 will gives us two candies for 3 children.
+
+        Therefore we got 2 candies for all k=7 children even if we didn't use
+        our index=1 pile of candies(1 in this case).
+
+
+        But if we had this instead:
+
+            Input: candies = [1,1,7,6], k = 7
+
+        Here we WOULD NOT be able to allocate two candies for all k=2 children.
+        How is that possible if--in total--we have same amount of candies?
+
+        We can allocate one candy for each of k=7 children. If the total sum of
+        candies is GREATER THAN OR EQUALS than k, then we can, at least,
+        allocate 1 candy per child.
+
+        However, in this example, even if we could, in theory allocate 2
+        candies for each of k=7 children, we can't do that in practice because
+        every child MUST get all of its X candies from a SINGLE pile.
+
+        Let me say that again: 
+
+            Every child MUST get all of its X candies from a SINGLE pile!
+
+        In our example:
+
+            Input: candies = [1,1,7,6], k = 7
+
+        We have a total of 15 candies. In theory we could give 2 candies to
+        each of our k=7 children.
+
+        However, since we're NOT allowed to give any child candies from more
+        than a SINGLE pile, we can't do that.
+
+        Why?
+        Let's see:
+
+            pile 0: We can't allocate 2 candies for    any   children.
+            pile 1: We can't allocate 2 candies for    any   children.
+            pile 2: We CAN   allocate 2 candies for  7/2 = 3 children.
+            pile 3: We CAN   allocate 2 candies for  7/2 = 3 children.
+
+        Therefore, given our rule, we can allocate 2 candies for 6 children,
+        but we needed to do that for k=7 children.
+
+        Therefore, since we are NOT allowed to allocate candies to any child
+        from more than a SINGLE pile, we can't "pile up" pile 0 and pile 1 to
+        allocate 2 candies for the last child.
+
+
+    Now let's ask our "magic" question:
+
+        "Is there a MINIMAL and MAXIMAL possible answer to this problem?"
+
+    Yes, there is!
+
+    MINIMAL possible answer is to allocate 1 candy   for k children.
+    MAXIMAL possible answer is to allocate X candies for k children.
+
+        where 'X' can be any number from 1 up to sum of all candies.
+
+    For example, if we had:
+
+        Input: candies = [0, 0, 17, 0], k=1
+
+    MINIMAL: 1
+    MAXIMAL: 17
+
+    Computer doesn't know immediately which one is it, but the answer lays
+    between 1 and 17, that's for sure.
+
+    Since we've established that there is a RANGE between which the answer is
+    guaranteed--We now know Binary Search is technique to be used.
+
+
+    Instead of:
+        Trying to see if we can allocate 1  candy   for k children, then
+        trying to see if we can allocate 2  candies for k children, then
+        trying to see if we can allocate 3  candies for k children, ...
+
+        trying to se eif we can allocate 17 candies for k children
+
+    Which would take us O(M * N), where M is the sum of all candies, i.e. 17
+    and N is the size of candies through which we have to iterate each time
+    and see how many can we allocate FROM WITHIN EACH PILE SEPARATELY.
+
+    Then, if there are >= than k allocated, we say that we can allocate that
+    many candies, otherwise we can't and we stop immediately.
+
+
+    But instead of doing it in this Brute-Force way, since we've established
+    that there is a RANGE between which the answer is guaranteed, we can do
+    Binary Search on this outer part to reduce Time Complexity.
+
+    Therefore, now we'll try to allocate "mid" amount of candies, where "mid"
+    is the calculation of "mid" in a Binary Search, i.e.:
+
+        mid = L + (R - L) / 2;      // Be careful NOT to do this: (L + R) / 2
+
+    Then we try to see if we can allocate "mid" amount of candies for k
+    children by iterating thorugh all of our N candies and counting how many
+    candies can we allocate WITHIN EACH PILE SEPARATELY.
+
+    If we can allocate >= k candies, then:
+        L = mid + 1
+    else
+        R = mi - 1
+
+    This way, we've reduced our Time Complexity:
+
+        from O(M * N) down to (logM * N)
 
 */
 
