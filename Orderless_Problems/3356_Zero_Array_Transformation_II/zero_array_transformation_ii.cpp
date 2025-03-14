469dd9ddc4cfdfbc4ae5e3972d1911e8818d6871
Noa Vukasovic <noavukasovic@gmail.com>
2025-03-14 15:18:42 +0100

    Add Detailed Explanation(IDEA) to Orderless_Problems: 3356_Zero_Array_Transformation_II to Orderless_Problems

--- Orderless_Problems/3356_Zero_Array_Transformation_II/zero_array_transformation_ii.cpp
+++ Orderless_Problems/3356_Zero_Array_Transformation_II/zero_array_transformation_ii.cpp
@@ -79,7 +79,128 @@ using namespace std;
     --- IDEA ---
     ------------
 
-    TODO
+    This is by-the-book Binary Search Solution.
+
+    Whenever you're asked to check how many FROM SOME RANGE you need in order
+    to accmoplish X, you can perform a Binary Search.
+
+    Here we immediately try all of them, i.e. R = M.size(). If we're unable to
+    accomplish what we're being asked to do even by using all of the queries
+    then we ought to return -1.
+
+    However, if we CAN do it by, at least, using all of them, we can try to see
+    if we can accomplish the same thing by using less than all of them.
+
+    But how are we going to do it? Are we going to try first querry and then
+    if we are unable, then try with first 2, etc. until we find it?
+
+    Well, no. That would be considered a Brute Force.
+
+    Instead, we can try first mid, where mid is the middle query element
+    between 0 and "mid"(i.e. queries.size()).
+
+    Then, if we can indeed accomplish what we're being asked, then try with
+    fewer queries, i.e. try to accomplish the same with lesser 'k' value.
+
+    In that case we'd do:
+
+        R = mid - 1
+
+    Meaning, we're sliding our right edge before our current "mid" value.
+
+    However, if we couldn't accmoplish what we're being asked with our "mid",
+    then, instead, we do:
+
+        L = mid + 1
+
+
+    We're going to be repeating this until L <= R, it's important to be '<='
+    and not '<', because we want to return 'L' as a final result.
+
+
+
+    But now, how are we going to check if first "mid" queries CAN form a zero
+    array?
+
+    We're going to use a Technique called "Line Sweep".
+    This is a fundamental technique when working with ranges.
+
+    Let's say we have some ranges:
+
+        ranges = [0,4,1], [2,7,3], [4,9,5], [1,6,2], [0,5,4], [3,7,4], [1,3,2]
+
+    Instead of going through EACH element within EACH range and add the
+    corresponding value, what we can do is denote a BEGINNING of some range by
+    adding the value at that index and then once that same range is done, we
+    SUBRACT the same value.
+
+    It sounds unintuitive if this is your first time dealing with LineSweep,
+    but take a look at ASCII drawing below and I'm sure you'll understand it.
+
+          0   1     2    3    4    5    6    7    8    9   10   11
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       | +1 |    |    |    |    | -1 |    |    |    |    |    |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       |    |    | +3 |    |    |    |    |    | -3 |    |    |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       |    |    |    |    | +5 |    |    |    |    |    | -5 |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       |    | +2 |    |    |    |    |    | -2 |    |    |    |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       | +4 |    |    |    |    |    | -4 |    |    |    |    |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       |    |    |    | +4 |    |    |    |    | -4 |    |    |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       |    | +2 |    |    | -2 |    |    |    |    |    |    |    |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+
+
+    This is how we would do it "by-the-book", however, what really happens is
+    that we are ONLY adding and subtracting elements at certain indices in our
+    "line_sweep" array.
+
+    In other words--This is NOT a 2D array, instead it is a 1D array called
+    "line sweep".
+
+    This equivalent array of the above drawing would be:
+
+          0    1    2    3    4    5    6    7    8    9   10   11
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+       | +5 | +4 | +3 | +4 | +3 | -1 | -4 | -2 | -7 |  0 | -5 |  0 |
+       +----+----+----+----+----+----+----+----+----+----+----+----+
+
+
+    Iterating through this array "line_sweep" would give us the maximum value
+    AT EACH INDEX.
+
+    It would look like this:
+
+          0      1      2      3     4     5     6     7     8     9    10
+          5  ->  9 ->  12 ->  16 -> 19 -> 18 -> 14 -> 12 ->  5 ->  5 ->  0
+
+    That way, since we know the MAXIMUM POSSIBLE VALUE AT EACH INDEX, if we see
+    that at any index along the way, nums[idx] is GREATER than the MAXIMUM
+    POSSIBLE VALUE AT THAT INDEX, we return false.
+
+    Why?
+    Because we're certain that we CANNOT make a zero-array using these first k
+    queries.
+
+    That's the way we're going to check if we can make a zero-array using our
+    first 'k'(i.e. "mid") queries.
+
+    As we've already said, we if CAN, then:
+
+            R = mid - 1;
+
+    else
+
+            L = mid + 1;
+
+
+    Once we're done doing a Binary Search, we're going to return 'L' which is
+    guaranteed to be our minimal 'k' number of queries we need to use in order
+    to make our array zero-array.
 
 */
 
