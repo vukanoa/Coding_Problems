f96f86538a07991aa21e4675a84239209c3ba709
Noa Vukasovic <noavukasovic@gmail.com>
2025-03-19 16:25:19 +0100

    Add LeetCode: 3191_Minimum_Operations_To_Make_Binary_Array_Elements_Equal_To_One_I to Orderless_Problems

---
+++ Orderless_Problems/3191_Minimum_Operations_To_Make_Binary_Array_Elements_Equal_To_One_I/minimum_operations_to_make_binary_array_elements_equal_to_one_i.cpp
@@ -0,0 +1,105 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    =====================================================================
+    3191) Minimum Operations to Make Binary Array Elements Equal to One I
+    =====================================================================
+
+    ============
+    Description:
+    ============
+
+    You are given a binary array nums.
+
+    You can do the following operation on the array any number of times
+    (possibly zero):
+
+        + Choose any 3 consecutive elements from the array and flip all of them
+
+    Flipping an element means changing its value from 0 to 1, and from 1 to 0.
+
+    Return the minimum number of operations required to make all elements in
+    nums equal to 1. If it is impossible, return -1.
+
+    ===============================================
+    FUNCTION: int minOperations(vector<int>& nums);
+    ===============================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [0,1,1,1,0,0]
+    Output: 3
+    Explanation:
+    We can do the following operations:
+        Choose the elements at indices 0, 1 and 2. The resulting array is nums = [1,0,0,1,0,0].
+        Choose the elements at indices 1, 2 and 3. The resulting array is nums = [1,1,1,0,0,0].
+        Choose the elements at indices 3, 4 and 5. The resulting array is nums = [1,1,1,1,1,1].
+
+
+    --- Example 2 ---
+    Input: nums = [0,1,1,1]
+    Output: -1
+    Explanation:
+    It is impossible to make all elements equal to 1.
+
+
+    *** Constraints ***
+    3 <= nums.length <= 10^5
+    0 <= nums[i] <= 1
+
+*/
+
+#include <numeric>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    It is difficult to prove the correctness of this Solution, but Solution
+    itself is very easy and intuitive. It comes to mind almost instantenously,
+    but it's difficult to know whether it will work or not, other than to try
+    and see if it's going to pass.
+
+    Or to prove it mathematically, which is difficult.
+
+*/
+
+/* Time  Beats: 64.84% */
+/* Space Beats: 15.99% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    int minOperations(vector<int>& nums)
+    {
+        int count = 0;
+
+        for (int i = 2; i < nums.size(); i++)
+        {
+            if (nums[i - 2] == 0)
+            {
+                count++;
+
+                nums[i - 2] ^= 1;
+                nums[i - 1] ^= 1;
+                nums[i] ^= 1;
+            }
+        }
+
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+
+        if (sum == nums.size())
+            return count;
+
+        return -1;
+    }
+};
