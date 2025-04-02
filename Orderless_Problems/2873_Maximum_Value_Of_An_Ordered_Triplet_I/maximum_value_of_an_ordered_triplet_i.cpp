732492e7e8d9701d53ff687a21828ed68f7b5a81
Noa Vukasovic <noavukasovic@gmail.com>
2025-04-02 17:42:53 +0200

    Add Greedy Optimal Solution to LeetCode: 2873_Maximum_Value_Of_An_Ordered_Triplet_I to Orderless_Problems

--- Orderless_Problems/2873_Maximum_Value_Of_An_Ordered_Triplet_I/maximum_value_of_an_ordered_triplet_i.cpp
+++ Orderless_Problems/2873_Maximum_Value_Of_An_Ordered_Triplet_I/maximum_value_of_an_ordered_triplet_i.cpp
@@ -142,3 +142,146 @@ public:
         return result < 0 ? 0 : result;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This is a famous "trick" when you're asked to do something with triplets
+    such that:
+
+        i < j < k
+
+    You iterate only with 'j' pointer, and the remaining two are pre-calculated
+
+
+    Similar thing is true for when you have quadruplets such that:
+
+        i < j < k < m
+
+    However, this one can't be done in O(N), but it CAN be done in O(N^2) which
+    is an optimization of order O(N^2) as well.
+
+    Here you'd fix 'j' and 'k' and you'd pre-calculate 'i' and 'm'.
+
+
+    This is a fundamental trick for these kinds of problems.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  12.05% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(N) */
+class Solution_Greedy_and_Prefix_Sum {
+public:
+    long long maximumTripletValue(vector<int>& nums)
+    {
+        const int N = nums.size();
+        long long result = 0;
+
+        vector<int> left_max(N);
+        vector<int> right_max(N);
+
+        for (int i = 1; i < N; i++)
+        {
+            left_max[i]        = max(left_max[i - 1],  nums[i - 1]);
+            right_max[N-1 - i] = max(right_max[N - i], nums[N - i]);
+        }
+
+        for (int j = 1; j < N-1; j++)
+            result = max(result, 1LL * (left_max[j] - nums[j]) * right_max[j]);
+
+        return result;
+    }
+};
+
+
+
+
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution_Greedy {
+public:
+    long long maximumTripletValue(vector<int>& nums)
+    {
+        const int N = nums.size();
+        long long result = 0;
+
+        vector<int> left_max(N);
+        vector<int> right_max(N);
+
+        for (int i = 1; i < N; i++)
+        {
+            left_max[i]        = max(left_max[i - 1],  nums[i - 1]);
+            right_max[N-1 - i] = max(right_max[N - i], nums[N - i]);
+        }
+
+        for (int j = 1; j < N-1; j++)
+            result = max(result, 1LL * (left_max[j] - nums[j]) * right_max[j]);
+
+        return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This is one additional Optimization on a "Triplet" Problem.
+    Instead of having O(N) Space Complexity, we can calculate both:
+
+        1. left_max and
+        2. diff_max
+
+    Along the way.
+
+    left_max is the maximum number     on the LEFT of "idx".
+    diff_max is the maximum DIFFERENCE on the LEFT of "idx".
+
+    Code is maybe confusing at first, but make sure to pick one Example and go
+    through it using pen and paper and I'm sure you'll understand it.
+
+    It's not trivial to come up with either the idea or the implementation of
+    this approach.
+
+    However, this is the most optimal Solution there is for this problem.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  88.55% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums)
+    {
+        const int N = nums.size();
+        long long result = 0;
+
+        long long left_max = 0;
+        long long diff_max = 0;
+
+        for (int idx = 0; idx < N; idx++)
+        {
+            result = max(result, diff_max * nums[idx]);
+
+            diff_max = max(diff_max, left_max - nums[idx]);
+            left_max = max(left_max, 1LL * nums[idx]);
+        }
+
+        return result;
+    }
+};
