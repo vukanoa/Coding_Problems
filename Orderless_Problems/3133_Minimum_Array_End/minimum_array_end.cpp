30ceb797faab5ead2ffeb287a90fc50a0f26f1de
Noa Vukasovic <noavukasovic@gmail.com>
2025-04-13 18:01:53 +0200

    Exchange Solutions and Add Explanation(IDEA) to LeetCode: 3133_Minimum_Array_End to Orderless_Problems

--- Orderless_Problems/3133_Minimum_Array_End/minimum_array_end.cpp
+++ Orderless_Problems/3133_Minimum_Array_End/minimum_array_end.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 /*
     ==============
     === MEDIUM ===
@@ -46,79 +44,104 @@
 
 */
 
-class Solution_TLE {
-public:
-    long long minEnd(int n, int x)
-    {
-        vector<int> nums;
-        nums.push_back(x);
-
-        vector<int> set_bit_idx;
-        for (int i = 0; i < 32; i++)
-        {
-            if (x & (1 << i))
-                set_bit_idx.push_back(i);
-        }
-
-        int i = 1;
-        while (nums.size() < n)
-        {
-            int curr_num = x + i;
-
-            int has_all_needed_set_bits = true;
-            for (int j = 0; j < set_bit_idx.size(); j++)
-            {
-                if ( ! (curr_num & (1 << set_bit_idx[j])) )
-                {
-                    has_all_needed_set_bits = false;
-                    break;
-                }
-            }
-
-            if (has_all_needed_set_bits)
-                nums.push_back(curr_num);
-
-            i++;
-        }
-
-        return nums[n-1];
-    }
-};
-
-
-
-
 /*
     ------------
     --- IDEA ---
     ------------
 
-    TODO
+    Take pen and paper and go through one example. You'll get it by reading the
+    code.
+
+    The main takeaway is this: The result MUST begin at x and ALL of the
+    subsequent numbers MUST HAVE all set-bits of x also set and more.
+
+    Example 1:
+        n = 4
+        x = 8
+
+        8  = 0 0 0 0 ... 1 0 0 0
+        9  = 0 0 0 0 ... 1 0 0 1
+        10 = 0 0 0 0 ... 1 0 1 0
+        11 = 0 0 0 0 ... 1 0 1 1
+
+
+        4th element starting at x=8 is 11.
+
+
+    Example 2:
+        n = 2
+        x = 7
+
+        7  = 0 0 0 0 ... 0 1 1 1 <---- 1st
+        8  = 0 0 0 0 ... 1 0 0 0
+        9  = 0 0 0 0 ... 1 0 0 1
+        10 = 0 0 0 0 ... 1 0 1 0
+        11 = 0 0 0 0 ... 1 0 1 1
+        12 = 0 0 0 0 ... 1 1 0 0
+        13 = 0 0 0 0 ... 1 0 1 1
+        14 = 0 0 0 0 ... 1 1 1 0
+        15 = 0 0 0 0 ... 1 1 1 1  <--- 2nd 
+
+
+        2nd element starting at x=7 is 15.
+
+
+    Any number must have ALL of the same set-bits as x. Initially 'n' is NOT
+    a 0-indexed number, that's why we subtract 1.
+
+
+
+    So just think about this:
+        n = 4
+        x = 8
+
+        8  = 0 0 0 0 ... 1 0 0 0
+        9  = 0 0 0 0 ... 1 0 0 1
+        10 = 0 0 0 0 ... 1 0 1 0
+        11 = 0 0 0 0 ... 1 0 1 1
+
+
+        4th element starting at x=8 is 11.
+
+    We need 4th number starting at x. Since x will takeaway one from n, we end
+    up with 3.
+
+    3 in binary is 0000....0011
+
+    Now we go and put bits of this 3 in HOLES(i.e. 0 bits in x).
+
+    And that's it.
 
 */
 
 /* Time  Beats: 100.00% */
-/* Space Beats:  52.80% */
+/* Space Beats:  58.02% */
 
-/* Time  Complexity: O(1) (O(b) but integers have a fixes size (32 or 64)) */
-/* Space Complexity: O(1)                                                  */
+/* Time  Complexity: O(logN) */
+/* Space Complexity: O(1)    */
 class Solution {
 public:
     long minEnd(int n, int x)
     {
-        long result    = x;
-        long remaining = n - 1;
-        long position  = 1;
+        long result = x;
 
-        while (remaining)
+        int th_LSB_of_result = 0;
+
+        int zero_indexed_n           = n-1;
+        int th_LSB_of_zero_indexed_n = 0;
+
+
+        while (th_LSB_of_result < 64)
         {
-            if ( ! (x & position))
+            if ((result & (1LL << th_LSB_of_result)) == 0)
             {
-                result |= (remaining & 1) * position;
-                remaining >>= 1;
+                if (zero_indexed_n & (1LL << th_LSB_of_zero_indexed_n))
+                    result |= (1LL << th_LSB_of_result);
+
+                th_LSB_of_zero_indexed_n++;
             }
 
-            position <<= 1;
+            th_LSB_of_result++;
         }
 
         return result;
