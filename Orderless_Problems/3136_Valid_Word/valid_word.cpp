f06ac727a916b815cf64cf17bb1d21a94a1fab6d
Noa Vukasovic <noavukasovic@gmail.com>
2025-04-16 20:14:16 +0200

    Add LeetCode: 3136_Valid_Word to Orderless_Problems

---
+++ Orderless_Problems/3136_Valid_Word/valid_word.cpp
@@ -0,0 +1,108 @@
+/*
+    ============
+    === EASY ===
+    ============
+
+    ===========================
+    3136) Valid Word
+    ===========================
+
+    ============
+    Description:
+    ============
+
+
+    A word is considered valid if:
+
+        + It contains a minimum of 3 characters.
+
+        + It contains only digits (0-9), and English letters
+          (uppercase and lowercase).
+
+        + It includes at least one vowel.
+
+        + It includes at least one consonant.
+
+    You are given a string word.
+
+    Return true if word is valid, otherwise, return false.
+
+    =====
+    Note: 'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+          A consonant is an English letter that is not a vowel.
+    =====
+
+    ====================================
+    FUNCTION: bool isValid(string word);
+    ====================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: word = "234Adas"
+    Output: true
+    Explanation:
+    This word satisfies the conditions.
+
+    --- Example 2 ---
+    Input: word = "b3"
+    Output: false
+    Explanation:
+    The length of this word is fewer than 3, and does not have a vowel.
+
+    --- Example 3 ---
+    Input: word = "a3$e"
+    Output: false
+    Explanation:
+    This word contains a '$' character and does not have a consonant.
+
+
+    *** Constraints ***
+    1 <= word.length <= 20
+    word consists of English uppercase and lowercase letters, digits, '@', '#',
+    and '$'.
+
+*/
+
+#include <string>
+#include <unordered_set>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-Explanatory.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:   5.45% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    bool isValid(string word)
+    {
+        unordered_set<int> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        int vow  = 0;
+        int cons = 0;
+        for (const char& chr : word)
+        {
+            if ( ! isalnum(chr))
+                return false;
+
+            if (vowels.find(chr) != vowels.end())
+                vow++;
+            else if ( chr < '0' || chr > '9')
+                cons++;
+        }
+
+        return (word.length() >= 3 && vow >= 1 && cons >= 1);
+    }
+};
