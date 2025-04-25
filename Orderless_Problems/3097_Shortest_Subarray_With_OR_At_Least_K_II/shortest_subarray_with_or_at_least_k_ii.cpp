23fd416fd32799b409cd35fbe58ee65d7bf6ec97
Noa Vukasovic <noavukasovic@gmail.com>
2025-04-25 19:37:12 +0200

    Add SlidingWindow Solution to LeetCode: 3097_Shortest_Subarray_With_OR_At_Least_K_II to Orderless_Problems

--- Orderless_Problems/3097_Shortest_Subarray_With_OR_At_Least_K_II/shortest_subarray_with_or_at_least_k_ii.cpp
+++ Orderless_Problems/3097_Shortest_Subarray_With_OR_At_Least_K_II/shortest_subarray_with_or_at_least_k_ii.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <vector>
-
 /*
     ==============
     === MEDIUM ===
@@ -57,6 +54,10 @@
     0 <= k <= 10^9
 
 */
+
+#include <climits>
+#include <vector>
+using namespace std;
 
 /*
     ------------
@@ -119,5 +120,128 @@ private:
         }
 
         return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This one uses Sliding-Window technique on vectors of bits of size 64.
+
+    However, LeetCode "rates" this Solution worse because the purpose of this
+    one is to be solved using Brute-Force.
+
+    However there's a 2nd version of this exact same problem, the only
+    difference is the given Constraints.
+
+    This Solution solves both.
+
+*/
+
+/* Time  Beats: 6.25% */
+/* Space Beats: 5.80% */
+
+/* Time  Complexity: O(N * 64) ---> O(N * 1) ---> O(N) */
+/* Space Complexity: O(N * 64) ---> O(N * 1) ---> O(N) */
+class Solution_Sliding_Window {
+public:
+    int minimumSubarrayLength(vector<int>& nums, int k)
+    {
+        const int N = nums.size();
+        long long result = LLONG_MAX;
+
+        if (k == 0)
+            return 1;
+
+        vector<vector<long long>> bits(N, vector<long long>(64, 0));
+        vector<long long> k_bits(64, 0);
+
+        int i = 0;
+        while (k > 0)
+        {
+            k_bits[63 - i] = k & 0x1;
+
+            k >>= 1;
+            i++;
+        }
+
+        for (i = 0; i < N; i++)
+        {
+            long long pos = 0;
+            while (nums[i] > 0)
+            {
+                bits[i][63 - pos] = nums[i] & 0x1;
+
+                nums[i] >>= 1;
+                pos++;
+            }
+
+        }
+
+        long long L = 0;
+        long long R = 0;
+        vector<long long> tmp(64, 0);
+        while (R < N)
+        {
+            tmp = add(tmp, bits[R]);
+
+            while (GE(tmp, k_bits))
+            {
+                result = min(result, R - L + 1);
+                sub(tmp, bits[L]);
+                L++;
+            }
+
+            // Increment
+            R++;
+        }
+
+        return result == LLONG_MAX ? -1 : result;
+    }
+
+private:
+    bool GE(vector<long long>& bits, vector<long long>& k_bits)
+    {
+        int left  = 64;
+        int right = 64;
+        for (long long i = 0; i < 64; i++)
+        {
+            if (bits[i] >= 1 && k_bits[i] >= 1)
+                continue;
+
+            if (bits[i] >= 1 && k_bits[i] == 0)
+                return true;
+
+            if (bits[i] == 0 && k_bits[i] >= 1)
+                return false;
+        }
+
+        return true;
+    }
+
+    vector<long long> add(vector<long long>& one, vector<long long>& two)
+    {
+        vector<long long> tmp(64, 0);
+
+        for (long long i = 0; i < 64; i++)
+        {
+            tmp[i] = one[i] + two[i];
+        }
+
+        return tmp;
+    }
+
+    void sub(vector<long long>& one, vector<long long>& two)
+    {
+        for (long long i = 0; i < 64; i++)
+        {
+            if (two[i] == 1)
+                one[i]--;
+        }
     }
 };
