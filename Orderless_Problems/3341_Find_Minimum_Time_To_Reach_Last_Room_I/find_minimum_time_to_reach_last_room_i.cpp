32e43e35ab2020c30e34bb25ed91e98e00847eeb
Noa Vukasovic <noavukasovic@gmail.com>
2025-04-25 18:46:42 +0200

    Add another Dijkstra Implementation to LeetCode: 3341_Find_Minimum_Time_To_Reach_Last_Room_I to Orderless_Problems

--- Orderless_Problems/3341_Find_Minimum_Time_To_Reach_Last_Room_I/find_minimum_time_to_reach_last_room_i.cpp
+++ Orderless_Problems/3341_Find_Minimum_Time_To_Reach_Last_Room_I/find_minimum_time_to_reach_last_room_i.cpp
@@ -131,6 +131,75 @@ public:
             }
         }
 
-        return -1; // Unreachable
+        return -1; // Unreachable code
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Another way of implementing it. This one is a bit easier I would say, but
+    it's always beneficial to look at the same problem from different angles.
+
+*/
+
+/* Time  Beats: 28.25% */
+/* Space Beats: 22.81% */
+
+/* Time  Complexity: O(ROWS * COLS * log(ROWS * COLS)) */
+/* Space Complexity: O(ROWS * COLS)                    */
+class Solution_2 {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime)
+    {
+        const int ROWS = moveTime.size();
+        const int COLS = moveTime[0].size();
+
+        vector<vector<bool>> visited(ROWS, vector<bool>(COLS, false));
+        visited[0][0] = true;
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> min_heap;
+        min_heap.push( {0, 0, 0} );
+
+        /* Signing Cross */
+        std::vector<std::pair<int, int>> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+
+        while ( ! min_heap.empty())
+        {
+            auto top = min_heap.top();
+            min_heap.pop();
+
+            int curr_time = top[0];
+            int curr_row  = top[1];
+            int curr_col  = top[2];
+
+            for (auto dir : directions)
+            {
+                int new_row = curr_row + dir.first;
+                int new_col = curr_col + dir.second;
+
+                if (new_row < 0 || new_col < 0 || new_row == ROWS || new_col == COLS)
+                    continue;
+
+                if (visited[new_row][new_col])
+                    continue;
+
+                int new_time = curr_time >= moveTime[new_row][new_col] ? curr_time + 1 : moveTime[new_row][new_col] + 1;
+
+                // If we've reached last cell
+                if (new_row == ROWS-1 && new_col == COLS-1)
+                    return new_time >= moveTime[ROWS-1][COLS-1] ? new_time : moveTime[ROWS-1][COLS-1] + 1;
+
+                min_heap.push( {new_time, new_row, new_col} );
+                visited[new_row][new_col] = true;
+            }
+        }
+
+        return -1; // Unreachable code
     }
 };
