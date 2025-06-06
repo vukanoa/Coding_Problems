60d91347eae8816c7ea6b2b2c3fa99209ea0076f
Noa Vukasovic <noavukasovic@gmail.com>
2025-06-06 15:12:02 +0200

    Add LeetCode: 2434_Using_A_Robot_To_Print_The_Lexicographically_Smallest_String to Orderless_Problems

---
+++ Orderless_Problems/2434_Using_A_Robot_To_Print_The_Lexicographically_Smallest_String/using_a_robot_to_print_the_lexicographically_smallest_string.cpp
@@ -0,0 +1,116 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ==================================================================
+    2434) Using a Robot to Print the Lexicographically Smallest String 
+    ==================================================================
+
+    ============
+    Description:
+    ============
+
+    You are given a string s and a robot that currently holds an empty string t
+
+    Apply one of the following operations until s and t are both empty:
+
+        + Remove the first character of a string s and give it to the robot.
+          The robot will append this character to the string t.
+
+        + Remove the last character of a string t and give it to the robot.
+          The robot will write this character on paper.
+
+    Return the lexicographically smallest string that can be written on the
+    paper.
+
+    ===========================================
+    FUNCTION: string robotWithString(string s);
+    ===========================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: s = "zza"
+    Output: "azz"
+    Explanation: Let p denote the written string.
+    Initially p="", s="zza", t="".
+    Perform first operation three times p="", s="", t="zza".
+    Perform second operation three times p="azz", s="", t="".
+
+    --- Example 2 ---
+    Input: s = "bac"
+    Output: "abc"
+    Explanation: Let p denote the written string.
+    Perform first operation twice p="", s="c", t="ba". 
+    Perform second operation twice p="ab", s="c", t="". 
+    Perform first operation p="ab", s="", t="c". 
+    Perform second operation p="abc", s="", t="".
+
+    --- Example 3 ---
+    Input: s = "bdda"
+    Output: "addb"
+    Explanation: Let p denote the written string.
+    Initially p="", s="bdda", t="".
+    Perform first operation four times p="", s="", t="bdda".
+    Perform second operation four times p="addb", s="", t="".
+
+
+    *** Constraints ***
+    1 <= s.length <= 10^5
+    s consists of only English lowercase letters.
+
+*/
+
+#include <stack>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: % */
+/* Space Beats: `% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(N) */
+class Solution {
+public:
+    string robotWithString(string s)
+    {
+        string result;
+        unordered_map<char, int> umap_counter;
+
+        for (const char& chr : s)
+            umap_counter[chr]++;
+
+        stack<char> stack;
+        char min_character = 'a';
+
+        for (const char& chr : s)
+        {
+            stack.push(chr);
+            umap_counter[chr]--;
+
+            while (min_character != 'z' && umap_counter[min_character] == 0)
+                min_character++;
+
+            while ( ! stack.empty() && stack.top() <= min_character)
+            {
+                result.push_back(stack.top());
+                stack.pop();
+            }
+        }
+
+        return result;
+    }
+};
