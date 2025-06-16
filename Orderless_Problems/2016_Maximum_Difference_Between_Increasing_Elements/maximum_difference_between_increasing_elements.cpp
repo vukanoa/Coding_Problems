c7850542697fc3015b1f83c544cd6980332966b7
Noa Vukasovic <noavukasovic@gmail.com>
2025-06-16 18:39:16 +0200

    Add LeetCode: 2016_Maximum_Difference_Between_Increasing_Elements to Orderless_Problems

---
+++ Orderless_Problems/2016_Maximum_Difference_Between_Increasing_Elements/maximum_difference_between_increasing_elements.cpp
@@ -0,0 +1,99 @@
+/*
+    ============
+    === EASY ===
+    ============
+
+    ====================================================
+    2016) Maximum Difference Between Increasing Elements
+    ====================================================
+
+    ============
+    Description:
+    ============
+
+    Given a 0-indexed integer array nums of size n, find the maximum difference
+    between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that
+    0 <= i < j < n and nums[i] < nums[j].
+
+    Return the maximum difference. If no such i and j exists, return -1.
+
+    ===================================================
+    FUNCTION: int maximumDifference(vector<int>& nums);
+    ===================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [7,1,5,4]
+    Output: 4
+    Explanation:
+    The maximum difference occurs with i = 1 and j = 2,
+    nums[j] - nums[i] = 5 - 1 = 4.
+    Note that with i = 1 and j = 0,
+    the difference nums[j] - nums[i] = 7 - 1 = 6, but i > j, so it is not valid.
+
+    --- Example 2 ---
+    Input: nums = [9,4,3,2]
+    Output: -1
+    Explanation:
+    There is no i and j such that i < j and nums[i] < nums[j].
+
+    --- Example 3 ---
+    Input: nums = [1,5,2,10]
+    Output: 9
+    Explanation:
+    The maximum difference occurs with i = 0 and j = 3,
+    nums[j] - nums[i] = 10 - 1 = 9.
+
+
+    *** Constraints ***
+    n == nums.length
+    2 <= n <= 1000
+    1 <= nums[i] <= 10^9
+
+*/
+
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-explanatory.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:   5.67% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(N) */
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums)
+    {
+        const int N = nums.size();
+        int result = -1;
+
+        vector<int> max_to_the_right(N, 0);
+        max_to_the_right[N-1] = nums.back();
+
+        for (int i = N-2; i >= 0; i--)
+            max_to_the_right[i] = max(nums[i], max_to_the_right[i+1]);
+
+        int min_to_the_left = nums[0];
+        for (int i = 1; i < N; i++)
+        {
+            if (min_to_the_left < max_to_the_right[i])
+                result = max(result, max_to_the_right[i] - min_to_the_left);
+
+            min_to_the_left = min(min_to_the_left, nums[i]);
+        }
+
+        return result;
+    }
+};
