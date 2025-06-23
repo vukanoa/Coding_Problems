9652bae4d04d0ea7e268bf97f5176698ddc1fdd3
Noa Vukasovic <noavukasovic@gmail.com>
2025-06-23 18:53:03 +0200

    Add LeetCode: 3592_Inverse_Coin_Change to Orderless_Problems

---
+++ Orderless_Problems/3592_Inverse_Coin_Change/inverse_coin_change.cpp
@@ -0,0 +1,121 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===========================
+    3592) Inverse Coin Change
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    You are given a 1-indexed integer array numWays, where numWays[i]
+    represents the number of ways to select a total amount i using an infinite
+    supply of some fixed coin denominations. Each denomination is a positive
+    integer with value at most numWays.length.
+
+    However, the exact coin denominations have been lost. Your task is to
+    recover the set of denominations that could have resulted in the given
+    numWays array.
+
+    Return a sorted array containing unique integers which represents this set
+    of denominations.
+
+    If no such set exists, return an empty array.
+
+    ======================================================
+    FUNCTION: vector<int> findCoins(vector<int>& numWays);
+    ======================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: numWays = [0,1,0,2,0,3,0,4,0,5]
+    Output: [2,4,6]
+    Explanation:
+     1    0    There is no way to select coins with total value 1.
+     2    1    The only way is [2].
+     3    0    There is no way to select coins with total value 3.
+     4    2    The ways are [2, 2] and [4].
+     5    0    There is no way to select coins with total value 5.
+     6    3    The ways are [2, 2, 2], [2, 4], and [6].
+     7    0    There is no way to select coins with total value 7.
+     8    4    The ways are [2, 2, 2, 2], [2, 2, 4], [2, 6], and [4, 4].
+     9    0    There is no way to select coins with total value 9.
+    10    5    The ways are [2, 2, 2, 2, 2], [2, 2, 2, 4], [2, 4, 4], [2, 2, 6], and [4, 6].
+
+    --- Example 2 ---
+    Input: numWays = [1,2,2,3,4]
+    Output: [1,2,5]
+    Explanation:
+    Amount    Number of ways    Explanation
+    1    1    The only way is [1].
+    2    2    The ways are [1, 1] and [2].
+    3    2    The ways are [1, 1, 1] and [1, 2].
+    4    3    The ways are [1, 1, 1, 1], [1, 1, 2], and [2, 2].
+    5    4    The ways are [1, 1, 1, 1, 1], [1, 1, 1, 2], [1, 2, 2], and [5].
+
+    --- Example 3 ---
+    Input: numWays = [1,2,3,4,15]
+    Output: []
+    Explanation:
+    No set of denomination satisfies this array.
+
+
+    *** Constraints ***
+    1 <= numWays.length <= 100
+    0 <= numWays[i] <= 2 * 108
+
+*/
+
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  11.76% */
+
+/* Time  Complexity: O(N^2) */
+/* Space Complexity: O(N)   */
+class Solution {
+public:
+    vector<int> findCoins(const vector<int>& numWays)
+    {
+        const int N = numWays.size();
+
+        vector<long long> dp(N+1, 0);
+        dp[0] = 1;
+
+        vector<int> result;
+
+        for (int amount = 1; amount <= N; amount++)
+        {
+            int target = numWays[amount-1];
+
+            if (dp[amount] == 1LL*target - 1)
+            {
+                result.push_back(amount);
+
+                for (int j = amount; j <= N; j++)
+                    dp[j] += dp[j - amount];
+            }
+
+            if (dp[amount] != target)
+                return {};
+        }
+
+        return result;
+    }
+};
