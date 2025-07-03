90628eef5914e8e43aa4874d315d35c6fad44e83
Noa Vukasovic <noavukasovic@gmail.com>
2025-07-03 21:51:38 +0200

    Add Efficient Solution to LeetCode: 3304_Find_The_K_th_Character_In_String_Game_I to Orderless_Problems

--- Orderless_Problems/3304_Find_The_K-th_Character_In_String_Game_I/find_the_k-th_character_in_string_game_i.cpp
+++ Orderless_Problems/3304_Find_The_K-th_Character_In_String_Game_I/find_the_k-th_character_in_string_game_i.cpp
@@ -77,7 +77,7 @@ using namespace std;
     first we subtract 'a' from our "chr", that way we get a number from [0, 25]
     inclusively. Now we need the next character in the alphabet, however if it
     is letter 'z' then we need to wrap around. We do that by using MODULO 26,
-    becase there are 26 letter in the English alphabet.
+    becase there are 26 letters in the English alphabet.
 
     Then once we're done with that we have the number from [0, 25] of this new
     character, and now we need to add 'a'(i.e. 97 because of ASCII) to it and
@@ -94,7 +94,7 @@ using namespace std;
 /* Time  Beats: 56.73% */
 /* Space Beats:  6.49% */
 
-/* Time  Complexity: O(k^2) */
+/* Time  Complexity: O(k) */
 /* Space Complexity: O(k)   */
 class Solution {
 public:
@@ -115,5 +115,77 @@ public:
         }
 
         return str[k-1];
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    __lg is a GCC/Clang built-in function that computes the base-2 logarithm of
+    an integer, returning the largest integer x such that 2^x <= n.
+
+    __lg(1) = 0     // 2^0 = 1
+    __lg(2) = 1     // 2^1 = 2
+    __lg(3) = 1     // 2^1 = 2 (largest power of 2 ≤ 3)
+    __lg(4) = 2     // 2^2 = 4
+    __lg(5) = 2     // 2^2 = 4
+    __lg(8) = 3     // 2^3 = 8
+    ...
+
+
+    It's EQUIVALENT to:
+        int log_value = static_cast<int>(log2(n));
+
+    but __lg is faster and doesn't use floating-point math.
+
+
+    It is undefined for n = 0 — just like log(0) is undefined.
+
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    !!! This is compiler-specific but works in both GCC and Clang !!!
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    A portable alternative would be:
+
+        int floor_log2(int n)
+        {
+            int result = 0;
+            while (n >>= 1)
+                result++;
+
+            return result;
+        }
+
+*/
+
+/* Time  Complexity: O(logK) */
+/* Space Complexity: O(1)    */
+class Solution_Optimized {
+public:
+    char kthCharacter(int k)
+    {
+        int character_shift_count = 0;
+        int highest_power_of_two_exponent;
+
+        while (k != 1)
+        {
+            highest_power_of_two_exponent = __lg(k);
+
+            if ((1 << highest_power_of_two_exponent) == k)
+            {
+                highest_power_of_two_exponent--;
+            }
+
+            k = k - (1 << highest_power_of_two_exponent);
+            character_shift_count++;
+        }
+
+        return 'a' + character_shift_count;
     }
 };
