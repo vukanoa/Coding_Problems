0c68b54fa2c05cc75a7eca7bd1a6d0951cce7a41
Noa Vukasovic <noavukasovic@gmail.com>
2025-07-06 22:54:24 +0200

    Add Optimized Binary Search version to LeetCode: 1865_Finding_Pairs_WIth_A_Certain_Sum to Orderless_Problems

--- Orderless_Problems/1865_Finding_Pairs_With_A_Certain_Sum/finding_pairs_with_a_certain_sum.cpp
+++ Orderless_Problems/1865_Finding_Pairs_With_A_Certain_Sum/finding_pairs_with_a_certain_sum.cpp
@@ -38,15 +38,15 @@
     class FindSumPairs {
     public:
         FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-            
+
         }
-        
+
         void add(int index, int val) {
-            
+
         }
-        
+
         int count(int tot) {
-            
+
         }
     };
     ===============================
@@ -85,6 +85,7 @@
 
 */
 
+#include <algorithm>
 #include <vector>
 #include <unordered_map>
 
@@ -99,6 +100,8 @@
 
 /* Time  Beats: 57.39% */
 /* Space Beats: 88.13% */
+
+/* Space Complexity: O(M) */
 class FindSumPairs {
 private:
     std::unordered_map<int, int> freq;
@@ -106,16 +109,16 @@ private:
     std::vector<int> nums2;
 
 public:
+    /* Time  Complexity: O(M) */
     FindSumPairs(std::vector<int>& nums1, std::vector<int>& nums2)
         : nums1(nums1), nums2(nums2)
     {
         for (const auto& n2 : nums2)
             freq[n2]++;
     }
-    
+
 
     /* Time  Complexity: O(1) */
-    /* Space Complexity: O(N) */
     void add(int index, int val)
     {
         int curr_val = nums2[index];
@@ -129,9 +132,8 @@ public:
 
         freq[curr_val]++;
     }
-    
+
     /* Time  Complexity: O(N) */
-    /* Space Complexity: O(N) */
     int count(int tot)
     {
         const int N = nums1.size();
@@ -143,6 +145,97 @@ public:
 
             if (freq.find(target) != freq.end())
                 result += freq[target];
+        }
+
+        return result;
+    }
+};
+
+/**
+ * Your FindSumPairs object will be instantiated and called as such:
+ * FindSumPairs* obj = new FindSumPairs(nums1, nums2);
+ * obj->add(index,val);
+ * int param_2 = obj->count(tot);
+ */
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Since nums1 is not changed, we can sort it.
+    Later it can be used binary search for a shortened searching range.
+
+    That's the optimization.
+
+     If the use case involves frequent queries on a mostly stable dataset, then
+     FindSumPairs2 is faster, even if our Big O notation is SLOWER than the
+     above Solution.
+
+*/
+
+/* Space Complexity: O(M) */
+class FindSumPairs2{
+public:
+    std:: vector<int> nums1, nums2;
+    std:: unordered_map<int, int> freq;
+
+    int N;
+    int max_val_from_nums2 = 0;
+
+
+    /* Time  Complexity: O(N * logN + M) */
+    FindSumPairs2(std::vector<int>& nums1, std::vector<int>& nums2)
+        : nums1(nums1), nums2(nums2), N(nums1.size())
+    {
+
+        /* Sort */
+        std::sort(this->nums1.begin(), this->nums1.end());
+
+        freq.reserve(100000);
+
+        for (int n2: nums2)
+        {
+            freq[n2]++;
+            max_val_from_nums2 = std::max(max_val_from_nums2, n2);
+        }
+    }
+
+    /* Time  Complexity: O(1) */
+    void add(int index, int val)
+    {
+        int curr_val = nums2[index];
+
+        if (--freq[curr_val]==0)
+            freq.erase(curr_val);
+
+        nums2[index] += val;
+        freq[curr_val+val]++;
+
+        if (curr_val + val > max_val_from_nums2)
+            max_val_from_nums2 = curr_val + val;
+    }
+
+    /* Time  Complexity: O(N) */
+    int count(int tot)
+    {
+        int result = 0;
+        int target = std::max(1, tot - max_val_from_nums2);
+
+        auto iter = lower_bound(nums1.begin(), nums1.end(), target);
+        int i0 = iter - nums1.begin();
+
+        for (int i = i0; i < N; i++)
+        {
+            const int n1 = nums1[i];
+
+            if (n1 >= tot)
+                break;
+
+            result += freq[tot - n1];
         }
 
         return result;
