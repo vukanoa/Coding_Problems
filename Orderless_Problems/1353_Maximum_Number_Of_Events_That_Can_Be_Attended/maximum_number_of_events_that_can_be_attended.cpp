63955936acc474e8e9edde7b98edb909d35dfc2d
Noa Vukasovic <noavukasovic@gmail.com>
2025-07-07 23:24:44 +0200

    Add LeetCode: 1353_Maximum_Number_Of_Events_That_Can_Be_Attended to ORderless_Problems

---
+++ Orderless_Problems/1353_Maximum_Number_Of_Events_That_Can_Be_Attended/maximum_number_of_events_that_can_be_attended.cpp
@@ -0,0 +1,118 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===================================================
+    1353) Maximum Number of Events That Can Be Attended
+    ===================================================
+
+    ============
+    Description:
+    ============
+
+    You are given an array of events where events[i] = [startDayi, endDayi].
+    Every event i starts at startDayi and ends at endDayi.
+
+    You can attend an event i at any day d where startTimei <= d <= endTimei.
+    You can only attend one event at any time d.
+
+    Return the maximum number of events you can attend.
+
+    =====================================================
+    FUNCTION: int maxEvents(vector<vector<int>>& events);
+    =====================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: events = [[1,2],[2,3],[3,4]]
+    Output: 3
+    Explanation: You can attend all the three events.
+    One way to attend them all is as shown.
+    Attend the first event on day 1.
+    Attend the second event on day 2.
+    Attend the third event on day 3.
+
+    --- Example 2 ---
+    Input: events= [[1,2],[2,3],[3,4],[1,2]]
+    Output: 4
+
+
+    *** Constraints ***
+    1 <= events.length <= 10^5
+    events[i].length == 2
+    1 <= startDayi <= endDayi <= 10^5
+
+*/
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Unconventional use of DSU's "find" functionality with path compression.
+
+*/
+
+/* Time  Beats: 98.63% */
+/* Space Beats: 25.49% */
+
+/* Time  Complexity: O(N * logN) */
+/* Space Complexity: O(N)        */
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events)
+    {
+        
+        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1]; // ASCENDING by end_time value
+        });
+
+        int max_day = 0;
+        for (const auto& ev : events)
+            max_day = max(max_day, ev[1]);
+
+        vector<int> next_free_day(max_day + 2);
+
+        for (unsigned d = 0; d < next_free_day.size(); ++d)
+            next_free_day[d] = d;
+
+        int result = 0;
+        for (const auto& ev : events)
+        {
+            int start_day_of_this_event = ev[0];
+            int end_day_of_this_event   = ev[1];
+
+            int curr_first_free_day = find_next_free_day(start_day_of_this_event, next_free_day);
+            if (curr_first_free_day <= end_day_of_this_event)
+            {
+                result++;
+                next_free_day[curr_first_free_day] = find_next_free_day(curr_first_free_day+1, next_free_day);
+            }
+        }
+
+        return result;
+    }
+
+private:
+    int find_next_free_day(int day, vector<int>& next_free_day)
+    {
+        while (day != next_free_day[day])
+        {
+            // Huge Optimization (From O(n) to Amortized O(1) Time Complexity)
+            // If there is no grandparent, nothing will happen
+            next_free_day[day] = next_free_day[next_free_day[day]];
+
+            day = next_free_day[day];
+        }
+
+        return day;
+    }
+};
