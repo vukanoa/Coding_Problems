9bfe7a5ca49babb80898b3bd89a87928a2155c52
Noa Vukasovic <noavukasovic@gmail.com>
2025-07-18 23:49:13 +0200

    Add Math Solution to LeetCode: 858_Mirror_Reflection to Orderless_Problems

--- Orderless_Problems/858_Mirror_Reflection/mirror_reflection.cpp
+++ Orderless_Problems/858_Mirror_Reflection/mirror_reflection.cpp
@@ -106,3 +106,63 @@ private:
         return std::abs(x - y) < EPSILON;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Even if this is a tedious problem, there is a smart Math Solution to this
+    that is worth exploring and looking at.
+
+
+    Instead of modelling the ray as a bouncing line, model it as a straight
+    line through reflections of the room.
+
+    For example, if p = 2, q = 1, then we can reflect the room horizontally,
+    and draw a straight line from (0, 0) to (4, 2). The ray meets the receptor
+    2, which was reflected from (0, 2) to (4, 2).
+
+    In general, the ray goes to the first integer point (kp, kq) where k is an
+    integer, and kp and kq are multiples of p. Thus, the goal is just to find
+    the smallest k for which kq is a multiple of p.
+
+    The mathematical answer is k = p / gcd(p, q).
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  40.57% */
+
+/* Time  Complexity: O(logP) */
+/* Space Complexity: O(1)    */
+class Solution_Math {
+public:
+    int mirrorReflection(int p, int q)
+    {
+        int gcd_value = gcd(p, q);
+
+        p /= gcd_value;
+        p %= 2;
+
+        q /= gcd_value;
+        q %= 2;
+
+        if (p == 1 && q == 1)
+            return 1;
+
+        return p == 1 ? 0 : 2;
+    }
+
+private:
+    int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+
+        return gcd(b % a, a);
+    }
+};
