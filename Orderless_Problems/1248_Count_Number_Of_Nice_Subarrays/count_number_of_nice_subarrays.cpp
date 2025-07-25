a64a66a1b604ff535febbcd65d9d30b5b4c81175
vukanoa <108334101+vukanoa@users.noreply.github.com>
2025-07-25 23:26:52 +0200

    Add Hashing SOlution to LeetCOde: 1248_Count_NUmber_Of_Nice_Subarrays to Orderless_PRoblems

--- Orderless_Problems/1248_Count_Number_Of_Nice_Subarrays/count_number_of_nice_subarrays.cpp
+++ Orderless_Problems/1248_Count_Number_Of_Nice_Subarrays/count_number_of_nice_subarrays.cpp
@@ -28,13 +28,23 @@
     ==========================================================================
 
     --- Example 1 ---
-    <++>
+    Input: nums = [1,1,2,1,1], k = 3
+    Output: 2
+    Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
 
     --- Example 2 ---
+    Input: nums = [2,4,6], k = 1
+    Output: 0
+    Explanation: There are no odd numbers in the array.
 
     --- Example 3 ---
+    Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+    Output: 16
 
     *** Constraints ***
+    1 <= nums.length <= 50000
+    1 <= nums[i] <= 10^5
+    1 <= k <= nums.length
 
 */
 
@@ -72,5 +82,37 @@ public:
         }
 
         return result;
+    }
+};
+
+
+
+
+
+/* Time  Beats: 19.63% */
+/* Space Beats: 16.63% */
+
+/* Time  Complexity: O(n) */
+/* Space Complexity: O(n) */
+class Solution_Hashing {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k)
+    {
+        int curr_sum = 0;
+        int subarrays = 0;
+        unordered_map<int, int> prefix_sum;
+        prefix_sum[curr_sum] = 1;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            curr_sum += nums[i] % 2;
+
+            if (prefix_sum.find(curr_sum - k) != prefix_sum.end())
+                subarrays = subarrays + prefix_sum[curr_sum - k];
+            
+            prefix_sum[curr_sum]++;
+        }
+
+        return subarrays;
     }
 };
