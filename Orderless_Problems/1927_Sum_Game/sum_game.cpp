da9cdd7cb806b860874671e2deadd1bd98bcae28
Noa Vukasovic <noavukasovic@gmail.com>
2025-08-06 21:31:00 +0200

    Add LeetCode: 1927_Sum_Game to Orderless_Problems

---
+++ Orderless_Problems/1927_Sum_Game/sum_game.cpp
@@ -0,0 +1,103 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===========================
+    1927) Sum game
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    Alice and Bob take turns playing a game, with Alice starting first.
+
+    You are given a string num of even length consisting of digits and '?'
+    characters. On each turn, a player will do the following if there is still
+    at least one '?' in num:
+
+        1. Choose an index i where num[i] == '?'.
+        2. Replace num[i] with any digit between '0' and '9'.
+
+    The game ends when there are no more '?' characters in num.
+
+    For Bob to win, the sum of the digits in the first half of num must be
+    equal to the sum of the digits in the second half. For Alice to win, the
+    sums must not be equal.
+
+        + For example, if the game ended with num = "243801", then Bob wins
+          because 2+4+3 = 8+0+1. If the game ended with num = "243803", then
+          Alice wins because 2+4+3 != 8+0+3.
+
+    Assuming Alice and Bob play optimally, return true if Alice will win and
+    false if Bob will win.
+
+    ===================================
+    FUNCTION: bool sumGame(string num);
+    ===================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: num = "5023"
+    Output: false
+    Explanation: There are no moves to be made.
+    The sum of the first half is equal to the sum of the second half: 5 + 0 = 2 + 3.
+
+    --- Example 2 ---
+    Input: num = "25??"
+    Output: true
+    Explanation: Alice can replace one of the '?'s with '9' and it will be
+                 impossible for Bob to make the sums equal.
+
+    --- Example 3 ---
+    Input: num = "?3295???"
+    Output: false
+    Explanation: It can be proven that Bob will always win. One possible outcome is:
+    - Alice replaces the first '?' with '9'. num = "93295???".
+    - Bob replaces one of the '?' in the right half with '9'. num = "932959??".
+    - Alice replaces one of the '?' in the right half with '2'. num = "9329592?".
+    - Bob replaces the last '?' in the right half with '7'. num = "93295927".
+    Bob wins because 9 + 3 + 2 + 9 = 5 + 9 + 2 + 7.
+
+
+    *** Constraints ***
+    2 <= num.length <= 10^5
+    num.length is even.
+    num consists of only digits and '?'.
+
+*/
+
+#include <string>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 46.99% */
+/* Space Beats: 34.96% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    bool sumGame(string s)
+    {
+        const int N = s.length();
+
+        double result = 0;
+        for (int i = 0; i < N; i++)
+            result += (i < N / 2 ? 1 : -1) * (s[i] == '?' ? 4.5 : s[i] - '0');
+
+        return result != 0.0;
+    }
+};
