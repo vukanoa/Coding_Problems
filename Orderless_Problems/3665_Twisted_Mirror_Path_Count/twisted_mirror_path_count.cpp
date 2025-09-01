8a28be362ef420decad5f6200c19240ce6f246c8
Noa Vukasovic <noavukasovic@gmail.com>
2025-09-01 21:14:14 +0200

    Add Memoization Solution to LeetCode: 3665_Twisted_Mirror_Path_Count to Orderless_Problems

---
+++ Orderless_Problems/3665_Twisted_Mirror_Path_Count/twisted_mirror_path_count.cpp
@@ -0,0 +1,173 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===============================
+    3665) Twisted Mirror Path Count
+    ===============================
+
+    ============
+    Description:
+    ============
+
+    Given an m x n binary grid grid where:
+
+        + grid[i][j] == 0 represents an empty cell, and
+        + grid[i][j] == 1 represents a mirror.
+
+    A robot starts at the top-left corner of the grid (0, 0) and wants to
+    reach the bottom-right corner (m - 1, n - 1). It can move only right or
+    down. If the robot attempts to move into a mirror cell, it is reflected
+    before entering that cell:
+
+        + If it tries to move right into a mirror, it is turned down and moved
+          into the cell directly below the mirror.
+
+        + If it tries to move down into a mirror, it is turned right and moved
+          into the cell directly to the right of the mirror.
+
+    If this reflection would cause the robot to move outside the grid
+    boundaries, the path is considered invalid and should not be counted.
+
+    Return the number of unique valid paths from (0, 0) to (m - 1, n - 1).
+
+    Since the answer may be very large, return it modulo 109 + 7.
+
+    =====
+    Note: If a reflection moves the robot into a mirror cell, the robot is
+          immediately reflected again based on the direction it used to enter
+          that mirror: if it entered while moving right, it will be turned
+          down; if it entered while moving down, it will be turned right. This
+          process will continue until either the last cell is reached, the
+          robot moves out of bounds or the robot moves to a non-mirror cell.
+    =====
+
+    =====================================================
+    FUNCTION: int uniquePaths(vector<vector<int>>& grid);
+    =====================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: grid = [[0,1,0],[0,0,1],[1,0,0]]
+    Output: 5
+    Explanation:
+    Number    Full Path
+    1         (0, 0) --> (0, 1) [M] --> (1, 1)     -->  (1, 2) [M] --> (2, 2)
+    2         (0, 0) --> (0, 1) [M] --> (1, 1)     -->  (2, 1)     --> (2, 2)
+    3         (0, 0) --> (1, 0)     --> (1, 1)     -->  (1, 2) [M] --> (2, 2)
+    4         (0, 0) --> (1, 0)     --> (1, 1)     -->  (2, 1)     --> (2, 2)
+    5         (0, 0) --> (1, 0)     --> (2, 0) [M] -->  (2, 1)     --> (2, 2)
+
+    + [M] indicates the robot attempted to enter a mirror cell and instead
+      reflected.
+
+
+
+    --- Example 2 ---
+    Input: grid = [[0,0],[0,0]]
+    Output: 2
+    Explanation:
+    Number    Full Path
+    1         (0, 0) --> (0, 1) --> (1, 1)
+    2         (0, 0) --> (1, 0) --> (1, 1)
+
+
+
+    --- Example 3 ---
+    Input: grid = [[0,1,1],[1,1,0]]
+    Output: 1
+    Explanation:
+    Number    Full Path
+    1    (0, 0) --> (0, 1) [M] --> (1, 1) [M] --> (1, 2)
+
+    (0, 0) --> (1, 0) [M] --> (1, 1) [M] --> (2, 1) goes out of bounds, so it
+    is invalid.
+
+
+
+    *** Constraints ***
+    m == grid.length
+    n == grid[i].length
+    2 <= m, n <= 500
+    grid[i][j] is either 0 or 1.
+    grid[0][0] == grid[m - 1][n - 1] == 0
+
+*/
+
+#include <cstring>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Standard Memoization.
+
+*/
+
+/* Time  Beats: -% */
+/* Space Beats: 100.00% */
+
+/* Time  Complexity: O(ROWS * COLS * 2) --> (ROWS * COLS) */
+/* Space Complexity: O(ROWS * COLS * 2) --> (ROWS * COLS) */
+class Solution {
+private:
+    const int MOD = 1e9 + 7;
+    int memo[501][501][2];
+
+public:
+    int uniquePaths(vector<vector<int>>& grid)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        /* Memset */
+        memset(memo, -1, sizeof(memo));
+
+        return (solve(1, 0, 0, grid) + solve (0, 1, 1, grid)) % MOD;
+    }
+
+private:
+    int solve(int row, int col, int came_from, vector<vector<int>>& grid)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        if (row < 0 || col < 0 || row >= ROWS || col >= COLS)
+            return 0;
+
+        if (row == ROWS-1 && col == COLS-1)
+            return 1;
+
+        if (memo[row][col][came_from] != -1)
+            return memo[row][col][came_from];
+
+        int go_down  = 0;
+        int go_right = 0;
+
+        if (grid[row][col] == 1) // Mirror
+        {
+            if (came_from == 0) // Came from ABOVE
+            {
+                go_right = solve(row , col+1, 1, grid);
+            }
+            else // came_from == 1 // Came from LEFT
+            {
+                go_down = solve(row+1, col  , 0, grid);
+            }
+        }
+        else // NOT a Mirror
+        {
+            go_down  = solve(row+1, col   , 0, grid);
+            go_right = solve(row  , col+1 , 1, grid);
+        }
+
+        return memo[row][col][came_from] = (go_down + go_right) % MOD;
+    }
+};
