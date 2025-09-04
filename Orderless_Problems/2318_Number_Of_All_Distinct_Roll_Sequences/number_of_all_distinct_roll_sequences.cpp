82f7739aea67f0ee7806ac632b67aec952e4c167
Noa Vukasovic <noavukasovic@gmail.com>
2025-09-04 18:02:28 +0200

    Add Bottom-Up DP Solution to LeetCode: 2318_Number_Of_All_Distinct_Roll_Sequences to ORerless_Problems

--- Orderless_Problems/2318_Number_Of_All_Distinct_Roll_Sequences/number_of_all_distinct_roll_sequences.cpp
+++ Orderless_Problems/2318_Number_Of_All_Distinct_Roll_Sequences/number_of_all_distinct_roll_sequences.cpp
@@ -150,3 +150,99 @@ private:
         return memo[second_prev][first_prev][rolls_left] = result;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same idea, implemented using Bottom-Up approach.
+    This is VERY unintuitive to me. Memoization is so much easier to grasp.
+
+*/
+
+/* Time  Beats: 47.31% */
+/* Space Beats:  5.38% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(N) */
+class Solution_2 {
+private:
+    const int MOD = 1e9 + 7;
+
+public:
+    int distinctSequences(int n)
+    {
+        if (n == 1)
+            return 6;
+
+        vector<vector<vector<long long>>> dp(7, vector<vector<long long>>(7, vector<long long>(10001, 0)));
+
+        unordered_map<int, vector<int>> allowed_for_num;
+        for (int a = 1; a <= 6; a++)
+        {
+            for (int b = 1; b <= 6; b++)
+            {
+                if (gcd(a, b) == 1)
+                    allowed_for_num[a].push_back(b);
+            }
+        }
+
+        vector<pair<int, int>> starts = {{1,2}, {1,3}, {1,4}, {1,5}, {1,6},
+                                         {2,3}, {2,5},
+                                         {3,4}, {3,5},
+                                         {4,5},
+                                         {5,6}};
+
+
+        // Base case: with 0 rolls left, there's exactly 1 sequence
+        for (int second_prev = 1; second_prev <= 6; second_prev++)
+        {
+            for (int first_prev = 1; first_prev <= 6; first_prev++)
+            {
+                dp[second_prev][first_prev][0] = 1;
+            }
+        }
+
+        // Build DP Bottom-Up
+        for (int rolls_left = 1; rolls_left <= n-2; rolls_left++)
+        {
+            for (int second_prev = 1; second_prev <= 6; second_prev++)
+            {
+                for (int first_prev = 1; first_prev <= 6; first_prev++)
+                {
+                    long long local_result = 0LL;
+
+                    for (int allowed : allowed_for_num[first_prev])
+                    {
+                        if (allowed == second_prev)
+                            continue;
+
+                        if (allowed == first_prev)
+                            continue;
+
+                        local_result += dp[first_prev][allowed][rolls_left - 1];
+                        local_result %= MOD;
+                    }
+
+                    dp[second_prev][first_prev][rolls_left] = local_result;
+                }
+            }
+        }
+
+        long long result = 0LL;
+        for (const auto& pair : starts)
+        {
+            result += dp[pair.first][pair.second][n - 2];
+            result %= MOD;
+
+            result += dp[pair.second][pair.first][n - 2];
+            result %= MOD;
+        }
+
+        return static_cast<int>(result);
+    }
+};
