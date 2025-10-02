c526a253a06fe55c4959df293c730634b6ccd578
Noa Vukasovic <noavukasovic@gmail.com>
2025-10-02 20:15:45 +0200

    Add LeetCode: 2571_Minimum_Operations_To_Reduce_An_Integer_To_0 to Orderless_Problems

---
+++ Orderless_Problems/2571_Minimum_Operations_To_Reduce_An_Integer_To_0/minimum_operations_to_reduce_an_integer_to_0.cpp
@@ -0,0 +1,125 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ==================================================
+    2573) Minimum Operations to Reduce an Integer to 0
+    ==================================================
+
+    ============
+    Description:
+    ============
+
+    You are given a positive integer n, you can do the following operation any
+    number of times:
+
+        + Add or subtract a power of 2 from n.
+
+    Return the minimum number of operations to make n equal to 0.
+
+    A number x is power of 2 if x == 2i where i >= 0.
+
+    ===================================
+    FUNCTION: int minOperations(int n);
+    ===================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: n = 39
+    Output: 3
+    Explanation: We can do the following operations:
+    - Add 20 = 1 to n, so now n = 40.
+    - Subtract 23 = 8 from n, so now n = 32.
+    - Subtract 25 = 32 from n, so now n = 0.
+    It can be shown that 3 is the minimum number of operations we need to make
+    n equal to 0.
+
+    --- Example 2 ---
+    Input: n = 54
+    Output: 3
+    Explanation: We can do the following operations:
+    - Add 21 = 2 to n, so now n = 56.
+    - Add 23 = 8 to n, so now n = 64.
+    - Subtract 26 = 64 from n, so now n = 0.
+    So the minimum number of operations is 3.
+
+
+    *** Constraints ***
+    1 <= n <= 10^5
+
+*/
+
+#include <algorithm>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  60.30% */
+
+/* Time  Complexity: O(logN) */
+/* Space Complexity: O(logN) */
+class Solution {
+public:
+    int minOperations(int n)
+    {
+        if (n == 1)
+            return 1;
+
+        int result = __builtin_popcount(static_cast<unsigned int>(n));
+
+        if (n & 1) // If n is ODD
+        {
+            // O(logN)
+            solve(n + 1, 1, result); // Add 1
+            solve(n - 1, 1, result); // Subtract 1
+        }
+        else
+        {
+            // O(logN)
+            solve(n, 0, result);
+        }
+
+        return result;
+    }
+
+private:
+    void solve(int curr_number, int operations_count, int& result)
+    {
+        if (curr_number > 1e5)
+            return;
+
+        // O(1)
+        int number_of_set_bits = __builtin_popcount(static_cast<unsigned int>(curr_number));
+
+        result = min(result, operations_count + number_of_set_bits);
+
+        if (number_of_set_bits == 1)
+            return;
+
+        int ending_zeroes_count = 0;
+        int tmp = curr_number;
+
+        // O(1), since according to Constraints, N = 10^5, which is < 2^17
+        // 17 is essentially a constant
+        while ((tmp & 1) == 0)
+        {
+            ending_zeroes_count++;
+            tmp >>= 1; // Logical Shift Right
+        }
+
+        solve(curr_number + (1 << ending_zeroes_count), operations_count + 1, result);
+        solve(curr_number - (1 << ending_zeroes_count), operations_count + 1, result);
+    }
+};
