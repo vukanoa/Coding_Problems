d1d4f8384c0f2e6b957141e056e28c14b577fd02
Noa Vukasovic <noavukasovic@gmail.com>
2025-10-15 23:17:01 +0200

    Add LeetCode: 2445_Determine_If_Two_Events_Have_COnflicts to Orderless_Problems

---
+++ Orderless_Problems/2446_Determine_If_Two_Events_Have_Conflict/determine_if_two_events_have_conflict.cpp
@@ -0,0 +1,85 @@
+/*
+    ============
+    === EASY ===
+    ============
+
+    ==========================================
+    2446) Deterine if Two Events Have Conflict
+    ==========================================
+
+    ============
+    Description:
+    ============
+
+    You are given two arrays of strings that represent two inclusive events
+    that happened on the same day, event1 and event2, where:
+
+        + event1 = [startTime1, endTime1] and
+        + event2 = [startTime2, endTime2].
+
+    Event times are valid 24 hours format in the form of HH:MM.
+
+    A conflict happens when two events have some non-empty intersection
+    (i.e., some moment is common to both events).
+
+    Return true if there is a conflict between two events. Otherwise, return
+    false.
+
+    ============================================================================
+    FUNCTION: bool haveConflict(vector<string>& event1, vector<string>& event2);
+    ============================================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: event1 = ["01:15","02:00"], event2 = ["02:00","03:00"]
+    Output: true
+    Explanation: The two events intersect at time 2:00.
+
+    --- Example 2 ---
+    Input: event1 = ["01:00","02:00"], event2 = ["01:20","03:00"]
+    Output: true
+    Explanation: The two events intersect starting from 01:20 to 02:00.
+
+    --- Example 3 ---
+    Input: event1 = ["01:00","02:00"], event2 = ["01:20","03:00"]
+    Output: true
+    Explanation: The two events intersect starting from 01:20 to 02:00.
+
+
+    *** Constraints ***
+    event1.length == event2.length == 2
+    event1[i].length == event2[i].length == 5
+    startTime1 <= endTime1
+    startTime2 <= endTime2
+    All the event times follow the HH:MM format.
+
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    We can compare strings directly.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  84.14% */
+
+/* Time  Complexity: O(1) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    bool haveConflict(vector<string>& event1, vector<string>& event2)
+    {
+        return event1[0] <= event2[1] && event2[0] <= event1[1];
+    }
+};
