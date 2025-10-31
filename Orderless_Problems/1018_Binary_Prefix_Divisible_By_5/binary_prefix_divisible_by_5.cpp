e593a375cefcd860d332bfca3db068506535af9b
Noa Vukasovic <noavukasovic@gmail.com>
2025-10-31 11:06:39 +0100

    Add LeetCode: 1018_Binary_Prefix_Divisible_By_5 to Orderless_Problems

---
+++ Orderless_Problems/1018_Binary_Prefix_Divisible_By_5/binary_prefix_divisible_by_5.cpp
@@ -0,0 +1,84 @@
+/*
+    ============
+    === EASY ===
+    ============
+
+    ==================================
+    1018) Binary Prefix Divisible By 5
+    ==================================
+
+    ============
+    Description:
+    ============
+
+    You are given a binary array nums (0-indexed).
+
+    We define xi as the number whose binary representation is the subarray
+    nums[0..i] (from most-significant-bit to least-significant-bit).
+
+        For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
+
+    Return an array of booleans answer where answer[i] is true if xi is
+    divisible by 5.
+
+    =========================================================
+    FUNCTION: vector<bool> prefixesDivBy5(vector<int>& nums);
+    =========================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [0,1,1]
+    Output: [true,false,false]
+    Explanation: The input numbers in binary are 0, 01, 011; which are 0, 1,
+                 and 3 in base-10. Only the first number is divisible by 5, so
+                 answer[0] is true.
+
+    --- Example 2 ---
+    Input: nums = [1,1,1]
+    Output: [false,false,false]
+
+
+    *** Constraints ***
+    1 <= nums.length <= 10^5
+    nums[i] is either 0 or 1.
+
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  38.35% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums)
+    {
+        const int N = nums.size();
+        vector<bool> answer;
+
+        int num = 0;
+        for (int i = 0; i < N; i++)
+        {
+            num = (num*2 + nums[i]) % 5;
+            answer.push_back(num == 0);
+        }
+
+        return answer;
+    }
+};
