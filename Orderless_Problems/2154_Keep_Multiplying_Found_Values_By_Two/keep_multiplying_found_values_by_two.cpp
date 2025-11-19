e69c4c76f5e673978cb51af73ff67e35ce874882
Noa Vukasovic <noavukasovic@gmail.com>
2025-11-19 17:25:21 +0100

    Add LeetCodE: 2154_Keep_Multiplying_Found_Values_By_Two to Orderless_Problems

---
+++ Orderless_Problems/2154_Keep_Multiplying_Found_Values_By_Two/keep_multiplying_found_values_by_two.cpp
@@ -0,0 +1,88 @@
+/*
+    ============
+    === EASY ===
+    ============
+
+    ===========================================
+    2154) Keep Multiplying Found Values by Two
+    ===========================================
+
+    ============
+    Description:
+    ============
+
+    You are given an array of integers nums. You are also given an integer
+    original which is the first number that needs to be searched for in nums.
+
+    You then do the following steps:
+
+        + If original is found in nums, multiply it by two
+          (i.e., set original = 2 * original).
+
+        + Otherwise, stop the process.
+
+        + Repeat this process with the new number as long as you keep finding
+          the number.
+
+    Return the final value of original.
+
+    ==============================================================
+    FUNCTION: int findFinalValue(vector<int>& nums, int original);
+    ==============================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [5,3,6,1,12], original = 3
+    Output: 24
+    Explanation:
+    - 3 is found in nums. 3 is multiplied by 2 to obtain 6.
+    - 6 is found in nums. 6 is multiplied by 2 to obtain 12.
+    - 12 is found in nums. 12 is multiplied by 2 to obtain 24.
+    - 24 is not found in nums. Thus, 24 is returned.
+
+    --- Example 2 ---
+    Input: nums = [2,7,9], original = 4
+    Output: 4
+    Explanation:
+    - 4 is not found in nums. Thus, 4 is returned.
+
+
+    *** Constraints ***
+    1 <= nums.length <= 1000
+    1 <= nums[i], original <= 1000
+
+*/
+
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-explanatory.
+
+*/
+
+/* Time  Beats: 22.40% */
+/* Space Beats: 20.00% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(N) */
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original)
+    {
+        unordered_set<int> uset(nums.begin(), nums.end());
+
+        while (uset.count(original))
+            original *= 2;
+
+        return original;
+    }
+};
