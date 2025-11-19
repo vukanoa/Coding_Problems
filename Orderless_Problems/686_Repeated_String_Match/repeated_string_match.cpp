18a74e73a9bf28c13cb2de70a30ea9f04ed56704
Noa Vukasovic <noavukasovic@gmail.com>
2025-11-19 18:01:31 +0100

    Add KMP Solution to LeetCode: 686_Repeated_String_Match to Orderless_Problems

--- Orderless_Problems/686_Repeated_String_Match/repeated_string_match.cpp
+++ Orderless_Problems/686_Repeated_String_Match/repeated_string_match.cpp
@@ -46,6 +46,7 @@
 */
 
 #include <string>
+#include <vector>
 using namespace std;
 
 /*
@@ -60,8 +61,8 @@ using namespace std;
 /* Time  Beats: 24.86% */
 /* Space Beats: 84.20% */
 
-/* Time  Complexity: O(N) */
-/* Space Complexity: O(1) */
+/* Time  Complexity: O(N + M) */
+/* Space Complexity: O(1)     */
 class Solution {
 public:
     int repeatedStringMatch(string a, string b)
@@ -82,6 +83,129 @@ public:
 
         if (repeated_a.find(b) != string::npos)
             return repetitions + 1;
+
+        return -1;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same as above, however instead brute-forcely trying to match, we can simply
+    use a KMP for that part and thus have an optimized Solution.
+
+    However, this is not actually an optimization in practice because std::find
+    is optimized, however it's always good to practice writing your own
+    functions instead.
+
+*/
+
+/* Time  Complexity: O(N + M) */
+/* Space Complexity: O(M)     */
+class Solution_General_KMP {
+public:
+    bool kmp_algorithm(string s, string pattern)
+    {
+        const int N = s.length();
+        const int M = pattern.length();
+
+        vector<int> LPS = compute_LPS(pattern);
+
+        int i = 0;
+        int j = 0;
+        while (i < N && j < M)
+        {
+            if (s[i] == pattern[j])
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                if (j > 0)
+                {
+                    j = LPS[j - 1];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+
+        return j == M;
+    }
+
+private:
+    vector<int> compute_LPS(string pattern)
+    {
+        const int M = pattern.length();
+        vector<int> LPS(M);
+
+        int prefix_len = 0;
+        int i = 1;
+        while (i < M)
+        {
+            if (pattern[prefix_len] == pattern[i])
+            {
+                LPS[i] = prefix_len + 1;
+                prefix_len++;
+                i++;
+            }
+            else
+            {
+                if (prefix_len > 0)
+                {
+                    prefix_len = LPS[prefix_len - 1];
+                }
+                else
+                {
+                    LPS[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return LPS;
+    }
+};
+
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  21.14% */
+
+/* Time  Complexity: O(N + M) */
+/* Space Complexity: O(M)     */
+/* Repeated String Match using the above KMP class */
+class Solution_KMP {
+public:
+    int repeatedStringMatch(string A, string B)
+    {
+        Solution_General_KMP kmp;
+
+        string builder;
+        int repetitions = 0;
+
+        while (builder.length() < B.length())
+        {
+            builder += A;
+            repetitions++;
+        }
+
+        if (kmp.kmp_algorithm(builder, B))
+            return repetitions;
+
+        builder += A;
+
+        if (kmp.kmp_algorithm(builder, B))
+        {
+            return repetitions + 1;
+        }
 
         return -1;
     }
