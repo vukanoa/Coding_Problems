3926329c81e9207696cedcb4c960bf04b0a48f54
Noa Vukasovic <noavukasovic@gmail.com>
2026-02-09 09:51:03 +0100

    Add Divide_and_Conquer Solution to LeetCodE: 23_Merge_K_Sorted_Lists in Top_100_Liked, Blind_75, NeetCodE_150, NeetCode_250, NeetCode_All

--- 4_NeetCode_250/06_Linked_List/23_Merge_K_Sorted_Lists/merge_k_sorted_lists.cpp
+++ 4_NeetCode_250/06_Linked_List/23_Merge_K_Sorted_Lists/merge_k_sorted_lists.cpp
@@ -129,3 +129,89 @@ private:
         return dummy.next;
     }
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Instead of iterating through each list and merging it with all the previous
+    nodes, we can do a "Divide and Conquer" technique where we're merging N
+    nodes exactly logk times.
+
+    Where logk is the "heigh of the tree", meaning it's the total number of
+    times we can divide 'k' lists before we hit 1.
+
+    That's the definition of "logarithm".
+
+*/
+
+/* Time  Beats: 66.43% */
+/* Space Beats:  24.61% */
+
+/* Time  Complexity: O(N * logK) */
+/* Space Complexity: O(logK)     */ // Because of the Call-Stack from Recursion
+class Solution_Divide_and_Conquer {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        const int N = lists.size();
+
+        // O(N * logk) (entire function)
+        return divide_and_conquer(0, N-1, lists);
+    }
+
+private:
+    // O(N * logk) (entire function)
+    ListNode* divide_and_conquer(int low, int high, vector<ListNode*>& lists)
+    {
+        if (low > high)
+            return nullptr;
+
+        if (low == high)
+            return lists[low]; // Or "lists[high]" it does NOT matter
+
+        int mid = low + (high - low) / 2;
+
+        // Divide
+        ListNode* left  = divide_and_conquer(low  ,  mid, lists); // O(logk)
+        ListNode* right = divide_and_conquer(mid+1, high, lists); // O(logk)
+
+        // Conquer
+        return merge_two_sorted_linked_lists(left, right); // O(N)
+    }
+
+    // O(list1_size + list2_size) --> O(N), i.e. "total number of nodes"
+    ListNode* merge_two_sorted_linked_lists(ListNode* list1, ListNode* list2)
+    {
+        ListNode dummy(-1);
+        ListNode* tail = &dummy;
+
+        // O(list1_size + list2_size) (entire block)
+        while (list1 && list2)
+        {
+            if (list1->val <= list2->val)
+            {
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                tail->next = list2;
+                list2 = list2->next;
+            }
+
+            tail = tail->next;
+        }
+
+        if ( ! list1)
+            tail->next = list2;
+        else
+            tail->next = list1;
+
+        return dummy.next;
+    }
+};
