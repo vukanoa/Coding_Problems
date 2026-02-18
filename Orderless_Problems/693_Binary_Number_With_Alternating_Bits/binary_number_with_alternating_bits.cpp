53a720d70caa3624e8bc13f5000e4f19a6e0fbf9
Noa Vukasovic <noavukasovic@gmail.com>
2026-02-18 11:24:47 +0100

    Add another Solution to LeetCodE: 693_Binary_Number_With_Alternating_Bits to Orderless_Problems

--- Orderless_Problems/693_Binary_Number_With_Alternating_Bits/binary_number_with_alternating_bits.cpp
+++ Orderless_Problems/693_Binary_Number_With_Alternating_Bits/binary_number_with_alternating_bits.cpp
@@ -43,7 +43,8 @@
 
 */
 
-#include <sstream>
+
+#include <string>
 using namespace std;
 
 /*
@@ -51,33 +52,78 @@ using namespace std;
     --- IDEA ---
     ------------
 
-    Self-explanatory.
+    Initially set the "prev_bit" to be the OPPOSITE of the LSB(Least
+    Significant Bit), that way our while loop won't break immediately.
+
+    Then the "curr_bit" is always obtained by doing: n & 1.
+
+    If prev_bit and curr_bit are the same, then immediately return false.
+
+    Otherwise, set prev_bit to be curr_bit and right-shift our n.
+
+    Thath's all.
+
+*/
+
+/* Time  Complexity: O(1) */ // Because log(2^32 - 1) is ~10
+/* Space Complexity: O(1) */
+class Solution {
+public:
+    bool hasAlternatingBits(int n)
+    {
+        int prev_bit = ! (n & 1); // Initially it's the OPPOSITE of LSB
+
+        while (n > 0)
+        {
+            int curr_bit = n & 1;
+
+            if (curr_bit == prev_bit)
+                return false;
+
+            prev_bit = curr_bit;
+            n >>= 1;
+        }
+
+        return true;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Here we first form a binary string, then we find the index of the leftmost
+    set bit.
+
+    Then simply check if that string has alternating characters.
 
 */
 
 /* Time  Beats: 100.00% */
 /* Space Beats:  7.04% */
 
-/* Time  Complexity: O(N) */
-/* Space Complexity: O(1) */ // Because max value of n is 10^9, i.e. 10 digits
-class Solution {
+/* Time  Complexity: O(1) */ // Because log(2^32 - 1) is ~10
+/* Space Complexity: O(1) */
+class Solution_String {
 public:
     bool hasAlternatingBits(int n)
     {
-        ostringstream out;
+        string bin_str;
+        bin_str.reserve(32); // To prevent reallocations
+
         for (int i = 31; i >= 0; i--)
-        {
-            if (n & (1 << i))
-                out << 1;
-            else
-                out << 0;
-        }
-        string bin_str = out.str();
+            bin_str += (n & (1 << i)) ? '1' : '0';
 
-        int idx = bin_str.find('1');
-        idx++;
+        /* We don't actually need to reverse it. Think about it.  */
 
-        for (unsigned i = idx; i < bin_str.length(); i++)
+        int leftmost_set_bit_idx = bin_str.find('1');
+        leftmost_set_bit_idx++;
+
+        for (unsigned i = leftmost_set_bit_idx; i < bin_str.length(); i++)
         {
             if (bin_str[i-1] == bin_str[i])
                 return false;
