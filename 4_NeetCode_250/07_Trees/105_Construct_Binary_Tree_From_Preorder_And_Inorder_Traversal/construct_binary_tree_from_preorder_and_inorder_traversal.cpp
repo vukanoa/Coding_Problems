8ee9a16fc321f8b9c5d3264924edc7518fec6a7d
Noa Vukasovic <noavukasovic@gmail.com>
2026-02-23 10:18:27 +0100

    Add Morris-Traversal Solution to LeetCode: 105_Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal to Top_100_Liked, Blind_75, NeetCode_150, NeetCode_250, NeetCodE_All

--- 4_NeetCode_250/07_Trees/105_Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal/construct_binary_tree_from_preorder_and_inorder_traversal.cpp
+++ 4_NeetCode_250/07_Trees/105_Construct_Binary_Tree_From_Preorder_And_Inorder_Traversal/construct_binary_tree_from_preorder_and_inorder_traversal.cpp
@@ -240,3 +240,156 @@ private:
         return node;
     }
 };
+
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    It is really difficult to try and explain this. I highly suggest you take
+    pen and paper and go through one example by going through the code line by
+    line.
+
+    But let me roughly try to explain how this works.
+
+    First, let's consider this example:
+
+        preorder = [3, 9, 1,  2, 8, 11, 4, 5, 20, 15, 16, 7]
+         inorder = [2, 1, 8, 11, 9,  5, 4, 3, 15, 16, 20, 7]
+                    0  1  2   3  4   5  6  7   8   9  10 11
+
+
+    First, notice the obivous -- The first node(vlaue at index 0) of inorder
+    vector is the LEFTMOST node in the entire tree.
+
+    On the other hand -- The first node(value at index 0) of preorder vector
+    is the ROOT node.
+
+
+            _______________________3_______________________
+            ___________9_______________________X___________
+            _____1___________4__________15___________7_____
+            __2_____8_____5________________16______________
+            _________11____________________________________
+
+
+                                    3 
+                                  /   \
+                                 /     \
+                                /       \
+                               /         \
+                              /           \
+                             /             \
+                            /               \
+                           /                 \
+                          /                   \
+                         /                     \
+                        /                       \
+                       9                        20
+                     /   \                     /   \
+                    /     \                   /     \
+                   /       \                 /       \
+                  /         \               /         \
+                 /           \             /           \
+                1             4          15             7
+               / \           /             \
+              /   \         /               \
+             /     \       /                 \
+            2       8     5                  16              
+                     \
+                     11
+
+
+    So, what we're doing here is -- "Go through preorder vector and build nodes
+    as long as we don't hit the leftmost node(i.e. the j-th(0 initially) value
+    of inorder vector) of the tree."
+
+    However, if we were just building and going to the left, we would have to
+    use recursion. This Solution tries to REMOVE recursion, thus making this
+    entire Solution run in O(1) Space Complexity(the tree itself doesn't count
+    since we always have to build and return that. There's no way around it)
+
+    So, instead of doing a recursion, i.e. having a "Call Stack" and knowing
+    where to go next, we're going to use a "Morris Traversal", which remembers
+    the "call stack" by pointing to the previous, non fully traversed nodes,
+    with its "right" pointer.
+
+    As soon as we hit the leftmost node, we want to go to our previous node
+    (i.e. the node we're point at with our "right" pointer currently), thus we
+    assign "prev" to point to that node and we set our current "right" to point
+    at nullptr, since we've hit the bottom of the lefmost part.
+
+    Then, as long as the current j-th value is the "prev" node, we repeat this
+    above paragraph part. (It sounds confusing, but it's impossible to explain
+    without simulating on pen and paper)
+
+    Once we're done with that, we simply try to do this entire process again
+    and we repeat it as long as there are nodes in the tree.
+
+
+    I know this sounds confusing, but--Trust me, if you go through this example
+    by hand on pen and paper, slowly, I guarantee that you will have the "AHA!
+    WOOOOW" moment, if you've never heard of "Morris Traversal".
+
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  99.93% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */ // Result Tree isn't "Extra" Space
+class Solution_Morris_Traversal {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder)
+    {
+        const int N = preorder.size();
+
+        TreeNode dummy_root(-1);
+        TreeNode* curr = &dummy_root;
+
+        int i = 0;
+        int j = 0;
+        while (i < N && j < N)
+        {
+            TreeNode* node = new TreeNode(preorder[i]);
+            node->right = curr->right;
+
+            curr->right = node;
+
+            // Move forward
+            curr = curr->right;
+            i++;
+
+            while (i < N && curr->val != inorder[j])
+            {
+                TreeNode* new_node = new TreeNode(preorder[i]);
+                new_node->right = curr;
+
+                curr->left = new_node;
+
+                // Move forward
+                curr = curr->left;
+                i++;
+            }
+
+            j++; // Since we've hit and created the j-th value of INORDER vec
+
+            while (j < N && curr->right && curr->right->val == inorder[j])
+            {
+                TreeNode* prev = curr->right;
+                curr->right = nullptr;
+
+                // Simulate popping the Call Stack
+                curr = prev;
+                j++;
+            }
+        }
+
+        return dummy_root.right;
+    }
+};
