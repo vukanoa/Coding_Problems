5591c325b5605a3c61d0ce21fc1e35322cc220d8
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-03 09:50:55 +0100

    Remove 4 Redundant SOlutions & Exchange Solution in LeetCode: 215_Kth_Largest_Element_In_An_Array in Top_100_Liked, NeetCode_150, NeetCode_250, NeetCode_All

---
+++ 5_NeetCode_All/08_Heap/215_Kth_Largest_Element_In_An_Array/kth_largest_element_in_an_array.cpp
@@ -0,0 +1,81 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    =====================================
+    215) Kth Largest Element in an Array
+    =====================================
+
+    ============
+    Description:
+    ============
+
+    Given an integer array "nums" and an integer "k", return the k-th largest
+    element in the array.
+
+    Note that it is the kth largest element in the sorted order, not the kth
+    distinct element.
+
+    You must solve it in O(n) time complexity.
+
+    =============================================================
+    FUNCTION: int findKthLargest(std::vector<int>& nums, int k);
+    =============================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input:  nums = [3, 2, 1, 5, 6, 4], k = 2
+    Output: 5
+
+    --- Example 2 ---
+    Input:  nums = [3, 2, 3, 1, 2, 4, 5, 5, 6], k = 4
+    Output: 4
+
+    *** Constraints ***
+    1 <= k <= nums.length <= 10^5
+    -10^4 <= nums[i] <= 10^4
+
+*/
+
+#include <queue>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 12.59% */
+/* Space Beats: 63.67% */
+
+/* Time  Complexity: O(N * logK) */
+/* Space Complexity: O(K)        */
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k)
+    {
+        const int N = nums.size();
+
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+
+        // O(N * logK) (entire block)
+        for (const int& num : nums) // O(N)
+        {
+            min_heap.push(num); // O(logK)
+
+            if (min_heap.size() > k)
+                min_heap.pop(); // O(logK)
+        }
+
+        return min_heap.top();
+    }
+};
