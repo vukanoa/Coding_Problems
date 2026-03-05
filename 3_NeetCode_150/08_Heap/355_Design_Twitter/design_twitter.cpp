12b6842aa5f2fdb6b0e6261c0387e5bbff842fdb
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-05 09:52:25 +0100

    Remove Redundant Solution & Exchange other SOlution in LeetCode: 355_Design_Twitter in NeetCode_150, NeetCode_250, NeetCode_All

---
+++ 3_NeetCode_150/08_Heap/355_Design_Twitter/design_twitter.cpp
@@ -0,0 +1,188 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===========================
+    355) Design Twitter
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    Design a simplified version of Twitter where users can post tweets,
+    follow/unfollow another user, and is able to see the 10 most recent tweets
+    in the user's news feed.
+
+    Implement the Twitter class:
+
+        + Twitter() Initializes your twitter object.
+
+        + void postTweet(int userId, int tweetId) Composes a new tweet with ID
+          tweetId by the user userId. Each call to this function will be made
+          with a unique tweetId.
+
+        + List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent
+          tweet IDs in the user's news feed. Each item in the news feed must be
+          posted by users who the user followed or by the user themself. Tweets
+          must be ordered from most recent to least recent.
+
+        + void follow(int followerId, int followeeId) The user with ID
+          followerId started following the user with ID followeeId.
+
+        + void unfollow(int followerId, int followeeId) The user with ID
+          followerId started unfollowing the user with ID followeeId.
+
+
+    ===============================
+    CLASS:
+    class Twitter {
+    public:
+        Twitter() {
+
+        }
+
+        void postTweet(int userId, int tweetId) {
+
+        }
+
+        vector<int> getNewsFeed(int userId) {
+
+        }
+
+        void follow(int followerId, int followeeId) {
+
+        }
+
+        void unfollow(int followerId, int followeeId) {
+
+        }
+    };
+    ===============================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Example 1:
+
+    Input
+    ["Twitter", "postTweet", "getNewsFeed", "follow", "postTweet",
+    "getNewsFeed", "unfollow", "getNewsFeed"]
+
+    [[], [1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+
+    Output
+    [null, null, [5], null, null, [6, 5], null, [5]]
+
+    Explanation
+    Twitter twitter = new Twitter();
+    twitter.postTweet(1, 5); // User 1 posts a new tweet (id = 5).
+    twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5]. return [5]
+    twitter.follow(1, 2);    // User 1 follows user 2.
+    twitter.postTweet(2, 6); // User 2 posts a new tweet (id = 6).
+    twitter.getNewsFeed(1);  // User 1's news feed should return a list with 2 tweet ids -> [6, 5]. Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+    twitter.unfollow(1, 2);  // User 1 unfollows user 2.
+    twitter.getNewsFeed(1);  // User 1's news feed should return a list with 1 tweet id -> [5], since user 1 is no longer following user 2.
+
+
+    *** Constraints ***
+    1 <= userId, followerId, followeeId <= 500
+    0 <= tweetId <= 10^4
+    All the tweets have unique IDs.
+    At most 3 * 10^4 calls will be made to postTweet, getNewsFeed, follow, and unfollow.
+
+*/
+
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 93.94% */
+/* Space Beats: 86.38% */
+
+/* Time  Complexity: O(N * logN) */
+/* Space Complexity: O(N)        */
+static int my_time = 0;
+
+class Twitter {
+public:
+    Twitter()
+    {
+
+    }
+    
+    void postTweet(int userId, int tweetId)
+    {
+        user_to_tweets[userId].push_back( {my_time++, tweetId} ); // O(1)
+    }
+    
+    vector<int> getNewsFeed(int userId)
+    {
+        priority_queue<pair<int,int>> max_heap;
+        for (const auto& [time_posted, tweetId] : user_to_tweets[userId])
+        {
+            max_heap.push( {time_posted, tweetId} );
+        }
+
+        // O(N * logN) (entire block)
+        for (const auto& folowee : user_to_folowees[userId])
+        {
+            for (const auto& [time_posted, tweetId] : user_to_tweets[folowee])
+            {
+                max_heap.push( {time_posted, tweetId} ); // O(logN)
+            }
+        }
+
+        vector<int> news_feed;
+
+        int tweet_count = 0;
+        while ( ! max_heap.empty() && tweet_count < 10)
+        {
+            auto [time_posted, tweetId] = max_heap.top();
+            news_feed.push_back(tweetId);
+            max_heap.pop(); // O(logN)
+
+            tweet_count++;
+        }
+
+        return news_feed;
+    }
+
+    void follow(int followerId, int followeeId)
+    {
+        user_to_folowees[followerId].insert(followeeId); // O(1)
+    }
+    
+    void unfollow(int followerId, int followeeId)
+    {
+        user_to_folowees[followerId].erase(followeeId); // O(1)
+    }
+
+private:
+    unordered_map<int, vector<pair<int,int>>> user_to_tweets;
+    unordered_map<int, unordered_set<int>> user_to_folowees;
+};
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
