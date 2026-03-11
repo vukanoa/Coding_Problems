491a8b9c7b804f4f37f682422849ec2ec691a4d3
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-11 19:43:16 +0100

    Exchange Solutions & Add Analysis of TIme Complexity in LeetCode: 39_Combination Sum in Top_100_Liked, Blind_75, NeetCodE_150, NeetCode_250, NeetCode_All

--- 5_NeetCode_All/09_Bactracking/39_Combination_Sum/combination_sum.cpp
+++ 5_NeetCode_All/09_Bactracking/39_Combination_Sum/combination_sum.cpp
@@ -73,85 +73,141 @@ using namespace std;
     sum=target____NOT______NOT_______NOT________NOT___________________________________________________________________________________________________________________________
 
     So how does this Algorithm work?
-    1. If the sum of the current combination is greater than target, then even
-    if we move forward with this combination, the sum will only increase. So
-    there is no point moving further for we will never be able to reach the
-    target. Backtrack from this.
 
-    2. If the current combination is equal to the target, then store the
-    solution and Backtrack from this since we have the same situations as in
-    case 1. We certainly won't be able to reach the target again with greater
-    values.
+    1.
+        If the sum of the current combination is greater than target, then even
+        if we move forward with this combination, the sum will only increase.
+        So there is no point moving further because we will never be able to
+        reach the target. Backtrack from this by breaking the for loop.
 
-    3. If the current combination is still less than our target, then:
-        i) Consider all possible options into this combination, one at a time.
+    2.
+        If the current combination is equal to the target, then store it in the
+        result vector and Backtrack from this since we have the same situations
+        as in case 1. We certainly won't be able to reach the target again if
+        we continue down this path.
 
-        ii) Check if current option can be == target with current combination.
+    3.
+        If the current combination is still less than our target, then:
 
-        iii) When this options backtracks to this place again, remove this
-        option and try another option, e.fl [2, 2, ...], we have 3 options to
+        i)
+            Consider all possible options into this combination, one at a time.
+
+        ii)
+            Check if current option can be == target with current combination.
+
+        iii)
+            When this options backtracks to this place again, remove this
+            option and try another option, e.fl [2, 2, ...], we have 3 options
+            to
+
         fill the 3rd place i.e [2, 3, 5].
         So firstly we will go on with [2, 2, 2]. Then when this backtracks to
         this place again, remove last 2, and try next option which is "3", that
         means [2, 2, 3].
+
         When this also backtracks remove 3 to try 5, that means [2, 2, 5].
         Now, as all options are exhausted for [2, 2, ...], now backtrack to its
         previous state which is [2, 2], and so on...
 
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ~~~ Analysis of the Time complexity ~~~
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Why is the Time Complexity this weird: O(2^(target / min_value)).
+
+        Consider this example:
+
+            candidates = [2, 3, 5], target = 10
+
+
+             ^                                  .
+             |                                /   \
+             |                               /     \
+             |                       take 2 /       \ skip 2
+             |                             /         \
+             |                            /           \
+             |                          [2]            []
+             |                         /   \           ...
+             |                        /     \          ...
+             | H              take 2 /       \ skip 2  ...
+             |                      /         \
+             |                     /           \
+             |                  [2,2]          [2]
+             |                  ...            ...
+             |                  ...            ...
+             |                  ...            ...
+             |                [2]
+             |               /   \
+             |              /     \
+             |      take 2 /       \ skip 2
+             |            /         \
+             |           /           \
+             v     [2,2,2,2,2]      [2,2,2,2]
+
+
+
+     This height "H" is going to be the deepest for the leaf where we
+     repeatedly take the MINIMUM value in "candidates".
+
+     Think about it.
+
+     In this case we'll have H be 5 because the target we're trying to sum up
+     to is 10 and the "step" is 2. Therefore: 10 / 2 is 5.
+
+
+     So the total Time Complexity is O(2^H) i.e. O(2^(target / min_value)).
+
 */
 
-/* Time  Beats: 78.14% */
-/* Space Beats: 48.43% */
+/* Time  Beats: 58.19% */
+/* Space Beats: 29.91% */
 
-/*
-    Time  Complexity: O(2^n)
-
-    The worst-case scenario is when all combinations of "candidates" sum up to
-    "target".
-*/
-/*
-    Space Complexity: O(2^n)
-
-    The worst-case scenario is when all combinations of "candidates" sum up to
-    "target".
-*/
-class Solution_Combination_Sum {
+/* Time  Complexity: O(2^(target  / min_value)) */
+/* Space Complexity: O(2^(target  / min_value)) */
+class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target)
     {
-        // Sorting won't make the Time Complexity worse, so make sure to sort
+        const int N = candidates.size();
+        vector<vector<int>> result;
+
+        /*
+           Sort.
+
+           It's important to sort because we want to avoid counting the same
+           combination more than once.
+
+           [2,2,3] and [2,3,2] are the same.
+
+           Therefore, in order to prevent counting them twice, we can simply
+           sort and once we move onto the next candidate, we're sure that its
+           a unique one.
+        */
         sort(candidates.begin(), candidates.end());
 
-        vector<vector<int>> results;
-        backtracking(candidates, target, 0, {}, 0, results);
+        backtracking(0, {}, 0, candidates, target, result);
 
-        return results;
+        return result;
     }
 
 private:
-    void backtracking(vector<int>& candidates,
-                      int target,
-                      int start,
-                      vector<int> curr_comb,
-                      int curr_sum,
-                      vector<vector<int>>& results)
+    void backtracking(int idx, vector<int> curr_comb, int curr_sum, vector<int>& candidates, int& target, vector<vector<int>>& result)
     {
-        /* Backtrack */
         if (curr_sum == target)
         {
-            results.push_back(curr_comb);
+            result.push_back(curr_comb);
             return;
         }
 
-        for (int i = start; i < candidates.size(); i++)
+        const int N = candidates.size();
+        for (int i = idx; i < N; i++)
         {
-            /* Backtrack */
             if (curr_sum + candidates[i] > target)
-                return;
+                break;
 
             curr_comb.push_back(candidates[i]);
-            int new_sum = curr_sum + candidates[i];
-            backtracking(candidates, target, i, curr_comb, new_sum, results);
+            backtracking(i, curr_comb, curr_sum + candidates[i], candidates, target, result);
             curr_comb.pop_back();
         }
     }
