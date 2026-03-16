3876a089f685a195da499d2e49d0747ccae49ca3
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-16 09:36:58 +0100

    Exchange Solution & Reformat in LeetCodE: 17_Letter_Combinations in Top_100_Liked, NeetCode_150, NeetCode_250, NeetCodE_All

---
+++ 4_NeetCode_250/09_Backtracking/17_Letter_Combinations_Of_A_Phone_Number/letter_combinations_of_a_phone_number.cpp
@@ -0,0 +1,99 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    =========================================
+    17) Letter Combinations of a Phone Number
+    =========================================
+
+    ============
+    Description:
+    ============
+
+    Given a string containing digits from 2-9 inclusive, return all possible
+    letter combinations that the number could represent. Return the answer
+    in any order.
+
+    =====
+    Node: Number 1 does not map to any letters.
+    =====
+
+    ===========================================================
+    FUNCTION: vector<string> letterCombinations(string digits);
+    ===========================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input:  digits = "23"
+    Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+    --- Example 2 ---
+    Input:  digits = ""
+    Output: []
+
+    --- Example 3 ---
+    Input:  digits = "2"
+    Output: ["a", "b", "c"]
+
+    *** Constraints ***
+    0 <= digits.length <= 4
+    digits[i] is a digit in the range ['2', '9'].
+
+*/
+
+#include <string>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Self-explanatory. Basic Backtracking.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  98.97% */
+
+/* Time  Complexity: O(N * 4^N) */
+/* Space Complexity: O(N)       */
+class Solution {
+public:
+    vector<string> letterCombinations(string digits)
+    {
+        const int N = digits.size();
+        vector<string> result;
+
+        const string buttons[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        string combination;
+        backtracking(0, combination, digits, buttons, result);
+
+        return result;
+    }
+
+private:
+    void backtracking(int digit_idx, string& combination, string& digits, const string buttons[10], vector<string>& result)
+    {
+        if (combination.size() == digits.size())
+        {
+            result.push_back(combination);
+            return;
+        }
+
+        int curr_digit = digits[digit_idx] - '0';
+
+        for (const char& letter : buttons[curr_digit])
+        {
+            combination += letter;
+            backtracking(digit_idx + 1, combination, digits, buttons, result);
+            combination.pop_back();
+        }
+    }
+};
