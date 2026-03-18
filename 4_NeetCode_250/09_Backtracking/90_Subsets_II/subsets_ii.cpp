112abf7a21ff0678ff9e3149cad786a46452cd28
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-18 09:33:34 +0100

    Add Iterative Solution to LeetCode: 90_Subsets_II to NeetCode_150, NeetCode_250, NeetCode_All

--- 4_NeetCode_250/09_Backtracking/90_Subsets_II/subsets_ii.cpp
+++ 4_NeetCode_250/09_Backtracking/90_Subsets_II/subsets_ii.cpp
@@ -102,3 +102,64 @@ private:
         backtracking(nums, i+1, subset, results);
     }
 };
+
+
+
+
+/* Time  Beats: 40.31% */
+/* Space Beats: 30.78% */
+
+/* Time  Complexity: O(N * 2^N) */
+/* Space Complexity: O(N * 2^N) */
+class Solution_Iterative {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums)
+    {
+        const int N = nums.size();
+
+        // Total number of subsets is up to 2^N, each subset up to size N
+        // Space: O(N * 2^N)
+        vector<vector<int>> result = {{}};
+
+        sort(nums.begin(), nums.end()); // O(N log N)
+
+        int latest_subsets = 0;
+
+        for (int i = 0; i < N; i++) // Contributes to overall O(N * 2^N)
+        {
+            vector<vector<int>> new_subsets;
+
+            if (i > 0 && nums[i-1] == nums[i])
+            {
+                // Only extend subsets created in previous step
+                // Number of such subsets is at most 2^N in total across
+                // execution
+                for (int x = result.size() - latest_subsets; x < result.size(); x++)
+                {
+                    vector<int> subset = result[x]; // O(N)
+
+                    subset.push_back(nums[i]); // O(1)
+
+                    new_subsets.push_back(subset); // O(N)
+                }
+            }
+            else
+            {
+                // Iterate over all existing subsets (up to 2^N total)
+                for (vector<int> subset : result)
+                {
+                    subset.push_back(nums[i]); // O(N) (copying subset)
+
+                    new_subsets.push_back(subset); // O(N)
+                }
+            }
+
+            latest_subsets = new_subsets.size();
+
+            // O(N)
+            result.insert(result.end(), new_subsets.begin(), new_subsets.end());
+        }
+
+        return result;
+    }
+};
