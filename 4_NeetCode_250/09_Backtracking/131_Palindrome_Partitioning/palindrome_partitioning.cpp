84c52d8e8a5d469fdbccbb6819e5d1e24123535b
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-21 12:03:50 +0100

    Add Dynamic Programming Solution to LetCode: 131_Palindrome_Partitioning to Top_100_Liked, NeetCode_150, NeetCode_250, NeetCode_All

--- 4_NeetCode_250/09_Backtracking/131_Palindrome_Partitioning/palindrome_partitioning.cpp
+++ 4_NeetCode_250/09_Backtracking/131_Palindrome_Partitioning/palindrome_partitioning.cpp
@@ -195,3 +195,85 @@ private:
     }
 
 };
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    In the ABOVE Solution we are repeatedly checking whether substrings are
+    palindromes, which costs extra time, O(N) extra time to be precise.
+
+    We can optimize this by precomputing all palindrome substrings. We're going
+    to store them in a matrix called "dp"(i.e. Dynamic Programming).
+
+*/
+
+/* Time  Beats: 88.83% */
+/* Space Beats: 82.78% */
+
+/* Time  Complexity: O(N * 2^N) */
+/* Space Complexity: O(N^2 + N) */
+class Solution_DP {
+private:
+    vector<vector<bool>> dp_palindrome;
+
+public:
+    vector<vector<string>> partition(string s)
+    {
+        const int N = s.size();
+        vector<vector<string>> results;
+
+        dp_palindrome.resize(N, vector<bool>(N, false));
+
+        /* Precomputation */
+        for (int substr_len = 1; substr_len <= N; substr_len++)
+        {
+            for (int start_idx = 0; start_idx <= N - substr_len; start_idx++)
+            {
+                int end_idx = start_idx + substr_len-1;
+
+                dp_palindrome[start_idx][end_idx] =
+                (
+                    s[start_idx] == s[end_idx]
+                    &&
+                    (
+                     start_idx + 1 >= end_idx - 1
+                     ||
+                     dp_palindrome[start_idx + 1][end_idx - 1]
+                    )
+                );
+            }
+        }
+
+        vector<string> curr_partition;
+        backtracking(0, curr_partition, results, s);
+
+        return results;
+    }
+
+private:
+    void backtracking(int start_idx, vector<string>& curr_partition, vector<vector<string>>& results, const string& s)
+    {
+        const int N = s.size();
+
+        if (start_idx >= N)
+        {
+            results.push_back(curr_partition);
+            return;
+        }
+
+        for (int end_idx = start_idx; end_idx < N; end_idx++)
+        {
+            if ( ! dp_palindrome[start_idx][end_idx])
+                continue;
+
+            curr_partition.push_back( s.substr(start_idx, end_idx - start_idx + 1) );
+            backtracking(end_idx + 1, curr_partition, results, s);
+            curr_partition.pop_back();
+        }
+    }
+};
