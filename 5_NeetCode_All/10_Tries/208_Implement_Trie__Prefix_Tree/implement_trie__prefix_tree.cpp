2ce746116340733995fc13e4d84bb8d4daa91731
Noa Vukasovic <noavukasovic@gmail.com>
2026-03-24 11:14:06 +0100

    Exchange Solutions & Improve Explanation(IDEA) in LeetCodE: 208_Implement_Trie__Prefix_Tree in Top_100_Liked, Blind_75, NeetCode_150, NeetCodE_250, NeetCode_All

--- 5_NeetCode_All/10_Tries/208_Implement_Trie__Prefix_Tree/implement_trie__prefix_tree.cpp
+++ 5_NeetCode_All/10_Tries/208_Implement_Trie__Prefix_Tree/implement_trie__prefix_tree.cpp
@@ -89,9 +89,10 @@ using namespace std;
 
     Applications
 
-    Trie (we pronounce "try") or prefix tree is a tree data structure, which is
-    used for retrieval of a key in a dataset of strings. There are various
-    applications of this very efficient data structure such as:
+    Trie (pronounced as "try") or prefix tree is a tree data structure, which
+    is used for retrieval of a key in a dataset of strings.
+
+    Some applications are:
         1. Autocomplete
         2. Spell Checker
         3. IP routing (Longest prefix matching)
@@ -100,104 +101,96 @@ using namespace std;
 
     There are several other data structures, like balanced trees and hash
     tables, which give us the possibility to search for a word in a dataset of
-    strings. Then why do we need trie? Although hash table has O(1) time
-    complexity for looking for a key, it is not efficient in the following
-    operations:
-        - Finding all keys with a common prefix.
-        - Enumerating a dataset of strings in lexicographical order.
+    strings
 
-    Another reason why trie outperforms hash table, is that as has table
-    increases in size, there are lots of hash collisions and the search time
-    complexity could deteriorate to O(n), where n is the number of keys
-    inserted. Trie could use less space compared to hash Table when storing
-    many keys with the same prefix. In this case using trie has only O(m) time
-    complexity, where "m" is the key length. Searching for a key in a balanced
-    tree costs O(m * logn) time complexity.
+    So, why---then---do we need trie?
+
+    Although hash table has O(1) time complexity for looking for a key, however
+    it is not efficient in the following operations:
+
+        1. Finding all keys with a common prefix.
+        2. Enumerating a dataset of strings in lexicographical order.
+
+    Another reason why Trie outperforms hash table, is that as has table
+    INCREASES in size, there are lots of HASH COLLISIONS and the search time
+    complexity could deteriorate to O(N), where N is the number of keys
+    inserted.
+
+    Trie could use less space compared to Hash Table when storing many keys
+    with the same prefix.
+
+    In this case using trie has only O(M) time complexity, where "M" is the key
+    length. Searching for a key in a balanced tree costs O(M * logM) time
+    complexity.
 
 
     Trie node structure:
     Trie is a rooted tree. Its nodes have the following fields:
-        - Maximum of "R" links to its children, where each link corresponds to
-          one of "R" character values from dataset alphabet. In this article
-          we assume that "R" is 26, the number of loewrcase latin letters.
 
-        - Boolean field which specifies whether the node corresponds to the end
+        + Maximum of "R" links to its children(26 in this specific problem),
+          where each link corresponds to one of "R" character values from
+          dataset alphabet.
+
+        + Boolean field which specifies whether the node corresponds to the end
           of the key, or is just a key prefix.
 
+    Let's say we've inserted a word "leet".
+
     Root:
-        isEnd: false
-        a b c d e f g h i j k l m n o p q r s t u v w x y z
-                              #
+    --------------------------------------------------------------
+    |   is_end: false                                            |  node
+    |   a b c d e f g h i j k l m n o p q r s t u v w x y z      |
+    |_________________________#__________________________________|
+                              |
+                              |
+                        ______|
                         |
                         | link
-                        v
-        isEnd: false
-        a b c d e f g h i j k l m n o p q r s t u v w x y z
-                #
-                        |
                         |
                         v
-        isEnd: false
-        a b c d e f g h i j k l m n o p q r s t u v w x y z
-                #
+    --------------------------------------------------------------
+    |   is_end: false                                            |  node
+    |   a b c d e f g h i j k l m n o p q r s t u v w x y z      |
+    |___________#________________________________________________|
+                |
+                |________
                         |
-                        |
-                        v
-        isEnd: false
-        a b c d e f g h i j k l m n o p q r s t u v w x y z
-                                              #
                         |
                         |
                         v
-        isEnd: true
-        a b c d e f g h i j k l m n o p q r s t u v w x y z
+    -------------------------------------------------------------
+    |   is_end: false                                           |  node
+    |   a b c d e f g h i j k l m n o p q r s t u v w x y z     |
+    |___________#_______________________________________________|
+                |
+                |________
+                        |
+                        |
+                        v
+     ------------------------------------------------------------
+     |  is_end: false                                           |  node
+     |  a b c d e f g h i j k l m n o p q r s t u v w x y z     |
+     |________________________________________#_________________|
+                                              |
+                                              |
+                        ______________________|
+                        |
+                        |
+                        v
+    |------------------------------------------------------------
+    |   is_end: true                                            |  node
+    |   a b c d e f g h i j k l m n o p q r s t u v w x y z     |
+    |___________________________________________________________|
 
-    ============
-    === CODE ===
-    ============
 
-    class TrieNode {
-    private:
-        const int R = 26;
-        bool is_end;
 
-    public:
-        TrieNode()
-        {
-            // R links to node children
-            std::vector<struct TrieNode*> links;
-        }
-
-        bool contains_key(char ch)
-        {
-            return links[ch - 'a'] != nullptr;
-        }
-
-        struct TrieNode* get_node(char ch)
-        {
-            return links[ch - 'a'];
-        }
-
-        void put_node(char ch, TrieNode* node)
-        {
-            links[ch - 'a'] = node;
-        }
-
-        void set_end()
-        {
-            is_end = true;
-        }
-
-        bool is_end()
-        {
-            return is_end();
-        }
-    }
 
     Two of the most common operations in a trie are insertion of a key and
     search for a key.
 
-    ## Insertion of a key to a trie.
+    #################
+    ### Insertion ###
+    #################
 
     We insert a key by searching into the trie. We start from the root and
     search a link, which corresponds to the first key character. There are two
@@ -207,193 +200,106 @@ using namespace std;
           key character.
 
         - A link does not eist. Then we create a new node and link it with the
-          parent's link matching the curent key character. We repeat this step
+          parent's link matching the curent character. We repeat this step
           until we encounter the last character of the key, then we mark the
-          current node as an end node and the algorithm finishes.
+          current node as an end node(i.e. is_end = true) and the algorithm
+          finishes.
 
     Inserting "le" into the Trie:
 
-    root
-     1
-      \'l'
-       2
-        \'e'
-         3 (end of key "le")
+                            root
+                             1
+                              \'l'
+                               2
+                                \'e'
+                                 3 (end of key "le")
 
 
     Inserting "leet" into the Trie:
 
-    root
-     1
-      \'l'
-       2
-        \'e'
-         3 (end of key "le")
-          \'e'
-           4
-            \'t'
-             5 (end of key "leet")
+                            root
+                             1
+                              \'l'
+                               2
+                                \'e'
+                                 3 (end of key "le")
+                                  \'e'
+                                   4
+                                    \'t'
+                                     5 (end of key "leet")
 
 
 
     Inserting "code" into the Trie:
-                        root
-                          1
-                      'c'/ \'l'
-                        6   2
-                    'o'/     \'e'
-                      7       3 (end of key "le")
-                  'd'/         \'e'
-                    8           4
-                'e'/             \'t'
-                  9 (end "code")  5 (end "leet")
+
+                            root
+                              1
+                          'c'/ \'l'
+                            6   2
+                        'o'/     \'e'
+                          7       3 (end of key "le")
+                      'd'/         \'e'
+                        8           4
+                    'e'/             \'t'
+                      9 (end "code")  5 (end "leet")
+
+
+    Inserting "co" into the Trie:
+
+                            root
+                              1
+                          'c'/ \'l'
+                            6   2
+                        'o'/     \'e'
+     (JUST MARK IS_END)   7       3 (end of key "le")
+                      'd'/         \'e'
+                        8           4
+                    'e'/             \'t'
+                      9 (end "code")  5 (end "leet")
+
 
     This was built from dataset {"le", "leet", "code"}
 
-    ============
-    === CODE ===
-    ============
-
-    class Trie{
-    private:
-        struct TrieNode* root;
-
-    public:
-        Trie()
-        {
-            root = new TrieNode();
-        }
-
-        // Inserts a word into the trie
-        void insert(std::string word)
-        {
-            struct TrieNode* node = root;
-
-            for (int i = 0; i < word.length(); i++)
-            {
-                char curr_char = word[i];
-                if (!node.contains_key(current_char))
-                    node->put(current_char, new TrieNode());
-
-                node = node->get_node(current_char);
-            }
-
-            node->set_end();
-        }
-    };
-
-    ** Complexity Analysis **
-    - Time Complexity: O(m), where m is the key length.
-    In each iteration of the algorithm, we either examine or create a node in
-    the trie till we reach the end of the key. This takes only "m" operations.
-
-    - Space Complexity: O(m)
-    In the worst case newly inserted key doesn't share a prefix with the keys
-    already inserted in the trie. We have to add "m" new nodes, which takes us
-    O(m) space.
-
-
-    ## Search for a key in a trie.
-
-    Each key is represented in the trie as a path from the root to the internal
-    node or leaf.
-    We start from the root with the first key character. We examine the current
-    node for a link to the key character. There are two cases:
-        1. A link exist. We move to the next node in hte path following this
-           link, and proceed searching for the next key character.
-
-        2. A link does not exist. IF there are no available key characters and
-           current node is marked as "isEnd" we return true. Otherwise there
-           are possible two cases in each of them we return false:
-               - There are key characters left, but it is impossible to follow
-                 the key path in the trie, and the key is missing.
-
-               - No key characters left, but current node is not marked as
-                 "isEnd". Therefore the search key is only a prefix of another
-                 key in the trie.
-
-
-        Search for key "leet" in the Trie
-
-                        root
-                          1
-                      'c'/ \'l'        <---------------- 'l'
-                        6   2
-                    'o'/     \'e'      <---------------- 'e'
-                      7       3 (end of key "le")
-                  'd'/         \'e'    <---------------- 'e'
-                    8           4
-                'e'/             \'t'  <---------------- 't'
-                  9(end "code")   5(end "leet") <----- KEY FOUND
-
-           Searching for a key in a Trie from dataset {le, leet, code}
-
-    ============
-    === CODE ===
-    ============
-
-    class Trie{
-    private:
-        struct TrieNode* search_prefix(std::string word)
-        {
-            structNode* node = root;
-
-            for (int i = 0; i < word.length(); i++)
-            {
-                char curr_letter = word[i];
-                if (node->contains_key(curr_letter))
-                    node = node->get_node(curr_letter);
-                else
-                    return nullptr;
-            }
-
-            return node;
-        }
-
-    public:
-        // Returns if the word is in the trie
-        boolean search(std::string word)
-        {
-            struct TrieNode* node = search_prefix(word);
-
-            return node != nullptr && node->is_end();
-        }
-    };
-
-    ** Complexity Analysis **
-        - Time complexity: O(m) In each step of the algorithm we search for
-          the next key character. In the worst case the algorithm performs "m"
-          operations.
-
-        - Space complexity: O(1)
 
 
 
-    ## Search for a key prefix in a trie
+    ##############
+    ### Search ###
+    ##############
 
-    The approach is very similar to the one we used for searching a key in a
-    trie. We traverse the trie from the root, till there are no characters left
-    in key prefix or it is impossible to continue the path in the trie with the
-    current key character. The only difference with the mentioned above "search
-    for a key" algorithm is that when we come to an end of the kye prefix, we
-    always return true. We don't need to consider the "is_end" mark of the
-    current trie node, because we are searching for a prefix of a key, not for
-    a whole key.
+        Traverse the above Trie and if at ANY point you CANNOT "jump" to the
+        next node, then immediately return false.
 
-            Searching for "co" in the Trie
+        Otherwise, if you've successfully found the entire word inserted in
+        the Trie, you MUST(this is VERY important) check if that node has the
+        is_end=true set.
 
-                         root
-                          1
-    c ----------->    'c'/ \'l'
-                        6   2
-    o ----------->  'o'/     \'e'
-    PREFIX FOUND      7       3 (end of key "le")
-                  'd'/         \'e'
-                    8           4
-                'e'/             \'t'
-                  9 (end "code")  5 (end "leet")
+        It can happen, as in the above example that we're searching for "cod".
 
-    Searching for a prefix in a Trie from dataset {le, leet, code};
+                                root
+                                  1
+                              'c'/ \'l'
+                                6   2
+                            'o'/     \'e'
+         (end of key "co")    7       3 (end of key "le")
+                          'd'/         \'e'
+                            8           4
+                        'e'/             \'t'
+                          9 (end "code")  5 (end "leet")
+
+        Here we DO find "cod", however that node does NOT have is_end=true,
+        therefore we'd return FALSE!
+
+
+
+
+    ###########################
+    ### Search for a prefix ###
+    ###########################
+
+        Here we would indeed find "cod" and we do NOT care whether or not its
+        is_end=true because we're just checking the existence of a PREFIX!
+
 
 
     Practice Problems:
@@ -402,95 +308,91 @@ using namespace std;
 
     1. Add and Search Word - Data structure design
     2. Word Search II
+
 */
 
-/* Time  Beats: 95.61% */
-/* Space Beats: 27.43% */
+/* Time  Beats: 73.09% */
+/* Space Beats: 54.88% */
 
-/* Time  Complexity: O(n) */
-/* Space Complexity: O(n) */
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(T) */ // 'T' <==> total number of created "TrieNode"s
 class Trie {
-public:
-    struct TrieNode{
-        TrieNode* child[26];
-        bool is_leaf_node;
+private:
+    struct TrieNode {
+        TrieNode* letter[26] = {nullptr};
+        bool is_end;
+
+        TrieNode()
+            : is_end(false)
+        {}
     };
 
-    TrieNode* root = nullptr;
+    TrieNode* root;
 
-    TrieNode* get_trie_node()
-    {
-        TrieNode* node = new TrieNode();
-        for (int i = 0; i < 26; i++)
-            node->child[i] = nullptr;
-
-        node->is_leaf_node = false;
-
-        return node;
-    }
-
-
+public:
+    Trie()
+        : root(nullptr)
+    {}
+    
     void insert(string word)
     {
-        // Already inserted then return
-        if (search(word))
+        if ( ! root)
+            root = new TrieNode();
+
+        if (search(word)) // If it already exists, do nothing
             return;
 
-        // If inserting for first time then create root node first
-        if (!root)
-            root = get_trie_node();
-
         TrieNode* node = root;
-
-        // Iterate over all characters of the word
-        for (int i = 0; i <word.length(); i++)
+        for (const char& chr : word)
         {
-            int idx = word[i] - 'a';
-            if (!node->child[idx])
-                node->child[idx] = get_trie_node();
+            if ( ! node->letter[chr - 'a'])
+                node->letter[chr - 'a'] = new TrieNode();
 
-            node = node->child[idx];
+            node = node->letter[chr - 'a'];
         }
 
-        node->is_leaf_node = true;
+        node->is_end = true;
     }
-
-
+    
     bool search(string word)
     {
-        TrieNode* node = root;
-        if (!root)
-        {
-            // cout << "root is null" << "\n";
+        if ( ! root)
             return false;
-        }
 
-        for(int i = 0; i < word.length() && node; i++)
+        TrieNode* node = root;
+        for (const char& chr : word)
         {
-            int idx = word[i] - 'a';
-            node = node->child[idx];
+            if ( ! node->letter[chr - 'a'])
+                return false;
+
+            node = node->letter[chr - 'a'];
         }
 
-        if (node)
-            return node->is_leaf_node;
-
-        return false;
+        return node->is_end;
     }
-
-
+    
     bool startsWith(string prefix)
     {
-        TrieNode* node = root;
-
-        if (!root)
+        if ( ! root)
             return false;
 
-        for (int i = 0; (i < prefix.length()) && node; i++)
+        TrieNode* node = root;
+        for (const char& chr : prefix)
         {
-            int idx = prefix[i] - 'a';
-            node = node->child[idx];
+            if ( ! node->letter[chr - 'a'])
+                return false;
+
+            node = node->letter[chr - 'a'];
         }
 
-        return (node != nullptr);
+        return true;
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
