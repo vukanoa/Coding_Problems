84a9ee2cfa4e856151cf6754cc20e31892c5a425
Noa Vukasovic <noavukasovic@gmail.com>
2026-04-27 14:31:38 +0200

    Add Space-optimized Solution using Bitmask to LeetCode: 1391_Check_If_There_Is_A_Valid_Path_In_A_Grid to Orderless_Problems

--- Orderless_Problems/1391_Check_If_There_Is_A_Valid_Path_In_A_Grid/check_if_there_is_a_valid_path_in_a_grid.cpp
+++ Orderless_Problems/1391_Check_If_There_Is_A_Valid_Path_In_A_Grid/check_if_there_is_a_valid_path_in_a_grid.cpp
@@ -86,7 +86,7 @@ using namespace std;
 
 /* Time  Complexity: O(ROWS * COLS) */
 /* Space Complexity: O(ROWS * COLS) */
-class Solution {
+class Solution_DFS_Unordered_Set {
 public:
     bool hasValidPath(vector<vector<int>>& grid)
     {
@@ -219,6 +219,168 @@ public:
 
                 // UP
                 if (row-1 >= 0 && allowed[UP].count(grid[row-1][col]))
+                {
+                    if (dfs(row-1, col, allowed, visited, grid))
+                        return true;
+                }
+                break;
+        }
+
+        return false;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This is a Space optimization on "allowed". It's reallly beneficial to be
+    able to see both and appreciate the optimization.
+
+*/
+
+/* Time  Beats: 79.03% */
+/* Space Beats: 32.26% */
+
+/* Time  Complexity: O(ROWS * COLS) */
+/* Space Complexity: O(ROWS * COLS) */
+class Solution_DFS_Bitmask {
+public:
+    bool hasValidPath(vector<vector<int>>& grid)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        // Can't go OUT from cell (0,0) or can't go IN cell (ROWS-1, COLS-1)
+        if (grid[0][0] == 5 || grid[ROWS-1][COLS-1] == 4)
+            return false;
+
+        int allowed[4] = {0};
+        allowed[0] = (1 << 2) | (1 << 3) | (1 << 4); // Up
+        allowed[1] = (1 << 2) | (1 << 5) | (1 << 6); // Down
+        allowed[2] = (1 << 1) | (1 << 4) | (1 << 6); // Left
+        allowed[3] = (1 << 1) | (1 << 3) | (1 << 5); // Right
+
+        vector<vector<bool>> visited(ROWS, vector<bool>(COLS, false));
+
+        /* DFS */
+        return dfs(ROWS-1, COLS-1, allowed, visited, grid);
+    }
+
+    bool dfs(int row, int col, int (& allowed)[4], vector<vector<bool>>& visited, vector<vector<int>>& grid)
+    {
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+
+        const int UP    = 0;
+        const int DOWN  = 1;
+        const int LEFT  = 2;
+        const int RIGHT = 3;
+
+        if (visited[row][col])
+            return false;
+
+        if (row == 0 && col == 0)
+            return true;
+
+        visited[row][col] = true;
+
+        switch (grid[row][col])
+        {
+            case 1:
+                // LEFT
+                if (col-1 >= 0 && (allowed[LEFT] & (1 << grid[row][col-1])))
+                {
+                    if (dfs(row, col-1, allowed, visited, grid))
+                        return true;
+                }
+
+                // RIGHT
+                if (col+1 < COLS && (allowed[RIGHT] & (1 << grid[row][col+1])))
+                {
+                    if (dfs(row, col+1, allowed, visited, grid))
+                        return true;
+                }
+                break;
+
+            case 2:
+                // UP
+                if (row-1 >= 0 && (allowed[UP] & (1 << grid[row-1][col])))
+                {
+                    if (dfs(row-1, col, allowed, visited, grid))
+                        return true;
+                }
+
+                // DOWN
+                if (row+1 < ROWS && (allowed[DOWN] & (1 << grid[row+1][col])))
+                {
+                    if (dfs(row+1, col, allowed, visited, grid))
+                        return true;
+                }
+                break;
+
+            case 3:
+                // LEFT
+                if (col-1 >= 0 && (allowed[LEFT] & (1 << grid[row][col-1])))
+                {
+                    if (dfs(row, col-1, allowed, visited, grid))
+                        return true;
+                }
+
+                // DOWN
+                if (row+1 < ROWS && (allowed[DOWN] & (1 << grid[row+1][col])))
+                {
+                    if (dfs(row+1, col, allowed, visited, grid))
+                        return true;
+                }
+                break;
+
+            case 4:
+                // RIGHT
+                if (col+1 < COLS && (allowed[RIGHT] & (1 << grid[row][col+1])))
+                {
+                    if (dfs(row, col+1, allowed, visited, grid))
+                        return true;
+                }
+
+                // DOWN
+                if (row+1 < ROWS && (allowed[DOWN] & (1 << grid[row+1][col])))
+                {
+                    if (dfs(row+1, col, allowed, visited, grid))
+                        return true;
+                }
+                break;
+
+            case 5:
+                // LEFT
+                if (col-1 >= 0 && (allowed[LEFT] & (1 << grid[row][col-1])))
+                {
+                    if (dfs(row, col-1, allowed, visited, grid))
+                        return true;
+                }
+
+                // UP
+                if (row-1 >= 0 && (allowed[UP] & (1 << grid[row-1][col])))
+                {
+                    if (dfs(row-1, col, allowed, visited, grid))
+                        return true;
+                }
+                break;
+
+            case 6:
+                // RIGHT
+                if (col+1 < COLS && (allowed[RIGHT] & (1 << grid[row][col+1])))
+                {
+                    if (dfs(row, col+1, allowed, visited, grid))
+                        return true;
+                }
+
+                // UP
+                if (row-1 >= 0 && (allowed[UP] & (1 << grid[row-1][col])))
                 {
                     if (dfs(row-1, col, allowed, visited, grid))
                         return true;
