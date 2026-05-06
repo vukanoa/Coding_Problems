aafb76c6abb3fce9d4e92502340036648b6684b3
Noa Vukasovic <noavukasovic@gmail.com>
2026-05-06 13:34:56 +0200

    Remove Redundtant Solutions & Exchange the main Solution in LeetCode: 1861_Rotating_The_Box in ORderless_Problems

--- Orderless_Problems/1861_Rotating_The_Box/rotating_the_box.cpp
+++ Orderless_Problems/1861_Rotating_The_Box/rotating_the_box.cpp
@@ -1,6 +1,3 @@
-#include <iostream>
-#include <vector>
-
 /*
     ==============
     === MEDIUM ===
@@ -77,184 +74,65 @@
 
 */
 
+#include <vector>
+using namespace std;
+
 /*
     ------------
     --- IDEA ---
     ------------
 
-    You can choose which way you want to simulate the "fall". Here what I do is
-    I make rocks "fall to the right" and then I rotate.
+    You can choose which way you want to simulate the "fall".
+    Here what I do is I make rocks "fall to the right" and only thenI rotate.
 
-    Since we MUST actually return the rotated 2D vector, there's no other way
-    around it other than actually doing it yourself.
-    (In other words we MUST iterate through the rows and do it ourselves, but
-    as you will see in the 2nd Solution down below, this part CAN be optimized)
+    Keep in mind that this Solution DOES change the original "boxGrid" which is
+    passed by reference.
 
-    That's it.
+    So this may not be allowed. In that case we'd need to first rotate and only
+    then simulate the fall, but this time the fall is "fall downwards" and NOT
+    "to the right".
 
 */
 
-/* Time  Beats: 13.75% */
-/* Space Beats: 98.78% */
+/* Time  Beats: 90.91% */
+/* Space Beats: 15.72% */
 
-/* Time  Complexity: O(m * n) */
-/* Space Complexity: O(1)     */ // Result is not considered extra space
+/* Time  Complexity: O(ROWS * COLS) */
+/* Space Complexity: O(ROWS * COLS) */
 class Solution {
 public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box)
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid)
     {
-        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0); // Accelerates
+        const int ROWS = boxGrid.size();
+        const int COLS = boxGrid[0].size();
 
-        const int m = box.size();
-        const int n = box[0].size();
-
-        if (m * n == 1)
-            return box;
-
-        // Fall to the right
-        for (int i = 0; i < m; i++)
+        /* Simulate "fall to the right" */
+        for (int row = 0; row < ROWS; row++)
         {
-            for (int j = n-1; j > 0; j--)
+            int col_to_put = 0;
+
+            for (int col = 0; col < COLS; col++)
             {
-                if (box[i][j-1] == '#' && box[i][j] == '.')
+                if (boxGrid[row][col] == '.')
                 {
-                    while (j < n && box[i][j] == '.')
-                    {
-                        box[i][j-1] = '.';
-                        box[i][j  ] = '#';
-                        j++;
-                    }
+                    swap(boxGrid[row][col], boxGrid[row][col_to_put]);
+                    col_to_put++;
+                }
+                else if (boxGrid[row][col] == '*')
+                {
+                    col_to_put = col + 1;
                 }
             }
         }
-
-        // Rotate
-        vector<vector<char>> result(n, vector<char>(m, '.'));
-        for (int i = 0; i < m; i++)
+        
+        /* Rotate */
+        vector<vector<char>> result(COLS, vector<char>(ROWS));
+        for (int row = 0; row < ROWS; row++)
         {
-            for (int j = 0; j < n; j++)
-                result[j][m-1 - i] = box[i][j];
+            for (int col = 0; col < COLS; col++)
+                result[col][ROWS-1 - row] = boxGrid[row][col];
         }
-
-        return result;
-    }
-};
-
-
-
-
-/*
-    ------------
-    --- IDEA ---
-    ------------
-
-    Overall Time Complexity is unchanged, however this one is MUCH more
-    efficient since we are not "moving" each rock cell-by-cell, instead we are
-    swapping with the rightmost empty cell.
-
-    Checkout:LeetCode 26 "Remove Duplicates from Sorted Array" if you aren't
-    familiar with this technique.
-
-    However, in this one that wouldn't be enough, we actually need to SWAP the
-    rock with the empty cell we wish to place rock at.
-
-    In LeetCode 26, we do NOT care about elements after mth unique, but here we
-    very much DO.
-
-*/
-
-/* Time  Beats: 100.00% */
-/* Space Beats:  98.78% */
-
-/* Time  Complexity: O(m * n) */
-/* Space Complexity: O(1)     */ // Result is not considered extra space
-class Solution_Efficient {
-public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box)
-    {
-        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0); // Accelerates
-
-        const int m = box.size();
-        const int n = box[0].size();
-
-        if (m * n == 1)
-            return box;
-
-        // Fall to the right
-        for (int i = 0; i < m; i++)
-        {
-            int to_place_idx = n-1;
-
-            for (int j = n-1; j >= 0; j--)
-            {
-                if (box[i][j] == '#')
-                    swap(box[i][to_place_idx--], box[i][j]);
-                else if (box[i][j] == '*')
-                    to_place_idx = j-1;
-            }
-        }
-
-        // Rotate
-        vector<vector<char>> result(n, vector<char>(m, '.'));
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n; j++)
-                result[j][m-1 - i] = box[i][j];
-        }
-
-        return result;
-    }
-};
-
-
-
-
-/*
-    ------------
-    --- IDEA ---
-    ------------
-
-    Here we are NOT modifying the Input and we're doing both "the fall" and the
-    rotation at the same time.
-
-*/
-
-/* Time  Beats: 100.00% */
-/* Space Beats:  98.78% */
-
-/* Time  Complexity: O(m * n) */
-/* Space Complexity: O(1)     */ // Result is not considered extra space
-class Solution_Efficient_Non-Modifying {
-public:
-    vector<vector<char>> rotateTheBox(vector<vector<char>>& box)
-    {
-        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0); // Accelerates
-
-        const int m = box.size();
-        const int n = box[0].size();
-
-        vector<vector<char>> result(n, vector<char>(m, '.'));
-
-        // Fall to the right and Rotate at the same time
-        for (int i = 0; i < m; i++)
-        {
-            int to_place_idx = n-1;
-
-            for (int j = n-1; j >= 0; j--)
-            {
-                if (box[i][j] == '#')
-                {
-                    result[to_place_idx][m-1 - i] = '#';
-                    to_place_idx--;
-                }
-                else if (box[i][j] == '*')
-                {
-                    result[j][m-1 - i] = '*';
-                    to_place_idx = j-1;
-                }
-            }
-        }
-
+                
         return result;
     }
 };
