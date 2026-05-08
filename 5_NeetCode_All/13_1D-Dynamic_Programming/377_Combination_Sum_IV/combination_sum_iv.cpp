840ddd8366125c354693cd15200174e3f7d22b33
Noa Vukasovic <noavukasovic@gmail.com>
2026-05-08 13:33:22 +0200

    Add another Memoization Solution to LetCode: 377_Combination_Sum_IV to NeetCodE_250, NeetCodE_All

--- 5_NeetCode_All/13_1D-Dynamic_Programming/377_Combination_Sum_IV/combination_sum_iv.cpp
+++ 5_NeetCode_All/13_1D-Dynamic_Programming/377_Combination_Sum_IV/combination_sum_iv.cpp
@@ -100,7 +100,71 @@ public:
     --- IDEA ---
     ------------
 
-    TODO
+    Same as above, but implemented in a Top-Down fashion, using Memoization.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  99.27% */
+
+/* Time  Complexity: O(N * target) */
+/* Space Complexity: O(N)          */
+class Solution_Top_Down__Memoization {
+private:
+    int memo[1001];
+
+public:
+    int combinationSum4(vector<int>& nums, int target)
+    {
+        const int N = nums.size();
+
+        /* Sort */
+        sort(nums.begin(), nums.end());
+
+        /* Memset */
+        memset(memo, 0xff, sizeof(memo));
+
+        return solve(target, nums);
+    }
+
+private:
+    int solve(int target, vector<int>& nums)
+    {
+        if (target == 0)
+            return 1;
+
+        if (memo[target] != -1)
+            return memo[target];
+
+        int result = 0;
+        for (const int& num : nums)
+        {
+            if (target - num < 0)
+                break;
+
+            result += solve(target - num, nums);
+        }
+
+        return memo[target] = result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Both this and the Solution above are correct implementationt of a
+    Memoization technique, however this one is more GENERAL and thus LESS
+    efficient.
+
+    This one does NOT prune early, but makes one more recursion call.
+
+    However, if you're getting into learning about Memoization it's always
+    beneficial to see multiple different implementations of it.
 
 */
 
