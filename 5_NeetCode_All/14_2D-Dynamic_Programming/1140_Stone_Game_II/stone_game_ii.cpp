8d3a576e4c7a0c4cffd9061487e2bf28e20954b6
Noa Vukasovic <noavukasovic@gmail.com>
2026-05-23 10:25:22 +0200

    Add another Bottom-Up Tabulation Solution to LeetCodE: 1140_StonE_Game_II to NeetCode_250, NeetCode_All

--- 5_NeetCode_All/14_2D-Dynamic_Programming/1140_Stone_Game_II/stone_game_ii.cpp
+++ 5_NeetCode_All/14_2D-Dynamic_Programming/1140_Stone_Game_II/stone_game_ii.cpp
@@ -75,7 +75,7 @@ using namespace std;
 
 /* Time  Complexity: O(N^3) */
 /* Space Complexity: O(N^2) */
-class Solution_Memooooo {
+class Solution_Top_Down__Memoization {
 private:
     int memo[101][101][2];
 
@@ -133,41 +133,92 @@ private:
 
 */
 
+/* Time  Beats: 6.80% */
+/* Space Beats: 8.58% */
+
+/* Time  Complexity: O(N^3) */
+/* Space Complexity: O(N^2) */
+class Solution_Bottom_Up__Tabulation {
+public:
+    int stoneGameII(vector<int>& piles)
+    {
+        const int N = piles.size();
+        vector<vector<vector<int>>> dp(N+1, vector<vector<int>>(N+1, vector<int>(2, 0)));
+
+        for (int i = N-1; i >= 0; i--)
+        {
+            for (int M = 1; M <= N; M++)
+            {
+                int value = 0;
+
+                dp[i][M][1] = 0;       // Alice
+                dp[i][M][0] = INT_MAX; // Bob
+
+                for (int X = 1; X <= 2 * M; X++)
+                {
+                    if (i + X-1 >= N)
+                        break;
+
+                    value += piles[i + X-1];
+
+                    dp[i][M][1] = max(dp[i][M][1], value + dp[i + X][max(M, X)][0]);
+                    dp[i][M][0] = min(dp[i][M][0], 0     + dp[i + X][max(M, X)][1]);
+                }
+            }
+        }
+
+        return dp[0][1][1];
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
 /* Time  Beats: 73.45% */
 /* Space Beats: 52.69% */
 
-/* Time  Complexity: O(n^3) */
-/* Space Complexity: O(n^2) */
-class Solution_Bottom_Up {
+/* Time  Complexity: O(N^3) */
+/* Space Complexity: O(N^2) */
+class Solution_Bottom_Up__Tabulation__Suffix_Sum {
 public:
     int stoneGameII(vector<int>& piles)
     {
         const int N = piles.size();
         
         vector<vector<int>> dp(N, vector<int>(N + 1, 0));
-        vector<int> suffix_sum(N, 0);
 
+        /* Suffix Sum */
+        vector<int> suffix_sum(N, 0);
         suffix_sum[N - 1] = piles[N - 1];
-        
         for (int i = N-2; i >= 0; i--)
             suffix_sum[i] = suffix_sum[i + 1] + piles[i];
         
+        /* Solve */
         for (int i = N-1; i >= 0; i--)
         {
-            for (int j = 1; j <= N; j++)
+            for (int M = 1; M <= N; M++)
             {
-                if (i + 2*j >= N)
+                if (i + 2*M >= N)
                 {
-                    dp[i][j] = suffix_sum[i];
+                    dp[i][M] = suffix_sum[i];
                 }
                 else
                 {
-                    for (int x = 1; x <= 2 * j; x++)
-                        dp[i][j] = max(dp[i][j], suffix_sum[i] - dp[i + x][max(j, x)]);
+                    for (int X = 1; X <= 2 * M; X++)
+                        dp[i][M] = max(dp[i][M], suffix_sum[i] - dp[i + X][max(M, X)]);
                 }
             }
         }
-        
+
         return dp[0][1];
     }
 };
