fe2c46496ba7a0ff26c581e12faaf3d77e607521
Noa Vukasovic <noavukasovic@gmail.com>
2026-05-27 10:31:53 +0200

    Add Sliding-Window's version of Kadane's Algorithm to LeetCode: 53_Maximum_Subarray to Top_100_Liked, Blind_75, NeetCode_150, NeetCode_250, NeetCode_All

--- 2_Blind_75/15_Greedy/53_Maximum_Subarray/maximum_subarray.cpp
+++ 2_Blind_75/15_Greedy/53_Maximum_Subarray/maximum_subarray.cpp
@@ -41,6 +41,7 @@
 
 */
 
+#include <climits>
 #include <vector>
 using namespace std;
 
@@ -56,21 +57,76 @@ using namespace std;
 /* Time  Beats: 86.15% */
 /* Space Beats: 88.10% */
 
-/* Time  Complexity: O(n) */
+/* Time  Complexity: O(N) */
 /* Space Complexity: O(1) */
-class Solution_Efficient {
+class Solution_Kadane_Algorithm__Dynamic_Programming {
 public:
-    int maxSubArray(std::vector<int>& nums)
+    int maxSubArray(vector<int>& nums)
     {
-        int dp  = nums[0];
-        int max = nums[0];
+        const int N = nums.size();
+        int result = INT_MAX;
 
-        for (int i = 1; i < nums.size(); i++)
+        int curr_max_sum = nums[0];
+
+        for (int i = 1; i < N; i++)
         {
-            dp  = std::max(dp + nums[i], nums[i]);
-            max = std::max(max, dp);
+            curr_max_sum = max(curr_max_sum + nums[i], nums[i]);
+
+            result = max(result, curr_max_sum);
         }
 
-        return max;
+        return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+    Same Kadane's Algorithm as above, however this one is conceptually
+    different. It leverages "Sliding Window" technique.
+
+    It depends on how you think about the Solution. It's always beneficial to
+    see more than one way of writing effectively the same Algorithm.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  51.43% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution_Kadane_Algorithm__Sliding_Window {
+public:
+    int maxSubArray(vector<int>& nums)
+    {
+        const int N = nums.size();
+        int result = INT_MIN;
+
+        int curr_sum = 0;
+
+        int L = 0;
+        int R = 0;
+        while (R < N)
+        {
+            curr_sum += nums[R];
+            result = max(result, curr_sum);
+
+            if (curr_sum < 0)
+            {
+                L = R+1;
+                curr_sum = 0; // Reset
+            }
+
+            // Increment
+            R++;
+        }
+
+        return result;
     }
 };
