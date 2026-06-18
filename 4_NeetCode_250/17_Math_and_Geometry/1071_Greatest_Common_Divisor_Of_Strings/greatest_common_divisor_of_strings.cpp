ce3849ec8e9d2850028d3439a04ae77ba999eca3
Noa Vukasovic <noavukasovic@gmail.com>
2026-06-18 10:30:47 +0200

    Remove Redundant Solution & Reformat in LeetCodE: 1071_Greatest_Common_Divisor_Of_Strings in NeetCode_250, NeetCode_All

--- 4_NeetCode_250/17_Math_and_Geometry/1071_Greatest_Common_Divisor_Of_Strings/greatest_common_divisor_of_strings.cpp
+++ 4_NeetCode_250/17_Math_and_Geometry/1071_Greatest_Common_Divisor_Of_Strings/greatest_common_divisor_of_strings.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 /*
     ==============
     === MEDIUM ===
@@ -46,96 +44,86 @@
 
 */
 
+#include <numeric>
+#include <string>
+using namespace std;
+
 /*
     ------------
     --- IDEA ---
     ------------
 
-    ********************************
-    *** Explanation for the Code ***
-    ********************************
+    There is a "trick" here.
 
-    First we must check if (str1 + str2 == str2 + str1). Why?
+    If a common divisor string exists, then concatenating the two strings in
+    either order MUST produce the same result:
+
+        str1 + str2 == str2 + str1. 
+
 
     Consider this example:
 
-    str1 = "LEETCODE"
-    str2 = "LEET"
+        str1 = "LEETCODE"
+        str2 = "LEET"
 
     "LEET"(str2) is indeed a substring of "LEETCODE"(str1), however we cannot
-    say that str2 divides str1 because of the str1's suffix "CODE", therefore
-    we must check if (str1 + str2 == str2 + str1), i.e.:
+    say that str2 divides str1 because of the str1's suffix "CODE".
 
-        str1 + str2 ==> "LEETCODE" + "LEET" ==> "LEETCODELEET"
-        str2 + str1 ==> "LEET" + "LEETCODE" ==> "LEETLEETCODE"
+    Let's check it using our trick:
 
-    These are NOT equivalent, which further means that no part of the, shorter,
-    string str2 can divide, longer, string str1 and as a result we return "".
+        str1 + str2 ==> "LEETCODE" + "LEET"     ==> "LEETCODELEET"
+        str2 + str1 ==> "LEET"     + "LEETCODE" ==> "LEETLEETCODE"
+
+    These are NOT equivalent, which further means that no part of the shorter
+    string can divide longer string, therefore we return "".
 
     However, if we find that these concatenated are indeed equivalent, then we
-    have to return the GCD of the length those two.
+    have to return the GCD of lengths those two strings.
 
     Let's look at Example 2:
     str1 = "ABCABC"
     str2 = "ABC"
 
-        str1 + str2 ==> "ABCABC" + "ABC" ==> "ABCABCABC"
-        str2 + str1 ==> "ABC" + "ABCABC" ==> "ABCABCABC"
+        str1 + str2 ==> "ABCABC" + "ABC"    ==> "ABCABCABC"
+        str2 + str1 ==> "ABC"    + "ABCABC" ==> "ABCABCABC"
 
     These are INDEED equivalent, but the result should be the GCD of the length
-    of the two strings.
+    of these two strings.
 
     Fortunately, we don't have to write GCD function ourselves since STL has it
-    built-in.
+    built-in. (C++17)
 
     Now we just return the substring of any of the two(since GCD will never
     exceed the length of the shorter one), starting from the beginning and
-    taking first std::gcd(str1.length(), str2.length()) characters.
+    taking first:
+
+        gcd(str1.size(), str2.size())
+
+    characters.
 
     For Example 2:
-        std::gdc(6, 3) is 3
+        gdc(6, 3) is 3
 
-    therefore, we return str1.substr(0, 3), i.e. "ABCABC".substr(0, 3) = "ABC".
+    therefore, we return str1.substr(0, 3), i.e.:
+
+        "ABCABC".substr(0, 3) = "ABC".
 
 */
 
 /* Time  Beats: 100.00% */
 /* Space Beats:  84.86% */
 
-/* Time  Complexity: O(n) */
-/* Space Complexity: O(1) */
-class Solution_Easier_To_Read {
+/* Time  Complexity: O(N + M) */
+/* Space Complexity: O(1)     */
+class Solution {
 public:
-    std::string gcdOfStrings(string str1, string str2)
+    string gcdOfStrings(string str1, string str2)
     {
-        if(str1 + str2 != str2 + str1)
+        if (str1 + str2 != str2 + str1)
             return "";
 
-        return str1.substr(0, std::gcd(str1.length(), str2.length()));
-    }
-};
+        int g = gcd(str1.size(), str2.size());
 
-
-
-
-/*
-    ------------
-    --- IDEA ---
-    ------------
-
-    Same as above, but writte as a one-liner.
-
-*/
-
-/* Time  Beats: 100.00% */
-/* Space Beats:  57.48% */
-
-/* Time  Complexity: O(n) */
-/* Space Complexity: O(1) */
-class Solution_One_Liner {
-public:
-    std::string gcdOfStrings(std::string str1, std::string str2)
-    {
-        return (str1 + str2 == str2 + str1) ? str1.substr(0, std::gcd(str1.length(), str2.length())) : "";
+        return str1.substr(0, g);
     }
 };
