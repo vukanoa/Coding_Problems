cfa0bc2de073a8cee82949560c9cd83cd2780256
Noa Vukasovic <noavukasovic@gmail.com>
2026-06-22 10:53:19 +0200

    Remove Redundant & Exchange Bitiwse Solution & Explanation(IDEA) In LeetCode: 371_Sum_Of_Two_Integers in Top_150_Interview, Blind_75, NeetCode_150, NeetCode_250, NeetCode_All

---
+++ 2_Blind_75/18_Bit_Manipulation/371_Sum_Of_Two_Integers/sum_of_two_integers.cpp
@@ -0,0 +1,271 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===========================
+    371) Sum Of Two Integers
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    Given two integers a and b, return the sum of the two integers without
+    using the operators + and -.
+
+    ===================================
+    FUNCTION: int getSum(int a, int b);
+    ===================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: a = 1, b = 2
+    Output: 3
+
+    --- Example 2 ---
+    Input: a = 2, b = 3
+    Output: 5
+
+    *** Constraints ***
+    -1000 <= a, b <= 1000
+*/
+
+#include <cmath>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    1)
+        a + b
+
+
+    2)
+        2^a * 2^b = log2(2^a * 2^b)
+                  = log2(2^(a + b))
+                  = (a + b) * log2(2)
+                  = (a + b) * 1
+                  = (a + b)
+*/
+
+/* Time  Beats:   100% */
+/* Space Beats: 12.66% */
+
+/* Time  Complexity: O(1) */
+/* Space Complexity: O(1) */
+class Solution_Math {
+public:
+    int getSum(int a, int b)
+    {
+        return log(pow(2, a) * pow(2, b)) / log(2);
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Since we are not allowed to use a + or - sign, we must get creative.
+
+    Summing 1 and 2, in binary looks like this:
+
+          0001 = 1
+        + 0010 = 2
+        ------
+          0011 = 3
+
+    So which bitwise operation could we do on these two binary values so that
+    we get 3?
+
+    It's XOR.
+
+    However, there's a caveat. Consider this example:
+
+          0101 = 5
+        + 0100 = 4
+        ------
+          1001 - 9
+
+    If we were to use XOR only, we would get 1(i.e 0001 in binary), which is
+    false.
+
+    So, the problem is--What are we supposed to do when both bits are 1?
+
+    We HAVE TO put 0 as a result of those two bits, but we also need a carry
+    that indicates that one bit gets carried further.
+
+    So how are we going to do that?
+
+    If we have two 0s, we definitely don't have a carry.
+    If we have one 0,  we also       don't have a carry.
+
+    However, if we have TWO 1s then we DO have a carry.
+
+    So--How do we know if we have TWO 1s?
+    It's bitwise "AND" operation.
+
+    If a & b gives us 1, then that means that we have do have a carry.
+    However, that 1 has to be added with its left bit.
+
+    Example:
+
+         _____________________________________
+         |                                   |
+         |                                   |
+         v                                   |
+       0011                                  |
+    +  1010                                  |
+    -------                                  |
+       1101                                  |
+                                             |
+                                             v
+    2nd from the right(1-based), we have two 1s. However, we have to add that
+    carry with two bits 3rd from the right(1-based).
+
+    So how can we ensure that?
+        (a & b) << 1
+
+    This means that we're going to "catch" every TWO 1s and Logically shift the
+    whole number by one to the left and then we're going to XOR that new number
+    with previously XOR-ed value, and so on.
+
+    We're going to have a loop.
+
+    Let's do one example. Note that when we're doing these logical bitwise
+    operators--We do NOT have to do them one by one.
+
+    We can do them on entirety of 'a' and 'b' that we're given.
+
+    Example:
+
+           1001 = 9
+        +  1011 = 11
+        -------
+          10100 = 20
+
+
+    If we, firstly, XOR these two number(9 and 11), we get:
+
+            1001 = 9
+        XOR 1011 = 11
+        --------
+            0010 = 2
+
+    9 + 11 is definitely not 2, what are we missing?
+    We didn't do the AND operation.
+
+            1001 = 9
+        AND 1011 = 11
+        --------
+            1001 = 9
+        <<
+        --------
+           10010 = 18
+
+    With "AND" we'll get all the carry bits, but we have to Logically Shift
+    those bits to the LEFT because that's how the binary addition works.
+
+    And now we have to add this new number--that is a result of AND operation
+    and LSL (Logical Shift Left)--with the previously XOR-ed number.
+
+    Again, we cannot use a '+' sign, so we have to do the same thing which is
+    XOR.
+
+    But even if we do XOR again, it won't take care of the FURTHER carry bits,
+    thus we have to keep repeating this.
+
+    We'll have a loop.
+
+    So simulation of (9 + 11):
+
+             1001
+        XOR  1011
+        ---------
+             0010
+
+             1001
+        AND  1011
+        ---------
+             1001
+        <<
+        ---------
+            10010
+
+    Now repeat these steps:
+
+        a ^ b        =  0010
+        (a & b) << 1 = 10010
+
+
+             0010
+        XOR 10010
+        ---------
+            10000
+
+             0010
+        AND 10010
+        ---------
+            00010
+        <<
+        ---------
+            00100
+
+
+        a ^ b        = 10000
+        (a & b) << 1 = 00100
+
+
+    Repeat again:
+
+            10000
+        XOR 00100
+        ---------
+            10100 = 20 (Our DESIRED result)
+
+            10000
+        AND 00100
+        ---------
+            00000
+        <<
+        ---------
+            00000
+
+        a ^ b        = 10100
+        (a & b) << 1 = 00000
+
+    Since ((a & b) << 1) == 0, that means that the previous XOR did the job and
+    that we should return the result;
+
+*/
+
+/* Time  Beats:  100.00% */
+/* Space Beats:   15.19% */
+
+/* Time  Complexity: O(1) */ // O(32) --> O(1)
+/* Space Complexity: O(1) */
+class Solution_Bitwise {
+public:
+    int getSum(int a, int b)
+    {
+        while (b != 0)
+        {
+            int sum = a ^ b;          // sum WITHOUT carry
+            int carry = (a & b) << 1; // carry after LSL (Logical Shift Left)
+
+            a = sum;
+            b = carry;
+        }
+
+        return a;
+    }
+};
