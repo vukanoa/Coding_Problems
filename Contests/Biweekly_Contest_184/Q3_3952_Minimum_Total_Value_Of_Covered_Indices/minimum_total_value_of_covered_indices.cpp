73554acd7ba8625c6c327450e279310cfb84fc64
Noa Vukasovic <noavukasovic@gmail.com>
2026-07-01 11:24:20 +0200

    Add Skip-Take Memoization Solution to Q3 in LeetCodE: 3952_Minimum_Total_Value_Of_Covered_Indices to Biweekly_Contest_184

---
+++ Contests/Biweekly_Contest_184/Q3_3952_Minimum_Total_Value_Of_Covered_Indices/minimum_total_value_of_covered_indices.cpp
@@ -0,0 +1,132 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ============================================
+    3952) Minimum Total Value of Covered Indices
+    ============================================
+
+    ============
+    Description:
+    ============
+
+    You are given an integer array nums of length n and a binary string s of
+    length n, where s[i] == '1' means index i initially contains a token and
+    s[i] == '0' means it does not.
+
+    You may perform the following operation any number of times:
+
+        + Choose a token currently located at index i, where i > 0, such that
+          this token has not been moved before.
+
+        + Move this token from index i to index i - 1.
+
+    An index is considered covered if it contains a token after all moves.
+
+    Return an integer denoting the maximum total value of nums at the covered
+    indices after optimally performing the operations.
+
+
+    ==========================================================
+    FUNCTION: long long maxTotal(vector<int>& nums, string s);
+    ==========================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [9,2,6,1], s = "0101"
+    Output: 15
+    Explanation:
+        Initially, indices 1 and 3 contain tokens.
+        Move the token from index 3 to index 2.
+        Move the token from index 1 to index 0.
+        The covered indices are [0, 2], so the total value is nums[0] + nums[2]
+        = 9 + 6 = 15.
+
+    --- Example 2 ---
+    Input: nums = [5,1,4], s = "001"
+    Output: 4
+    Explanation:
+        Initially, only index 2 contains a token.
+        It is optimal to leave the token at index 2.
+        The covered index is [2], so the total value is nums[2] = 4.
+
+    --- Example 3 ---
+    Input: nums = [9,3,5], s = "011"
+    Output: 14
+    Explanation:
+        Initially, indices 1 and 2 contain tokens.
+        Move the token from index 1 to index 0.
+        The covered indices are [0, 2], so the total value is nums[0] + nums[2]
+        = 9 + 5 = 14.
+
+
+    *** Constraints ***
+    1 <= n == nums.length == s.length <= 10^5
+    1 <= nums[i] <= 10^5
+    s[i] is either '0' or '1'
+
+*/
+
+#include <cstring>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Simple Take-Skip Memoization problem.
+
+*/
+
+/* Time  Beats: 40.89% */
+/* Space Beats: 42.65% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(N) */
+class Solution {
+public:
+    static constexpr int MAX_SIZE = 1e5;
+    long long memo[MAX_SIZE + 1][2];
+
+    long long maxTotal(vector<int>& nums, string s)
+    {
+        const int N = s.size();
+
+        /* Memset */
+        memset(memo, 0xff, sizeof(memo));
+
+        return solve(0, 0, nums, s);
+    }
+
+private:
+    long long solve(int idx, int taken, vector<int>& nums, string& s)
+    {
+        const int N = s.size();
+
+        if (idx >= N)
+            return 0;
+
+        if (memo[idx][taken] != -1)
+            return memo[idx][taken];
+
+        long long skip = solve(idx+1, 0, nums, s); /* Skip */
+        long long take = 0LL;
+
+        /* Take */
+        if (s[idx] == '1' &&  ! taken)
+            take = max(take, 1LL * nums[idx] + solve(idx+1, 0, nums, s));
+
+        if (idx+1 < N && s[idx+1] == '1')
+            take = max(take, 1LL * nums[idx] + solve(idx+1, 1, nums, s));
+
+        return memo[idx][taken] = max(skip, take);
+    }
+};
