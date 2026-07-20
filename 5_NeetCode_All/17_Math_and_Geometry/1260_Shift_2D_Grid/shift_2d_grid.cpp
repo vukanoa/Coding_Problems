ab9682abfdee733eac671878f5352958fd746606
Noa Vukasovic <noavukasovic@gmail.com>
2026-07-20 10:09:09 +0200

    Exchange & Improve (2D to flattened 1D to 2D) Solution in LeetCode: 1260_Shift_2D_Grid in NeetCode_All

--- 5_NeetCode_All/17_Math_and_Geometry/1260_Shift_2D_Grid/shift_2d_grid.cpp
+++ 5_NeetCode_All/17_Math_and_Geometry/1260_Shift_2D_Grid/shift_2d_grid.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <vector>
-#include <algorithm> // For lambda functions
-
 /*
     ============
     === EASY ===
@@ -97,6 +93,9 @@
 
 */
 
+#include <vector>
+using namespace std;
+
 /*
     ------------
     --- IDEA ---
@@ -104,6 +103,9 @@
 
     All you have to do is to "figure out" how to shift the grid by one and then
     write that function and call it k times.
+
+    That is, of course, a BRUTE FORCE way of solving it. But we have to start
+    with something.
 
     I've implemented it by having an additional vector called "first_in_row"
     which would store the rightmost element which is "shifted out" at each
@@ -119,20 +121,21 @@
     (i + 1)  % ROWS will result in every i-th row, except the very last one, to
     be put in (i + 1)th position in "first_by_row". However, the very last one
     will be put in 0-th row, but since we don't want to write an if statement
-    we can just use this formula that mods accordingly.
+    we can just use this formula that does MODULO accordingly.
 
     Imagine that ROWS = 3;
 
     Last element in the 0-th row must go to the first element in (0 + 1)th row.
     Last element in the 1-st row must go to the first element in (1 + 1)th row.
     Last element in the 2-nd row must go to the first element in (2 + 1)th row,
-    however since (2 + 1) = 3, that means we would get an "out of bounds" error
-    and therefore we can realize that we need to mod it with the number of rows
-    in order to get every index at the right place.
+
+    However since (2 + 1) = 3, that means we would get an "out of bounds" error
+    and therefore we realize that we need to do a MODULO on it with the number
+    of rows in order to get every index at the right place.
 
     (2 + 1) % ROWS ==> (3) % 3 = 0
 
-    That's the only "tricky" part for this problem.
+    That's the only "tricky" part for this problem if we're doing Brute Force.
 
 */
 
@@ -140,10 +143,10 @@
 /* Space Beats: 20.37% */
 
 /* Time  Complexity: O(k * ROWS * COLS) */
-/* Space Complexity: O(ROWS) */
-class Solution {
+/* Space Complexity: O(ROWS)            */
+class Solution_Brute_Force {
 public:
-    std::vector<std::vector<int>> shiftGrid(std::vector<sd::vector<int>>& grid, int k)
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k)
     {
         for (int i = 0; i < k; i++)
             shift_grid_by_one(grid);
@@ -152,19 +155,19 @@ public:
     }
 
 private:
-    void shift_grid_by_one(std::vector<std::vector<int>>& grid)
+    void shift_grid_by_one(vector<vector<int>>& grid)
     {
         int ROWS = grid.size();
         int COLS = grid[0].size();
 
-        std::vector<int> first_in_row(ROWS, 0);
+        vector<int> first_in_row(ROWS, 0);
 
-        for (int i = 0; i < ROWS; i++)
+        for (int row = 0; row < ROWS; row++)
         {
-            first_in_row[(i + 1) % ROWS] = grid[i][COLS-1];
+            first_in_row[(row + 1) % ROWS] = grid[row][COLS-1];
 
-            for (int j = COLS-1; j > 0; j--)
-                grid[i][j] = grid[i][j-1];
+            for (int col = COLS-1; col > 0; col--)
+                grid[row][col] = grid[row][col-1];
         }
 
         for (int i = 0; i < ROWS; i++)
@@ -184,40 +187,36 @@ private:
 
 */
 
-/* Time  Beats: 92.74% */
-/* Space Beats: 51.99% */
+/* Time  Beats: 53.88% */
+/* Space Beats: 39.39% */
 
 /* Time  Complexity: O(ROWS * COLS) */
 /* Space Complexity: O(ROWS * COLS) */
-class Solution_Neat {
+class Solution {
 public:
-    std::vector<std::vector<int>> shiftGrid(std::vector<std::vector<int>>& grid, int k)
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k)
     {
-        int ROWS = grid.size();
-        int COLS = grid[0].size();
+        const int ROWS = grid.size();
+        const int COLS = grid[0].size();
+        const int TOTAL = ROWS * COLS;
 
-        std::function<int(int, int)> pos_2D_to_index_1D;
-        pos_2D_to_index_1D = [&](int row, int col) -> int
-                                {
-                                    return row * COLS + col;
-                                };
+        k %= TOTAL;
 
-        std::function<std::pair<int, int>(int)> index_1D_to_pos_2D;
-        index_1D_to_pos_2D = [&](int idx) -> std::pair<int, int>
-                                {
-                                    return {idx / COLS, idx % COLS};
-                                };
+        vector<vector<int>> result(ROWS, vector<int>(COLS));
 
-        std::vector<std::vector<int>> result(ROWS, std::vector(COLS, 0));
         for (int row = 0; row < ROWS; row++)
         {
             for (int col = 0; col < COLS; col++)
             {
-                int idx = (pos_2D_to_index_1D(row, col) + k) % (ROWS * COLS);
+                // Index in 1D array (BEFORE rotation)
+                int old_idx = row * COLS + col;
 
-                std::pair<int, int> pair = index_1D_to_pos_2D(idx);
-                int new_row = pair.first;
-                int new_col = pair.second;
+                // Index in 1D array (AFTER rotation)
+                int new_idx = (old_idx + k) % TOTAL;
+
+                // Converting from 1D back to 2D
+                int new_row = new_idx / COLS;
+                int new_col = new_idx % COLS;
 
                 result[new_row][new_col] = grid[row][col];
             }
