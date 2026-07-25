797190b18a577ed8b183806718cc1915c0ac1ab0
Noa Vukasovic <noavukasovic@gmail.com>
2026-07-25 09:51:07 +0200

    Add 3 Bit_Manipulation Implementations to LeetCode: 3536_Maximum_Product_Of_Two_Digits to Orderless_Problems

--- Orderless_Problems/3536_Maximum_Product_Of_Two_Digits/maximum_product_of_two_digits.cpp
+++ Orderless_Problems/3536_Maximum_Product_Of_Two_Digits/maximum_product_of_two_digits.cpp
@@ -59,6 +59,7 @@
 
 */
 
+#include <cstdint>
 using namespace std;
 
 /*
@@ -73,8 +74,8 @@ using namespace std;
 /* Time  Beats: 100.00% */
 /* Space Beats:  43.75% */
 
-/* Time  Complexity: O(1) */
-/* Space Complexity: O(1) */
+/* Time  Complexity: O(log10(N)) */
+/* Space Complexity: O(1)        */
 class Solution {
 public:
     int maxProduct(int n)
@@ -99,5 +100,245 @@ public:
         }
 
         return one * two;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    We use one unsigned 64−bit integer as a frequency table for the digits.
+
+    Each digit is assigned a 4−BIT BLOCK, allowing us to store its frequency
+    independently.
+
+          9    8    7    6    5    4    3    2    1
+        0000 0000 0000 0000 0000 0000 0000 0000 0000
+           ^                        ^    ^         ^
+           |                        |    |         |
+           |                        |    |         |
+          36th          ...       16th  12th      0th
+          bit                     bit   bit       bit
+
+
+
+    For each extracted digit(i.e.: n % 10), we increase its counter by adding:
+
+        1ULL << (4 * digit)
+
+    Example:
+
+        n = 87486538
+
+    we increase the count for each digit:
+
+        mask += 1ULL << (digit * 4) 
+
+          9    8    7    6    5    4    3    2    1
+        0000 0001 0000 0000 0000 0000 0000 0000 0000        n = 87486538
+                ^                                                      #
+                |
+               32nd
+               bit
+
+          9    8    7    6    5    4    3    2    1
+        0000 0001 0000 0000 0000 0000 0001 0000 0000        n = 8748653
+                                         ^                            #
+                                         |
+                                        12th
+                                        bit
+
+          9    8    7    6    5    4    3    2    1
+        0000 0001 0000 0000 0001 0000 0001 0000 0000        n = 874865
+                               ^                                     #
+                               |
+                              24th
+                              bit
+
+
+          9    8    7    6    5    4    3    2    1
+        0000 0001 0000 0001 0001 0000 0001 0000 0000        n = 87486
+                          ^                                         #
+                          |
+                        28th
+                         bit
+
+          9    8    7    6    5    4    3    2    1
+        0000 0010 0000 0001 0001 0000 0001 0000 0000        n = 8748
+               ^                                                   #
+               |
+              33rd
+              bit
+
+          9    8    7    6    5    4    3    2    1
+        0000 0011 0000 0001 0001 0001 0001 0000 0000        n = 874
+                                    ^                             #
+                                    |
+                                   16th
+                                   bit
+
+
+          9    8    7    6    5    4    3    2    1
+        0000 0011 0001 0001 0001 0001 0001 0000 0000        n = 87
+                     ^                                           #
+                     |
+                    28th
+                    bit
+
+          9    8    7    6    5    4    3    2    1
+        0000 0011 0001 0001 0001 0001 0001 0000 0000        n = 8
+                ^                                               #
+                |
+               32rd
+               bit
+
+
+
+    After extracting ALL digits, the HIGHEST SET BIT belongs to the largest
+    digit that appears in this mask.
+
+    We extract this digit using it's bit length or (bit_width),
+
+          9    8    7    6    5    4    3    2    1
+        0000 0011 0001 0001 0001 0001 0001 0000 0000
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+                               |
+                               |
+                            bit length
+
+    Then diving it by 4, we'll get 8 which is indeed a largest digit in n.
+
+          9    8    7    6    5    4    3    2    1
+        0000 0011 0001 0001 0001 0001 0001 0000 0000  
+             ^^^^
+
+    We then remove one occurrence of that digit by subtracting:
+
+        1ULL << (4 * digit)
+
+    and repeat the same process to obtain the second largest digit.
+    In this case, that's also 8.
+
+    At the end we just return the product of these two digits:
+
+        return largest * second_largest;
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  59.34% */
+
+/* Time  Complexity: O(log10(N)) */
+/* Space Complexity: O(1)        */
+class Solution_Bit_manipulation {
+public:
+    int maxProduct(int n)
+    {
+        uint64_t mask = 0;
+
+        while (n)
+        {
+            int digit = n % 10;
+            mask += (1ULL << (digit * 4));
+            n /= 10;
+        }
+
+        int largest        = (bit_width(mask) - 1) / 4;
+        mask -= (1ULL << (largest * 4));
+        int second_largest = (bit_width(mask) - 1) / 4;
+
+        return largest * second_largest;
+    }
+
+    int bit_width(uint64_t x)
+    {
+        return x ? 64 - __builtin_clzll(x) : 0;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same as two above, implemented differently.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  59.34% */
+
+/* Time  Complexity: O(log10(N)) */
+/* Space Complexity: O(1)        */
+class Solution_Bit_manipulation_2 {
+public:
+    int maxProduct(int n)
+    {
+        uint64_t mask = 0;
+
+        while (n)
+        {
+            int digit = n % 10;
+            mask += (1ULL << (digit * 4));
+            n /= 10;
+        }
+
+        // (63 - __builtin_clzll(mask))  returns an INDEX of a highest SET bit
+        // (0-based):
+        //
+        //     1 (001) -> 0
+        //     2 (010) -> 1
+        //     3 (011) -> 1
+        //     4 (100) -> 2
+        int largest        = (63 - __builtin_clzll(mask)) / 4;
+        mask -= (1ULL << (largest * 4));
+        int second_largest = (63 - __builtin_clzll(mask)) / 4;
+
+        return largest * second_largest;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same as two above, implemented differently.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  59.34% */
+
+/* Time  Complexity: O(log10(N)) */
+/* Space Complexity: O(1)        */
+class Solution_Bit_Manipulation_3 {
+public:
+    int maxProduct(int n)
+    {
+        uint64_t mask = 0;
+
+        while (n)
+        {
+            int digit = n % 10;
+            mask += (1ULL << (digit << 2));
+            n /= 10;
+        }
+
+        int largest        = (63 - __builtin_clzll(mask)) >> 2;
+        mask -= (1ULL << (largest << 2));
+        int second_largest = (63 - __builtin_clzll(mask)) >> 2;
+
+        return largest * second_largest;
     }
 };
