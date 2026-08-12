f810569255e700486ebf7827776823a475d4754e
Noa Vukasovic <noavukasovic@gmail.com>
2026-08-12 10:43:43 +0200

    Exchange DFS Solution in LeetCode: 1462_Course_Schedule_IV to NeetCode_250, NeetCodE_All

---
+++ 4_NeetCode_250/11_Graphs/1462_Course_Schedule_IV/course_schedule_iv.cpp
@@ -0,0 +1,156 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===========================
+    1462) Course Schedule IV
+    ===========================
+
+    ============
+    Description:
+    ============
+
+    There are a total of numCourses courses you have to take, labeled from 0 to
+    numCourses - 1. You are given an array prerequisites where prerequisites[i]
+    = [ai, bi] indicates that you must take course ai first if you want to take
+    course bi.
+
+        For example, the pair [0, 1] indicates that you have to take course 0
+        before you can take course 1.
+
+    Prerequisites can also be indirect. If course a is a prerequisite of course
+    b, and course b is a prerequisite of course c, then course a is a
+    prerequisite of course c.
+
+    You are also given an array queries where queries[j] = [uj, vj]. For the
+    jth query, you should answer whether course uj is a prerequisite of course
+    vj or not.
+
+    Return a boolean array answer, where answer[j] is the answer to the jth
+    query.
+
+    =============================================================================================================================
+    FUNCTION: vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries);
+    =============================================================================================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: numCourses = 2, prerequisites = [[1,0]], queries = [[0,1],[1,0]]
+    Output: [false,true]
+    Explanation: The pair [1, 0] indicates that you have to take course 1
+                 before you can take course 0.  Course 0 is not a prerequisite
+                 of course 1, but the opposite is true.
+
+
+    --- Example 2 ---
+    Input: numCourses = 2, prerequisites = [], queries = [[1,0],[0,1]]
+    Output: [false,false]
+    Explanation: There are no prerequisites, and each course is independent.
+
+
+    --- Example 3 ---
+    Input: numCourses = 3,
+           prerequisites = [[1,2],[1,0],[2,0]],
+           queries = [[1,0],[1,2]]
+    Output: [true,true]
+
+
+    *** Constraints ***
+    2 <= numCourses <= 100
+    0 <= prerequisites.length <= (numCourses * (numCourses - 1) / 2)
+    prerequisites[i].length == 2
+    0 <= ai, bi <= n - 1
+    ai != bi
+    All the pairs [ai, bi] are unique.
+    The prerequisites graph has no cycles.
+    1 <= queries.length <= 10^4
+    0 <= ui, vi <= n - 1
+    ui != vi
+
+*/
+
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Since this is a DAG (Directed Acyclic Graph), if course 'a' is a
+    prerequisite of course 'b', then all of the descendants of 'b' are also
+    indirect descendants of 'b'.
+
+    We're going to use that in our advantange.
+
+    We'll do a DFS starting from each course and we'll append ALL of the
+    INDICRENT descendants of our DIRECT neighbor into our children[curr_course]
+    and that way we'll be able to answer each query in O(1).
+
+*/
+
+/* Time  Beats: 44.83% */
+/* Space Beats: 21.31% */
+
+/* Time  Complexity: O(V^3  +  E  +  Q) */
+/* Space Complexity: O(V^2  +  E  +  Q) */
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries)
+    {
+        const int N = prerequisites.size();
+        const int Q = queries.size();
+
+        vector<bool> answer;
+        answer.reserve(Q); // To prevent repeated reallocations
+
+        /* Build an Adjacency List */
+        vector<unordered_set<int>> adj_list(numCourses, unordered_set<int>());
+
+        for (const vector<int>& edge : prerequisites)
+        {
+            const int& a = edge[0];
+            const int& b = edge[1];
+
+            adj_list[a].insert(b);
+        }
+
+        vector<unordered_set<int>> children(numCourses, unordered_set<int>());
+        vector<bool> visited(numCourses, false);
+
+        for (int course = 0; course < numCourses; course++)
+            dfs_populate(course, children, adj_list, visited);
+
+        for (const vector<int>& query : queries)
+        {
+            const int& u = query[0];
+            const int& v = query[1];
+
+            answer.push_back(children[u].count(v));
+        }
+
+        return answer;
+    }
+
+private:
+    void dfs_populate(int course, vector<unordered_set<int>>& children, const vector<unordered_set<int>>& adj_list, vector<bool>& visited)
+    {
+        if (visited[course])
+            return;
+
+        visited[course] = true;
+
+        for (const int& neighbor : adj_list[course])
+        {
+            dfs_populate(neighbor, children, adj_list, visited);
+
+            children[course].insert(neighbor);
+            children[course].insert(children[neighbor].begin(), children[neighbor].end());
+        }
+    }
+};
