7c7f48e3e7ae99242e20223c12644f01ee3253fd
Noa Vukasovic <noavukasovic@gmail.com>
2026-08-14 08:59:17 +0200

    Add Bit_Trick indexing Solution to Leetcode: 3090_Maximum_Length_Susbtring_With_Two_Occurrences to ORderless_Problems

--- Orderless_Problems/3090_Maximum_Length_Substring_With_Two_Occurrences/maximum_length_substring_with_two_occurrences.cpp
+++ Orderless_Problems/3090_Maximum_Length_Substring_With_Two_Occurrences/maximum_length_substring_with_two_occurrences.cpp
@@ -54,7 +54,7 @@ using namespace std;
 */
 
 /* Time  Beats: 100.00% */
-/* Space Beats:  92.69% */
+/* Space Beats:  77.33% */
 
 /* Time  Complexity: O(N) */
 /* Space Complexity: O(1) */
@@ -76,6 +76,96 @@ public:
             while (freq[s[R] - 'a'] > 2)
             {
                 --freq[s[L] - 'a'];
+
+                // Increment
+                ++L;
+            }
+
+            result = max(result, R - L + 1);
+
+            // Incrment
+            ++R;
+        }
+
+        return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    This is a small "trick" with indexing both lowercase and upperase English
+    letters.
+
+
+
+    Both 'a' = 97 and 'A' = 65 will result in 1 when we BITIWSE AND with 31.
+    Then we just subtract by 1 and voila, we get 0-based indexing.
+
+    ---------------------
+    --- lowercase 'a' ---
+    ---------------------
+
+        97 = 0110 0001
+        31 = 0001 1111  & (Bitwise AND)
+       ---------------
+         1 = 0000 0001
+         1 = 0000 0001  - (Minus)
+       ---------------
+         0 = 0000 0000  <-- Final index of of: 'a' = 97
+
+
+    ---------------------
+    --- uppercase 'A' ---
+    ---------------------
+
+        64 = 0100 0001
+        31 = 0001 1111  & (Bitwise AND)
+       ---------------
+         1 = 0000 0001
+         1 = 0000 0001  - (Minus)
+       ---------------
+         0 = 0000 0000  <-- Final index of of: 'A' = 65
+
+
+
+    Now:
+        + both 'A' and 'a' will be indexed at 0.
+        + both 'B' and 'b' will be indexed at 1.
+        + ...
+        + both 'Y' and 'y' will be indexed at 24
+        + both 'Z' and 'z' will be indexed at 25.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  77.33% */
+
+/* Time  Complexity: O(N) */
+/* Space Complexity: O(1) */
+class Solution_Bit_Trick {
+public:
+    int maximumLengthSubstring(string s)
+    {
+        const int N = s.size();
+        int result = 0;
+
+        int freq[26] = {0}; // On the Stack
+
+        int L = 0;
+        int R = 0;
+        while (R < N)
+        {
+            ++freq[(s[R] & 31) - 1];
+
+            while (freq[(s[R] & 31) - 1] > 2)
+            {
+                --freq[(s[L] & 31) - 1];
 
                 // Increment
                 ++L;
