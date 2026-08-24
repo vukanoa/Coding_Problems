3903cc90822d1b3f4bfce5e1e125931f18f7c910
Noa Vukasovic <noavukasovic@gmail.com>
2026-08-24 15:49:41 +0200

    Add Q1, LeetCode: 4000_Larger_Integer_With_Given_Digit_Sum to Weekly_Conest_512

---
+++ Contests/Weekly_Contest_512/4000_Larger_Integer_With_Given_Digit_Sum/larger_integer_with_given_digit_sum.cpp
@@ -0,0 +1,98 @@
+/*
+    ============
+    === EASY ===
+    ============
+
+    =========================================
+    4000) Larger Integer With Given Digit Sum
+    =========================================
+
+    ============
+    Description:
+    ============
+
+    You are given two non-negative integers n and s.
+
+    Return the largest integer that has at most n digits and whose sum of
+    digits is s. If no such integer exists, return -1.
+
+    ===========================================
+    FUNCTION: int largestInteger(int n, int s);
+    ===========================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: n = 2, s = 9
+    Output: 90
+    Explanation:
+    The largest integer with at most 2 digits that has a sum of digits of 9 is
+    90.
+
+    --- Example 2 ---
+    Input: n = 2, s = 19
+    Output: -1
+    Explanation:
+    There is no integer with at most 2 digits that has a sum of digits of 19,
+    so the answer is -1.
+
+    --- Example 3 ---
+    Input: n = 5, s = 0
+    Output: 0
+    Explanation:
+    The only non-negative integer whose digits sum to 0 is 0.
+
+
+    *** Constraints ***
+    1 <= n <= 5
+    0 <= s <= 100
+
+*/
+
+#include <cmath>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Allowed by the Constraints.
+
+*/
+
+/* Time  Beats: 14.76% */
+/* Space Beats: 12.02% */
+
+/* Time  Complexity: O(10^n * n) */
+/* Space Complexity: O(1)        */
+class Solution {
+public:
+    int largestInteger(int n, int s)
+    {
+        int result = pow(10, n) - 1;
+
+        while (result >= 0)
+        {
+            int tmp = result;
+
+            int sum = 0;
+            while (tmp > 0)
+            {
+                int digit = tmp % 10;
+                sum += digit;
+                
+                tmp /= 10;
+            }
+
+            if (sum == s)
+                return result;
+
+            --result;
+        }
+
+        return -1;
+    }
+};
