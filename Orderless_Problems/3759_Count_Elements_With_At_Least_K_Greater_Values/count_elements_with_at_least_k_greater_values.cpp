70ae07546be59040e054d64cbe1e13eacfddaaeb
Noa Vukasovic <noavukasovic@gmail.com>
2026-08-29 13:27:08 +0200

    Add LeetCodE 3759_Count_Elements_With_At_Least_K_Greater_Values to ORderless_PRoblems

---
+++ Orderless_Problems/3759_Count_Elements_With_At_Least_K_Greater_Values/count_elements_with_at_least_k_greater_values.cpp
@@ -0,0 +1,82 @@
+/*
+    ==============
+    === MEDIUM ===
+    ==============
+
+    ===================================================
+    3759) Count Elements With at Least K Greater values
+    ===================================================
+
+    ============
+    Description:
+    ============
+
+    You are given an integer array nums of length n and an integer k.
+
+    An element in nums is said to be qualified if there exist at least k
+    elements in the array that are strictly greater than it.
+
+    Return an integer denoting the total number of qualified elements in nums.
+
+    ======================================================
+    FUNCTION: int countElements(vector<int>& nums, int k);
+    ======================================================
+
+    ==========================================================================
+    ================================ EXAMPLES ================================
+    ==========================================================================
+
+    --- Example 1 ---
+    Input: nums = [3,1,2], k = 1
+    Output: 2
+    Explanation:
+    The elements 1 and 2 each have at least k = 1 element greater than
+    themselves. No element is greater than 3. Therefore, the answer is 2.
+
+    --- Example 2 ---
+    Input: nums = [5,5,5], k = 2
+    Output: 0
+    Explanation:
+    Since all elements are equal to 5, no element is greater than the other.
+    Therefore, the answer is 0.
+
+
+    *** Constraints ***
+    1 <= n == nums.length <= 10^5
+    1 <= nums[i] <= 10^9
+    0 <= k < n
+
+*/
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 82.22% */
+/* Space Beats: 56.69% */
+
+/* Time  Complexity: O(N * logN) */
+/* Space Complexity: O(logN)     */ // SC of C++'s Intro Sort
+class Solution {
+public:
+    int countElements(vector<int>& nums, int k)
+    {
+        const int N = nums.size();
+        if (k == 0)
+            return N;
+
+        /* Sort */
+        sort(nums.begin(), nums.end());
+
+        return lower_bound(nums.begin(), nums.end(), nums[N - k]) - nums.begin();
+    }
+};
