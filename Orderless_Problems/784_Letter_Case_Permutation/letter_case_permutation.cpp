25cd618e268411522a383c51563c0e139eb31e9c
Noa Vukasovic <noavukasovic@gmail.com>
2026-09-07 11:27:15 +0200

    Add Three DFS Solutions to LeetCode: 784_Letter_Case_Permutation to Orderless_Problems

--- Orderless_Problems/784_Letter_Case_Permutation/letter_case_permutation.cpp
+++ Orderless_Problems/784_Letter_Case_Permutation/letter_case_permutation.cpp
@@ -60,7 +60,7 @@ using namespace std;
 
 /* Time  Complexity: O(N * 2^N) */
 /* Space Complexity: O(N * 2^N) */
-class Solution {
+class Solution_BFS {
 public:
     vector<string> letterCasePermutation(string S)
     {
@@ -100,5 +100,179 @@ public:
         }
 
         return result;
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    TODO
+
+*/
+
+/* Time  Beats: 20.38% */
+/* Space Beats: 36.11% */
+
+/* Time  Complexity: O(N * 2^N) */
+/* Space Complexity: O(N * 2^N) */
+class Solution_DFS_1 {
+public:
+    vector<string> letterCasePermutation(string s)
+    {
+        const int N = s.size();
+        vector<string> result;
+
+        dfs(0, "", s, result);
+
+        return result;
+    }
+
+private:
+    void dfs(int idx, string curr_str, const string& s, vector<string>& result)
+    {
+        const int N = s.size();
+
+        if (idx == N)
+        {
+            result.push_back(curr_str);
+            return;
+        }
+
+        if (isdigit(s[idx]))
+        {
+            dfs(idx + 1, curr_str + s[idx], s, result);
+            return;
+        }
+
+        dfs(idx+1, curr_str + string(1, tolower(s[idx])), s, result);
+        dfs(idx+1, curr_str + string(1, toupper(s[idx])), s, result);
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same as above, but a bit more efficiently implemented.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  63.39% */
+
+/* Time  Complexity: O(N * 2^N) */
+/* Space Complexity: O(N * 2^N) */
+class Solution_DFS_2 {
+public:
+    vector<string> letterCasePermutation(string s)
+    {
+        const int N = s.size();
+        vector<string> result;
+
+        string curr_str;
+        curr_str.reserve(N); // To prevent repeated reallocations
+
+        dfs(0, curr_str, s, result);
+
+        return result;
+    }
+
+private:
+    void dfs(int idx, string& curr_str, const string& s, vector<string>& result)
+    {
+        const int N = s.size();
+
+        if (idx == N)
+        {
+            result.push_back(curr_str);
+            return;
+        }
+
+        if (isdigit(s[idx]))
+        {
+            curr_str.push_back(s[idx]);
+            dfs(idx + 1, curr_str, s, result);
+            curr_str.pop_back();
+
+            return;
+        }
+
+        /* Lowercase */
+        curr_str.push_back(tolower(s[idx]));
+        dfs(idx + 1, curr_str, s, result);
+        curr_str.pop_back();
+
+        /* Uppercase */
+        curr_str.push_back(toupper(s[idx]));
+        dfs(idx + 1, curr_str, s, result);
+        curr_str.pop_back();
+    }
+};
+
+
+
+
+/*
+    ------------
+    --- IDEA ---
+    ------------
+
+    Same as above, but written in the MOST efficient way out of all 3 DFS
+    Solutions.
+
+*/
+
+/* Time  Beats: 100.00% */
+/* Space Beats:  87.38% */
+
+/* Time  Complexity: O(N * 2^N) */
+/* Space Complexity: O(N * 2^N) */
+class Solution_DFS_3 {
+public:
+    vector<string> letterCasePermutation(string s)
+    {
+        if (s.empty())
+            return {};
+
+        vector<string> result;
+        dfs(0, s, result);
+
+        return result;
+    }
+
+    void dfs(int idx, string& s, vector<string>& result)
+    {
+        const int N = s.size();
+
+        if (idx == N)
+        {
+            result.push_back(s);
+            return;
+        }
+
+        if (isdigit(s[idx]))
+        {
+            dfs(idx+1, s, result);
+            return;
+        }
+
+         char original_chr = s[idx]; // Save original character
+
+        s[idx] = tolower(s[idx]);
+        dfs(idx+1, s, result);
+
+        s[idx] = toupper(s[idx]);
+        dfs(idx+1, s, result);
+
+        s[idx] = original_chr;      // Restore (optional but good practice)
     }
 };
